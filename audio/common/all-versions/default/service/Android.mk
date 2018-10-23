ec7e2d90974c2fbeaf2a5363e2114bd450af60d7
openvela-robot <openvela-robot@xiaomi.com>
2018-10-24 03:04:20 +0800

    Snap for 5087486 from 5ef198aa6e7bea74ad373f1ba2e561373c5ee8f7 to pi-qpr2-release
    Change-Id: Ic86f880a88c99eec11fccfe16aad151566e5e0a0

--- audio/common/all-versions/default/service/Android.mk
+++ audio/common/all-versions/default/service/Android.mk
@@ -31,14 +31,12 @@ LOCAL_SRC_FILES := \
 LOCAL_CFLAGS := -Wall -Werror
 
 LOCAL_SHARED_LIBRARIES := \
-    libcutils \
     libbinder \
     libhidlbase \
     libhidltransport \
     liblog \
     libutils \
     libhardware \
-    libhwbinder \
     android.hardware.audio@2.0 \
     android.hardware.audio@4.0 \
     android.hardware.audio.common@2.0 \
@@ -56,8 +54,5 @@ LOCAL_MULTILIB := 32
 else
 LOCAL_MULTILIB := $(AUDIOSERVER_MULTILIB)
 endif
-
-# b/117506164
-LOCAL_SANITIZE := never
 
 include $(BUILD_EXECUTABLE)
--- bluetooth/1.0/vts/functional/VtsHalBluetoothV1_0TargetTest.cpp
+++ bluetooth/1.0/vts/functional/VtsHalBluetoothV1_0TargetTest.cpp
@@ -170,9 +170,8 @@ class BluetoothHidlTest : public ::testing::VtsHalHidlTargetTestBase {
     acl_cb_count = 0;
     sco_cb_count = 0;
 
-    ASSERT_FALSE(initialized);
-    // Should not be checked in production code
-    ASSERT_TRUE(bluetooth->initialize(bluetooth_cb).isOk());
+    ASSERT_EQ(initialized, false);
+    bluetooth->initialize(bluetooth_cb);
 
     bluetooth_cb->SetWaitTimeout(kCallbackNameInitializationComplete,
                                  WAIT_FOR_INIT_TIMEOUT);
@@ -187,16 +186,15 @@ class BluetoothHidlTest : public ::testing::VtsHalHidlTargetTestBase {
         bluetooth_cb->WaitForCallback(kCallbackNameInitializationComplete)
             .no_timeout);
 
-    ASSERT_TRUE(initialized);
+    ASSERT_EQ(initialized, true);
   }
 
   virtual void TearDown() override {
-      // Should not be checked in production code
-      ASSERT_TRUE(bluetooth->close().isOk());
-      handle_no_ops();
-      EXPECT_EQ(static_cast<size_t>(0), event_queue.size());
-      EXPECT_EQ(static_cast<size_t>(0), sco_queue.size());
-      EXPECT_EQ(static_cast<size_t>(0), acl_queue.size());
+    bluetooth->close();
+    handle_no_ops();
+    EXPECT_EQ(static_cast<size_t>(0), event_queue.size());
+    EXPECT_EQ(static_cast<size_t>(0), sco_queue.size());
+    EXPECT_EQ(static_cast<size_t>(0), acl_queue.size());
   }
 
   void setBufferSizes();
