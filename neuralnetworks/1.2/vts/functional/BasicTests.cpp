6780db2a675501fc4c25a2d8f72aa47ccba1ca30
openvela-robot <openvela-robot@xiaomi.com>
2018-12-11 15:15:17 +0800

    Y16 format enablement for external provider
    added Y16 (depth) support for the external provider.
    refactored initOutputCharsKeys to support both depth and color metadata according to the connected camera.
    the VTS changes will be pushed to pie-vts-dev
    
    Test: tested on intel depth camera D415 manually in addition to the VTS tests.
    
    Change-Id: I358686e7c4330bb180dec4a9cce3bc1cf5475260
    Merged-In: I358686e7c4330bb180dec4a9cce3bc1cf5475260
    Signed-off-by: Emil Jahshan <emil.jahshan@intel.com>

--- neuralnetworks/1.2/vts/functional/BasicTests.cpp
+++ neuralnetworks/1.2/vts/functional/BasicTests.cpp
@@ -64,12 +64,7 @@ TEST_F(NeuralnetworksHidlTest, GetDeviceSupportedExtensionsTest) {
                 for (auto& extension : extensions) {
                     std::string extensionName = extension.name;
                     EXPECT_FALSE(extensionName.empty());
-                    for (char c : extensionName) {
-                        EXPECT_TRUE(('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || c == '_' ||
-                                    c == '.')
-                                << "Extension name contains an illegal character: " << c;
-                    }
-                    EXPECT_NE(extensionName.find('.'), std::string::npos)
+                    EXPECT_NE(extensionName.find("."), std::string::npos)
                             << "Extension name must start with the reverse domain name of the "
                                "vendor";
                 }
