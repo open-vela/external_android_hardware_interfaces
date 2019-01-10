7f07cf60e53f80cb02f6aa47f180b180f132d881
openvela-robot <openvela-robot@xiaomi.com>
2019-01-10 10:58:41 +0800

    Add TENSOR_QUANT8_SYMM_PER_CHANNEL to operand types am: 4766f8b1a1
    Change-Id: I86a5441d3f0126fc2e240e15346c68a940533b90

--- neuralnetworks/1.2/vts/functional/ValidateModel.cpp
+++ neuralnetworks/1.2/vts/functional/ValidateModel.cpp
@@ -325,9 +325,6 @@ static bool mutateOperationOperandTypeSkip(size_t operand, OperandType type, con
         // - ARGMIN and ARGMAX's first argument can be any of
         // TENSOR_(FLOAT16|FLOAT32|INT32|QUANT8_ASYMM).
         // - CAST's argument can be any of TENSOR_(FLOAT16|FLOAT32|INT32|QUANT8_ASYMM).
-        // - RANDOM_MULTINOMIAL's argument can be either TENSOR_FLOAT16 or TENSOR_FLOAT32.
-        // - CONV_2D filter type (arg 1) can be QUANT8_ASYMM or QUANT8_SYMM_PER_CHANNEL
-        // - DEPTHWISE_CONV_2D filter type (arg 1) can be QUANT8_ASYMM or QUANT8_SYMM_PER_CHANNEL
         switch (operation.type) {
             case OperationType::LSH_PROJECTION: {
                 if (operand == operation.inputs[1]) {
@@ -339,18 +336,6 @@ static bool mutateOperationOperandTypeSkip(size_t operand, OperandType type, con
             case OperationType::ARGMIN: {
                 if (type == OperandType::TENSOR_FLOAT16 || type == OperandType::TENSOR_FLOAT32 ||
                     type == OperandType::TENSOR_INT32 || type == OperandType::TENSOR_QUANT8_ASYMM) {
-                    return true;
-                }
-            } break;
-            case OperationType::RANDOM_MULTINOMIAL: {
-                if (type == OperandType::TENSOR_FLOAT16 || type == OperandType::TENSOR_FLOAT32) {
-                    return true;
-                }
-            } break;
-            case OperationType::DEPTHWISE_CONV_2D:
-            case OperationType::CONV_2D: {
-                if (operand == 1 && (type == OperandType::TENSOR_QUANT8_ASYMM ||
-                                     type == OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL)) {
                     return true;
                 }
             } break;
