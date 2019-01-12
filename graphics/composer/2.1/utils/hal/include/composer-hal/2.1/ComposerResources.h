466fd10028cca4c09684c0b6c6830329c05fc069
openvela-robot <openvela-robot@xiaomi.com>
2019-01-13 04:01:01 +0800

    Snap for 5228332 from bc91e1388cbaef398c706d5e4af7afd98c83d1c1 to qt-release
    Change-Id: I122a3bd0b6ca9e76fca510c2201f55b17f42a8e6

--- graphics/composer/2.1/utils/hal/include/composer-hal/2.1/ComposerResources.h
+++ graphics/composer/2.1/utils/hal/include/composer-hal/2.1/ComposerResources.h
@@ -26,7 +26,6 @@
 #include <vector>
 
 #include <android/hardware/graphics/mapper/2.0/IMapper.h>
-#include <android/hardware/graphics/mapper/3.0/IMapper.h>
 #include <log/log.h>
 
 namespace android {
@@ -40,16 +39,9 @@ namespace hal {
 class ComposerHandleImporter {
    public:
     bool init() {
-        mMapper3 = mapper::V3_0::IMapper::getService();
-        if (mMapper3) {
-            return true;
-        }
-        ALOGW_IF(!mMapper3, "failed to get mapper 3.0 service");
-
-        mMapper2 = mapper::V2_0::IMapper::getService();
-        ALOGE_IF(!mMapper2, "failed to get mapper 2.0 service");
-
-        return mMapper2 != nullptr;
+        mMapper = mapper::V2_0::IMapper::getService();
+        ALOGE_IF(!mMapper, "failed to get mapper service");
+        return mMapper != nullptr;
     }
 
     Error importBuffer(const native_handle_t* rawHandle, const native_handle_t** outBufferHandle) {
@@ -58,28 +50,14 @@ class ComposerHandleImporter {
             return Error::NONE;
         }
 
+        mapper::V2_0::Error error;
         const native_handle_t* bufferHandle;
-        if (mMapper2) {
-            mapper::V2_0::Error error;
-            mMapper2->importBuffer(
-                rawHandle, [&](const auto& tmpError, const auto& tmpBufferHandle) {
-                    error = tmpError;
-                    bufferHandle = static_cast<const native_handle_t*>(tmpBufferHandle);
-                });
-            if (error != mapper::V2_0::Error::NONE) {
-                return Error::NO_RESOURCES;
-            }
-        }
-        if (mMapper3) {
-            mapper::V3_0::Error error;
-            mMapper3->importBuffer(
-                rawHandle, [&](const auto& tmpError, const auto& tmpBufferHandle) {
-                    error = tmpError;
-                    bufferHandle = static_cast<const native_handle_t*>(tmpBufferHandle);
-                });
-            if (error != mapper::V3_0::Error::NONE) {
-                return Error::NO_RESOURCES;
-            }
+        mMapper->importBuffer(rawHandle, [&](const auto& tmpError, const auto& tmpBufferHandle) {
+            error = tmpError;
+            bufferHandle = static_cast<const native_handle_t*>(tmpBufferHandle);
+        });
+        if (error != mapper::V2_0::Error::NONE) {
+            return Error::NO_RESOURCES;
         }
 
         *outBufferHandle = bufferHandle;
@@ -88,13 +66,7 @@ class ComposerHandleImporter {
 
     void freeBuffer(const native_handle_t* bufferHandle) {
         if (bufferHandle) {
-            if (mMapper2) {
-                mMapper2->freeBuffer(
-                    static_cast<void*>(const_cast<native_handle_t*>(bufferHandle)));
-            } else if (mMapper3) {
-                mMapper3->freeBuffer(
-                    static_cast<void*>(const_cast<native_handle_t*>(bufferHandle)));
-            }
+            mMapper->freeBuffer(static_cast<void*>(const_cast<native_handle_t*>(bufferHandle)));
         }
     }
 
@@ -119,8 +91,7 @@ class ComposerHandleImporter {
     }
 
    private:
-    sp<mapper::V2_0::IMapper> mMapper2;
-    sp<mapper::V3_0::IMapper> mMapper3;
+    sp<mapper::V2_0::IMapper> mMapper;
 };
 
 class ComposerHandleCache {
