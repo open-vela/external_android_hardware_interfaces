73f9c860c0cea5b037902d1c5c6e396923ca8863
openvela-robot <openvela-robot@xiaomi.com>
2019-01-15 17:49:24 +0800

    mutateOperationOperandTypeSkip for new TRANSPOSE_CONV_2D variant
    Bug: 119255406
    Test: Vts NNAPI tests.
    Change-Id: I63f7fc67cedcc2a9db8c359ff96be3c9403b9a20
    Merged-In: I63f7fc67cedcc2a9db8c359ff96be3c9403b9a20
    (cherry picked from commit 1509a2664cef4ab883a6ddf4556fbd544c8ca22f)

--- neuralnetworks/1.2/vts/functional/GeneratedTestsV1_0.cpp
+++ neuralnetworks/1.2/vts/functional/GeneratedTestsV1_0.cpp
@@ -33,8 +33,7 @@ namespace neuralnetworks {
 namespace generated_tests {
 using ::test_helper::MixedTypedExample;
 extern void Execute(const sp<V1_2::IDevice>&, std::function<V1_2::Model(void)>,
-                    std::function<bool(int)>, const std::vector<MixedTypedExample>&,
-                    bool testDynamicOutputShape = false);
+                    std::function<bool(int)>, const std::vector<MixedTypedExample>&);
 }  // namespace generated_tests
 
 namespace V1_2 {
