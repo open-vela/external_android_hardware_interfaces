453518a33307235b160b6e20fab09fbc5650cd19
liujinye <liujinye@xiaomi.com>
2019-01-15 17:49:24 +0800

    mutateOperationOperandTypeSkip for new TRANSPOSE_CONV_2D variant
    Bug: 119255406
    Test: Vts NNAPI tests.
    Change-Id: I63f7fc67cedcc2a9db8c359ff96be3c9403b9a20
    Merged-In: I63f7fc67cedcc2a9db8c359ff96be3c9403b9a20
    (cherry picked from commit 1509a2664cef4ab883a6ddf4556fbd544c8ca22f)

--- neuralnetworks/1.0/vts/functional/Callbacks.cpp
+++ neuralnetworks/1.0/vts/functional/Callbacks.cpp
@@ -135,18 +135,14 @@ ExecutionCallback::~ExecutionCallback() {}
 
 Return<void> ExecutionCallback::notify(ErrorStatus errorStatus) {
     mErrorStatus = errorStatus;
-    mOutputShapes = {};
-    mTiming = {.timeOnDevice = UINT64_MAX, .timeInDriver = UINT64_MAX};
     CallbackBase::notify();
     return Void();
 }
 
 Return<void> ExecutionCallback::notify_1_2(ErrorStatus errorStatus,
-                                           const hidl_vec<OutputShape>& outputShapes,
-                                           const Timing& timing) {
+                                           const hidl_vec<OutputShape>& outputShapes) {
     mErrorStatus = errorStatus;
     mOutputShapes = outputShapes;
-    mTiming = timing;
     CallbackBase::notify();
     return Void();
 }
@@ -159,11 +155,6 @@ ErrorStatus ExecutionCallback::getStatus() {
 const std::vector<OutputShape>& ExecutionCallback::getOutputShapes() {
     wait();
     return mOutputShapes;
-}
-
-Timing ExecutionCallback::getTiming() {
-    wait();
-    return mTiming;
 }
 
 }  // namespace implementation
