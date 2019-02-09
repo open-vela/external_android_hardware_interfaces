7cfa874d94e69d01bdcd852e91b01088d30bbeff
openvela-robot <openvela-robot@xiaomi.com>
2019-02-09 19:49:23 +0800

    Updates openDisplay() API description
    This change corrects the description of openDisplay()'s behavior when
    new client requests to access a display resource.
    
    Bug: 122671398
    Test: N/A
    Change-Id: I0605489506f3a011d2d3ea405d2b87f7cb0bd883
    Signed-off-by: Changyeon Jo <changyeon@google.com>

--- gnss/2.0/default/Gnss.cpp
+++ gnss/2.0/default/Gnss.cpp
@@ -17,10 +17,7 @@
 #define LOG_TAG "Gnss"
 
 #include "Gnss.h"
-
 #include <log/log.h>
-#include <utils/SystemClock.h>
-
 #include "AGnss.h"
 #include "AGnssRil.h"
 #include "GnssConfiguration.h"
@@ -43,24 +40,6 @@ using GnssSvFlags = IGnssCallback::GnssSvFlags;
 sp<V2_0::IGnssCallback> Gnss::sGnssCallback_2_0 = nullptr;
 sp<V1_1::IGnssCallback> Gnss::sGnssCallback_1_1 = nullptr;
 
-namespace {
-
-V2_0::GnssLocation getMockLocationV2_0() {
-    const ElapsedRealtime timestamp = {
-            .flags = ElapsedRealtimeFlags::HAS_TIMESTAMP_NS |
-                     ElapsedRealtimeFlags::HAS_TIME_UNCERTAINTY_NS,
-            .timestampNs = static_cast<uint64_t>(::android::elapsedRealtimeNano()),
-            // This is an hardcoded value indicating a 1ms of uncertainty between the two clocks.
-            // In an actual implementation provide an estimate of the synchronization uncertainty
-            // or don't set the field.
-            .timeUncertaintyNs = 1000000};
-
-    V2_0::GnssLocation location = {.v1_0 = Utils::getMockLocation(), .elapsedRealtime = timestamp};
-    return location;
-}
-
-}  // namespace
-
 Gnss::Gnss() : mMinIntervalMs(1000) {}
 
 Gnss::~Gnss() {
@@ -69,7 +48,7 @@ Gnss::~Gnss() {
 
 // Methods from V1_0::IGnss follow.
 Return<bool> Gnss::setCallback(const sp<V1_0::IGnssCallback>&) {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return bool{};
 }
 
@@ -82,7 +61,7 @@ Return<bool> Gnss::start() {
     mIsActive = true;
     mThread = std::thread([this]() {
         while (mIsActive == true) {
-            const auto location = getMockLocationV2_0();
+            const auto location = Utils::getMockLocation();
             this->reportLocation(location);
 
             std::this_thread::sleep_for(std::chrono::milliseconds(mMinIntervalMs));
@@ -100,44 +79,44 @@ Return<bool> Gnss::stop() {
 }
 
 Return<void> Gnss::cleanup() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return Void();
 }
 
 Return<bool> Gnss::injectTime(int64_t, int64_t, int32_t) {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return bool{};
 }
 
 Return<bool> Gnss::injectLocation(double, double, float) {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return bool{};
 }
 
 Return<void> Gnss::deleteAidingData(V1_0::IGnss::GnssAidingData) {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return Void();
 }
 
 Return<bool> Gnss::setPositionMode(V1_0::IGnss::GnssPositionMode,
                                    V1_0::IGnss::GnssPositionRecurrence, uint32_t, uint32_t,
                                    uint32_t) {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return bool{};
 }
 
 Return<sp<V1_0::IAGnssRil>> Gnss::getExtensionAGnssRil() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IAGnssRil>{};
 }
 
 Return<sp<V1_0::IGnssGeofencing>> Gnss::getExtensionGnssGeofencing() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IGnssGeofencing>{};
 }
 
 Return<sp<V1_0::IAGnss>> Gnss::getExtensionAGnss() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IAGnss>{};
 }
 
@@ -152,27 +131,27 @@ Return<sp<V1_0::IGnssMeasurement>> Gnss::getExtensionGnssMeasurement() {
 }
 
 Return<sp<V1_0::IGnssNavigationMessage>> Gnss::getExtensionGnssNavigationMessage() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IGnssNavigationMessage>{};
 }
 
 Return<sp<V1_0::IGnssXtra>> Gnss::getExtensionXtra() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IGnssXtra>{};
 }
 
 Return<sp<V1_0::IGnssConfiguration>> Gnss::getExtensionGnssConfiguration() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IGnssConfiguration>{};
 }
 
 Return<sp<V1_0::IGnssDebug>> Gnss::getExtensionGnssDebug() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IGnssDebug>{};
 }
 
 Return<sp<V1_0::IGnssBatching>> Gnss::getExtensionGnssBatching() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_0::IGnssBatching>{};
 }
 
@@ -215,7 +194,7 @@ Return<bool> Gnss::setPositionMode_1_1(V1_0::IGnss::GnssPositionMode,
 }
 
 Return<sp<V1_1::IGnssConfiguration>> Gnss::getExtensionGnssConfiguration_1_1() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<V1_1::IGnssConfiguration>{};
 }
 
@@ -225,7 +204,7 @@ Return<sp<V1_1::IGnssMeasurement>> Gnss::getExtensionGnssMeasurement_1_1() {
 }
 
 Return<bool> Gnss::injectBestLocation(const V1_0::GnssLocation&) {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return bool{};
 }
 
@@ -249,7 +228,7 @@ Return<sp<V2_0::IGnssMeasurement>> Gnss::getExtensionGnssMeasurement_2_0() {
 
 Return<sp<measurement_corrections::V1_0::IMeasurementCorrections>>
 Gnss::getExtensionMeasurementCorrections() {
-    // TODO(b/124012850): Implement function.
+    // TODO implement
     return sp<measurement_corrections::V1_0::IMeasurementCorrections>{};
 }
 
@@ -289,19 +268,14 @@ Return<bool> Gnss::setCallback_2_0(const sp<V2_0::IGnssCallback>& callback) {
     return true;
 }
 
-Return<void> Gnss::reportLocation(const V2_0::GnssLocation& location) const {
+Return<void> Gnss::reportLocation(const GnssLocation& location) const {
     std::unique_lock<std::mutex> lock(mMutex);
-    if (sGnssCallback_2_0 == nullptr) {
-        ALOGE("%s: sGnssCallback 2.0 is null.", __func__);
+    if (sGnssCallback_1_1 == nullptr) {
+        ALOGE("%s: sGnssCallback is null.", __func__);
         return Void();
     }
-    sGnssCallback_2_0->gnssLocationCb_2_0(location);
+    sGnssCallback_1_1->gnssLocationCb(location);
     return Void();
-}
-
-Return<bool> Gnss::injectBestLocation_2_0(const V2_0::GnssLocation&) {
-    // TODO(b/124012850): Implement function.
-    return bool{};
 }
 
 }  // namespace implementation
