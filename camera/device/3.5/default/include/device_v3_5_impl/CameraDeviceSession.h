ada056697737cdf5c5f3c2cb370b39973b793484
liujinye <liujinye@xiaomi.com>
2019-02-14 04:11:24 +0800

    Snap for 5307677 from d4ef921860ac4706ece5f4eb84ab7dbdae60c4b4 to qt-release
    Change-Id: I6d9ea234217302498b559054844b7401b0ee38c6

--- camera/device/3.5/default/include/device_v3_5_impl/CameraDeviceSession.h
+++ camera/device/3.5/default/include/device_v3_5_impl/CameraDeviceSession.h
@@ -93,9 +93,6 @@ protected:
             hidl_vec<buffer_handle_t*>& allBufPtrs,
             hidl_vec<int>& allFences) override;
 
-    Return<void> isReconfigurationRequired(const V3_2::CameraMetadata& oldSessionParams,
-            const V3_2::CameraMetadata& newSessionParams,
-            ICameraDeviceSession::isReconfigurationRequired_cb _hidl_cb);
     /**
      * Static callback forwarding methods from HAL to instance
      */
@@ -241,11 +238,6 @@ private:
             return mParent->signalStreamFlush(requests, streamConfigCounter);
         }
 
-        virtual Return<void> isReconfigurationRequired(const V3_2::CameraMetadata& oldSessionParams,
-                const V3_2::CameraMetadata& newSessionParams,
-                ICameraDeviceSession::isReconfigurationRequired_cb _hidl_cb) override {
-            return mParent->isReconfigurationRequired(oldSessionParams, newSessionParams, _hidl_cb);
-        }
     private:
         sp<CameraDeviceSession> mParent;
     };
