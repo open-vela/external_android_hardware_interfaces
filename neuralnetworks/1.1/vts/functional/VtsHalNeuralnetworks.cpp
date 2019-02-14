11064bd18f657630afcf5980620bc51bacfcc933
liujinye <liujinye@xiaomi.com>
2019-02-14 18:27:26 +0800

    Modify several 1.2 nn operation specs. am: 3ee1f4a333
    Change-Id: I2f86409729979909d0299bc1ffc1c40cab15408b

--- neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.cpp
+++ neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.cpp
@@ -60,12 +60,6 @@ void NeuralnetworksHidlTest::TearDown() {
 
 }  // namespace functional
 }  // namespace vts
-}  // namespace V1_1
-}  // namespace neuralnetworks
-}  // namespace hardware
-}  // namespace android
-
-namespace android::hardware::neuralnetworks::V1_0 {
 
 ::std::ostream& operator<<(::std::ostream& os, ErrorStatus errorStatus) {
     return os << toString(errorStatus);
@@ -75,7 +69,10 @@ namespace android::hardware::neuralnetworks::V1_0 {
     return os << toString(deviceStatus);
 }
 
-}  // namespace android::hardware::neuralnetworks::V1_0
+}  // namespace V1_1
+}  // namespace neuralnetworks
+}  // namespace hardware
+}  // namespace android
 
 using android::hardware::neuralnetworks::V1_1::vts::functional::NeuralnetworksHidlEnvironment;
 
