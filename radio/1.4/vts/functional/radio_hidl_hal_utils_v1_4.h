a8bb2f903f8df6f8a096691dd62e376c0c847c45
openvela-robot <openvela-robot@xiaomi.com>
2019-02-25 15:22:42 +0800

    Remove update scripts for VTS makefiles.
    These are automatically included as part of hidl_interface and no
    longer need to be checked in.
    
    Bug: 117348839
    Test: manual
    Change-Id: If07460e1b88c5929e23256c30ddb332ee64e9c3d
    Merged-In: If07460e1b88c5929e23256c30ddb332ee64e9c3d

--- radio/1.4/vts/functional/radio_hidl_hal_utils_v1_4.h
+++ radio/1.4/vts/functional/radio_hidl_hal_utils_v1_4.h
@@ -61,9 +61,6 @@ class RadioResponse_v1_4 : public ::android::hardware::radio::V1_4::IRadioRespon
     bool isModemEnabled;
     bool enableModemResponseToggle;
 
-    ::android::hardware::hidl_bitfield<::android::hardware::radio::V1_4::RadioAccessFamily>
-    networkTypeBitmapResponse;
-
     // Data
     ::android::hardware::radio::V1_4::DataRegStateResult dataRegResp;
 
