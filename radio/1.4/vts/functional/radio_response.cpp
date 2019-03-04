d8b13640e7aad7969344b2310df1c307739c952b
openvela-robot <openvela-robot@xiaomi.com>
2019-03-04 11:31:09 +0800

    Update automotive makefile.
    Bug: 123976090
    Test: N/A
    Change-Id: I34add915c896b4b5fcbcfa0ad0c42950fff9e9fc
    Merged-In: I84b659b163b63d39535b462a3df8a9c19f680976

--- radio/1.4/vts/functional/radio_response.cpp
+++ radio/1.4/vts/functional/radio_response.cpp
@@ -862,18 +862,12 @@ Return<void> RadioResponse_v1_4::setupDataCallResponse_1_4(
     return Void();
 }
 
-Return<void> RadioResponse_v1_4::setAllowedCarriersResponse_1_4(const RadioResponseInfo& info) {
-    rspInfo = info;
-    parent_v1_4.notify(info.serial);
+Return<void> RadioResponse_v1_4::setAllowedCarriersResponse_1_4(const RadioResponseInfo& /*info*/) {
     return Void();
 }
 
 Return<void> RadioResponse_v1_4::getAllowedCarriersResponse_1_4(
-        const RadioResponseInfo& info, const CarrierRestrictionsWithPriority& carriers,
-        SimLockMultiSimPolicy multiSimPolicy) {
-    rspInfo = info;
-    carrierRestrictionsResp = carriers;
-    multiSimPolicyResp = multiSimPolicy;
-    parent_v1_4.notify(info.serial);
+        const RadioResponseInfo& /*info*/, const CarrierRestrictionsWithPriority& /*carriers*/,
+        SimLockMultiSimPolicy /*multiSimPolicy*/) {
     return Void();
 }
