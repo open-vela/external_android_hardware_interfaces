ed35124b0fbeefdde6046cc6344967c791859906
openvela-robot <openvela-robot@xiaomi.com>
2019-05-08 14:23:19 +0800

    Merge "Fix LSTM documentation" into qt-dev am: 881e261f0e am: 9201bd34ca
    Change-Id: Ie838e84c562e584fb5588ebc96cb259b439c2c54

--- neuralnetworks/1.2/vts/functional/VtsHalNeuralnetworks.h
+++ neuralnetworks/1.2/vts/functional/VtsHalNeuralnetworks.h
@@ -72,7 +72,7 @@ class NeuralnetworksHidlTest : public ::testing::VtsHalHidlTargetTestBase {
 // Tag for the validation tests
 class ValidationTest : public NeuralnetworksHidlTest {
    protected:
-     void validateEverything(const Model& model, const std::vector<Request>& requests);
+     void validateEverything(const Model& model, const std::vector<Request>& request);
 
    private:
      void validateModel(const Model& model);
