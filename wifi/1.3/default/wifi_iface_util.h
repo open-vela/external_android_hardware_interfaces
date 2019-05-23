c808baf2761615ae0679bbf7345b091a192feb72
openvela-robot <openvela-robot@xiaomi.com>
2019-05-23 23:42:37 +0800

    Merge changes I760dc5ca,Ia2da1dcf into qt-dev am: c733f3bbed am: 770125deea
    Change-Id: Id183b93bfe794508b73a1c99065b9513aefbb3b0

--- wifi/1.3/default/wifi_iface_util.h
+++ wifi/1.3/default/wifi_iface_util.h
@@ -40,7 +40,7 @@ struct IfaceEventHandlers {
  */
 class WifiIfaceUtil {
    public:
-    WifiIfaceUtil(const std::weak_ptr<wifi_system::InterfaceTool> iface_tool);
+    WifiIfaceUtil();
     virtual ~WifiIfaceUtil() = default;
 
     virtual std::array<uint8_t, 6> getFactoryMacAddress(
@@ -60,7 +60,7 @@ class WifiIfaceUtil {
    private:
     std::array<uint8_t, 6> createRandomMacAddress();
 
-    std::weak_ptr<wifi_system::InterfaceTool> iface_tool_;
+    wifi_system::InterfaceTool iface_tool_;
     std::unique_ptr<std::array<uint8_t, 6>> random_mac_address_;
     std::map<std::string, IfaceEventHandlers> event_handlers_map_;
 };
