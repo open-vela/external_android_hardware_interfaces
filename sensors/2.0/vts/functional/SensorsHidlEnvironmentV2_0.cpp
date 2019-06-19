d334e5519a1f3feaf5071e0d877ac999fb8d0f97
openvela-robot <openvela-robot@xiaomi.com>
2019-06-19 15:54:26 +0800

    Merge "VMS Utils: Fixed createDataMessage method." into qt-dev am: 20ee67c070 am: 866d0b861c
    Change-Id: I34d980fb70b6f1f219bc0e342be9fba85082ecee

--- sensors/2.0/vts/functional/SensorsHidlEnvironmentV2_0.cpp
+++ sensors/2.0/vts/functional/SensorsHidlEnvironmentV2_0.cpp
@@ -139,7 +139,6 @@ void SensorsHidlEnvironmentV2_0::readEvents() {
     size_t eventsToRead = std::min(availableEvents, mEventBuffer.size());
     if (eventsToRead > 0) {
         if (mEventQueue->read(mEventBuffer.data(), eventsToRead)) {
-            mEventQueueFlag->wake(asBaseType(EventQueueFlagBits::EVENTS_READ));
             for (size_t i = 0; i < eventsToRead; i++) {
                 addEvent(mEventBuffer[i]);
             }
