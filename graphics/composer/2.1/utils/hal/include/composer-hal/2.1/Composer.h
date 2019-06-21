0f5f6cd03c95d7dc58befc200e17df42c31fdf7f
openvela-robot <openvela-robot@xiaomi.com>
2019-06-22 03:13:46 +0800

    Snap for 5680585 from 5943c0bf3ddf94661dde6e170da96e6b031403b9 to rvc-release
    Change-Id: Ib04bd774b8230ef2c2e4ab0b6fe38d15cd798eb2

--- graphics/composer/2.1/utils/hal/include/composer-hal/2.1/Composer.h
+++ graphics/composer/2.1/utils/hal/include/composer-hal/2.1/Composer.h
@@ -80,8 +80,7 @@ class ComposerImpl : public Interface {
 
     Return<void> createClient(IComposer::createClient_cb hidl_cb) override {
         std::unique_lock<std::mutex> lock(mClientMutex);
-        bool destroyed = waitForClientDestroyedLocked(lock);
-        if (!destroyed) {
+        if (!waitForClientDestroyedLocked(lock)) {
             hidl_cb(Error::NO_RESOURCES, nullptr);
             return Void();
         }
@@ -109,12 +108,10 @@ class ComposerImpl : public Interface {
             // inverted (create and then destroy). Wait for a brief period to
             // see if the existing client is destroyed.
             ALOGD("waiting for previous client to be destroyed");
-            mClientDestroyedCondition.wait_for(
-                lock, 1s, [this]() -> bool { return mClient.promote() == nullptr; });
-            if (mClient.promote() != nullptr) {
+            mClientDestroyedCondition.wait_for(lock, 1s,
+                                               [this]() -> bool { return mClient == nullptr; });
+            if (mClient != nullptr) {
                 ALOGD("previous client was not destroyed");
-            } else {
-                mClient.clear();
             }
         }
 
