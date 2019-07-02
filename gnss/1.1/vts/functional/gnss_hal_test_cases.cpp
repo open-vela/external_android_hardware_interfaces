d904f743e9a9e6c3ef7f2c96a72e43898b5e76dd
openvela-robot <openvela-robot@xiaomi.com>
2019-07-02 12:54:26 +0800

    [automerger skipped] Merge "DO NOT MERGE - Merge qt-dev-plus-aosp-without-vendor (5699924) into stage-aosp-master" into stage-aosp-master am: 26f1b51bfb -s ours am skip reason: subject contains skip directive
    Change-Id: I2de4b01d878190166887d48488ef42c24a4ba65b

--- gnss/1.1/vts/functional/gnss_hal_test_cases.cpp
+++ gnss/1.1/vts/functional/gnss_hal_test_cases.cpp
@@ -202,11 +202,6 @@ IGnssConfiguration::BlacklistedSource FindStrongFrequentNonGpsSource(
  * formerly strongest satellite
  */
 TEST_F(GnssHalTest, BlacklistIndividualSatellites) {
-    if (!IsGnssHalVersion_1_1()) {
-        ALOGI("Test BlacklistIndividualSatellites skipped. GNSS HAL version is greater than 1.1.");
-        return;
-    }
-
     const int kLocationsToAwait = 3;
     const int kRetriesToUnBlacklist = 10;
 
@@ -328,11 +323,6 @@ TEST_F(GnssHalTest, BlacklistIndividualSatellites) {
  * 4a & b) Clean up by turning off location, and send in empty blacklist.
  */
 TEST_F(GnssHalTest, BlacklistConstellation) {
-    if (!IsGnssHalVersion_1_1()) {
-        ALOGI("Test BlacklistConstellation skipped. GNSS HAL version is greater than 1.1.");
-        return;
-    }
-
     const int kLocationsToAwait = 3;
 
     StartAndCheckLocations(kLocationsToAwait);
