fab743008eae3fe28223cbed97246f04db1fc9c0
openvela-robot <openvela-robot@xiaomi.com>
2019-07-12 11:41:24 +0800

    Merge "Disable startNetworkScan tests with parameters." into qt-dev

--- neuralnetworks/1.2/vts/functional/CompilationCachingTests.cpp
+++ neuralnetworks/1.2/vts/functional/CompilationCachingTests.cpp
@@ -27,9 +27,8 @@
 #include <cstdlib>
 #include <random>
 
-#include "1.2/Callbacks.h"
+#include "Callbacks.h"
 #include "GeneratedTestHarness.h"
-#include "MemoryUtils.h"
 #include "TestHarness.h"
 #include "Utils.h"
 #include "VtsHalNeuralnetworks.h"
@@ -50,7 +49,7 @@ namespace float32_model {
 
 // In frameworks/ml/nn/runtime/test/generated/, creates a hidl model of float32 mobilenet.
 #include "examples/mobilenet_224_gender_basic_fixed.example.cpp"
-#include "vts/V1_2/models/mobilenet_224_gender_basic_fixed.model.cpp"
+#include "vts_models/mobilenet_224_gender_basic_fixed.model.cpp"
 
 // Prevent the compiler from complaining about an otherwise unused function.
 [[maybe_unused]] auto dummy_createTestModel = createTestModel_dynamic_output_shape;
@@ -73,7 +72,7 @@ namespace quant8_model {
 
 // In frameworks/ml/nn/runtime/test/generated/, creates a hidl model of quant8 mobilenet.
 #include "examples/mobilenet_quantized.example.cpp"
-#include "vts/V1_2/models/mobilenet_quantized.model.cpp"
+#include "vts_models/mobilenet_quantized.model.cpp"
 
 // Prevent the compiler from complaining about an otherwise unused function.
 [[maybe_unused]] auto dummy_createTestModel = createTestModel_dynamic_output_shape;
