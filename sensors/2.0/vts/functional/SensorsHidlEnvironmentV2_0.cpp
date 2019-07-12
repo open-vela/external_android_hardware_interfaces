4e0b42e1aec599e9ab26fe9c41887692ec68d5d2
liujinye <liujinye@xiaomi.com>
2019-07-12 17:55:30 +0800

    Verify mPollThread is joinable before detaching
    If HidlSetUp() bails before startPollingThread() is called (which can
    happen if the HAL isn't implemented on the given device), mPollThread
    will initialize with the default constructor resulting in joinable()
    returning false which means calling detach() throws an exception.
    
    Checking joinable() before detaching allows the test suite to be skipped
    successfully.
    
    Fixes: 136736906
    Test: Run vts-tradefed on VtsHalSensorsV1_0Target and verify that it is
        skipped successfully on a device that doesn't support HAL 1.0
    
    Merged-In: Ie685ae2dc314edb8df2f3cc7112141a2f5e46008
    Change-Id: Ie685ae2dc314edb8df2f3cc7112141a2f5e46008

--- sensors/2.0/vts/functional/SensorsHidlEnvironmentV2_0.cpp
+++ sensors/2.0/vts/functional/SensorsHidlEnvironmentV2_0.cpp
@@ -130,8 +130,8 @@ void SensorsHidlEnvironmentV2_0::HidlTearDown() {
 
 void SensorsHidlEnvironmentV2_0::startPollingThread() {
     mStopThread = false;
-    mEvents.reserve(MAX_RECEIVE_BUFFER_EVENT_COUNT);
     mPollThread = std::thread(pollingThread, this);
+    mEvents.reserve(MAX_RECEIVE_BUFFER_EVENT_COUNT);
 }
 
 void SensorsHidlEnvironmentV2_0::readEvents() {
