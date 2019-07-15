a50ead9ac89e4060caabfe6a038e9186b7b4b380
liujinye <liujinye@xiaomi.com>
2019-07-15 18:52:00 +0800

    Add GetLocationLowPower VTS 2.0 test
    - Run test only if LOW_POWER_MODE capability is supported.
    - Modify the corresponding VTS 1.1 test to run only if the GNSS
      HAL version is 1.1.
    
    Fixes: 127434062
    Fixes: 146010868
    Test: atest VtsHalGnssV2_0Target
    Change-Id: Icbd18d80468e9e5a542f2eace5bfbe45af08ca47

--- graphics/composer/2.3/utils/command-buffer/include/composer-command-buffer/2.3/ComposerCommandBuffer.h
+++ graphics/composer/2.3/utils/command-buffer/include/composer-command-buffer/2.3/ComposerCommandBuffer.h
@@ -79,7 +79,6 @@ class CommandWriterBase : public V2_2::CommandWriterBase {
 
     void setLayerPerFrameMetadataBlobs(
         const hidl_vec<IComposerClient::PerFrameMetadataBlob>& metadata) {
-        // in units of uint32_t's
         size_t commandLength = 0;
 
         if (metadata.size() > std::numeric_limits<uint32_t>::max()) {
@@ -87,12 +86,12 @@ class CommandWriterBase : public V2_2::CommandWriterBase {
             return;
         }
 
-        // space for numElements
-        commandLength += 1;
+        // number of blobs
+        commandLength += metadata.size();
 
         for (auto metadataBlob : metadata) {
-            commandLength += 1;  // key of metadata blob
-            commandLength += 1;  // size information of metadata blob
+            commandLength += sizeof(int32_t);  // key of metadata blob
+            commandLength += 1;                // size information of metadata blob
 
             // metadata content size
             size_t metadataSize = metadataBlob.blob.size() / sizeof(uint32_t);
--- radio/1.0/vts/functional/radio_hidl_hal_stk.cpp
+++ radio/1.0/vts/functional/radio_hidl_hal_stk.cpp
@@ -21,7 +21,7 @@ using namespace ::android::hardware::radio::V1_0;
 /*
  * Test IRadio.sendEnvelope() for the response returned.
  */
-TEST_P(RadioHidlTest, sendEnvelope) {
+TEST_F(RadioHidlTest, sendEnvelope) {
     serial = GetRandomSerialNumber();
 
     // Test with sending empty string
@@ -44,7 +44,7 @@ TEST_P(RadioHidlTest, sendEnvelope) {
 /*
  * Test IRadio.sendTerminalResponseToSim() for the response returned.
  */
-TEST_P(RadioHidlTest, sendTerminalResponseToSim) {
+TEST_F(RadioHidlTest, sendTerminalResponseToSim) {
     serial = GetRandomSerialNumber();
 
     // Test with sending empty string
@@ -67,7 +67,7 @@ TEST_P(RadioHidlTest, sendTerminalResponseToSim) {
 /*
  * Test IRadio.handleStkCallSetupRequestFromSim() for the response returned.
  */
-TEST_P(RadioHidlTest, handleStkCallSetupRequestFromSim) {
+TEST_F(RadioHidlTest, handleStkCallSetupRequestFromSim) {
     serial = GetRandomSerialNumber();
     bool accept = false;
 
@@ -88,7 +88,7 @@ TEST_P(RadioHidlTest, handleStkCallSetupRequestFromSim) {
 /*
  * Test IRadio.reportStkServiceIsRunning() for the response returned.
  */
-TEST_P(RadioHidlTest, reportStkServiceIsRunning) {
+TEST_F(RadioHidlTest, reportStkServiceIsRunning) {
     serial = GetRandomSerialNumber();
 
     radio->reportStkServiceIsRunning(serial);
@@ -107,7 +107,7 @@ TEST_P(RadioHidlTest, reportStkServiceIsRunning) {
  * Test IRadio.sendEnvelopeWithStatus() for the response returned with empty
  * string.
  */
-TEST_P(RadioHidlTest, sendEnvelopeWithStatus) {
+TEST_F(RadioHidlTest, sendEnvelopeWithStatus) {
     serial = GetRandomSerialNumber();
 
     // Test with sending empty string
