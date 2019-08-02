ffd7b7cd9424a3484161bd6cf680d6f07cb25bec
liujinye <liujinye@xiaomi.com>
2019-08-02 21:21:41 +0800

    Merge "VTS Sensor HAL 2.0: Fix Batch for One Shot sensors" into qt-r1-dev am: effdb89c97 am: 0ef0800de8
    Change-Id: I9591fd0c86d0bd33d5a2e3e4b99e1d57843f8c21

--- neuralnetworks/1.1/vts/functional/GeneratedTestHarness.cpp
+++ neuralnetworks/1.1/vts/functional/GeneratedTestHarness.cpp
@@ -34,7 +34,6 @@
 namespace android {
 namespace hardware {
 namespace neuralnetworks {
-namespace V1_1 {
 namespace generated_tests {
 
 using ::android::hardware::neuralnetworks::V1_0::ErrorStatus;
@@ -228,7 +227,6 @@ void Execute(const sp<IDevice>& device, std::function<Model(void)> create_model,
 }
 
 }  // namespace generated_tests
-}  // namespace V1_1
 }  // namespace neuralnetworks
 }  // namespace hardware
 }  // namespace android
