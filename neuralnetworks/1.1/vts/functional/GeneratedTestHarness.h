5ef34d9710a1f09e167eed635b48d51d8d24ce63
liujinye <liujinye@xiaomi.com>
2019-08-02 16:32:24 +0800

    Merge "Keymaster should be developed in AOSP"

--- neuralnetworks/1.1/vts/functional/GeneratedTestHarness.h
+++ neuralnetworks/1.1/vts/functional/GeneratedTestHarness.h
@@ -26,7 +26,6 @@
 namespace android {
 namespace hardware {
 namespace neuralnetworks {
-namespace V1_1 {
 namespace generated_tests {
 
 void Execute(const sp<V1_1::IDevice>& device, std::function<V1_1::Model(void)> create_model,
@@ -34,7 +33,6 @@ void Execute(const sp<V1_1::IDevice>& device, std::function<V1_1::Model(void)> c
              const std::vector<::test_helper::MixedTypedExample>& examples);
 
 }  // namespace generated_tests
-}  // namespace V1_1
 }  // namespace neuralnetworks
 }  // namespace hardware
 }  // namespace android
