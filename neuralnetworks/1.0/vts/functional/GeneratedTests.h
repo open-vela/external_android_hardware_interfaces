864d8173c85458f0ce926c5835b04a3d58d91805
liujinye <liujinye@xiaomi.com>
2019-08-15 09:48:26 +0800

    Merge "Refactor generated NNAPI tests" am: caedc76b18 am: e2f9d55a26 am: 0272fa1984 am: 81aa1bf8ed
    Change-Id: I00639cc6b70deb38871e9b93f86e71c7a71a4f76

--- neuralnetworks/1.0/vts/functional/GeneratedTests.h
+++ neuralnetworks/1.0/vts/functional/GeneratedTests.h
@@ -32,4 +32,7 @@ namespace android::hardware::neuralnetworks::V1_0::generated_tests {
 
 using namespace android::hardware::neuralnetworks::V1_0::vts::functional;
 
+using ::android::hardware::neuralnetworks::V1_0::Model;
+using ::android::hardware::neuralnetworks::V1_0::Request;
+
 }  // namespace android::hardware::neuralnetworks::V1_0::generated_tests
