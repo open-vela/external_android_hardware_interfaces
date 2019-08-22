2d988c3806c75e59d268e2563d111d251d6beeb0
openvela-robot <openvela-robot@xiaomi.com>
2019-08-23 06:42:09 +0800

    DO NOT MERGE - Merge build QP1A.190711.001 into stage-aosp-master history
    Bug: 139893257
    Change-Id: Ibd3257fe6f6f10539a746592a4dcc1299adf596c

--- neuralnetworks/1.0/vts/functional/GeneratedTests.h
+++ neuralnetworks/1.0/vts/functional/GeneratedTests.h
@@ -14,10 +14,19 @@
  * limitations under the License.
  */
 
-#include "1.0/Utils.h"
+#include <android/hidl/memory/1.0/IMemory.h>
+#include <hidlmemory/mapping.h>
+
 #include "GeneratedTestHarness.h"
+#include "MemoryUtils.h"
 #include "TestHarness.h"
 #include "VtsHalNeuralnetworks.h"
+
+namespace android::hardware::neuralnetworks::V1_0::vts::functional {
+
+std::vector<Request> createRequests(const std::vector<::test_helper::MixedTypedExample>& examples);
+
+}  // namespace android::hardware::neuralnetworks::V1_0::vts::functional
 
 namespace android::hardware::neuralnetworks::V1_0::generated_tests {
 
