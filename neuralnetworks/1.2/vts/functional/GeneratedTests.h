b946cb37a024ec2eef09ad82d11891b5650a7946
openvela-robot <openvela-robot@xiaomi.com>
2019-08-26 13:57:34 +0800

    Merge "Lights: parameterized VTS tests." am: 8028a27e66 am: 7e83072833
    Change-Id: Ia8d7f0d542de2b526a13c7b78f1ad760b183feac

--- neuralnetworks/1.2/vts/functional/GeneratedTests.h
+++ neuralnetworks/1.2/vts/functional/GeneratedTests.h
@@ -14,10 +14,20 @@
  * limitations under the License.
  */
 
-#include "1.0/Utils.h"
+#include <android/hidl/memory/1.0/IMemory.h>
+#include <hidlmemory/mapping.h>
+
 #include "GeneratedTestHarness.h"
+#include "MemoryUtils.h"
 #include "TestHarness.h"
+#include "Utils.h"
 #include "VtsHalNeuralnetworks.h"
+
+namespace android::hardware::neuralnetworks::V1_2::vts::functional {
+
+std::vector<Request> createRequests(const std::vector<::test_helper::MixedTypedExample>& examples);
+
+}  // namespace android::hardware::neuralnetworks::V1_2::vts::functional
 
 namespace android::hardware::neuralnetworks::V1_2::generated_tests {
 
