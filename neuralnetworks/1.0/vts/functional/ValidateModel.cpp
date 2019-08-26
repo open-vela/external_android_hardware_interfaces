eccb2bb4a2f0a3c6ffda8a2e8c231557b2f9b41d
openvela-robot <openvela-robot@xiaomi.com>
2019-08-26 14:17:46 +0800

    Merge "Lights: parameterized VTS tests." am: 8028a27e66 am: 7e83072833 am: b6e491f109
    Change-Id: I2dd2ec9a192a5921399df32710e7da546f9f2bc4

--- neuralnetworks/1.0/vts/functional/ValidateModel.cpp
+++ neuralnetworks/1.0/vts/functional/ValidateModel.cpp
@@ -16,9 +16,9 @@
 
 #define LOG_TAG "neuralnetworks_hidl_hal_test"
 
-#include "1.0/Callbacks.h"
-#include "GeneratedTestHarness.h"
 #include "VtsHalNeuralnetworks.h"
+
+#include "1.0/Callbacks.h"
 
 namespace android {
 namespace hardware {
