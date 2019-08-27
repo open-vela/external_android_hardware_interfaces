7036f02b0e44ed91e97c0188a07bc06bd68df360
openvela-robot <openvela-robot@xiaomi.com>
2019-08-27 16:44:06 +0800

    [automerger skipped] Merge changes from topic "nnapi-cp-test-model" am: 70764914db -s ours am: e44f9e509c -s ours am skip reason: change_id I54ac97f62898e47a338b51cc6d895a0309ab001f with SHA1 6d0270b056 is in history
    Change-Id: Ifb8ef1b93a18fc0cf83effd3aae02423014e6203

--- neuralnetworks/1.2/vts/functional/ValidateRequest.cpp
+++ neuralnetworks/1.2/vts/functional/ValidateRequest.cpp
@@ -19,7 +19,6 @@
 #include "1.0/Utils.h"
 #include "1.2/Callbacks.h"
 #include "ExecutionBurstController.h"
-#include "GeneratedTestHarness.h"
 #include "TestHarness.h"
 #include "Utils.h"
 #include "VtsHalNeuralnetworks.h"
