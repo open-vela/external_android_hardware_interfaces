1cc1728e2fffa4602b4a99a4d6d6815204d14dbd
liujinye <liujinye@xiaomi.com>
2019-08-27 13:12:13 +0800

    Modify 1.0 VTS tests to consume test struct directly. am: 8487c5e21f am: 42677e97bb am: 3076ffc8ca
    Change-Id: Ia88918e05d036cbdd7305dafcd630e75db67d9f1

--- neuralnetworks/1.2/vts/functional/GeneratedTestHarness.h
+++ neuralnetworks/1.2/vts/functional/GeneratedTestHarness.h
@@ -30,15 +30,18 @@ namespace neuralnetworks {
 namespace V1_2 {
 namespace generated_tests {
 
-Model createModel(const ::test_helper::TestModel& testModel);
+using ::test_helper::MixedTypedExample;
 
 void PrepareModel(const sp<V1_2::IDevice>& device, const V1_2::Model& model,
                   sp<V1_2::IPreparedModel>* preparedModel);
 
-void EvaluatePreparedModel(const sp<V1_2::IPreparedModel>& preparedModel,
-                           const ::test_helper::TestModel& testModel, bool testDynamicOutputShape);
+void EvaluatePreparedModel(sp<V1_2::IPreparedModel>& preparedModel,
+                           std::function<bool(int)> is_ignored,
+                           const std::vector<MixedTypedExample>& examples,
+                           bool hasRelaxedFloat32Model, bool testDynamicOutputShape);
 
-void Execute(const sp<V1_2::IDevice>& device, const ::test_helper::TestModel& testModel,
+void Execute(const sp<V1_2::IDevice>& device, std::function<V1_2::Model(void)> create_model,
+             std::function<bool(int)> is_ignored, const std::vector<MixedTypedExample>& examples,
              bool testDynamicOutputShape = false);
 
 }  // namespace generated_tests
