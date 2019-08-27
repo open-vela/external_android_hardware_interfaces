9105ab168e831e0bcdebc5796d5bca77e44256a1
openvela-robot <openvela-robot@xiaomi.com>
2019-08-27 13:31:05 +0800

    Merge changes from topic "c2f2_sensors_vts-qt-dev" into qt-dev
    * changes:
      Fix handling of reportToken when stopping
      Change expected return type in direct report VTS
      Add support for new gralloc HAL versions
      Avoid NPD when re-initializing HAL fails
      Fix NPD in GrallocWrapper
      Fix log tag for sensors VTS tests

--- neuralnetworks/1.0/vts/functional/GeneratedTestHarness.h
+++ neuralnetworks/1.0/vts/functional/GeneratedTestHarness.h
@@ -14,56 +14,44 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_NEURALNETWORKS_V1_0_GENERATED_TEST_HARNESS_H
-#define ANDROID_HARDWARE_NEURALNETWORKS_V1_0_GENERATED_TEST_HARNESS_H
+#ifndef VTS_HAL_NEURALNETWORKS_GENERATED_TEST_HARNESS_H
+#define VTS_HAL_NEURALNETWORKS_GENERATED_TEST_HARNESS_H
+
+#include "TestHarness.h"
 
 #include <android/hardware/neuralnetworks/1.0/IDevice.h>
-#include "TestHarness.h"
-#include "VtsHalNeuralnetworks.h"
+#include <android/hardware/neuralnetworks/1.1/IDevice.h>
+#include <android/hardware/neuralnetworks/1.2/IDevice.h>
 
 namespace android {
 namespace hardware {
 namespace neuralnetworks {
-namespace V1_0 {
-namespace vts {
-namespace functional {
 
-class GeneratedTestBase
-    : public NeuralnetworksHidlTest,
-      public ::testing::WithParamInterface<test_helper::TestModelManager::TestParam> {
-  protected:
-    void SetUp() override {
-        NeuralnetworksHidlTest::SetUp();
-        ASSERT_NE(mTestModel, nullptr);
-    }
+namespace generated_tests {
+using ::test_helper::MixedTypedExample;
 
-    const test_helper::TestModel* mTestModel = GetParam().second;
-};
+void PrepareModel(const sp<V1_2::IDevice>& device, const V1_2::Model& model,
+                  sp<V1_2::IPreparedModel>* preparedModel);
 
-#define INSTANTIATE_GENERATED_TEST(TestSuite, filter)                                          \
-    INSTANTIATE_TEST_SUITE_P(                                                                  \
-            TestGenerated, TestSuite,                                                          \
-            ::testing::ValuesIn(::test_helper::TestModelManager::get().getTestModels(filter)), \
-            [](const auto& info) { return info.param.first; })
+void EvaluatePreparedModel(sp<V1_2::IPreparedModel>& preparedModel,
+                           std::function<bool(int)> is_ignored,
+                           const std::vector<MixedTypedExample>& examples,
+                           bool hasRelaxedFloat32Model, bool testDynamicOutputShape);
 
-// Tag for the validation tests, instantiated in VtsHalNeuralnetworks.cpp.
-// TODO: Clean up the hierarchy for ValidationTest.
-class ValidationTest : public GeneratedTestBase {
-  protected:
-    void validateEverything(const Model& model, const Request& request);
+void Execute(const sp<V1_0::IDevice>& device, std::function<V1_0::Model(void)> create_model,
+             std::function<bool(int)> is_ignored, const std::vector<MixedTypedExample>& examples);
 
-  private:
-    void validateModel(const Model& model);
-    void validateRequest(const sp<IPreparedModel>& preparedModel, const Request& request);
-};
+void Execute(const sp<V1_1::IDevice>& device, std::function<V1_1::Model(void)> create_model,
+             std::function<bool(int)> is_ignored, const std::vector<MixedTypedExample>& examples);
 
-Model createModel(const ::test_helper::TestModel& testModel);
+void Execute(const sp<V1_2::IDevice>& device, std::function<V1_2::Model(void)> create_model,
+             std::function<bool(int)> is_ignored, const std::vector<MixedTypedExample>& examples,
+             bool testDynamicOutputShape = false);
 
-}  // namespace functional
-}  // namespace vts
-}  // namespace V1_0
+}  // namespace generated_tests
+
 }  // namespace neuralnetworks
 }  // namespace hardware
 }  // namespace android
 
-#endif  // ANDROID_HARDWARE_NEURALNETWORKS_V1_0_GENERATED_TEST_HARNESS_H
+#endif  // VTS_HAL_NEURALNETWORKS_GENERATED_TEST_HARNESS_H
