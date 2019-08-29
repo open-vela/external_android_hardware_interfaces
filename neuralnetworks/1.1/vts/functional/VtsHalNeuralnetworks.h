d7e5e8dcbde8cb68132d46064a4b17b42efcd02e
openvela-robot <openvela-robot@xiaomi.com>
2019-08-29 10:34:49 +0800

    Fix wait_for timestamps in Sensors VTS am: eac8d9acc9
    Change-Id: I21c8577ecc56e2d2334352e2f3e9c386cc6aead6

--- neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.h
+++ neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.h
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef VTS_HAL_NEURALNETWORKS_V1_1_H
-#define VTS_HAL_NEURALNETWORKS_V1_1_H
+#ifndef ANDROID_HARDWARE_NEURALNETWORKS_V1_1_VTS_HAL_NEURALNETWORKS_H
+#define ANDROID_HARDWARE_NEURALNETWORKS_V1_1_VTS_HAL_NEURALNETWORKS_H
 
 #include <android/hardware/neuralnetworks/1.0/types.h>
 #include <android/hardware/neuralnetworks/1.1/IDevice.h>
@@ -28,6 +28,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
+
+#include "TestHarness.h"
 
 namespace android {
 namespace hardware {
@@ -69,20 +71,6 @@ class NeuralnetworksHidlTest : public ::testing::VtsHalHidlTargetTestBase {
     sp<IDevice> device;
 };
 
-// Tag for the validation tests
-class ValidationTest : public NeuralnetworksHidlTest {
-   protected:
-     void validateEverything(const Model& model, const std::vector<Request>& request);
-
-   private:
-     void validateModel(const Model& model);
-     void validateRequests(const sp<IPreparedModel>& preparedModel,
-                           const std::vector<Request>& requests);
-};
-
-// Tag for the generated tests
-class GeneratedTest : public NeuralnetworksHidlTest {};
-
 }  // namespace functional
 }  // namespace vts
 }  // namespace V1_1
@@ -98,4 +86,4 @@ namespace android::hardware::neuralnetworks::V1_0 {
 
 }  // namespace android::hardware::neuralnetworks::V1_0
 
-#endif  // VTS_HAL_NEURALNETWORKS_V1_1_H
+#endif  // ANDROID_HARDWARE_NEURALNETWORKS_V1_1_VTS_HAL_NEURALNETWORKS_H
