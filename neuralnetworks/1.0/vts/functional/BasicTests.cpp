146f96a1326a1bcdc06198447e936d1944a58685
liujinye <liujinye@xiaomi.com>
2019-09-05 13:16:24 +0800

    Merge "hostapd: Ignore ACS relate vts testcase for hostapd 1.0 and hostapd 1.1" into qt-dev am: e9e4020560 am: 6059e56d92
    Change-Id: If56d6f3d4f8cb22e8702b0225f292a064cffdf7d

--- neuralnetworks/1.0/vts/functional/BasicTests.cpp
+++ neuralnetworks/1.0/vts/functional/BasicTests.cpp
@@ -18,7 +18,12 @@
 
 #include "VtsHalNeuralnetworks.h"
 
-namespace android::hardware::neuralnetworks::V1_0::vts::functional {
+namespace android {
+namespace hardware {
+namespace neuralnetworks {
+namespace V1_0 {
+namespace vts {
+namespace functional {
 
 // create device test
 TEST_F(NeuralnetworksHidlTest, CreateDevice) {}
@@ -33,14 +38,19 @@ TEST_F(NeuralnetworksHidlTest, StatusTest) {
 // initialization
 TEST_F(NeuralnetworksHidlTest, GetCapabilitiesTest) {
     Return<void> ret =
-            device->getCapabilities([](ErrorStatus status, const Capabilities& capabilities) {
-                EXPECT_EQ(ErrorStatus::NONE, status);
-                EXPECT_LT(0.0f, capabilities.float32Performance.execTime);
-                EXPECT_LT(0.0f, capabilities.float32Performance.powerUsage);
-                EXPECT_LT(0.0f, capabilities.quantized8Performance.execTime);
-                EXPECT_LT(0.0f, capabilities.quantized8Performance.powerUsage);
-            });
+        device->getCapabilities([](ErrorStatus status, const Capabilities& capabilities) {
+            EXPECT_EQ(ErrorStatus::NONE, status);
+            EXPECT_LT(0.0f, capabilities.float32Performance.execTime);
+            EXPECT_LT(0.0f, capabilities.float32Performance.powerUsage);
+            EXPECT_LT(0.0f, capabilities.quantized8Performance.execTime);
+            EXPECT_LT(0.0f, capabilities.quantized8Performance.powerUsage);
+        });
     EXPECT_TRUE(ret.isOk());
 }
 
-}  // namespace android::hardware::neuralnetworks::V1_0::vts::functional
+}  // namespace functional
+}  // namespace vts
+}  // namespace V1_0
+}  // namespace neuralnetworks
+}  // namespace hardware
+}  // namespace android
