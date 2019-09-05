4309cbc576742cdd7486182077df45841e2a61c1
liujinye <liujinye@xiaomi.com>
2019-09-05 14:13:19 +0800

    Merge "hostapd: Ignore ACS relate vts testcase for hostapd 1.0 and hostapd 1.1" into qt-dev am: e9e4020560 am: 48b9de60c9 am: 91712ec204
    Change-Id: I29db01fff1a55d3b61fc46e104b4864dc6c1941a

--- neuralnetworks/1.0/vts/functional/Utils.cpp
+++ neuralnetworks/1.0/vts/functional/Utils.cpp
@@ -26,7 +26,6 @@
 #include <hidlmemory/mapping.h>
 
 #include <algorithm>
-#include <iostream>
 #include <vector>
 
 namespace android::hardware::neuralnetworks {
@@ -118,15 +117,3 @@ std::vector<TestBuffer> getOutputBuffers(const Request& request) {
 }
 
 }  // namespace android::hardware::neuralnetworks
-
-namespace android::hardware::neuralnetworks::V1_0 {
-
-::std::ostream& operator<<(::std::ostream& os, ErrorStatus errorStatus) {
-    return os << toString(errorStatus);
-}
-
-::std::ostream& operator<<(::std::ostream& os, DeviceStatus deviceStatus) {
-    return os << toString(deviceStatus);
-}
-
-}  // namespace android::hardware::neuralnetworks::V1_0
