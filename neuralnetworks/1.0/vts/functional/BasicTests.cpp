2cbe2ba99156b566428589fa9be9f58564810f7d
openvela-robot <openvela-robot@xiaomi.com>
2019-09-25 13:33:09 +0800

    Merge changes from topic "frontend" am: 6494b4c9a6 am: 3c2d9e1fb6 am: 95323a806a
    Change-Id: I60f9507af1c968c92cb55c37bfe8e49192d0827d

--- neuralnetworks/1.0/vts/functional/BasicTests.cpp
+++ neuralnetworks/1.0/vts/functional/BasicTests.cpp
@@ -21,17 +21,17 @@
 namespace android::hardware::neuralnetworks::V1_0::vts::functional {
 
 // create device test
-TEST_P(NeuralnetworksHidlTest, CreateDevice) {}
+TEST_F(NeuralnetworksHidlTest, CreateDevice) {}
 
 // status test
-TEST_P(NeuralnetworksHidlTest, StatusTest) {
+TEST_F(NeuralnetworksHidlTest, StatusTest) {
     Return<DeviceStatus> status = kDevice->getStatus();
     ASSERT_TRUE(status.isOk());
     EXPECT_EQ(DeviceStatus::AVAILABLE, static_cast<DeviceStatus>(status));
 }
 
 // initialization
-TEST_P(NeuralnetworksHidlTest, GetCapabilitiesTest) {
+TEST_F(NeuralnetworksHidlTest, GetCapabilitiesTest) {
     Return<void> ret =
             kDevice->getCapabilities([](ErrorStatus status, const Capabilities& capabilities) {
                 EXPECT_EQ(ErrorStatus::NONE, status);
