d46a3c7247e6523b001f763b3a3b515dcde1739a
liujinye <liujinye@xiaomi.com>
2019-10-17 13:08:45 +0800

    [automerger skipped] Merge "DO NOT MERGE: Audio HAL: Add missing device to XSD" into pie-vts-dev am: 999c5e83c4 -s ours am: 09947556ed -s ours am: 659e415612 am: 76d71b6a18 am: 5bd1e99744 am: 522386424b -s ours am skip reason: subject contains skip directive
    Change-Id: I217c552841e1ad8d1eb9f2915a0c982f220e1c2d

--- automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.cpp
+++ automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.cpp
@@ -286,8 +286,8 @@ void EmulatedVehicleHal::onCreate() {
 
             // Create a separate instance for each individual zone
             VehiclePropValue prop = {
-                    .areaId = curArea,
-                    .prop = cfg.prop,
+                .prop = cfg.prop,
+                .areaId = curArea,
             };
 
             if (it.initialAreaValues.size() > 0) {
