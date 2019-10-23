125905f34737100b2f9831c1aae39619915df07f
liujinye <liujinye@xiaomi.com>
2019-10-23 16:14:11 +0800

    Merge "TEST_MAPPING += hidl_implementation_test" am: fac4d20499
    Change-Id: I31de45d09ce5cc2e17a90c8adaad200f9d12e77f

--- dumpstate/1.0/default/DumpstateDevice.cpp
+++ dumpstate/1.0/default/DumpstateDevice.cpp
@@ -18,6 +18,7 @@
 
 #include "DumpstateDevice.h"
 
+#include <hidl/HidlBinderSupport.h>
 #include <log/log.h>
 
 #include "DumpstateUtil.h"
@@ -34,8 +35,7 @@ namespace implementation {
 // Methods from ::android::hardware::dumpstate::V1_0::IDumpstateDevice follow.
 Return<void> DumpstateDevice::dumpstateBoard(const hidl_handle& handle) {
     // NOTE: this is just an example on how to use the DumpstateUtil.h functions to implement
-    // this interface - since HIDL_FETCH_IDumpstateDevice() is not defined, this function will never
-    // be called by dumpstate.
+    // this interface.
 
     if (handle == nullptr || handle->numFds < 1) {
         ALOGE("no FDs\n");
