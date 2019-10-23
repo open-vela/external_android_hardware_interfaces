a29b17bdce2ec56bf303d8a76f9fb70c8a19c89e
liujinye <liujinye@xiaomi.com>
2019-10-23 16:46:54 +0800

    audio: Parametrize effect VTS tests for V6.0
    Parametrize tests to accept IEffectsFactory instance name.
    Discover the instances in the test.
    
    Bug: 141847510
    Bug: 141989952
    Test: atest VtsHalAudioEffectV5_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
    
    Change-Id: Iaf19109fc77a93b211cc3da85c21c0584d4f2b88
    Merged-In: Iaf19109fc77a93b211cc3da85c21c0584d4f2b88

--- vibrator/aidl/vts/VtsHalVibratorTargetTest.cpp
+++ vibrator/aidl/vts/VtsHalVibratorTargetTest.cpp
@@ -247,7 +247,7 @@ TEST_P(VibratorAidl, ExternalControlUnsupportedMatchingCapabilities) {
     }
 }
 
-INSTANTIATE_TEST_SUITE_P(Vibrator, VibratorAidl,
+INSTANTIATE_TEST_SUITE_P(, VibratorAidl,
                          testing::ValuesIn(android::getAidlHalInstanceNames(IVibrator::descriptor)),
                          android::PrintInstanceNameToString);
 
