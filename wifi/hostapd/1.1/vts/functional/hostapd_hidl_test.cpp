8fbfdf5fbb0293659ba766f5f717e60332ebf789
liujinye <liujinye@xiaomi.com>
2019-10-29 13:01:25 +0800

    Add cas@1.2 to compatibility matrix
    Test: m check-vintf-all
    Bug: 141783130
    Bug: 110261831
    
    Change-Id: Ie8fda69f5552e4e9d92262695884e0de0097fe76
    Merged-In: I487aa47c13ad1a50831ddbcb3b9b25de9972e5b7

--- wifi/hostapd/1.1/vts/functional/hostapd_hidl_test.cpp
+++ wifi/hostapd/1.1/vts/functional/hostapd_hidl_test.cpp
@@ -23,7 +23,6 @@
 
 #include <android/hardware/wifi/1.0/IWifi.h>
 #include <android/hardware/wifi/hostapd/1.1/IHostapd.h>
-#include <android/hardware/wifi/hostapd/1.3/IHostapd.h>
 
 #include "hostapd_hidl_call_util.h"
 #include "hostapd_hidl_test_utils.h"
@@ -44,7 +43,6 @@ constexpr unsigned char kNwSsid[] = {'t', 'e', 's', 't', '1',
 constexpr char kNwPassphrase[] = "test12345";
 constexpr int kIfaceChannel = 6;
 constexpr int kIfaceInvalidChannel = 567;
-
 }  // namespace
 
 class HostapdHidlTest
@@ -172,17 +170,10 @@ class IfaceCallback : public IHostapdCallback {
     }
 };
 
-bool is_1_3(const sp<IHostapd>& hostapd) {
-    sp<::android::hardware::wifi::hostapd::V1_3::IHostapd> hostapd_1_3 =
-        ::android::hardware::wifi::hostapd::V1_3::IHostapd::castFrom(hostapd);
-    return hostapd_1_3.get() != nullptr;
-}
-
 /*
  * RegisterCallback
  */
 TEST_P(HostapdHidlTest, registerCallback) {
-    if (is_1_3(hostapd_)) GTEST_SKIP() << "Ignore since current HIDL over 1.3";
     hostapd_->registerCallback(
         new IfaceCallback(), [](const HostapdStatus& status) {
             EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
@@ -242,11 +233,7 @@ TEST_P(HostapdHidlTest, AddOpenAccessPointWithAcs) {
 TEST_P(HostapdHidlTest, AddPskAccessPointWithoutAcs) {
     auto status = HIDL_INVOKE(hostapd_, addAccessPoint_1_1,
                               getIfaceParamsWithoutAcs(), getPskNwParams());
-    // FAILURE_UNKNOWN is used by higher versions to indicate this API is no
-    // longer supported (replaced by an upgraded API)
-    if (status.code != HostapdStatusCode::FAILURE_UNKNOWN) {
-        EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
-    }
+    EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
 }
 
 /**
@@ -256,11 +243,7 @@ TEST_P(HostapdHidlTest, AddPskAccessPointWithoutAcs) {
 TEST_P(HostapdHidlTest, AddOpenAccessPointWithoutAcs) {
     auto status = HIDL_INVOKE(hostapd_, addAccessPoint_1_1,
                               getIfaceParamsWithoutAcs(), getOpenNwParams());
-    // FAILURE_UNKNOWN is used by higher versions to indicate this API is no
-    // longer supported (replaced by an upgraded API)
-    if (status.code != HostapdStatusCode::FAILURE_UNKNOWN) {
-        EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
-    }
+    EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
 }
 
 /**
@@ -286,14 +269,10 @@ TEST_P(HostapdHidlTest, RemoveAccessPointWithAcs) {
 TEST_P(HostapdHidlTest, RemoveAccessPointWithoutAcs) {
     auto status = HIDL_INVOKE(hostapd_, addAccessPoint_1_1,
                               getIfaceParamsWithoutAcs(), getPskNwParams());
-    // FAILURE_UNKNOWN is used by higher versions to indicate this API is no
-    // longer supported (replaced by an upgraded API)
-    if (status.code != HostapdStatusCode::FAILURE_UNKNOWN) {
-        EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
-        status =
-            HIDL_INVOKE(hostapd_, removeAccessPoint, getPrimaryWlanIfaceName());
-        EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
-    }
+    EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
+    status =
+        HIDL_INVOKE(hostapd_, removeAccessPoint, getPrimaryWlanIfaceName());
+    EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
 }
 
 /**
@@ -318,7 +297,6 @@ TEST_P(HostapdHidlTest, AddInvalidPskAccessPointWithoutAcs) {
     EXPECT_NE(HostapdStatusCode::SUCCESS, status.code);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(HostapdHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, HostapdHidlTest,
     testing::Combine(
