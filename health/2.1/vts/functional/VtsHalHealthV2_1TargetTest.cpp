c26fabbce4ae926393e77b5c1048831f04a992e9
liujinye <liujinye@xiaomi.com>
2019-11-01 16:28:37 +0800

    audio: Run VTS tests for streams of non-primary modules for HAL V6
    Implement parsing of AudioPolicyManager config for finding
    out supported format configurations of streams. This only applies
    when running tests for HAL V6. Previously format configurations
    mandated by CDD were used for testing, this does not work well
    for non-primary modules.
    
    Fix the following issues found while running the tests
    for "r_submix" and "msd" modules:
    
    - IStream::getSupportedFormats must return a status
      to indicate that this capability is not supported by HAL;
    
    - it is allowed for IStream::setDevices to return
      NOT_SUPPORTED status.
    
    Other changes:
    
    - Factor out helper functions for generating format
      configurations;
    
    - Fix generation of the channel mask component in the names
      of tests that use AudioConfig, add sampling rate to test
      names.
    
    Squashed with the following commit to avoid breaking compilation:
    
    audio vts: Remove explicit dependency on the new types
    
    Avoid using the new ChannelMaskSet and SampleRateSet types
    directly to simplify upstreaming.
    
    Bug: 141989952
    Bug: 141847510
    Test: atest VtsHalAudioV5_0TargetTest
          atest VtsHalAudioV6_0TargetTest
                also, run modified V5_0 test using generators for V6_0
    
    Change-Id: If0d330881901908e546baab89f63d3333003e355
    Merged-In: If0d330881901908e546baab89f63d3333003e355

--- health/2.1/vts/functional/VtsHalHealthV2_1TargetTest.cpp
+++ health/2.1/vts/functional/VtsHalHealthV2_1TargetTest.cpp
@@ -252,7 +252,7 @@ TEST_P(HealthHidlTest, shouldKeepScreenOn) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-        PerInstance, HealthHidlTest,
+        , HealthHidlTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(IHealth::descriptor)),
         android::hardware::PrintInstanceNameToString);
 
