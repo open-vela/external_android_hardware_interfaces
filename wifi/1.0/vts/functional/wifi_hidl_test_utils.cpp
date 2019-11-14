30efe15b19fcafdfe157d2e71760a4735f7fea67
liujinye <liujinye@xiaomi.com>
2019-11-14 21:54:44 +0800

    Merge "Adding test to check another ASN.1 Encoding Case"

--- wifi/1.0/vts/functional/wifi_hidl_test_utils.cpp
+++ wifi/1.0/vts/functional/wifi_hidl_test_utils.cpp
@@ -18,15 +18,12 @@
 
 #include <VtsHalHidlTargetTestBase.h>
 
-#include <wifi_system/interface_tool.h>
-
 #include "wifi_hidl_call_util.h"
 #include "wifi_hidl_test_utils.h"
 
 using ::android::hardware::wifi::V1_0::IWifi;
 using ::android::hardware::wifi::V1_0::IWifiApIface;
 using ::android::hardware::wifi::V1_0::IWifiChip;
-using ::android::hardware::wifi::V1_0::IWifiIface;
 using ::android::hardware::wifi::V1_0::IWifiNanIface;
 using ::android::hardware::wifi::V1_0::IWifiP2pIface;
 using ::android::hardware::wifi::V1_0::IWifiRttController;
@@ -39,7 +36,6 @@ using ::android::hardware::wifi::V1_0::WifiStatusCode;
 using ::android::sp;
 using ::android::hardware::hidl_string;
 using ::android::hardware::hidl_vec;
-using ::android::wifi_system::InterfaceTool;
 
 extern WifiHidlEnvironment* gEnv;
 
@@ -135,16 +131,6 @@ sp<IWifiChip> getWifiChip(const std::string& instance_name) {
     return status_and_chip.second;
 }
 
-void setIfaceUp(const sp<IWifiIface>& iface) {
-    // Set the iface up before retrurning the object.
-    const auto& status_and_name = HIDL_INVOKE(iface, getName);
-    if (status_and_name.first.code == WifiStatusCode::SUCCESS) {
-        const auto& iface_name = status_and_name.second;
-        InterfaceTool iface_tool;
-        iface_tool.SetUpState(iface_name.c_str(), true);
-    }
-}
-
 sp<IWifiApIface> getWifiApIface(const std::string& instance_name) {
     sp<IWifiChip> wifi_chip = getWifiChip(instance_name);
     if (!wifi_chip.get()) {
@@ -157,7 +143,6 @@ sp<IWifiApIface> getWifiApIface(const std::string& instance_name) {
     if (status_and_iface.first.code != WifiStatusCode::SUCCESS) {
         return nullptr;
     }
-    setIfaceUp(status_and_iface.second);
     return status_and_iface.second;
 }
 
@@ -173,7 +158,6 @@ sp<IWifiNanIface> getWifiNanIface(const std::string& instance_name) {
     if (status_and_iface.first.code != WifiStatusCode::SUCCESS) {
         return nullptr;
     }
-    setIfaceUp(status_and_iface.second);
     return status_and_iface.second;
 }
 
@@ -189,7 +173,6 @@ sp<IWifiP2pIface> getWifiP2pIface(const std::string& instance_name) {
     if (status_and_iface.first.code != WifiStatusCode::SUCCESS) {
         return nullptr;
     }
-    setIfaceUp(status_and_iface.second);
     return status_and_iface.second;
 }
 
@@ -205,7 +188,6 @@ sp<IWifiStaIface> getWifiStaIface(const std::string& instance_name) {
     if (status_and_iface.first.code != WifiStatusCode::SUCCESS) {
         return nullptr;
     }
-    setIfaceUp(status_and_iface.second);
     return status_and_iface.second;
 }
 
