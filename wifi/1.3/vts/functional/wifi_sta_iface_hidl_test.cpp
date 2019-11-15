ccb4920bc577cdc5759f38555b63caa1e3562d05
liujinye <liujinye@xiaomi.com>
2019-11-15 15:53:40 +0800

    Merge "Convert VtsHalWifiSupplicantV1_0TargetTest to be parameterized test" am: b2193ac0d0 am: f12150b22e
    Change-Id: Id646ed8dece3110c4fcd755e33b52ea185492c00

--- wifi/1.3/vts/functional/wifi_sta_iface_hidl_test.cpp
+++ wifi/1.3/vts/functional/wifi_sta_iface_hidl_test.cpp
@@ -27,8 +27,6 @@
 #include "wifi_hidl_test_utils.h"
 
 using ::android::sp;
-using ::android::hardware::hidl_array;
-using ::android::hardware::wifi::V1_0::WifiStatus;
 using ::android::hardware::wifi::V1_0::WifiStatusCode;
 using ::android::hardware::wifi::V1_3::IWifiStaIface;
 
@@ -61,11 +59,14 @@ class WifiStaIfaceHidlTest : public ::testing::VtsHalHidlTargetTestBase {
  * and return a success status code.
  */
 TEST_F(WifiStaIfaceHidlTest, GetFactoryMacAddress) {
-    std::pair<WifiStatus, hidl_array<uint8_t, 6> > status_and_mac =
+    const auto& status_and_mac =
         HIDL_INVOKE(wifi_sta_iface_, getFactoryMacAddress);
     EXPECT_EQ(WifiStatusCode::SUCCESS, status_and_mac.first.code);
-    hidl_array<uint8_t, 6> all_zero{};
-    EXPECT_NE(all_zero, status_and_mac.second);
+    const int num_elements = sizeof(status_and_mac.second) / sizeof(uint8_t);
+    EXPECT_EQ(6, num_elements);
+    for (int i = 0; i < num_elements; i++) {
+        EXPECT_NE(0, status_and_mac.second[i]);
+    }
 }
 
 /*
