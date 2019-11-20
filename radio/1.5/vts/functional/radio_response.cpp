cbecf6770e2a05057ec199e47df0466d3a7c5321
openvela-robot <openvela-robot@xiaomi.com>
2019-11-20 11:25:02 +0800

    PhoneCapabilities changes for device capabilities
    Change PhoneCapability structure to contain static capabilities for:
    * the entire device
    * each modem/radio
    * each SIM slot
    
    Created SimSlotCapability and SlotType to hold information about
    device SIM slot capabilities (separate from SimSlotStatus).
    
    Created ModemFeatures to express modem capabilities as a bitfield
    of concurrently supportable modem features.
    
    PhoneCapability has fields for UE categories, bands, access networks,
    and modem switching linger time. It also contains a lists of logical
    modem UUIDs, SIM slot capabilities, and all possible concurrently
    supportable modem features.
    
    TelephonyManager will have an API that returns
    * the PhoneCapability for the device
    * active set of ModemFeatures
    
    Apps with CARRIER_PRIVILEGE can use these APIs to access all
    capabilities of a device (eg. 5G capable, DSDS capable, etc.) and
    use them to make decisions based on what the device can support.
    
    Test: atest FrameworksTelephonyTests
    Bug: 143238237
    Bug: 143238191
    Change-Id: I424ef176086d2308a4bf53b2c79407eabafb3178
    Merged-In: I424ef176086d2308a4bf53b2c79407eabafb3178

--- radio/1.5/vts/functional/radio_response.cpp
+++ radio/1.5/vts/functional/radio_response.cpp
@@ -421,6 +421,11 @@ Return<void> RadioResponse_v1_5::sendCdmaSmsResponse(const RadioResponseInfo& /*
     return Void();
 }
 
+Return<void> RadioResponse_v1_5::sendCdmaSmsExpectMoreResponse(const RadioResponseInfo& /*info*/,
+                                                     const SendSmsResult& /*sms*/) {
+    return Void();
+}
+
 Return<void> RadioResponse_v1_5::acknowledgeLastIncomingCdmaSmsResponse(
         const RadioResponseInfo& /*info*/) {
     return Void();
@@ -974,40 +979,5 @@ Return<void> RadioResponse_v1_5::getBarringInfoResponse(
         /*barringInfos*/) {
     rspInfo = info;
     parent_v1_5.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_5::getVoiceRegistrationStateResponse_1_5(
-        const RadioResponseInfo& info,
-        const ::android::hardware::radio::V1_5::RegStateResult& /*regResponse*/) {
-    rspInfo = info;
-    parent_v1_5.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_5::getDataRegistrationStateResponse_1_5(
-        const RadioResponseInfo& info,
-        const ::android::hardware::radio::V1_5::RegStateResult& /*regResponse*/) {
-    rspInfo = info;
-    parent_v1_5.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_5::getCellInfoListResponse_1_5(
-        const RadioResponseInfo& /*info*/,
-        const ::android::hardware::hidl_vec<
-                ::android::hardware::radio::V1_5::CellInfo>& /*cellInfo*/) {
-    return Void();
-}
-
-Return<void> RadioResponse_v1_5::setNetworkSelectionModeManualResponse_1_5(
-        const RadioResponseInfo& info) {
-    rspInfo = info;
-    parent_v1_5.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_5::sendCdmaSmsExpectMoreResponse(const RadioResponseInfo& /*info*/,
-                                                               const SendSmsResult& /*sms*/) {
     return Void();
 }
