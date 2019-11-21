6dae81b7c0b1d7e77f7dc1312cbcb206c81c8733
openvela-robot <openvela-robot@xiaomi.com>
2019-11-21 20:41:22 +0800

    Merge "gralloc: add get and set BufferMetadata support"

--- audio/core/all-versions/default/include/core/default/Device.h
+++ audio/core/all-versions/default/include/core/default/Device.h
@@ -130,7 +130,6 @@ struct Device : public IDevice, public ParametersUtil {
   private:
     bool mIsClosed;
     audio_hw_device_t* mDevice;
-    int mOpenedStreamsCount = 0;
 
     virtual ~Device();
 
