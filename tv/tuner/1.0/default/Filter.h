018a11577c7e239f50fb80e264ef656acc3fbbb2
openvela-robot <openvela-robot@xiaomi.com>
2019-11-28 15:59:00 +0800

    Merge changes from topic "quant8_signed_ops"
    * changes:
      Add TENSOR_QUANT8_ASYMM_SIGNED support for activations
      Add TENSOR_QUANT8_ASYMM_SIGNED support for pooling ops
      Add TENSOR_QUANT8_ASYMM_SIGNED support to ADD, MUL and SUB.

--- tv/tuner/1.0/default/Filter.h
+++ tv/tuner/1.0/default/Filter.h
@@ -68,8 +68,6 @@ class Filter : public IFilter {
 
     virtual Return<Result> flush() override;
 
-    virtual Return<Result> releaseAvHandle(const hidl_handle& avMemory) override;
-
     virtual Return<Result> close() override;
 
     /**
@@ -131,7 +129,6 @@ class Filter : public IFilter {
     Result startMediaFilterHandler();
     Result startRecordFilterHandler();
     Result startPcrFilterHandler();
-    Result startTemiFilterHandler();
     Result startFilterLoop();
 
     void deleteEventFlag();
