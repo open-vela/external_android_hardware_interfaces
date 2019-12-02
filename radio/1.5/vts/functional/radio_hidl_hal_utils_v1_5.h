5c2af17a60f430f1a1cbb75475274aa0c7c57b96
openvela-robot <openvela-robot@xiaomi.com>
2019-12-02 21:25:12 +0800

    Merge Android10 QPR1 into AOSP master
    Bug: 145570283
    Change-Id: I29f216a7bf6c85ff67e21f2052e2069990f74c22

--- radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
+++ radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
@@ -80,12 +80,6 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
     ::android::hardware::radio::V1_4::CarrierRestrictionsWithPriority carrierRestrictionsResp;
     ::android::hardware::radio::V1_4::SimLockMultiSimPolicy multiSimPolicyResp;
 
-    // Whether toggling uicc applications operation is supported.
-    bool canToggleUiccApplicationsEnablement;
-
-    // Whether Uicc applications are enabled or not.
-    bool areUiccApplicationsEnabled;
-
     RadioResponse_v1_5(RadioHidlTest_v1_5& parent_v1_5);
     virtual ~RadioResponse_v1_5() = default;
 
@@ -530,10 +524,6 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
 
     /* 1.5 Api */
     Return<void> setSignalStrengthReportingCriteriaResponse_1_5(const RadioResponseInfo& info);
-    Return<void> enableUiccApplicationsResponse(const RadioResponseInfo& info);
-    Return<void> areUiccApplicationsEnabledResponse(const RadioResponseInfo& info, bool enabled);
-    Return<void> canToggleUiccApplicationsEnablementResponse(const RadioResponseInfo& info,
-                                                             bool canToggle);
 };
 
 /* Callback class for radio indication */
@@ -544,9 +534,6 @@ class RadioIndication_v1_5 : public ::android::hardware::radio::V1_5::IRadioIndi
   public:
     RadioIndication_v1_5(RadioHidlTest_v1_5& parent_v1_5);
     virtual ~RadioIndication_v1_5() = default;
-
-    /* 1.5 Api */
-    Return<void> uiccApplicationsEnablementChanged(RadioIndicationType type, bool enabled);
 
     /* 1.4 Api */
     Return<void> currentEmergencyNumberList(
