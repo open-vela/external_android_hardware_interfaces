d680f009098496c55b6befa7156ab259e19bb13b
openvela-robot <openvela-robot@xiaomi.com>
2019-12-03 18:13:01 +0800

    Tuner HAL minor corrections
    Test: Manual
    bug: 135708935
    Change-Id: I2f74b2a4266ea86a80dab8b729bf57a78319b207
    (cherry picked from commit d5f570e7d791f6777e808fde6d7f7dffad14353d)
    Merged-In: I2f74b2a4266ea86a80dab8b729bf57a78319b207

--- tv/tuner/1.0/default/Filter.h
+++ tv/tuner/1.0/default/Filter.h
@@ -22,7 +22,6 @@
 #include <math.h>
 #include <set>
 #include "Demux.h"
-#include "Dvr.h"
 #include "Frontend.h"
 
 using namespace std;
@@ -45,7 +44,6 @@ using ::android::hardware::tv::tuner::V1_0::Result;
 using FilterMQ = MessageQueue<uint8_t, kSynchronizedReadWrite>;
 
 class Demux;
-class Dvr;
 
 class Filter : public IFilter {
   public:
@@ -82,17 +80,11 @@ class Filter : public IFilter {
     bool createFilterMQ();
     uint16_t getTpid();
     void updateFilterOutput(vector<uint8_t> data);
-    void updateRecordOutput(vector<uint8_t> data);
     Result startFilterHandler();
-    Result startRecordFilterHandler();
-    void attachFilterToRecord(const sp<Dvr> dvr);
-    void detachFilterFromRecord();
 
   private:
     // Tuner service
     sp<Demux> mDemux;
-    // Dvr reference once the filter is attached to any
-    sp<Dvr> mDvr = nullptr;
     /**
      * Filter callbacks used on filter events or FMQ status
      */
@@ -107,7 +99,6 @@ class Filter : public IFilter {
     sp<IFilter> mDataSource;
     bool mIsDataSourceDemux = true;
     vector<uint8_t> mFilterOutput;
-    vector<uint8_t> mRecordFilterOutput;
     unique_ptr<FilterMQ> mFilterMQ;
     EventFlag* mFilterEventFlag;
     DemuxFilterEvent mFilterEvent;
@@ -129,8 +120,6 @@ class Filter : public IFilter {
      */
     const uint16_t SECTION_WRITE_COUNT = 10;
 
-    bool DEBUG_FILTER = false;
-
     /**
      * Filter handlers to handle the data filtering.
      * They are also responsible to write the filtered output into the filter FMQ
@@ -140,6 +129,7 @@ class Filter : public IFilter {
     Result startPesFilterHandler();
     Result startTsFilterHandler();
     Result startMediaFilterHandler();
+    Result startRecordFilterHandler();
     Result startPcrFilterHandler();
     Result startTemiFilterHandler();
     Result startFilterLoop();
@@ -175,7 +165,6 @@ class Filter : public IFilter {
     std::mutex mFilterStatusLock;
     std::mutex mFilterThreadLock;
     std::mutex mFilterOutputLock;
-    std::mutex mRecordFilterOutputLock;
 
     // temp handle single PES filter
     // TODO handle mulptiple Pes filters
