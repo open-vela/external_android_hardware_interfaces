cf65dc80958c9f4fbcb659bf2ce75ce52e4a3456
liujinye <liujinye@xiaomi.com>
2019-12-06 16:16:26 +0800

    [automerger skipped] DO NOT MERGE: audio: Skip tests if audio HAL service lacks "primary" device am: 1ed70316c5 -s ours am skip reason: subject contains skip directive
    Change-Id: Idb5bb585dc22bd6027f1177bbb90392aad464d4b

--- graphics/composer/2.1/utils/hal/include/composer-hal/2.1/Composer.h
+++ graphics/composer/2.1/utils/hal/include/composer-hal/2.1/Composer.h
@@ -67,14 +67,6 @@ class ComposerImpl : public Interface {
             }
         }
 
-        // we do not have HWC2_CAPABILITY_SKIP_VALIDATE defined in
-        // IComposer::Capability.  However, this is defined in hwcomposer2.h,
-        // so if the device returns it, add it manually to be returned to the
-        // client
-        if (mHal->hasCapability(HWC2_CAPABILITY_SKIP_VALIDATE)) {
-            caps.push_back(static_cast<IComposer::Capability>(HWC2_CAPABILITY_SKIP_VALIDATE));
-        }
-
         hidl_vec<IComposer::Capability> caps_reply;
         caps_reply.setToExternal(caps.data(), caps.size());
         hidl_cb(caps_reply);
@@ -88,8 +80,7 @@ class ComposerImpl : public Interface {
 
     Return<void> createClient(IComposer::createClient_cb hidl_cb) override {
         std::unique_lock<std::mutex> lock(mClientMutex);
-        bool destroyed = waitForClientDestroyedLocked(lock);
-        if (!destroyed) {
+        if (!waitForClientDestroyedLocked(lock)) {
             hidl_cb(Error::NO_RESOURCES, nullptr);
             return Void();
         }
@@ -117,10 +108,12 @@ class ComposerImpl : public Interface {
             // inverted (create and then destroy). Wait for a brief period to
             // see if the existing client is destroyed.
             ALOGD("waiting for previous client to be destroyed");
-            mClientDestroyedCondition.wait_for(lock, 1s,
-                                               [this]() -> bool { return mClient == nullptr; });
-            if (mClient != nullptr) {
+            mClientDestroyedCondition.wait_for(
+                lock, 1s, [this]() -> bool { return mClient.promote() == nullptr; });
+            if (mClient.promote() != nullptr) {
                 ALOGD("previous client was not destroyed");
+            } else {
+                mClient.clear();
             }
         }
 
