6a8f7fb93923a3d61dbfcfdf26f246007e96cef3
liujinye <liujinye@xiaomi.com>
2019-12-11 21:40:41 +0800

    Merge "DO NOT MERGE: Audio HAL: do not test input stream if no Built-in mic on primary" into pie-vts-dev

--- audio/common/all-versions/test/utility/include/utility/ValidateXml.h
+++ audio/common/all-versions/test/utility/include/utility/ValidateXml.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,10 @@ namespace utility {
  */
 ::testing::AssertionResult validateXml(const char* xmlFilePathExpr, const char* xsdFilePathExpr,
                                        const char* xmlFilePath, const char* xsdFilePath);
+
+std::vector<std::string> findValidXmlFiles(const char* xsdFilePathExpr,
+        const char* xmlFileName, std::vector<const char*> xmlFileLocations, const char* xsdFilePath,
+        std::vector<std::string>* errors = nullptr);
 
 /** Helper gtest ASSERT to test XML validity against an XSD. */
 #define ASSERT_VALID_XML(xmlFilePath, xsdFilePath)                                      \
@@ -77,6 +81,9 @@ template <bool atLeastOneRequired = true>
     EXPECT_PRED_FORMAT3(                                                                       \
         ::android::hardware::audio::common::test::utility::validateXmlMultipleLocations<true>, \
         xmlFileName, xmlFileLocations, xsdFilePath)
+
+::testing::AssertionResult isNonEmptyXpath(
+        const char* xmlFilePath, const char* xpathQuery, bool* result);
 
 }  // namespace utility
 }  // namespace test
