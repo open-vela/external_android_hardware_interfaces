102f6d14100925376500c2f96f828a8e3b3298ca
liujinye <liujinye@xiaomi.com>
2019-12-11 13:48:01 +0800

    [automerger skipped] Merge "DO NOT MERGE: Audio HAL: do not test input stream if no Built-in mic on primary" into pie-vts-dev am: 356c36b96d -s ours am skip reason: subject contains skip directive
    Change-Id: I0ef467cc98106b98bd0b3926013e65c5eb8e0fb3

--- radio/1.0/vts/functional/radio_hidl_hal_data.cpp
+++ radio/1.0/vts/functional/radio_hidl_hal_data.cpp
@@ -21,7 +21,7 @@ using namespace ::android::hardware::radio::V1_0;
 /*
  * Test IRadio.getDataRegistrationState() for the response returned.
  */
-TEST_P(RadioHidlTest, getDataRegistrationState) {
+TEST_F(RadioHidlTest, getDataRegistrationState) {
     serial = GetRandomSerialNumber();
 
     radio->getDataRegistrationState(serial);
@@ -32,74 +32,13 @@ TEST_P(RadioHidlTest, getDataRegistrationState) {
 
     if (cardStatus.cardState == CardState::ABSENT) {
         EXPECT_EQ(RadioError::NONE, radioRsp->rspInfo.error);
-    } else if (cardStatus.cardState == CardState::PRESENT) {
-        ASSERT_TRUE(CheckAnyOfErrors(
-            radioRsp->rspInfo.error,
-            {RadioError::NONE, RadioError::NOT_PROVISIONED, RadioError::CANCELLED}));
-
-        // Check the mcc [0, 999] and mnc [0, 999].
-        string hidl_mcc;
-        string hidl_mnc;
-        bool checkMccMnc = true;
-        int totalIdentitySizeExpected = 1;
-        CellIdentity cellIdentities = radioRsp->dataRegResp.cellIdentity;
-        CellInfoType cellInfoType = cellIdentities.cellInfoType;
-
-        if (cellInfoType == CellInfoType::NONE) {
-            // All the fields are 0
-            totalIdentitySizeExpected = 0;
-            checkMccMnc = false;
-        } else if (cellInfoType == CellInfoType::GSM) {
-            EXPECT_EQ(1, cellIdentities.cellIdentityGsm.size());
-            CellIdentityGsm cig = cellIdentities.cellIdentityGsm[0];
-            hidl_mcc = cig.mcc;
-            hidl_mnc = cig.mnc;
-        } else if (cellInfoType == CellInfoType::LTE) {
-            EXPECT_EQ(1, cellIdentities.cellIdentityLte.size());
-            CellIdentityLte cil = cellIdentities.cellIdentityLte[0];
-            hidl_mcc = cil.mcc;
-            hidl_mnc = cil.mnc;
-        } else if (cellInfoType == CellInfoType::WCDMA) {
-            EXPECT_EQ(1, cellIdentities.cellIdentityWcdma.size());
-            CellIdentityWcdma ciw = cellIdentities.cellIdentityWcdma[0];
-            hidl_mcc = ciw.mcc;
-            hidl_mnc = ciw.mnc;
-        } else if (cellInfoType == CellInfoType::TD_SCDMA) {
-            EXPECT_EQ(1, cellIdentities.cellIdentityTdscdma.size());
-            CellIdentityTdscdma cit = cellIdentities.cellIdentityTdscdma[0];
-            hidl_mcc = cit.mcc;
-            hidl_mnc = cit.mnc;
-        } else {
-            // CellIndentityCdma has no mcc and mnc.
-            EXPECT_EQ(CellInfoType::CDMA, cellInfoType);
-            EXPECT_EQ(1, cellIdentities.cellIdentityCdma.size());
-            checkMccMnc = false;
-        }
-
-        // Check only one CellIdentity is size 1, and others must be 0.
-        EXPECT_EQ(totalIdentitySizeExpected, cellIdentities.cellIdentityGsm.size() +
-                                                 cellIdentities.cellIdentityCdma.size() +
-                                                 cellIdentities.cellIdentityLte.size() +
-                                                 cellIdentities.cellIdentityWcdma.size() +
-                                                 cellIdentities.cellIdentityTdscdma.size());
-
-        if (checkMccMnc) {
-            // 32 bit system gets result: "\xff\xff\xff..." from RIL, which is not testable. Only
-            // test for 64 bit here. TODO: remove this limit after b/113181277 being fixed.
-            if (hidl_mcc.size() < 4 && hidl_mnc.size() < 4) {
-                int mcc = stoi(hidl_mcc);
-                int mnc = stoi(hidl_mnc);
-                EXPECT_TRUE(mcc >= 0 && mcc <= 999);
-                EXPECT_TRUE(mnc >= 0 && mnc <= 999);
-            }
-        }
     }
 }
 
 /*
  * Test IRadio.setupDataCall() for the response returned.
  */
-TEST_P(RadioHidlTest, setupDataCall) {
+TEST_F(RadioHidlTest, setupDataCall) {
     serial = GetRandomSerialNumber();
 
     RadioTechnology radioTechnology = RadioTechnology::LTE;
@@ -147,7 +86,7 @@ TEST_P(RadioHidlTest, setupDataCall) {
 /*
  * Test IRadio.deactivateDataCall() for the response returned.
  */
-TEST_P(RadioHidlTest, deactivateDataCall) {
+TEST_F(RadioHidlTest, deactivateDataCall) {
     serial = GetRandomSerialNumber();
     int cid = 1;
     bool reasonRadioShutDown = false;
@@ -169,7 +108,7 @@ TEST_P(RadioHidlTest, deactivateDataCall) {
 /*
  * Test IRadio.getDataCallList() for the response returned.
  */
-TEST_P(RadioHidlTest, getDataCallList) {
+TEST_F(RadioHidlTest, getDataCallList) {
     serial = GetRandomSerialNumber();
 
     radio->getDataCallList(serial);
@@ -188,7 +127,7 @@ TEST_P(RadioHidlTest, getDataCallList) {
 /*
  * Test IRadio.setInitialAttachApn() for the response returned.
  */
-TEST_P(RadioHidlTest, setInitialAttachApn) {
+TEST_F(RadioHidlTest, setInitialAttachApn) {
     serial = GetRandomSerialNumber();
 
     DataProfileInfo dataProfileInfo;
@@ -231,7 +170,7 @@ TEST_P(RadioHidlTest, setInitialAttachApn) {
 /*
  * Test IRadio.setDataAllowed() for the response returned.
  */
-TEST_P(RadioHidlTest, setDataAllowed) {
+TEST_F(RadioHidlTest, setDataAllowed) {
     serial = GetRandomSerialNumber();
     bool allow = true;
 
@@ -249,7 +188,7 @@ TEST_P(RadioHidlTest, setDataAllowed) {
 /*
  * Test IRadio.setDataProfile() for the response returned.
  */
-TEST_P(RadioHidlTest, setDataProfile) {
+TEST_F(RadioHidlTest, setDataProfile) {
     serial = GetRandomSerialNumber();
 
     // Create a dataProfileInfo
