b56084f58318748a384b65bf9671c86c6b5f55ff
openvela-robot <openvela-robot@xiaomi.com>
2019-12-11 22:38:11 +0800

    Merge "vibrator: aidl: Add Descriptions for Primitives" am: e995668691 am: 7d3b70e75d
    Change-Id: I792c1ee1eefb08169269c7fdc8e962932bae4b9f

--- audio/effect/all-versions/vts/functional/VtsHalAudioEffectTargetTest.cpp
+++ audio/effect/all-versions/vts/functional/VtsHalAudioEffectTargetTest.cpp
@@ -134,16 +134,13 @@ EFFECT_TEST(AudioEffectsFactoryHidlTest, CreateEffect) {
     Result retval = Result::NOT_INITIALIZED;
     sp<IEffect> effect;
     ret = effectsFactory->createEffect(
-            effectUuid, 1 /*session*/, 1 /*ioHandle*/,
-#if MAJOR_VERSION >= 6
-            0 /*device*/,
-#endif
-            [&](Result r, const sp<IEffect>& result, uint64_t /*effectId*/) {
-                retval = r;
-                if (r == Result::OK) {
-                    effect = result;
-                }
-            });
+        effectUuid, 1 /*session*/, 1 /*ioHandle*/,
+        [&](Result r, const sp<IEffect>& result, uint64_t /*effectId*/) {
+            retval = r;
+            if (r == Result::OK) {
+                effect = result;
+            }
+        });
     EXPECT_TRUE(ret.isOk());
     EXPECT_EQ(Result::OK, retval);
     EXPECT_NE(nullptr, effect.get());
@@ -239,15 +236,12 @@ void AudioEffectHidlTest::findAndCreateEffect(const Uuid& type) {
     Uuid effectUuid;
     findEffectInstance(type, &effectUuid);
     Return<void> ret = effectsFactory->createEffect(
-            effectUuid, 1 /*session*/, 1 /*ioHandle*/,
-#if MAJOR_VERSION >= 6
-            0 /*device*/,
-#endif
-            [&](Result r, const sp<IEffect>& result, uint64_t /*effectId*/) {
-                if (r == Result::OK) {
-                    effect = result;
-                }
-            });
+        effectUuid, 1 /*session*/, 1 /*ioHandle*/,
+        [&](Result r, const sp<IEffect>& result, uint64_t /*effectId*/) {
+            if (r == Result::OK) {
+                effect = result;
+            }
+        });
     ASSERT_TRUE(ret.isOk());
 }
 
