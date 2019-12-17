6054728491c540bb43804762cf8dfeaefdc2d0b0
liujinye <liujinye@xiaomi.com>
2019-12-18 04:14:41 +0800

    Snap for 6081300 from cfb04a43264d237e3d3be886061414a95bd634d9 to rvc-release
    Change-Id: If8ac2ab974c62753fc83c7cace13ab5f6e6d0259

--- radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
+++ radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
@@ -549,8 +549,6 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
     Return<void> setInitialAttachApnResponse_1_5(const RadioResponseInfo& info);
 
     Return<void> setDataProfileResponse_1_5(const RadioResponseInfo& info);
-
-    Return<void> setRadioPowerResponse_1_5(const RadioResponseInfo& info);
 };
 
 /* Callback class for radio indication */
