a6dcb15e4cc7a48cc25902d9b18f72f64cbd11f3
openvela-robot <openvela-robot@xiaomi.com>
2020-01-06 18:46:52 +0800

    Merge "Add TAG_MAC_LENGTH to EncryptionOperationsTest.AesWrongPurpose" am: a4bffdbeb0 am: 336af5783c
    Change-Id: I89aaaf06faeb540428539e13130839490ae53d32

--- neuralnetworks/1.3/vts/functional/GeneratedTestHarness.cpp
+++ neuralnetworks/1.3/vts/functional/GeneratedTestHarness.cpp
@@ -215,7 +215,7 @@ static Return<ErrorStatus> ExecutePreparedModel(const sp<IPreparedModel>& prepar
                                                 hidl_vec<OutputShape>* outputShapes,
                                                 Timing* timing) {
     ErrorStatus result;
-    Return<void> ret = preparedModel->executeSynchronously_1_3(
+    Return<void> ret = preparedModel->executeSynchronously(
             request, measure,
             [&result, outputShapes, timing](ErrorStatus error, const hidl_vec<OutputShape>& shapes,
                                             const Timing& time) {
@@ -452,7 +452,7 @@ void Execute(const sp<IDevice>& device, const TestModel& testModel, TestKind tes
             EvaluatePreparedModel(preparedModel, testModel, TestKind::DYNAMIC_SHAPE);
         } break;
         case TestKind::QUANTIZATION_COUPLING: {
-            ASSERT_TRUE(testModel.hasQuant8CoupledOperands());
+            ASSERT_TRUE(testModel.hasQuant8AsymmOperands());
             createPreparedModel(device, model, &preparedModel, /*reportSkipping*/ false);
             TestModel signedQuantizedModel = convertQuant8AsymmOperandsToSigned(testModel);
             sp<IPreparedModel> preparedCoupledModel;
@@ -521,7 +521,7 @@ INSTANTIATE_GENERATED_TEST(DynamicOutputShapeTest,
                            [](const TestModel& testModel) { return !testModel.expectFailure; });
 
 INSTANTIATE_GENERATED_TEST(DISABLED_QuantizationCouplingTest, [](const TestModel& testModel) {
-    return testModel.hasQuant8CoupledOperands() && testModel.operations.size() == 1;
+    return testModel.hasQuant8AsymmOperands() && testModel.operations.size() == 1;
 });
 
 }  // namespace android::hardware::neuralnetworks::V1_3::vts::functional
