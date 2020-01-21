ea2ee341f132b51623b29a7a468622cdabd4113e
openvela-robot <openvela-robot@xiaomi.com>
2020-01-22 01:13:21 +0800

    Merge "Added TD-SCDMA bands for network scanning"

--- sensors/2.0/multihal/HalProxy.cpp
+++ sensors/2.0/multihal/HalProxy.cpp
@@ -651,12 +651,12 @@ void HalProxyCallback::postEvents(const std::vector<Event>& events, ScopedWakelo
     if (numWakeupEvents > 0) {
         ALOG_ASSERT(wakelock.isLocked(),
                     "Wakeup events posted while wakelock unlocked for subhal"
-                    " w/ index %" PRId32 ".",
+                    " w/ index %zu.",
                     mSubHalIndex);
     } else {
         ALOG_ASSERT(!wakelock.isLocked(),
                     "No Wakeup events posted but wakelock locked for subhal"
-                    " w/ index %" PRId32 ".",
+                    " w/ index %zu.",
                     mSubHalIndex);
     }
     mHalProxy->postEventsToMessageQueue(processedEvents, numWakeupEvents, std::move(wakelock));
