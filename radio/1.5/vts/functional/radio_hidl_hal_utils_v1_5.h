67a494e74bed3c51bce3c2234a098d7de4e2cfed
liujinye <liujinye@xiaomi.com>
2020-01-21 13:15:09 +0800

    Update sync fence related API for NNAPI
     - Allow IPreparedModel::executeFenced to measure gated durations.
     - Allow IPreparedModel::executeFenced to specify an optional deadline
     and aa optional post-gate-release maximum duration.
    
    Bug: 142778241
    Bug: 141363565
    Test: mm
    Change-Id: I1687ec5543f93ed9bb5d7fcf14dbe15ed5b5cd0d

--- radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
+++ radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
@@ -293,8 +293,6 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
 
     Return<void> sendCdmaSmsResponse(const RadioResponseInfo& info, const SendSmsResult& sms);
 
-    Return<void> sendCdmaSmsExpectMoreResponse(const RadioResponseInfo& info, const SendSmsResult& sms);
-
     Return<void> acknowledgeLastIncomingCdmaSmsResponse(const RadioResponseInfo& info);
 
     Return<void> getGsmBroadcastConfigResponse(
@@ -571,6 +569,16 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
     Return<void> getDataRegistrationStateResponse_1_5(
             const RadioResponseInfo& info,
             const ::android::hardware::radio::V1_5::RegStateResult& regResponse);
+
+    Return<void> getCellInfoListResponse_1_5(
+            const RadioResponseInfo& info,
+            const ::android::hardware::hidl_vec<::android::hardware::radio::V1_5::CellInfo>&
+                    cellInfo);
+
+    Return<void> setNetworkSelectionModeManualResponse_1_5(const RadioResponseInfo& info);
+
+    Return<void> sendCdmaSmsExpectMoreResponse(const RadioResponseInfo& info,
+                                               const SendSmsResult& sms);
 };
 
 /* Callback class for radio indication */
@@ -584,6 +592,15 @@ class RadioIndication_v1_5 : public ::android::hardware::radio::V1_5::IRadioIndi
 
     /* 1.5 Api */
     Return<void> uiccApplicationsEnablementChanged(RadioIndicationType type, bool enabled);
+
+    Return<void> networkScanResult_1_5(
+            RadioIndicationType type,
+            const ::android::hardware::radio::V1_5::NetworkScanResult& result);
+
+    Return<void> cellInfoList_1_5(
+            RadioIndicationType type,
+            const ::android::hardware::hidl_vec<::android::hardware::radio::V1_5::CellInfo>&
+                    records);
 
     /* 1.4 Api */
     Return<void> currentEmergencyNumberList(
