966a3d2bfb22e84ec01b3d0c8342f893aa00e60f
openvela-robot <openvela-robot@xiaomi.com>
2020-01-21 10:46:00 +0800

    Added NR band support
    Added band info in cell identity.
    
    Test: Telephony sanity tests
    Bug: 131061889
    Change-Id: I5205aa70db0d83fd049c5f25b0b427ab7484727b
    Merged-In: I5205aa70db0d83fd049c5f25b0b427ab7484727b

--- dumpstate/1.0/vts/functional/VtsHalDumpstateV1_0TargetTest.cpp
+++ dumpstate/1.0/vts/functional/VtsHalDumpstateV1_0TargetTest.cpp
@@ -78,7 +78,6 @@ TEST_P(DumpstateHidlTest, TestOk) {
     ASSERT_EQ(1, read(fds[0], &buff, 1)) << "dumped nothing";
 
     native_handle_close(handle);
-    native_handle_delete(handle);
 }
 
 // Positive test: make sure dumpstateBoard() doesn't crash with two FDs.
@@ -97,7 +96,6 @@ TEST_P(DumpstateHidlTest, TestHandleWithTwoFds) {
     ASSERT_TRUE(status.isOk()) << "Status should be ok: " << status.description();
 
     native_handle_close(handle);
-    native_handle_delete(handle);
 }
 
 INSTANTIATE_TEST_SUITE_P(
