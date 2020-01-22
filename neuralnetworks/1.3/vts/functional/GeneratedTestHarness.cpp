2c516b65adc7dfd959fe00b38083f5b214c7ef7b
openvela-robot <openvela-robot@xiaomi.com>
2020-01-22 17:51:22 +0800

    Merge "Wifi: add 3 new fields to ConnectionCapabilities."

--- neuralnetworks/1.3/vts/functional/GeneratedTestHarness.cpp
+++ neuralnetworks/1.3/vts/functional/GeneratedTestHarness.cpp
@@ -793,9 +793,8 @@ TEST_P(QuantizationCouplingTest, Test) {
 INSTANTIATE_GENERATED_TEST(GeneratedTest,
                            [](const TestModel& testModel) { return !testModel.expectFailure; });
 
-INSTANTIATE_GENERATED_TEST(DynamicOutputShapeTest, [](const TestModel& testModel) {
-    return !testModel.expectFailure && !testModel.hasScalarOutputs();
-});
+INSTANTIATE_GENERATED_TEST(DynamicOutputShapeTest,
+                           [](const TestModel& testModel) { return !testModel.expectFailure; });
 
 INSTANTIATE_GENERATED_TEST(MemoryDomainTest,
                            [](const TestModel& testModel) { return !testModel.expectFailure; });
