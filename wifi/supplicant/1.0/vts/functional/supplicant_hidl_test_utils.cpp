713368a63d1da340f31c3ac111f80c0b0f0f2270
liujinye <liujinye@xiaomi.com>
2020-01-23 16:42:19 +0800

    Merge "Disable Tuner HAL VTS test cases that use un-licensed stream."

--- wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
+++ wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
@@ -57,11 +57,6 @@ namespace {
 // Helper function to initialize the driver and firmware to STA mode
 // using the vendor HAL HIDL interface.
 void initilializeDriverAndFirmware(const std::string& wifi_instance_name) {
-    // Skip if wifi instance is not set.
-    if (wifi_instance_name == "") {
-        return;
-    }
-
     sp<IWifiChip> wifi_chip = getWifiChip(wifi_instance_name);
     ChipModeId mode_id;
     EXPECT_TRUE(configureChipToSupportIfaceType(
@@ -71,11 +66,6 @@ void initilializeDriverAndFirmware(const std::string& wifi_instance_name) {
 // Helper function to deinitialize the driver and firmware
 // using the vendor HAL HIDL interface.
 void deInitilializeDriverAndFirmware(const std::string& wifi_instance_name) {
-    // Skip if wifi instance is not set.
-    if (wifi_instance_name == "") {
-        return;
-    }
-
     stopWifi(wifi_instance_name);
 }
 
