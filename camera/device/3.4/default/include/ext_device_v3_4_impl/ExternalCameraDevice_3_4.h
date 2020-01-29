6c6fe6d3e2b9e192a20f1abd1463745838d6dc3b
openvela-robot <openvela-robot@xiaomi.com>
2020-01-29 20:30:57 +0800

    Merge "health 2.1 VTS: fix test on batteryFullCapacityUah"

--- camera/device/3.4/default/include/ext_device_v3_4_impl/ExternalCameraDevice_3_4.h
+++ camera/device/3.4/default/include/ext_device_v3_4_impl/ExternalCameraDevice_3_4.h
@@ -105,7 +105,7 @@ protected:
     // Calls into virtual member function. Do not use it in constructor
     status_t initCameraCharacteristics();
     // Init available capabilities keys
-    virtual status_t initAvailableCapabilities(
+    status_t initAvailableCapabilities(
             ::android::hardware::camera::common::V1_0::helper::CameraMetadata*);
     // Init non-device dependent keys
     virtual status_t initDefaultCharsKeys(
