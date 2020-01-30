d832b9931f6678c3d53721fa9e3fd7a26b590222
liujinye <liujinye@xiaomi.com>
2020-01-31 01:06:20 +0800

    Merge "Make IDumpstateDevice 1.1"

--- camera/provider/2.4/default/ExternalCameraProviderImpl_2_4.cpp
+++ camera/provider/2.4/default/ExternalCameraProviderImpl_2_4.cpp
@@ -26,7 +26,6 @@
 #include "ExternalCameraProviderImpl_2_4.h"
 #include "ExternalCameraDevice_3_4.h"
 #include "ExternalCameraDevice_3_5.h"
-#include "ExternalCameraDevice_3_6.h"
 
 namespace android {
 namespace hardware {
@@ -74,7 +73,6 @@ ExternalCameraProviderImpl_2_4::ExternalCameraProviderImpl_2_4() :
     switch(mPreferredHal3MinorVersion) {
         case 4:
         case 5:
-        case 6:
             // OK
             break;
         default:
@@ -173,12 +171,6 @@ Return<void> ExternalCameraProviderImpl_2_4::getCameraDeviceInterface_V3_x(
                     cameraId, mCfg);
             break;
         }
-        case 6: {
-            ALOGV("Constructing v3.6 external camera device");
-            deviceImpl = new device::V3_6::implementation::ExternalCameraDevice(
-                    cameraId, mCfg);
-            break;
-        }
         default:
             ALOGE("%s: Unknown HAL minor version %d!", __FUNCTION__, mPreferredHal3MinorVersion);
             _hidl_cb(Status::INTERNAL_ERROR, nullptr);
@@ -210,9 +202,7 @@ void ExternalCameraProviderImpl_2_4::addExternalCamera(const char* devName) {
     ALOGI("ExtCam: adding %s to External Camera HAL!", devName);
     Mutex::Autolock _l(mLock);
     std::string deviceName;
-    if (mPreferredHal3MinorVersion == 6) {
-        deviceName = std::string("device@3.6/external/") + devName;
-    } else if (mPreferredHal3MinorVersion == 5) {
+    if (mPreferredHal3MinorVersion == 5) {
         deviceName = std::string("device@3.5/external/") + devName;
     } else {
         deviceName = std::string("device@3.4/external/") + devName;
@@ -259,9 +249,7 @@ void ExternalCameraProviderImpl_2_4::deviceAdded(const char* devName) {
 void ExternalCameraProviderImpl_2_4::deviceRemoved(const char* devName) {
     Mutex::Autolock _l(mLock);
     std::string deviceName;
-    if (mPreferredHal3MinorVersion == 6) {
-        deviceName = std::string("device@3.6/external/") + devName;
-    } else if (mPreferredHal3MinorVersion == 5) {
+    if (mPreferredHal3MinorVersion == 5) {
         deviceName = std::string("device@3.5/external/") + devName;
     } else {
         deviceName = std::string("device@3.4/external/") + devName;
