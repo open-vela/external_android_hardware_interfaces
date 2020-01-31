ea3bb3600b353950405433a1bd965e2f25f0a416
liujinye <liujinye@xiaomi.com>
2020-01-31 22:52:32 +0800

    VTS: only assert that IGnssDebug present for non-automotive am: 2fd5c7cdf9 am: 984b3069bc
    Change-Id: I4694449509de580da4049cef13458d35aed117af

--- sensors/2.0/vts/functional/SensorsHidlEnvironmentV2_0.cpp
+++ sensors/2.0/vts/functional/SensorsHidlEnvironmentV2_0.cpp
@@ -58,7 +58,8 @@ struct SensorsCallback : ISensorsCallback {
 bool SensorsHidlEnvironmentV2_0::resetHal() {
     bool succeed = false;
     do {
-        mSensors = ISensors::getService(mServiceName);
+        mSensors = ISensors::getService(
+            SensorsHidlEnvironmentV2_0::Instance()->getServiceName<ISensors>());
         if (mSensors == nullptr) {
             break;
         }
@@ -129,8 +130,8 @@ void SensorsHidlEnvironmentV2_0::HidlTearDown() {
 
 void SensorsHidlEnvironmentV2_0::startPollingThread() {
     mStopThread = false;
-    mEvents.reserve(MAX_RECEIVE_BUFFER_EVENT_COUNT);
     mPollThread = std::thread(pollingThread, this);
+    mEvents.reserve(MAX_RECEIVE_BUFFER_EVENT_COUNT);
 }
 
 void SensorsHidlEnvironmentV2_0::readEvents() {
