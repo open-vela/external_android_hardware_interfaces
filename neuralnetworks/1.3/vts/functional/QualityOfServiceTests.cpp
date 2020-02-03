2da2ef6e03c4de0c48842af9669e51d7dcfe0680
liujinye <liujinye@xiaomi.com>
2020-02-03 16:25:57 +0800

    IDumpstateDevice@1.1 polish
    - Return a DumpstateStatus from dumpstateBoard_1_1
    - Better toggle API surface: set/getDeviceLoggingEnabled
    - Improved testing to allow for unsupported DumpstateMode values
    
    Bug: 143183758
    Bug: 143184495
    Test: atest VtsHalDumpstateV1_1TargetTest
    Merged-In: I505c2a790dc28ddce9b6f5b674394ef65b31c80c
    (cherry picked from commit 6e278a379841d2aca907300ee8ed60034eac730e)
    
    Change-Id: Ibbd15f65674cfa6b9f5c1d6a633277a419499d9f

--- neuralnetworks/1.3/vts/functional/QualityOfServiceTests.cpp
+++ neuralnetworks/1.3/vts/functional/QualityOfServiceTests.cpp
@@ -64,11 +64,11 @@ static OptionalTimePoint makeOptionalTimePoint(DeadlineBoundType deadlineBoundTy
                     std::chrono::time_point_cast<std::chrono::nanoseconds>(currentTime);
             const uint64_t nanosecondsSinceEpoch =
                     currentTimeInNanoseconds.time_since_epoch().count();
-            deadline.nanosecondsSinceEpoch(nanosecondsSinceEpoch);
+            deadline.nanoseconds(nanosecondsSinceEpoch);
         } break;
         case DeadlineBoundType::UNLIMITED: {
             uint64_t unlimited = std::numeric_limits<uint64_t>::max();
-            deadline.nanosecondsSinceEpoch(unlimited);
+            deadline.nanoseconds(unlimited);
         } break;
     }
     return deadline;
