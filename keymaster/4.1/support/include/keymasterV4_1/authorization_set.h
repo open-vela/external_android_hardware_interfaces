b18e492c48c41fc01e0b90638845f541c53357fc
openvela-robot <openvela-robot@xiaomi.com>
2020-02-07 09:49:42 +0800

    Merge "[vts-core] add VtsHalBluetoothV1_0TargetTest to vts-core"

--- keymaster/4.1/support/include/keymasterV4_1/authorization_set.h
+++ keymaster/4.1/support/include/keymasterV4_1/authorization_set.h
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-#pragma once
+#ifndef HARDWARE_INTERFACES_KEYMASTER_V4_1_SUPPORT_INCLUDE_AUTHORIZATION_SET_H_
+#define HARDWARE_INTERFACES_KEYMASTER_V4_1_SUPPORT_INCLUDE_AUTHORIZATION_SET_H_
 
 #include <keymasterV4_0/authorization_set.h>
 
@@ -27,3 +28,5 @@ using V4_0::AuthorizationSetBuilder;
 using V4_0::KeyParameter;
 
 }  // namespace android::hardware::keymaster::V4_1
+
+#endif  // HARDWARE_INTERFACES_KEYMASTER_V4_1_SUPPORT_INCLUDE_AUTHORIZATION_SET_H_
