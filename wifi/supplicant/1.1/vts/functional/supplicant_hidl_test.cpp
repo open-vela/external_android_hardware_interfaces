1b3af38ab9d5c5496bb99d813ac8e470997a0997
liujinye <liujinye@xiaomi.com>
2020-02-07 09:49:42 +0800

    Merge "[vts-core] add VtsHalBluetoothV1_0TargetTest to vts-core"

--- wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
+++ wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
@@ -17,12 +17,10 @@
 #include <android-base/logging.h>
 #include <cutils/properties.h>
 
-#include <VtsCoreUtil.h>
-#include <android/hardware/wifi/1.0/IWifi.h>
+#include <VtsHalHidlTargetTestBase.h>
+
 #include <android/hardware/wifi/supplicant/1.0/types.h>
 #include <android/hardware/wifi/supplicant/1.1/ISupplicant.h>
-#include <hidl/GtestPrinter.h>
-#include <hidl/ServiceManagement.h>
 
 #include "supplicant_hidl_test_utils.h"
 #include "supplicant_hidl_test_utils_1_1.h"
@@ -35,11 +33,22 @@ using ::android::hardware::wifi::supplicant::V1_0::IfaceType;
 using ::android::hardware::wifi::supplicant::V1_1::ISupplicant;
 using ::android::sp;
 
-class SupplicantHidlTest : public SupplicantHidlTestBase {
+extern WifiSupplicantHidlEnvironment* gEnv;
+
+class SupplicantHidlTest : public ::testing::VtsHalHidlTargetTestBase {
    public:
-    virtual void SetUp() override { SupplicantHidlTestBase::SetUp(); }
+    virtual void SetUp() override {
+        startSupplicantAndWaitForHidlService();
+        supplicant_ = getSupplicant_1_1();
+        ASSERT_NE(supplicant_.get(), nullptr);
+    }
+
+    virtual void TearDown() override { stopSupplicant(); }
 
    protected:
+    // ISupplicant object used for all tests in this fixture.
+    sp<ISupplicant> supplicant_;
+
     std::string getWlan0IfaceName() {
         std::array<char, PROPERTY_VALUE_MAX> buffer;
         property_get("wifi.interface", buffer.data(), "wlan0");
@@ -56,7 +65,7 @@ class SupplicantHidlTest : public SupplicantHidlTestBase {
 /*
  * AddStaInterface
  */
-TEST_P(SupplicantHidlTest, AddStaInterface) {
+TEST_F(SupplicantHidlTest, AddStaInterface) {
     ISupplicant::IfaceInfo iface_info;
     iface_info.name = getWlan0IfaceName();
     iface_info.type = IfaceType::STA;
@@ -73,8 +82,8 @@ TEST_P(SupplicantHidlTest, AddStaInterface) {
 /*
  * AddP2pInterface
  */
-TEST_P(SupplicantHidlTest, AddP2pInterface) {
-    if (isP2pOn_) return;
+TEST_F(SupplicantHidlTest, AddP2pInterface) {
+    if (!gEnv->isP2pOn) return;
     ISupplicant::IfaceInfo iface_info;
     iface_info.name = getP2pIfaceName();
     iface_info.type = IfaceType::P2P;
@@ -91,7 +100,7 @@ TEST_P(SupplicantHidlTest, AddP2pInterface) {
 /*
  * RemoveStaInterface
  */
-TEST_P(SupplicantHidlTest, RemoveStaInterface) {
+TEST_F(SupplicantHidlTest, RemoveStaInterface) {
     ISupplicant::IfaceInfo iface_info;
     iface_info.name = getWlan0IfaceName();
     iface_info.type = IfaceType::STA;
@@ -113,8 +122,8 @@ TEST_P(SupplicantHidlTest, RemoveStaInterface) {
 /*
  * RemoveP2pInterface
  */
-TEST_P(SupplicantHidlTest, RemoveP2pInterface) {
-    if (isP2pOn_) return;
+TEST_F(SupplicantHidlTest, RemoveP2pInterface) {
+    if (!gEnv->isP2pOn) return;
     ISupplicant::IfaceInfo iface_info;
     iface_info.name = getP2pIfaceName();
     iface_info.type = IfaceType::P2P;
@@ -137,23 +146,6 @@ TEST_P(SupplicantHidlTest, RemoveP2pInterface) {
  * Terminate
  * This terminates the service.
  */
-TEST_P(SupplicantHidlTest, Terminate) { supplicant_->terminate(); }
-
-static std::vector<std::string> get_wifi_instances() {
-    std::vector<std::string> instances =
-        android::hardware::getAllHalInstanceNames(
-            android::hardware::wifi::V1_0::IWifi::descriptor);
-    // Also test when wifi instance is not set.
-    instances.push_back("");
-
-    return instances;
+TEST_F(SupplicantHidlTest, Terminate) {
+    supplicant_->terminate();
 }
-
-INSTANTIATE_TEST_CASE_P(
-    PerInstance, SupplicantHidlTest,
-    testing::Combine(
-        testing::ValuesIn(get_wifi_instances()),
-        testing::ValuesIn(android::hardware::getAllHalInstanceNames(
-            android::hardware::wifi::supplicant::V1_1::ISupplicant::
-                descriptor))),
-    android::hardware::PrintInstanceTupleNameToString<>);
