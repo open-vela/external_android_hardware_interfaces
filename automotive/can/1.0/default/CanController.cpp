354e857f538da2a32df6400b92b80bcc30663aaf
Tomasz Wasilczyk <twasilczyk@google.com>
2020-02-18 15:19:35 -0800

    Add ICanController BAD_SERVICE_NAME error
    Bug: 137798319
    Test: VTS
    Change-Id: I4722346239728f3ab359688658c23441e83671a8

--- automotive/can/1.0/default/CanController.cpp
+++ automotive/can/1.0/default/CanController.cpp
@@ -48,7 +48,7 @@ Return<ICanController::Result> CanController::upInterface(
 
     if (!isValidName(config.name)) {
         LOG(ERROR) << "Bus name " << config.name << " is invalid";
-        return ICanController::Result::UNKNOWN_ERROR;
+        return ICanController::Result::BAD_SERVICE_NAME;
     }
 
     if (mCanBuses.find(config.name) != mCanBuses.end()) {
@@ -91,7 +91,7 @@ Return<ICanController::Result> CanController::upInterface(
         if (!busService->down()) {
             LOG(WARNING) << "Failed to bring down CAN bus that failed to register";
         }
-        return ICanController::Result::UNKNOWN_ERROR;
+        return ICanController::Result::BAD_SERVICE_NAME;
     }
 
     mCanBuses[config.name] = busService;
