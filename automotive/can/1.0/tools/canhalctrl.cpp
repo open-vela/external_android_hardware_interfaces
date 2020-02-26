454fef4c2d894d6b3a0e5950b3b537265a7d3c55
openvela-robot <openvela-robot@xiaomi.com>
2020-02-26 14:39:56 +0800

    Clean up errno logs and sto* conversions
    I learned that we should be using PLOG to log errno strings, and we
    should be avoiding stoi, stol, etc... conversions and instead use the
    built in Android ParseInt/ParseUint functions.
    
    Bug: 150250606
    Bug: 150245058
    Test: Manual for CLI tools, VTS for everything else
    Merged-In: Icdd8a6af8564d5de3bedd1bc934f7928eb5e66e9
    Change-Id: Icdd8a6af8564d5de3bedd1bc934f7928eb5e66e9
    (cherry picked from commit 1173a7253becba2b548057112372c8a94d9b6a41)

--- automotive/can/1.0/tools/canhalctrl.cpp
+++ automotive/can/1.0/tools/canhalctrl.cpp
@@ -19,7 +19,6 @@
 #include <android/hardware/automotive/can/1.0/ICanController.h>
 #include <android/hidl/manager/1.2/IServiceManager.h>
 #include <hidl-utils/hidl-utils.h>
-#include <libcanhaltools/libcanhaltools.h>
 
 #include <iostream>
 #include <string>
@@ -43,17 +42,34 @@ static void usage() {
     std::cerr << " bus name - name under which ICanBus will be published" << std::endl;
 }
 
+static hidl_vec<hidl_string> getControlServices() {
+    auto manager = hidl::manager::V1_2::IServiceManager::getService();
+    hidl_vec<hidl_string> services;
+    manager->listManifestByInterface(ICanController::descriptor, hidl_utils::fill(&services));
+    if (services.size() == 0) {
+        std::cerr << "No ICanController services registered (missing privileges?)" << std::endl;
+        exit(-1);
+    }
+    return services;
+}
+
+static bool isSupported(sp<ICanController> ctrl, ICanController::InterfaceType iftype) {
+    hidl_vec<ICanController::InterfaceType> supported;
+    if (!ctrl->getSupportedInterfaceTypes(hidl_utils::fill(&supported)).isOk()) return false;
+    return supported.contains(iftype);
+}
+
 static int up(const std::string& busName, ICanController::InterfaceType type,
               const std::string& interface, uint32_t bitrate) {
     bool anySupported = false;
-    for (auto&& service : libcanhaltools::getControlServices()) {
+    for (auto&& service : getControlServices()) {
         auto ctrl = ICanController::getService(service);
         if (ctrl == nullptr) {
             std::cerr << "Couldn't open ICanController/" << service;
             continue;
         }
 
-        if (!libcanhaltools::isSupported(ctrl, type)) continue;
+        if (!isSupported(ctrl, type)) continue;
         anySupported = true;
 
         ICanController::BusConfig config = {};
@@ -96,7 +112,7 @@ static int up(const std::string& busName, ICanController::InterfaceType type,
 }
 
 static int down(const std::string& busName) {
-    for (auto&& service : libcanhaltools::getControlServices()) {
+    for (auto&& service : getControlServices()) {
         auto ctrl = ICanController::getService(service);
         if (ctrl == nullptr) continue;
 
