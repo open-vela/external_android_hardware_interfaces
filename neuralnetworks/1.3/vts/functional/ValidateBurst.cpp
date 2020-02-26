8c4b19811d38d241e611c365642b552a293745a7
liujinye <liujinye@xiaomi.com>
2020-02-26 13:00:16 +0800

    Add timestamp for continuously property.
    Bug: 148960132
    Test: 1. apply google vhal to device
          2. atest CtsCarTestCases:CarPropertyManagerTest
    
    Change-Id: Ib6690b5e242287958017c87632f56a546d418674

--- neuralnetworks/1.3/vts/functional/ValidateBurst.cpp
+++ neuralnetworks/1.3/vts/functional/ValidateBurst.cpp
@@ -23,6 +23,7 @@
 #include "ExecutionBurstServer.h"
 #include "GeneratedTestHarness.h"
 #include "TestHarness.h"
+#include "Utils.h"
 
 #include <android-base/logging.h>
 #include <chrono>
@@ -301,7 +302,8 @@ static void validateBurstFmqLength(const sp<IPreparedModel>& preparedModel,
     // collect serialized result by running regular burst
     const auto [nRegular, outputShapesRegular, timingRegular, fallbackRegular] =
             controllerRegular->compute(request, MeasureTiming::NO, keys);
-    const V1_0::ErrorStatus statusRegular = nn::legacyConvertResultCodeToErrorStatus(nRegular);
+    const V1_0::ErrorStatus statusRegular =
+            nn::convertToV1_0(nn::convertResultCodeToErrorStatus(nRegular));
     EXPECT_FALSE(fallbackRegular);
 
     // skip test if regular burst output isn't useful for testing a failure
@@ -317,7 +319,8 @@ static void validateBurstFmqLength(const sp<IPreparedModel>& preparedModel,
     // large enough to return the serialized result
     const auto [nSmall, outputShapesSmall, timingSmall, fallbackSmall] =
             controllerSmall->compute(request, MeasureTiming::NO, keys);
-    const V1_0::ErrorStatus statusSmall = nn::legacyConvertResultCodeToErrorStatus(nSmall);
+    const V1_0::ErrorStatus statusSmall =
+            nn::convertToV1_0(nn::convertResultCodeToErrorStatus(nSmall));
     EXPECT_NE(V1_0::ErrorStatus::NONE, statusSmall);
     EXPECT_EQ(0u, outputShapesSmall.size());
     EXPECT_TRUE(badTiming(timingSmall));
