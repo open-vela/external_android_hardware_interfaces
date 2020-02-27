1fbccf5ecb4b641ffef0eb10da23e6f04a0a44ac
liujinye <liujinye@xiaomi.com>
2020-02-27 11:48:01 +0800

    vts: VtsHalGraphicsMapperV2_1TargetTest fixup's
    1) handles allocated through 'allocate' should be removed through
       'freeBuffer'.
    
    2) make use of intended buffer handle in GetTransportSizeBadBuffer.
    
    Bug: 146444563
    Test: build, boot, VtsHalGraphicsMapperV2_1TargetTest
    Change-Id: I6c1a67fc36dbc653ec2ada6a335d685d21e82800
    Merged-In: I6c1a67fc36dbc653ec2ada6a335d685d21e82800
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- radio/1.3/vts/functional/radio_hidl_hal_test.cpp
+++ radio/1.3/vts/functional/radio_hidl_hal_test.cpp
@@ -17,10 +17,18 @@
 #include <radio_hidl_hal_utils_v1_3.h>
 
 void RadioHidlTest_v1_3::SetUp() {
-    radio_v1_3 = ::android::hardware::radio::V1_3::IRadio::getService(GetParam());
+    radio_v1_3 = ::testing::VtsHalHidlTargetTestBase::getService<
+            ::android::hardware::radio::V1_3::IRadio>(
+            RadioHidlEnvironment::Instance()
+                    ->getServiceName<::android::hardware::radio::V1_3::IRadio>(
+                            hidl_string(RADIO_SERVICE_NAME)));
     if (radio_v1_3 == NULL) {
         sleep(60);
-        radio_v1_3 = ::android::hardware::radio::V1_3::IRadio::getService(GetParam());
+        radio_v1_3 = ::testing::VtsHalHidlTargetTestBase::getService<
+                ::android::hardware::radio::V1_3::IRadio>(
+                RadioHidlEnvironment::Instance()
+                        ->getServiceName<::android::hardware::radio::V1_3::IRadio>(
+                                hidl_string(RADIO_SERVICE_NAME)));
     }
     ASSERT_NE(nullptr, radio_v1_3.get());
 
