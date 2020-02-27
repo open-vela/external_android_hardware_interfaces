c8ea426d165601f6d61280d99fe116f19440be0f
openvela-robot <openvela-robot@xiaomi.com>
2020-02-27 10:51:04 +0800

    Parameterize VtsHalEvsV1_0TargetTest
    Bug: 142397658
    Test: VtsHalEvsV1_0Target
    Signed-off-by: Changyeon Jo <changyeon@google.com>
    Change-Id: Ide6f4f58b60936ca766feb3090645afedde37a4d
    (cherry picked from commit b46adc9493b90aa7ec306715b11a1be7944c32ff)

--- radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
+++ radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
@@ -86,6 +86,10 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
     // Whether Uicc applications are enabled or not.
     bool areUiccApplicationsEnabled;
 
+    // Barring Info Response
+    ::android::hardware::radio::V1_5::CellIdentity barringCellIdentity;
+    ::android::hardware::hidl_vec<::android::hardware::radio::V1_5::BarringInfo> barringInfos;
+
     RadioResponse_v1_5(RadioHidlTest_v1_5& parent_v1_5);
     virtual ~RadioResponse_v1_5() = default;
 
@@ -558,6 +562,7 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
 
     Return<void> getBarringInfoResponse(
             const RadioResponseInfo& info,
+            const ::android::hardware::radio::V1_5::CellIdentity& cellIdentity,
             const ::android::hardware::hidl_vec<::android::hardware::radio::V1_5::BarringInfo>&
                     barringInfos);
 
