ba305199b58ce2140b145a86b4e4ed52ebe88ac6
openvela-robot <openvela-robot@xiaomi.com>
2020-02-27 16:52:33 +0800

    [automerger skipped] Merge "Add parallel linking support for BIDIRECTIONAL_SEQUENCE_{LSTM|RNN}" am: ad54253647 am: 9f0dc5f8aa -s ours am: 17ff0792a7 -s ours am: d9a4b135d7 -s ours am skip reason: Change-Id I51396f82de8dd7282db85a26b0a1be84b236b8c1 with SHA-1 13c43c0059 is in history
    Change-Id: Ica2a32cb1ef548833d76ab25bc7ae2d5fc542506

--- radio/1.5/vts/functional/radio_hidl_hal_test.cpp
+++ radio/1.5/vts/functional/radio_hidl_hal_test.cpp
@@ -17,7 +17,19 @@
 #include <radio_hidl_hal_utils_v1_5.h>
 
 void RadioHidlTest_v1_5::SetUp() {
-    radio_v1_5 = android::hardware::radio::V1_5::IRadio::getService(GetParam());
+    radio_v1_5 = ::testing::VtsHalHidlTargetTestBase::getService<
+            ::android::hardware::radio::V1_5::IRadio>(
+            RadioHidlEnvironment::Instance()
+                    ->getServiceName<::android::hardware::radio::V1_5::IRadio>(
+                            hidl_string(RADIO_SERVICE_NAME)));
+    if (radio_v1_5 == NULL) {
+        sleep(60);
+        radio_v1_5 = ::testing::VtsHalHidlTargetTestBase::getService<
+                ::android::hardware::radio::V1_5::IRadio>(
+                RadioHidlEnvironment::Instance()
+                        ->getServiceName<::android::hardware::radio::V1_5::IRadio>(
+                                hidl_string(RADIO_SERVICE_NAME)));
+    }
     ASSERT_NE(nullptr, radio_v1_5.get());
 
     radioRsp_v1_5 = new (std::nothrow) RadioResponse_v1_5(*this);
@@ -36,8 +48,10 @@ void RadioHidlTest_v1_5::SetUp() {
     EXPECT_EQ(RadioError::NONE, radioRsp_v1_5->rspInfo.error);
 
     sp<::android::hardware::radio::config::V1_1::IRadioConfig> radioConfig =
-            ::android::hardware::radio::config::V1_1::IRadioConfig::getService();
-    /* Enforce Vts testing with RadioConfig is existed. */
+            ::testing::VtsHalHidlTargetTestBase::getService<
+                    ::android::hardware::radio::config::V1_1::IRadioConfig>();
+
+    /* Enforce Vts tesing with RadioConfig is existed. */
     ASSERT_NE(nullptr, radioConfig.get());
 
     /* Enforce Vts Testing with Sim Status Present only. */
