7243430936ef63514883e61d34d00bf8b624a539
liujinye <liujinye@xiaomi.com>
2020-03-02 23:02:19 +0800

    Merge "Make GetLocationLowPower test warning instead of failing" into android10-tests-dev

--- drm/1.1/vts/functional/drm_hal_clearkey_test.cpp
+++ drm/1.1/vts/functional/drm_hal_clearkey_test.cpp
@@ -16,21 +16,255 @@
 
 #define LOG_TAG "drm_hal_clearkey_test@1.1"
 
+#include <android/hardware/drm/1.1/ICryptoFactory.h>
+#include <android/hardware/drm/1.0/ICryptoPlugin.h>
+#include <android/hardware/drm/1.1/IDrmFactory.h>
+#include <android/hardware/drm/1.0/IDrmPlugin.h>
+#include <android/hardware/drm/1.1/IDrmPlugin.h>
+#include <android/hardware/drm/1.0/types.h>
+#include <android/hardware/drm/1.1/types.h>
+#include <android/hidl/allocator/1.0/IAllocator.h>
+#include <android/hidl/manager/1.2/IServiceManager.h>
+#include <gtest/gtest.h>
+#include <hidl/HidlSupport.h>
+#include <hidl/ServiceManagement.h>
+#include <hidlmemory/mapping.h>
 #include <log/log.h>
-#include <vector>
+#include <memory>
+#include <openssl/aes.h>
+#include <random>
 
-#include "android/hardware/drm/1.1/vts/drm_hal_clearkey_test.h"
+#include "VtsHalHidlTargetTestBase.h"
+#include "VtsHalHidlTargetTestEnvBase.h"
 
-namespace android {
-namespace hardware {
-namespace drm {
-namespace V1_1 {
-namespace vts {
+namespace drm = ::android::hardware::drm;
+using ::android::hardware::drm::V1_0::BufferType;
+using ::android::hardware::drm::V1_0::DestinationBuffer;
+using ::android::hardware::drm::V1_0::ICryptoPlugin;
+using ::android::hardware::drm::V1_0::KeyedVector;
+using ::android::hardware::drm::V1_0::KeyValue;
+using ::android::hardware::drm::V1_0::KeyType;
+using ::android::hardware::drm::V1_0::Mode;
+using ::android::hardware::drm::V1_0::Pattern;
+using ::android::hardware::drm::V1_0::SecureStop;
+using ::android::hardware::drm::V1_0::SecureStopId;
+using ::android::hardware::drm::V1_0::SessionId;
+using ::android::hardware::drm::V1_0::SharedBuffer;
+using ::android::hardware::drm::V1_0::Status;
+using ::android::hardware::drm::V1_0::SubSample;
+using ::android::hardware::drm::V1_0::SubSample;
 
-const uint8_t kClearKeyUUID[16] = {
+using ::android::hardware::drm::V1_1::DrmMetricGroup;
+using ::android::hardware::drm::V1_1::HdcpLevel;
+using ::android::hardware::drm::V1_1::ICryptoFactory;
+using ::android::hardware::drm::V1_1::IDrmFactory;
+using ::android::hardware::drm::V1_1::IDrmPlugin;
+using ::android::hardware::drm::V1_1::KeyRequestType;
+using ::android::hardware::drm::V1_1::SecureStopRelease;
+using ::android::hardware::drm::V1_1::SecurityLevel;
+using ::android::hardware::drm::V1_1::SecurityLevel;
+
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hidl::allocator::V1_0::IAllocator;
+using ::android::hidl::memory::V1_0::IMemory;
+using ::android::sp;
+
+using std::string;
+using std::unique_ptr;
+using std::random_device;
+using std::map;
+using std::mt19937;
+using std::vector;
+
+/**
+ * These clearkey tests use white box knowledge of the legacy clearkey
+ * plugin to verify that the HIDL HAL services and interfaces are working.
+ * It is not intended to verify any vendor's HAL implementation. If you
+ * are looking for vendor HAL tests, see drm_hal_vendor_test.cpp
+ */
+#define ASSERT_OK(ret) ASSERT_TRUE(ret.isOk())
+#define EXPECT_OK(ret) EXPECT_TRUE(ret.isOk())
+
+// To be used in mpd to specify drm scheme for players
+static const uint8_t kClearKeyUUID[16] = {
     0xE2, 0x71, 0x9D, 0x58, 0xA9, 0x85, 0xB3, 0xC9,
-    0x78, 0x1A, 0xB0, 0x30, 0xAF, 0x78, 0xD3, 0x0E
+    0x78, 0x1A, 0xB0, 0x30, 0xAF, 0x78, 0xD3, 0x0E};
+
+// Test environment for drm
+class DrmHidlEnvironment : public ::testing::VtsHalHidlTargetTestEnvBase {
+   public:
+    // get the test environment singleton
+    static DrmHidlEnvironment* Instance() {
+        static DrmHidlEnvironment* instance = new DrmHidlEnvironment;
+        return instance;
+    }
+
+    virtual void HidlSetUp() override { ALOGI("SetUp DrmHidlEnvironment"); }
+
+    virtual void HidlTearDown() override { ALOGI("TearDown DrmHidlEnvironment"); }
+
+    void registerTestServices() override {
+        registerTestService<ICryptoFactory>();
+        registerTestService<IDrmFactory>();
+        setServiceCombMode(::testing::HalServiceCombMode::NO_COMBINATION);
+    }
+
+   private:
+    DrmHidlEnvironment() {}
+
+    GTEST_DISALLOW_COPY_AND_ASSIGN_(DrmHidlEnvironment);
 };
+
+
+class DrmHalClearkeyTest : public ::testing::VtsHalHidlTargetTestBase {
+public:
+    virtual void SetUp() override {
+        const ::testing::TestInfo* const test_info =
+                ::testing::UnitTest::GetInstance()->current_test_info();
+
+        ALOGD("DrmHalClearkeyTest: Running test %s.%s", test_info->test_case_name(),
+                test_info->name());
+
+        auto manager = android::hardware::defaultServiceManager1_2();
+        ASSERT_NE(nullptr, manager.get());
+        manager->listManifestByInterface(IDrmFactory::descriptor,
+                [&](const hidl_vec<hidl_string> &registered) {
+                    for (const auto &instance : registered) {
+                        sp<IDrmFactory> drmFactory =
+                                ::testing::VtsHalHidlTargetTestBase::getService<IDrmFactory>(instance);
+                        drmPlugin = createDrmPlugin(drmFactory);
+                        if (drmPlugin != nullptr) {
+                            break;
+                        }
+                    }
+                }
+            );
+
+        manager->listManifestByInterface(ICryptoFactory::descriptor,
+                [&](const hidl_vec<hidl_string> &registered) {
+                    for (const auto &instance : registered) {
+                        sp<ICryptoFactory> cryptoFactory =
+                                ::testing::VtsHalHidlTargetTestBase::getService<ICryptoFactory>(instance);
+                        cryptoPlugin = createCryptoPlugin(cryptoFactory);
+                        if (cryptoPlugin != nullptr) {
+                            break;
+                        }
+                    }
+                }
+            );
+
+        ASSERT_NE(nullptr, drmPlugin.get()) << "Can't find clearkey drm@1.1 plugin";
+        ASSERT_NE(nullptr, cryptoPlugin.get()) << "Can't find clearkey crypto@1.1 plugin";
+    }
+
+
+    virtual void TearDown() override {}
+
+    SessionId openSession();
+    SessionId openSession(SecurityLevel level);
+    void closeSession(const SessionId& sessionId);
+    hidl_vec<uint8_t> loadKeys(const SessionId& sessionId, const KeyType& type);
+
+  private:
+    sp<IDrmPlugin> createDrmPlugin(sp<IDrmFactory> drmFactory) {
+        if (drmFactory == nullptr) {
+            return nullptr;
+        }
+        sp<IDrmPlugin> plugin = nullptr;
+        auto res = drmFactory->createPlugin(
+                kClearKeyUUID, "",
+                        [&](Status status, const sp<drm::V1_0::IDrmPlugin>& pluginV1_0) {
+                    EXPECT_EQ(Status::OK, status);
+                    plugin = IDrmPlugin::castFrom(pluginV1_0);
+                });
+
+        if (!res.isOk()) {
+            ALOGE("createDrmPlugin remote call failed");
+        }
+        return plugin;
+    }
+
+    sp<ICryptoPlugin> createCryptoPlugin(sp<ICryptoFactory> cryptoFactory) {
+        if (cryptoFactory == nullptr) {
+            return nullptr;
+        }
+        sp<ICryptoPlugin> plugin = nullptr;
+        hidl_vec<uint8_t> initVec;
+        auto res = cryptoFactory->createPlugin(
+                kClearKeyUUID, initVec,
+                        [&](Status status, const sp<drm::V1_0::ICryptoPlugin>& pluginV1_0) {
+                    EXPECT_EQ(Status::OK, status);
+                    plugin = pluginV1_0;
+                });
+        if (!res.isOk()) {
+            ALOGE("createCryptoPlugin remote call failed");
+        }
+        return plugin;
+    }
+
+protected:
+ template <typename CT>
+ bool ValueEquals(DrmMetricGroup::ValueType type, const std::string& expected, const CT& actual) {
+     return type == DrmMetricGroup::ValueType::STRING_TYPE && expected == actual.stringValue;
+ }
+
+ template <typename CT>
+ bool ValueEquals(DrmMetricGroup::ValueType type, const int64_t expected, const CT& actual) {
+     return type == DrmMetricGroup::ValueType::INT64_TYPE && expected == actual.int64Value;
+ }
+
+ template <typename CT>
+ bool ValueEquals(DrmMetricGroup::ValueType type, const double expected, const CT& actual) {
+     return type == DrmMetricGroup::ValueType::DOUBLE_TYPE && expected == actual.doubleValue;
+ }
+
+ template <typename AT, typename VT>
+ bool ValidateMetricAttributeAndValue(const DrmMetricGroup::Metric& metric,
+                                      const std::string& attributeName, const AT& attributeValue,
+                                      const std::string& componentName, const VT& componentValue) {
+     bool validAttribute = false;
+     bool validComponent = false;
+     for (const DrmMetricGroup::Attribute& attribute : metric.attributes) {
+         if (attribute.name == attributeName &&
+             ValueEquals(attribute.type, attributeValue, attribute)) {
+             validAttribute = true;
+         }
+     }
+     for (const DrmMetricGroup::Value& value : metric.values) {
+         if (value.componentName == componentName &&
+             ValueEquals(value.type, componentValue, value)) {
+             validComponent = true;
+         }
+     }
+     return validAttribute && validComponent;
+ }
+
+ template <typename AT, typename VT>
+ bool ValidateMetricAttributeAndValue(const hidl_vec<DrmMetricGroup>& metricGroups,
+                                      const std::string& metricName,
+                                      const std::string& attributeName, const AT& attributeValue,
+                                      const std::string& componentName, const VT& componentValue) {
+     bool foundMetric = false;
+     for (const auto& group : metricGroups) {
+         for (const auto& metric : group.metrics) {
+             if (metric.name == metricName) {
+                 foundMetric = foundMetric || ValidateMetricAttributeAndValue(
+                                                  metric, attributeName, attributeValue,
+                                                  componentName, componentValue);
+             }
+         }
+     }
+     return foundMetric;
+ }
+
+ sp<IDrmPlugin> drmPlugin;
+ sp<ICryptoPlugin> cryptoPlugin;
+};
+
 
 /**
  * Helper method to open a session and verify that a non-empty
@@ -137,7 +371,7 @@ hidl_vec<uint8_t> DrmHalClearkeyTest::loadKeys(
 /**
  * Test openSession negative case: security level higher than supported
  */
-TEST_P(DrmHalClearkeyTest, OpenSessionBadLevel) {
+TEST_F(DrmHalClearkeyTest, OpenSessionBadLevel) {
     auto res = drmPlugin->openSession_1_1(SecurityLevel::HW_SECURE_ALL,
             [&](Status status, const SessionId& /* id */) {
                 EXPECT_EQ(Status::ERROR_DRM_CANNOT_HANDLE, status);
@@ -148,7 +382,7 @@ TEST_P(DrmHalClearkeyTest, OpenSessionBadLevel) {
 /**
  * Test getKeyRequest_1_1 via loadKeys
  */
-TEST_P(DrmHalClearkeyTest, GetKeyRequest) {
+TEST_F(DrmHalClearkeyTest, GetKeyRequest) {
     auto sessionId = openSession();
     loadKeys(sessionId);
     closeSession(sessionId);
@@ -157,7 +391,7 @@ TEST_P(DrmHalClearkeyTest, GetKeyRequest) {
 /**
  * A get key request should fail if no sessionId is provided
  */
-TEST_P(DrmHalClearkeyTest, GetKeyRequestNoSession) {
+TEST_F(DrmHalClearkeyTest, GetKeyRequestNoSession) {
     SessionId invalidSessionId;
     hidl_vec<uint8_t> initData;
     hidl_string mimeType = "video/mp4";
@@ -175,7 +409,7 @@ TEST_P(DrmHalClearkeyTest, GetKeyRequestNoSession) {
  * Test that the plugin returns the expected error code in
  * this case.
  */
-TEST_P(DrmHalClearkeyTest, GetKeyRequestOfflineKeyTypeNotSupported) {
+TEST_F(DrmHalClearkeyTest, GetKeyRequestOfflineKeyTypeNotSupported) {
     auto sessionId = openSession();
     hidl_vec<uint8_t> initData;
     hidl_string mimeType = "video/mp4";
@@ -195,7 +429,7 @@ TEST_P(DrmHalClearkeyTest, GetKeyRequestOfflineKeyTypeNotSupported) {
 /**
  * Test that the plugin returns valid connected and max HDCP levels
  */
-TEST_P(DrmHalClearkeyTest, GetHdcpLevels) {
+TEST_F(DrmHalClearkeyTest, GetHdcpLevels) {
     auto res = drmPlugin->getHdcpLevels(
             [&](Status status, const HdcpLevel &connectedLevel,
                 const HdcpLevel &maxLevel) {
@@ -214,7 +448,7 @@ TEST_P(DrmHalClearkeyTest, GetHdcpLevels) {
 /**
  * Test that the plugin returns default open and max session counts
  */
-TEST_P(DrmHalClearkeyTest, GetDefaultSessionCounts) {
+TEST_F(DrmHalClearkeyTest, GetDefaultSessionCounts) {
     auto res = drmPlugin->getNumberOfSessions(
             [&](Status status, uint32_t currentSessions,
                     uint32_t maxSessions) {
@@ -230,7 +464,7 @@ TEST_P(DrmHalClearkeyTest, GetDefaultSessionCounts) {
  * Test that the plugin returns valid open and max session counts
  * after a session is opened.
  */
-TEST_P(DrmHalClearkeyTest, GetOpenSessionCounts) {
+TEST_F(DrmHalClearkeyTest, GetOpenSessionCounts) {
     uint32_t initialSessions = 0;
     auto res = drmPlugin->getNumberOfSessions(
             [&](Status status, uint32_t currentSessions,
@@ -271,7 +505,7 @@ TEST_P(DrmHalClearkeyTest, GetOpenSessionCounts) {
  * Test that the plugin returns the same security level
  * by default as when it is requested explicitly
  */
-TEST_P(DrmHalClearkeyTest, GetDefaultSecurityLevel) {
+TEST_F(DrmHalClearkeyTest, GetDefaultSecurityLevel) {
     SessionId session = openSession();
     SecurityLevel defaultLevel;
     auto res = drmPlugin->getSecurityLevel(session,
@@ -296,7 +530,7 @@ TEST_P(DrmHalClearkeyTest, GetDefaultSecurityLevel) {
  * Test that the plugin returns the lowest security level
  * when it is requested
  */
-TEST_P(DrmHalClearkeyTest, GetSecurityLevel) {
+TEST_F(DrmHalClearkeyTest, GetSecurityLevel) {
     SessionId session = openSession(SecurityLevel::SW_SECURE_CRYPTO);
     auto res = drmPlugin->getSecurityLevel(session,
             [&](Status status, SecurityLevel level) {
@@ -311,7 +545,7 @@ TEST_P(DrmHalClearkeyTest, GetSecurityLevel) {
  * Test that the plugin returns the documented error
  * when requesting the security level for an invalid sessionId
  */
-TEST_P(DrmHalClearkeyTest, GetSecurityLevelInvalidSessionId) {
+TEST_F(DrmHalClearkeyTest, GetSecurityLevelInvalidSessionId) {
     SessionId session;
     auto res = drmPlugin->getSecurityLevel(session,
             [&](Status status, SecurityLevel /*level*/) {
@@ -323,7 +557,7 @@ TEST_P(DrmHalClearkeyTest, GetSecurityLevelInvalidSessionId) {
 /**
  * Test metrics are set appropriately for open and close operations.
  */
-TEST_P(DrmHalClearkeyTest, GetMetricsOpenClose) {
+TEST_F(DrmHalClearkeyTest, GetMetricsOpenClose) {
     SessionId sessionId = openSession();
     // The first close should be successful.
     closeSession(sessionId);
@@ -355,7 +589,7 @@ TEST_P(DrmHalClearkeyTest, GetMetricsOpenClose) {
 /**
  * Test that there are no secure stop ids after clearing them
  */
-TEST_P(DrmHalClearkeyTest, GetSecureStopIdsCleared) {
+TEST_F(DrmHalClearkeyTest, GetSecureStopIdsCleared) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -370,7 +604,7 @@ TEST_P(DrmHalClearkeyTest, GetSecureStopIdsCleared) {
 /**
  * Test that there are secure stop ids after loading keys once
  */
-TEST_P(DrmHalClearkeyTest, GetSecureStopIdsOnce) {
+TEST_F(DrmHalClearkeyTest, GetSecureStopIdsOnce) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -405,7 +639,7 @@ TEST_P(DrmHalClearkeyTest, GetSecureStopIdsOnce) {
  * Test that the clearkey plugin reports no secure stops when
  * there are none.
  */
-TEST_P(DrmHalClearkeyTest, GetNoSecureStops) {
+TEST_F(DrmHalClearkeyTest, GetNoSecureStops) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -420,7 +654,7 @@ TEST_P(DrmHalClearkeyTest, GetNoSecureStops) {
 /**
  * Test get/remove of one secure stop
  */
-TEST_P(DrmHalClearkeyTest, GetOneSecureStopAndRemoveIt) {
+TEST_F(DrmHalClearkeyTest, GetOneSecureStopAndRemoveIt) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -454,7 +688,7 @@ TEST_P(DrmHalClearkeyTest, GetOneSecureStopAndRemoveIt) {
 /**
  * Test that there are no secure stops after clearing them
  */
-TEST_P(DrmHalClearkeyTest, GetSecureStopsCleared) {
+TEST_F(DrmHalClearkeyTest, GetSecureStopsCleared) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -469,7 +703,7 @@ TEST_P(DrmHalClearkeyTest, GetSecureStopsCleared) {
 /**
  * Test that there are secure stops after loading keys once
  */
-TEST_P(DrmHalClearkeyTest, GetSecureStopsOnce) {
+TEST_F(DrmHalClearkeyTest, GetSecureStopsOnce) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -504,7 +738,7 @@ TEST_P(DrmHalClearkeyTest, GetSecureStopsOnce) {
  * Test that releasing a secure stop with empty
  * release message fails with the documented error
  */
-TEST_P(DrmHalClearkeyTest, ReleaseEmptySecureStop) {
+TEST_F(DrmHalClearkeyTest, ReleaseEmptySecureStop) {
     SecureStopRelease emptyRelease = {.opaqueData = hidl_vec<uint8_t>()};
     Status status = drmPlugin->releaseSecureStops(emptyRelease);
     EXPECT_EQ(Status::BAD_VALUE, status);
@@ -529,7 +763,8 @@ SecureStopRelease makeSecureRelease(const SecureStop &stop) {
 /**
  * Test that releasing one secure stop works
  */
-TEST_P(DrmHalClearkeyTest, ReleaseOneSecureStop) {
+TEST_F(DrmHalClearkeyTest, ReleaseOneSecureStop) {
+
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -557,11 +792,12 @@ TEST_P(DrmHalClearkeyTest, ReleaseOneSecureStop) {
     EXPECT_OK(res);
 }
 
+
 /**
  * Test that removing a secure stop with an empty ID returns
  * documented error
  */
-TEST_P(DrmHalClearkeyTest, RemoveEmptySecureStopId) {
+TEST_F(DrmHalClearkeyTest, RemoveEmptySecureStopId) {
     hidl_vec<uint8_t> emptyId;
     auto stat = drmPlugin->removeSecureStop(emptyId);
     EXPECT_OK(stat);
@@ -572,7 +808,7 @@ TEST_P(DrmHalClearkeyTest, RemoveEmptySecureStopId) {
  * Test that removing a secure stop after it has already
  * been removed fails with the documented error code.
  */
-TEST_P(DrmHalClearkeyTest, RemoveRemovedSecureStopId) {
+TEST_F(DrmHalClearkeyTest, RemoveRemovedSecureStopId) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -599,7 +835,7 @@ TEST_P(DrmHalClearkeyTest, RemoveRemovedSecureStopId) {
 /**
  * Test that removing a secure stop by id works
  */
-TEST_P(DrmHalClearkeyTest, RemoveSecureStopById) {
+TEST_F(DrmHalClearkeyTest, RemoveSecureStopById) {
     auto stat = drmPlugin->removeAllSecureStops();
     EXPECT_OK(stat);
 
@@ -627,8 +863,12 @@ TEST_P(DrmHalClearkeyTest, RemoveSecureStopById) {
     EXPECT_OK(res);
 }
 
-}  // namespace vts
-}  // namespace V1_1
-}  // namespace drm
-}  // namespace hardware
-}  // namespace android
+
+int main(int argc, char** argv) {
+    ::testing::AddGlobalTestEnvironment(DrmHidlEnvironment::Instance());
+    ::testing::InitGoogleTest(&argc, argv);
+    DrmHidlEnvironment::Instance()->init(&argc, argv);
+    int status = RUN_ALL_TESTS();
+    ALOGI("Test result = %d", status);
+    return status;
+}
