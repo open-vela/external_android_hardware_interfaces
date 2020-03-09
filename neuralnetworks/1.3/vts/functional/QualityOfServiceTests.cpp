71aba5b3eb9e337c52a55da75454b800a54d930a
openvela-robot <openvela-robot@xiaomi.com>
2020-03-09 10:56:06 +0800

    Relax NNAPI QoS deadline parameter am: ff7d6c5493 am: ad6b027649
    Change-Id: I6e2fe77d63fffe825105a545d3942d16912e543a

--- neuralnetworks/1.3/vts/functional/QualityOfServiceTests.cpp
+++ neuralnetworks/1.3/vts/functional/QualityOfServiceTests.cpp
@@ -134,9 +134,10 @@ void runPrepareModelTest(const sp<IDevice>& device, const Model& model, Priority
     } else {
         switch (deadlineBound.value()) {
             case DeadlineBoundType::NOW:
-            case DeadlineBoundType::SHORT:
-                // Either the driver successfully completed the task or it
-                // aborted and returned MISSED_DEADLINE_*.
+                // If the execution was launched with a deadline of NOW, the
+                // deadline has already passed when the driver would launch the
+                // execution. In this case, the driver must return
+                // MISSED_DEADLINE_*.
                 EXPECT_TRUE(prepareReturnStatus == ErrorStatus::NONE ||
                             prepareReturnStatus == ErrorStatus::MISSED_DEADLINE_TRANSIENT ||
                             prepareReturnStatus == ErrorStatus::MISSED_DEADLINE_PERSISTENT);
@@ -146,6 +147,13 @@ void runPrepareModelTest(const sp<IDevice>& device, const Model& model, Priority
                 // proceed normally. In this case, check it normally by breaking out
                 // of the switch statement.
                 EXPECT_EQ(ErrorStatus::NONE, prepareReturnStatus);
+                break;
+            case DeadlineBoundType::SHORT:
+                // Either the driver successfully completed the task in time or
+                // it aborted within the compliance time.
+                EXPECT_TRUE(prepareReturnStatus == ErrorStatus::NONE ||
+                            prepareReturnStatus == ErrorStatus::MISSED_DEADLINE_TRANSIENT ||
+                            prepareReturnStatus == ErrorStatus::MISSED_DEADLINE_PERSISTENT);
                 break;
         }
     }
@@ -198,10 +206,7 @@ static MaybeResults executeSynchronously(const sp<IPreparedModel>& preparedModel
 
     // configure results callback
     MaybeResults results;
-    const auto cb = [&results](ErrorStatus status, const hidl_vec<OutputShape>& outputShapes,
-                               const Timing& timing) {
-        results.emplace(status, outputShapes, timing);
-    };
+    const auto cb = [&results](const auto&... args) { *results = {args...}; };
 
     // run execution
     const Return<void> ret =
@@ -230,18 +235,26 @@ void runExecutionTest(const sp<IPreparedModel>& preparedModel, const TestModel& 
     // Validate deadline information if applicable.
     switch (deadlineBound) {
         case DeadlineBoundType::NOW:
-        case DeadlineBoundType::SHORT:
-            // Either the driver successfully completed the task or it
-            // aborted and returned MISSED_DEADLINE_*.
+            // If the execution was launched with a deadline of NOW, the
+            // deadline has already passed when the driver would launch the
+            // execution. In this case, the driver must return
+            // MISSED_DEADLINE_*.
             ASSERT_TRUE(status == ErrorStatus::NONE ||
                         status == ErrorStatus::MISSED_DEADLINE_TRANSIENT ||
                         status == ErrorStatus::MISSED_DEADLINE_PERSISTENT);
-            break;
+            return;
         case DeadlineBoundType::UNLIMITED:
             // If an unlimited deadline is supplied, we expect the execution to
             // proceed normally. In this case, check it normally by breaking out
             // of the switch statement.
             ASSERT_EQ(ErrorStatus::NONE, status);
+            break;
+        case DeadlineBoundType::SHORT:
+            // Either the driver successfully completed the task in time or
+            // it aborted within the compliance time.
+            EXPECT_TRUE(status == ErrorStatus::NONE ||
+                        status == ErrorStatus::MISSED_DEADLINE_TRANSIENT ||
+                        status == ErrorStatus::MISSED_DEADLINE_PERSISTENT);
             break;
     }
 
@@ -264,9 +277,7 @@ void runExecutionTest(const sp<IPreparedModel>& preparedModel, const TestModel& 
     const std::vector<TestBuffer> outputs = getOutputBuffers(request10);
 
     // We want "close-enough" results.
-    if (status == ErrorStatus::NONE) {
-        checkResults(testModel, outputs);
-    }
+    checkResults(testModel, outputs);
 }
 
 void runExecutionTests(const sp<IPreparedModel>& preparedModel, const TestModel& testModel,
