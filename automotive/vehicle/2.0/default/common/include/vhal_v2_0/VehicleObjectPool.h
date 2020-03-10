c506689e839263356763346e4194dc0afa6d35f7
liujinye <liujinye@xiaomi.com>
2020-03-10 21:34:11 +0800

    Merge "Update audio VTS source code to match the change in AudioPolicyConfig" into rvc-dev am: ce3f17e662 am: def82fe687
    Change-Id: I68d47c54d523071b3765b630bb0615b6bb67e6e4

--- automotive/vehicle/2.0/default/common/include/vhal_v2_0/VehicleObjectPool.h
+++ automotive/vehicle/2.0/default/common/include/vhal_v2_0/VehicleObjectPool.h
@@ -206,7 +206,7 @@ private:
         InternalPool(VehiclePropertyType type, size_t vectorSize)
             : mPropType(type), mVectorSize(vectorSize) {}
 
-        RecyclableType obtain() override {
+        RecyclableType obtain() {
             return ObjectPool<VehiclePropValue>::obtain();
         }
     protected:
