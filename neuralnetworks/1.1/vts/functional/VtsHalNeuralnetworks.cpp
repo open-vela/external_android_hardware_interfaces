5fae3e34f8551c2f18c0388cb7ba043b441994fb
openvela-robot <openvela-robot@xiaomi.com>
2020-03-21 21:38:58 +0800

    Merge "Documentation for enum PersoSubstate" am: 3e3e78bf20 am: f48c56ebff
    Change-Id: I55412d12a4c5b4dc1980519557b249d137834d58

--- neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.cpp
+++ neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.cpp
@@ -132,8 +132,7 @@ void validateEverything(const sp<IDevice>& device, const Model& model, const Req
 
 TEST_P(ValidationTest, Test) {
     const Model model = createModel(kTestModel);
-    ExecutionContext context;
-    const Request request = context.createRequest(kTestModel);
+    const Request request = createRequest(kTestModel);
     ASSERT_FALSE(kTestModel.expectFailure);
     validateEverything(kDevice, model, request);
 }
