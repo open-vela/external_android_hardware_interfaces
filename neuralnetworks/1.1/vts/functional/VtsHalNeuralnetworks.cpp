34f0744989920ded9c194067cad42ba2f6109a9e
liujinye <liujinye@xiaomi.com>
2020-03-24 00:37:50 +0800

    Merge "Audio Effects: Run more tests for LoudnessEnhancerEffect" am: 7a2c9860f2 am: 004262eec4 am: f5f04f157c am: 7898fea19e am: f1e8e83884
    Change-Id: I195a7d6df76dbfa53b4f253bae53e018d345cdd2

--- neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.cpp
+++ neuralnetworks/1.1/vts/functional/VtsHalNeuralnetworks.cpp
@@ -138,11 +138,6 @@ TEST_P(ValidationTest, Test) {
     validateEverything(kDevice, model, request);
 }
 
-INSTANTIATE_GENERATED_TEST(ValidationTest, [](const std::string& testName) {
-    // Skip validation for the "inputs_as_internal" and "all_tensors_as_inputs"
-    // generated tests.
-    return testName.find("inputs_as_internal") == std::string::npos &&
-           testName.find("all_tensors_as_inputs") == std::string::npos;
-});
+INSTANTIATE_GENERATED_TEST(ValidationTest, [](const test_helper::TestModel&) { return true; });
 
 }  // namespace android::hardware::neuralnetworks::V1_1::vts::functional
