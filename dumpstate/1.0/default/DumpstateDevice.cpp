4147a6ead0c6acdb2a2923b5ac1c28d280cb698b
liujinye <liujinye@xiaomi.com>
2020-03-25 20:27:00 +0800

    [automerger skipped] Merge "Add default/android.hardware.dumpstate@1.1-service.example" into rvc-dev am: 85c0d4fb00 am: abe2d48612 -s ours
    am skip reason: Change-Id I7f4e50846e2dc4393563c602145afb5617b2d48f with SHA-1 23e3c29369 is in history
    
    Change-Id: If614790887c11050b1143f3de036352c8a3e58bd

--- dumpstate/1.0/default/DumpstateDevice.cpp
+++ dumpstate/1.0/default/DumpstateDevice.cpp
@@ -51,8 +51,9 @@ Return<void> DumpstateDevice::dumpstateBoard(const hidl_handle& handle) {
     ALOGI("Dumpstate HIDL not provided by device\n");
     dprintf(fd, "Dumpstate HIDL not provided by device; providing bogus data.\n");
 
-    // Shows an example on how to use the libdumpstateutil API.
-    DumpFileToFd(fd, "cmdline", "/proc/self/cmdline");
+    // Shows some examples on how to use the libdumpstateutil API.
+    RunCommandToFd(fd, "DATE", {"/vendor/bin/date"});
+    DumpFileToFd(fd, "HOSTS", "/system/etc/hosts");
 
     return Void();
 }
