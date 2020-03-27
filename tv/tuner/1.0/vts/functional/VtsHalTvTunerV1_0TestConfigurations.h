34bceb2cfb9b5a0f1bd44ba8d1c7983ab74de120
openvela-robot <openvela-robot@xiaomi.com>
2020-03-28 04:25:42 +0800

    Copy filtered av data to ion buffer to test on cuttlefish am: d6afead9fc am: 8d6483a617
    Change-Id: Ib6365bd409d8de7839a4cee9d7473f2b96112933

--- tv/tuner/1.0/vts/functional/VtsHalTvTunerV1_0TestConfigurations.h
+++ tv/tuner/1.0/vts/functional/VtsHalTvTunerV1_0TestConfigurations.h
@@ -113,19 +113,9 @@ inline void initFrontendConfig() {
 
 /** Configuration array for the frontend scan test */
 inline void initFrontendScanConfig() {
-    frontendScanArray[0].type = FrontendType::DVBT;
-    frontendScanArray[0].settings.dvbt({
-            .frequency = 578000,
-            .transmissionMode = FrontendDvbtTransmissionMode::MODE_8K,
-            .bandwidth = FrontendDvbtBandwidth::BANDWIDTH_8MHZ,
-            .constellation = FrontendDvbtConstellation::AUTO,
-            .hierarchy = FrontendDvbtHierarchy::AUTO,
-            .hpCoderate = FrontendDvbtCoderate::AUTO,
-            .lpCoderate = FrontendDvbtCoderate::AUTO,
-            .guardInterval = FrontendDvbtGuardInterval::AUTO,
-            .isHighPriority = true,
-            .standard = FrontendDvbtStandard::T,
-    });
+    frontendScanArray[0].type = FrontendType::DVBT, frontendScanArray[0].settings.dvbt({
+                                                            .frequency = 577000,
+                                                    });
 };
 
 /** Configuration array for the filter test */
