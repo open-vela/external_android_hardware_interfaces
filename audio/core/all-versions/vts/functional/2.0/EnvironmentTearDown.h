f34a64b87c8339e5416cf35dc6a52d7536ee70cd
liujinye <liujinye@xiaomi.com>
2020-03-30 17:59:00 +0800

    Merge "Remove IOperation and beginOp." into rvc-dev

--- audio/core/all-versions/vts/functional/2.0/EnvironmentTearDown.h
+++ audio/core/all-versions/vts/functional/2.0/EnvironmentTearDown.h
@@ -14,18 +14,21 @@
  * limitations under the License.
  */
 
-package android.hardware.keymaster@4.1;
+#ifndef ANDROID_HARDWARE_AUDIO_CORE_2_0_ENVIRONMENT_TEARDOWN_H
+#define ANDROID_HARDWARE_AUDIO_CORE_2_0_ENVIRONMENT_TEARDOWN_H
 
-import @4.0::ErrorCode;
-import @4.0::OperationHandle;
+#include <VtsHalHidlTargetTestEnvBase.h>
+#include <gtest/gtest.h>
 
-/**
- * IOperation represents an in-progress IKeymasterDevice operation.  It is returned by
- * IKeymasterDevice.beginOp().
- */
-interface IOperation {
-    /**
-     * Returns the operation handle to be used as an authentication challenge.
-     */
-    getOperationChallenge() generates (ErrorCode error, OperationHandle operation);
+#include "utility/EnvironmentTearDown.h"
+
+class Environment : public ::android::hardware::audio::common::test::utility::EnvironmentTearDown,
+                    public ::testing::VtsHalHidlTargetTestEnvBase {
+  private:
+    void HidlTearDown() override {
+        executeAllTearDowns();
+        VtsHalHidlTargetTestEnvBase::HidlTearDown();
+    }
 };
+
+#endif  // ANDROID_HARDWARE_AUDIO_CORE_2_0_ENVIRONMENT_TEARDOWN_H
--- media/omx/1.0/vts/functional/video/media_video_hidl_test_common.h
+++ media/omx/1.0/vts/functional/video/media_video_hidl_test_common.h
@@ -17,8 +17,6 @@
 #ifndef MEDIA_VIDEO_HIDL_TEST_COMMON_H
 #define MEDIA_VIDEO_HIDL_TEST_COMMON_H
 
-#include <media_hidl_test_common.h>
-
 /*
  * Common video utils
  */
