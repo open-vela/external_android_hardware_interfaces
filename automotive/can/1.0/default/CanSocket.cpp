9a4d037c1d7169933f8ca5d46846c3f048579217
openvela-robot <openvela-robot@xiaomi.com>
2020-03-30 20:05:37 +0800

    Merge "Using VINTF manifest fragment" into rvc-dev am: d11d6ee03f am: 201019850b am: fc32e2a0d5
    Change-Id: I77f7bca381fb89c01fddbeb94214474a7a085ec7

--- automotive/can/1.0/default/CanSocket.cpp
+++ automotive/can/1.0/default/CanSocket.cpp
@@ -67,7 +67,7 @@ CanSocket::~CanSocket() {
 bool CanSocket::send(const struct canfd_frame& frame) {
     const auto res = write(mSocket.get(), &frame, CAN_MTU);
     if (res < 0) {
-        PLOG(DEBUG) << "CanSocket send failed";
+        LOG(DEBUG) << "CanSocket send failed: " << errno;
         return false;
     }
     if (res != CAN_MTU) {
@@ -102,7 +102,7 @@ void CanSocket::readerThread() {
         const auto sel = selectRead(mSocket, kReadPooling);
         if (sel == 0) continue;  // timeout
         if (sel == -1) {
-            PLOG(ERROR) << "Select failed";
+            LOG(ERROR) << "Select failed: " << errno;
             break;
         }
 
@@ -130,7 +130,7 @@ void CanSocket::readerThread() {
             if (errno == EAGAIN) continue;
 
             errnoCopy = errno;
-            PLOG(ERROR) << "Failed to read CAN packet";
+            LOG(ERROR) << "Failed to read CAN packet: " << strerror(errno) << " (" << errno << ")";
             break;
         }
 
