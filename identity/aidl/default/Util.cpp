cabc9562d33ddec1ae9f3da5409983abdefc5d6a
openvela-robot <openvela-robot@xiaomi.com>
2020-04-03 20:59:24 +0800

    Merge "Skip enableModem vts if in single SIM mode." into rvc-dev

--- identity/aidl/default/Util.cpp
+++ identity/aidl/default/Util.cpp
@@ -39,12 +39,21 @@ const vector<uint8_t>& getHardwareBoundKey() {
     return hardwareBoundKey;
 }
 
+vector<uint8_t> byteStringToUnsigned(const vector<int8_t>& value) {
+    return vector<uint8_t>(value.begin(), value.end());
+}
+
+vector<int8_t> byteStringToSigned(const vector<uint8_t>& value) {
+    return vector<int8_t>(value.begin(), value.end());
+}
+
 vector<uint8_t> secureAccessControlProfileEncodeCbor(const SecureAccessControlProfile& profile) {
     cppbor::Map map;
     map.add("id", profile.id);
 
     if (profile.readerCertificate.encodedCertificate.size() > 0) {
-        map.add("readerCertificate", cppbor::Bstr(profile.readerCertificate.encodedCertificate));
+        map.add("readerCertificate",
+                cppbor::Bstr(byteStringToUnsigned(profile.readerCertificate.encodedCertificate)));
     }
 
     if (profile.userAuthenticationRequired) {
@@ -85,7 +94,7 @@ bool secureAccessControlProfileCheckMac(const SecureAccessControlProfile& profil
     if (!mac) {
         return false;
     }
-    if (mac.value() != profile.mac) {
+    if (mac.value() != byteStringToUnsigned(profile.mac)) {
         return false;
     }
     return true;
