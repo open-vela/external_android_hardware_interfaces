02b9be94915431e0f1c79281d6bd083d016af823
openvela-robot <openvela-robot@xiaomi.com>
2020-04-09 21:57:53 +0800

    Merge "Add vintf_fragments to android.hardware.confirmationui@1.0-service"

--- sensors/1.0/vts/functional/SensorsHidlEnvironmentV1_0.h
+++ sensors/1.0/vts/functional/SensorsHidlEnvironmentV1_0.h
@@ -29,11 +29,8 @@
 using ::android::sp;
 
 class SensorsHidlTest;
-class SensorsHidlEnvironmentV1_0
-    : public SensorsHidlEnvironmentBase<::android::hardware::sensors::V1_0::Event> {
-  public:
-    void HidlTearDown() override;
-
+class SensorsHidlEnvironmentV1_0 : public SensorsHidlEnvironmentBase {
+   public:
     using Event = ::android::hardware::sensors::V1_0::Event;
     SensorsHidlEnvironmentV1_0(const std::string& service_name)
         : SensorsHidlEnvironmentBase(service_name) {}
