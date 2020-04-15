838e3f958a058cc0471729c5cf4178978c44035d
liujinye <liujinye@xiaomi.com>
2020-04-15 18:28:06 +0800

    Merge "Correct UseHmacKey parameter" into rvc-dev am: ecd5e55cc1 am: 8be005008b am: 6cd49e8b6f am: 0fe130894e
    Change-Id: I07b924eada36f58422fa1d7dcb75799eaf5d243b

--- wifi/1.4/default/tests/mock_wifi_iface_util.h
+++ wifi/1.4/default/tests/mock_wifi_iface_util.h
@@ -40,7 +40,6 @@ class MockWifiIfaceUtil : public WifiIfaceUtil {
     MOCK_METHOD2(registerIfaceEventHandlers,
                  void(const std::string&, IfaceEventHandlers));
     MOCK_METHOD1(unregisterIfaceEventHandlers, void(const std::string&));
-    MOCK_METHOD2(setUpState, bool(const std::string&, bool));
 };
 }  // namespace iface_util
 }  // namespace implementation
