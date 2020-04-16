d8ed57334fb9aee73c7bce4f9520e48645802c81
openvela-robot <openvela-robot@xiaomi.com>
2020-04-17 01:56:02 +0800

    Merge "Update freezing notes." am: d05127d16d
    Change-Id: I8b119bcf8c51da2e5f0589402ba6851b23546cd3

--- tv/tuner/1.0/default/Frontend.cpp
+++ tv/tuner/1.0/default/Frontend.cpp
@@ -41,7 +41,6 @@ Return<Result> Frontend::close() {
     ALOGV("%s", __FUNCTION__);
     // Reset callback
     mCallback = nullptr;
-    mIsLocked = false;
 
     return Result::SUCCESS;
 }
@@ -64,8 +63,10 @@ Return<Result> Frontend::tune(const FrontendSettings& /* settings */) {
         return Result::INVALID_STATE;
     }
 
+    // TODO dynamically allocate file to the source file
+    mSourceStreamFile = FRONTEND_STREAM_FILE;
+
     mCallback->onEvent(FrontendEventType::LOCKED);
-    mIsLocked = false;
     return Result::SUCCESS;
 }
 
@@ -73,38 +74,12 @@ Return<Result> Frontend::stopTune() {
     ALOGV("%s", __FUNCTION__);
 
     mTunerService->frontendStopTune(mId);
-    mIsLocked = false;
 
     return Result::SUCCESS;
 }
 
-Return<Result> Frontend::scan(const FrontendSettings& settings, FrontendScanType type) {
+Return<Result> Frontend::scan(const FrontendSettings& /* settings */, FrontendScanType /* type */) {
     ALOGV("%s", __FUNCTION__);
-
-    if (mType == FrontendType::ATSC) {
-        return Result::SUCCESS;
-    }
-    if (mType != FrontendType::DVBT) {
-        return Result::UNAVAILABLE;
-    }
-
-    FrontendScanMessage msg;
-
-    if (mIsLocked) {
-        msg.isEnd(true);
-        mCallback->onScanMessage(FrontendScanMessageType::END, msg);
-        return Result::SUCCESS;
-    }
-
-    uint32_t frequency = settings.dvbt().frequency;
-    if (type == FrontendScanType::SCAN_BLIND) {
-        frequency += 100;
-    }
-    msg.frequencies({frequency});
-    mCallback->onScanMessage(FrontendScanMessageType::FREQUENCY, msg);
-    msg.isLocked(true);
-    mCallback->onScanMessage(FrontendScanMessageType::LOCKED, msg);
-    mIsLocked = true;
 
     return Result::SUCCESS;
 }
@@ -112,70 +87,14 @@ Return<Result> Frontend::scan(const FrontendSettings& settings, FrontendScanType
 Return<Result> Frontend::stopScan() {
     ALOGV("%s", __FUNCTION__);
 
-    mIsLocked = false;
     return Result::SUCCESS;
 }
 
-Return<void> Frontend::getStatus(const hidl_vec<FrontendStatusType>& statusTypes,
+Return<void> Frontend::getStatus(const hidl_vec<FrontendStatusType>& /* statusTypes */,
                                  getStatus_cb _hidl_cb) {
     ALOGV("%s", __FUNCTION__);
 
     vector<FrontendStatus> statuses;
-    for (int i = 0; i < statusTypes.size(); i++) {
-        FrontendStatusType type = statusTypes[i];
-        FrontendStatus status;
-        // assign randomly selected values for testing.
-        switch (type) {
-            case FrontendStatusType::DEMOD_LOCK: {
-                status.isDemodLocked(true);
-                break;
-            }
-            case FrontendStatusType::SNR: {
-                status.snr(221);
-                break;
-            }
-            case FrontendStatusType::FEC: {
-                status.innerFec(FrontendInnerFec::FEC_2_9);  // value = 1 << 7
-                break;
-            }
-            case FrontendStatusType::MODULATION: {
-                FrontendModulationStatus modulationStatus;
-                modulationStatus.isdbt(FrontendIsdbtModulation::MOD_16QAM);  // value = 1 << 3
-                status.modulation(modulationStatus);
-                break;
-            }
-            case FrontendStatusType::PLP_ID: {
-                status.plpId(101);  // type uint8_t
-                break;
-            }
-            case FrontendStatusType::LAYER_ERROR: {
-                vector<bool> v = {false, true, true};
-                status.isLayerError(v);
-                break;
-            }
-            case FrontendStatusType::ATSC3_PLP_INFO: {
-                vector<FrontendStatusAtsc3PlpInfo> v;
-                FrontendStatusAtsc3PlpInfo info1{
-                        .plpId = 3,
-                        .isLocked = false,
-                        .uec = 313,
-                };
-                FrontendStatusAtsc3PlpInfo info2{
-                        .plpId = 5,
-                        .isLocked = true,
-                        .uec = 515,
-                };
-                v.push_back(info1);
-                v.push_back(info2);
-                status.plpInfo(v);
-                break;
-            }
-            default: {
-                continue;
-            }
-        }
-        statuses.push_back(status);
-    }
     _hidl_cb(Result::SUCCESS, statuses);
 
     return Void();
@@ -202,7 +121,7 @@ FrontendId Frontend::getFrontendId() {
 }
 
 string Frontend::getSourceFile() {
-    return FRONTEND_STREAM_FILE;
+    return mSourceStreamFile;
 }
 
 }  // namespace implementation
