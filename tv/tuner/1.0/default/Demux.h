73019e80f79f7d59333c312bc4c9a2a404b5ce35
openvela-robot <openvela-robot@xiaomi.com>
2020-04-21 11:16:42 +0800

    Merge "Call scan callback for ATSC" into rvc-dev am: 873bfe9eb4
    Change-Id: Ie9012aa05494a5fa7b91ffc78aea7b6f68cb8c7b

--- tv/tuner/1.0/default/Demux.h
+++ tv/tuner/1.0/default/Demux.h
@@ -89,7 +89,6 @@ class Demux : public IDemux {
     void updateFilterOutput(uint16_t filterId, vector<uint8_t> data);
     uint16_t getFilterTpid(uint32_t filterId);
     void setIsRecording(bool isRecording);
-    void startFrontendInputLoop();
 
   private:
     // Tuner service
@@ -105,6 +104,7 @@ class Demux : public IDemux {
         uint32_t filterId;
     };
 
+    Result startFrontendInputLoop();
     static void* __threadLoopFrontend(void* user);
     void frontendInputThreadLoop();
 
