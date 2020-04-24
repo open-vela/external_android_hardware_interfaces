3c5122060353193be563b2f4b31272d46044d7aa
openvela-robot <openvela-robot@xiaomi.com>
2020-04-25 01:01:28 +0800

    [automerger skipped] Merge "android.hardware.tests.lazy@1.0::ILazy" into rvc-dev am: 0239729262 am: 22ec381f68 -s ours am: 12d7b66ed7 -s ours
    am skip reason: Change-Id I710c11c0cb59f04b4f162d627ca0d11135ab1437 with SHA-1 1f9aa386ba is in history
    
    Change-Id: I4a0f536ef9cc077226e9782556d4a2ef12dcbc17

--- tv/tuner/1.0/default/Tuner.h
+++ tv/tuner/1.0/default/Tuner.h
@@ -63,7 +63,6 @@ class Tuner : public ITuner {
 
     void setFrontendAsDemuxSource(uint32_t frontendId, uint32_t demuxId);
 
-    void frontendStartTune(uint32_t frontendId);
     void frontendStopTune(uint32_t frontendId);
 
   private:
