dc4c3b9391df687cf5e055be19eae074f52bf2c6
liujinye <liujinye@xiaomi.com>
2020-04-30 00:42:11 +0800

    Merge "Identity Credential: Pass additional information to HAL." am: 75bea4b6dc am: 560188e55c am: dfb820cd8d am: 3f637f9c09
    Change-Id: I0c6dad9efd9e9c4c065a8cf3a8f272c6a197f39a

--- tv/tuner/1.0/vts/functional/FilterTests.h
+++ tv/tuner/1.0/vts/functional/FilterTests.h
@@ -78,6 +78,9 @@ enum FilterEventType : uint8_t {
 using FilterMQ = MessageQueue<uint8_t, kSynchronizedReadWrite>;
 using MQDesc = MQDescriptorSync<uint8_t>;
 
+const uint32_t FMQ_SIZE_1M = 0x100000;
+const uint32_t FMQ_SIZE_16M = 0x1000000;
+
 #define WAIT_TIMEOUT 3000000000
 
 class FilterCallback : public IFilterCallback {
@@ -150,7 +153,7 @@ class FilterTests {
 
     std::map<uint32_t, sp<FilterCallback>> getFilterCallbacks() { return mFilterCallbacks; }
 
-    AssertionResult openFilterInDemux(DemuxFilterType type, uint32_t bufferSize);
+    AssertionResult openFilterInDemux(DemuxFilterType type);
     AssertionResult getNewlyOpenedFilterId(uint32_t& filterId);
     AssertionResult configFilter(DemuxFilterSettings setting, uint32_t filterId);
     AssertionResult getFilterMQDescriptor(uint32_t filterId);
