4b51d4144fd47e6d51be09271edeaf16be6896c0
liujinye <liujinye@xiaomi.com>
2020-04-30 02:10:19 +0800

    Snap for 6445742 from fd2eb3da79fd4c10271ecc57877a5bb00ad7e5df to sc-release
    Change-Id: I5ca9e026511aa55dda6c2e7a59c39bc157fa7d1f

--- identity/aidl/default/WritableIdentityCredential.h
+++ identity/aidl/default/WritableIdentityCredential.h
@@ -43,9 +43,6 @@ class WritableIdentityCredential : public BnWritableIdentityCredential {
                                                  const vector<uint8_t>& attestationChallenge,
                                                  vector<Certificate>* outCertificateChain) override;
 
-    ndk::ScopedAStatus setExpectedProofOfProvisioningSize(
-            int32_t expectedProofOfProvisioningSize) override;
-
     ndk::ScopedAStatus startPersonalization(int32_t accessControlProfileCount,
                                             const vector<int32_t>& entryCounts) override;
 
@@ -65,7 +62,7 @@ class WritableIdentityCredential : public BnWritableIdentityCredential {
             vector<uint8_t>* outCredentialData,
             vector<uint8_t>* outProofOfProvisioningSignature) override;
 
-  private:
+    // private:
     string docType_;
     bool testCredential_;
 
@@ -85,7 +82,6 @@ class WritableIdentityCredential : public BnWritableIdentityCredential {
     cppbor::Array signedDataAccessControlProfiles_;
     cppbor::Map signedDataNamespaces_;
     cppbor::Array signedDataCurrentNamespace_;
-    size_t expectedProofOfProvisioningSize_;
 
     // This field is initialized in addAccessControlProfile
     set<int32_t> accessControlProfileIds_;
