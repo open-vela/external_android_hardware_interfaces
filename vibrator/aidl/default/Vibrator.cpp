50a53ff38cabc354773d3866fd5404f37961097e
liujinye <liujinye@xiaomi.com>
2020-05-02 00:27:09 +0800

    Merge "Update FrontendInfo for CTS" into rvc-dev

--- vibrator/aidl/default/Vibrator.cpp
+++ vibrator/aidl/default/Vibrator.cpp
@@ -163,10 +163,6 @@ ndk::ScopedAStatus Vibrator::compose(const std::vector<CompositeEffect>& composi
             }
             LOG(INFO) << "triggering primitive " << static_cast<int>(e.primitive) << " @ scale "
                       << e.scale;
-
-            int32_t durationMs;
-            getPrimitiveDuration(e.primitive, &durationMs);
-            usleep(durationMs * 1000);
         }
 
         if (callback != nullptr) {
