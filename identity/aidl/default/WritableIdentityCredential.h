6e077fefd8682ccba063dd1906b2c0619bbd09d8
openvela-robot <openvela-robot@xiaomi.com>
2020-05-02 00:52:00 +0800

    Merge "Update FrontendInfo for CTS" into rvc-dev am: 11c711a434 am: 9d519e45b0
    Change-Id: Ia8f46a10668b283c943a318cd8bb2a8ba2f7b6b1

--- identity/aidl/default/WritableIdentityCredential.h
+++ identity/aidl/default/WritableIdentityCredential.h
@@ -39,12 +39,9 @@ class WritableIdentityCredential : public BnWritableIdentityCredential {
     bool initialize();
 
     // Methods from IWritableIdentityCredential follow.
-    ndk::ScopedAStatus getAttestationCertificate(const vector<uint8_t>& attestationApplicationId,
-                                                 const vector<uint8_t>& attestationChallenge,
+    ndk::ScopedAStatus getAttestationCertificate(const vector<int8_t>& attestationApplicationId,
+                                                 const vector<int8_t>& attestationChallenge,
                                                  vector<Certificate>* outCertificateChain) override;
-
-    ndk::ScopedAStatus setExpectedProofOfProvisioningSize(
-            int32_t expectedProofOfProvisioningSize) override;
 
     ndk::ScopedAStatus startPersonalization(int32_t accessControlProfileCount,
                                             const vector<int32_t>& entryCounts) override;
@@ -58,14 +55,14 @@ class WritableIdentityCredential : public BnWritableIdentityCredential {
                                      const string& nameSpace, const string& name,
                                      int32_t entrySize) override;
 
-    ndk::ScopedAStatus addEntryValue(const vector<uint8_t>& content,
-                                     vector<uint8_t>* outEncryptedContent) override;
+    ndk::ScopedAStatus addEntryValue(const vector<int8_t>& content,
+                                     vector<int8_t>* outEncryptedContent) override;
 
     ndk::ScopedAStatus finishAddingEntries(
-            vector<uint8_t>* outCredentialData,
-            vector<uint8_t>* outProofOfProvisioningSignature) override;
+            vector<int8_t>* outCredentialData,
+            vector<int8_t>* outProofOfProvisioningSignature) override;
 
-  private:
+    // private:
     string docType_;
     bool testCredential_;
 
@@ -85,7 +82,6 @@ class WritableIdentityCredential : public BnWritableIdentityCredential {
     cppbor::Array signedDataAccessControlProfiles_;
     cppbor::Map signedDataNamespaces_;
     cppbor::Array signedDataCurrentNamespace_;
-    size_t expectedProofOfProvisioningSize_;
 
     // This field is initialized in addAccessControlProfile
     set<int32_t> accessControlProfileIds_;
