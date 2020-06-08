eb64d7ed28d509604f1ea6aaf429e69dff696e41
openvela-robot <openvela-robot@xiaomi.com>
2020-06-08 14:54:41 +0800

    Remove the enforce to build 32 binary
    cherry-pick from internal master
    
    bug: 157096583
    Test: Manual
    Change-Id: Ice58cac7adf46f17606be8c4b2c3fe2e4bf1a472
    Merged-In: Ice58cac7adf46f17606be8c4b2c3fe2e4bf1a472

--- tv/tuner/1.0/default/Demux.h
+++ tv/tuner/1.0/default/Demux.h
@@ -89,7 +89,6 @@ class Demux : public IDemux {
     void updateFilterOutput(uint16_t filterId, vector<uint8_t> data);
     uint16_t getFilterTpid(uint32_t filterId);
     void setIsRecording(bool isRecording);
-    void startFrontendInputLoop();
 
   private:
     // Tuner service
@@ -105,6 +104,7 @@ class Demux : public IDemux {
         uint32_t filterId;
     };
 
+    Result startFrontendInputLoop();
     static void* __threadLoopFrontend(void* user);
     void frontendInputThreadLoop();
 
@@ -130,7 +130,6 @@ class Demux : public IDemux {
 
     uint32_t mDemuxId;
     uint32_t mCiCamId;
-    set<uint32_t> mPcrFilterIds;
     /**
      * Record the last used filter id. Initial value is -1.
      * Filter Id starts with 0.
@@ -142,6 +141,13 @@ class Demux : public IDemux {
      */
     set<uint32_t> mUsedFilterIds;
     /**
+     * Record all the unused filter Ids within mLastUsedFilterId.
+     * Removed filter Id should be added into this set.
+     * When this set is not empty, ids here should be allocated first
+     * and added into usedFilterIds.
+     */
+    set<uint32_t> mUnusedFilterIds;
+    /**
      * Record all the attached record filter Ids.
      * Any removed filter id should be removed from this set.
      */
@@ -151,11 +157,6 @@ class Demux : public IDemux {
      * The array number is the filter ID.
      */
     std::map<uint32_t, sp<Filter>> mFilters;
-
-    /**
-     * Local reference to the opened Timer Filter instance.
-     */
-    sp<TimeFilter> mTimeFilter;
 
     /**
      * Local reference to the opened DVR object.
@@ -187,7 +188,7 @@ class Demux : public IDemux {
     int mPesSizeLeft = 0;
     vector<uint8_t> mPesOutput;
 
-    const bool DEBUG_DEMUX = false;
+    const bool DEBUG_FILTER = false;
 };
 
 }  // namespace implementation
