0b92304c7bac3b5daf394b73683c9b1b2d0008b0
openvela-robot <openvela-robot@xiaomi.com>
2020-06-08 19:33:02 +0800

    Merge "Camera: Clarify ERROR_REQUEST callback" into rvc-dev am: ee71769ebc am: 4f382fe9e8
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/11689356
    
    Change-Id: I13d122ba618a84236663b04408bf0d1812c9a108

--- tv/tuner/1.0/vts/functional/VtsHalTvTunerV1_0TestConfigurations.h
+++ tv/tuner/1.0/vts/functional/VtsHalTvTunerV1_0TestConfigurations.h
@@ -99,7 +99,6 @@ typedef enum {
 
 typedef enum {
     LNB0,
-    LNB_EXTERNAL,
     LNB_MAX,
 } Lnb;
 
@@ -141,7 +140,6 @@ struct FrontendConfig {
 
 struct LnbConfig {
     bool usingLnb;
-    string name;
     LnbVoltage voltage;
     LnbTone tone;
     LnbPosition position;
@@ -228,11 +226,6 @@ inline void initLnbConfig() {
     lnbArray[LNB0].voltage = LnbVoltage::VOLTAGE_12V;
     lnbArray[LNB0].tone = LnbTone::NONE;
     lnbArray[LNB0].position = LnbPosition::UNDEFINED;
-    lnbArray[LNB_EXTERNAL].usingLnb = true;
-    lnbArray[LNB_EXTERNAL].name = "default_lnb_external";
-    lnbArray[LNB_EXTERNAL].voltage = LnbVoltage::VOLTAGE_5V;
-    lnbArray[LNB_EXTERNAL].tone = LnbTone::NONE;
-    lnbArray[LNB_EXTERNAL].position = LnbPosition::UNDEFINED;
 };
 
 /** Diseqc messages array for the Lnb test */
