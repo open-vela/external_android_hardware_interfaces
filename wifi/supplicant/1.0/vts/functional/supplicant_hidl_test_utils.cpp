a9800267274f72116ca56c88edca4d2aef5c9c86
liujinye <liujinye@xiaomi.com>
2020-06-09 10:42:12 +0800

    Allow compilations in TOCTOU tests to fail with GENERAL_FAILURE.
    A compilation failure is not related to the security aspect of the
    TOCTOU test, but it will skip one iteration of security testing. This CL
    allows the compilation to fail with GENERAL_FAILURE in TOCTOU tests, and
    issues a retry once it happens to ensure enough test coverage.
    
    Bug: 157489048
    Test: 1.2/1.3 VTS
    Change-Id: Idc88e0365c5d2799187093b6fd7b4abf8f8b463d

--- wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
+++ wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#include <VtsHalHidlTargetTestBase.h>
 #include <android-base/logging.h>
 #include <cutils/properties.h>
 
@@ -50,31 +49,31 @@ using ::android::hidl::manager::V1_0::IServiceNotification;
 using ::android::wifi_system::InterfaceTool;
 using ::android::wifi_system::SupplicantManager;
 
-extern WifiSupplicantHidlEnvironment* gEnv;
-
 namespace {
 
 // Helper function to initialize the driver and firmware to STA mode
 // using the vendor HAL HIDL interface.
-void initilializeDriverAndFirmware() {
-    if (getWifi() != nullptr) {
-        sp<IWifiChip> wifi_chip = getWifiChip();
-        ChipModeId mode_id;
-        EXPECT_TRUE(configureChipToSupportIfaceType(
-            wifi_chip, ::android::hardware::wifi::V1_0::IfaceType::STA, &mode_id));
-    } else {
-        LOG(WARNING) << __func__ << ": Vendor HAL not supported";
+void initilializeDriverAndFirmware(const std::string& wifi_instance_name) {
+    // Skip if wifi instance is not set.
+    if (wifi_instance_name == "") {
+        return;
     }
+
+    sp<IWifiChip> wifi_chip = getWifiChip(wifi_instance_name);
+    ChipModeId mode_id;
+    EXPECT_TRUE(configureChipToSupportIfaceType(
+        wifi_chip, ::android::hardware::wifi::V1_0::IfaceType::STA, &mode_id));
 }
 
 // Helper function to deinitialize the driver and firmware
 // using the vendor HAL HIDL interface.
-void deInitilializeDriverAndFirmware() {
-    if (getWifi() != nullptr) {
-        stopWifi();
-    } else {
-        LOG(WARNING) << __func__ << ": Vendor HAL not supported";
+void deInitilializeDriverAndFirmware(const std::string& wifi_instance_name) {
+    // Skip if wifi instance is not set.
+    if (wifi_instance_name == "") {
+        return;
     }
+
+    stopWifi(wifi_instance_name);
 }
 
 // Helper function to find any iface of the desired type exposed.
@@ -164,28 +163,32 @@ class ServiceNotificationListener : public IServiceNotification {
     std::condition_variable condition_;
 };
 
-void stopSupplicant() {
+void stopSupplicant() { stopSupplicant(""); }
+
+void stopSupplicant(const std::string& wifi_instance_name) {
     SupplicantManager supplicant_manager;
 
     ASSERT_TRUE(supplicant_manager.StopSupplicant());
-    deInitilializeDriverAndFirmware();
+    deInitilializeDriverAndFirmware(wifi_instance_name);
     ASSERT_FALSE(supplicant_manager.IsSupplicantRunning());
 }
 
-void startSupplicantAndWaitForHidlService() {
-    initilializeDriverAndFirmware();
+void startSupplicantAndWaitForHidlService(
+    const std::string& wifi_instance_name,
+    const std::string& supplicant_instance_name) {
+    initilializeDriverAndFirmware(wifi_instance_name);
 
     android::sp<ServiceNotificationListener> notification_listener =
         new ServiceNotificationListener();
-    string service_name = gEnv->getServiceName<ISupplicant>();
     ASSERT_TRUE(notification_listener->registerForHidlServiceNotifications(
-        service_name));
+        supplicant_instance_name));
 
     SupplicantManager supplicant_manager;
     ASSERT_TRUE(supplicant_manager.StartSupplicant());
     ASSERT_TRUE(supplicant_manager.IsSupplicantRunning());
 
-    ASSERT_TRUE(notification_listener->waitForHidlService(500, service_name));
+    ASSERT_TRUE(notification_listener->waitForHidlService(
+        500, supplicant_instance_name));
 }
 
 bool is_1_1(const sp<ISupplicant>& supplicant) {
@@ -228,22 +231,22 @@ void addSupplicantP2pIface_1_1(const sp<ISupplicant>& supplicant) {
         });
 }
 
-sp<ISupplicant> getSupplicant() {
+sp<ISupplicant> getSupplicant(const std::string& supplicant_instance_name,
+                              bool isP2pOn) {
     sp<ISupplicant> supplicant =
-        ::testing::VtsHalHidlTargetTestBase::getService<ISupplicant>(
-            gEnv->getServiceName<ISupplicant>());
+        ISupplicant::getService(supplicant_instance_name);
     // For 1.1 supplicant, we need to add interfaces at initialization.
     if (is_1_1(supplicant)) {
         addSupplicantStaIface_1_1(supplicant);
-        if (gEnv->isP2pOn) {
+        if (isP2pOn) {
             addSupplicantP2pIface_1_1(supplicant);
         }
     }
     return supplicant;
 }
 
-sp<ISupplicantStaIface> getSupplicantStaIface() {
-    sp<ISupplicant> supplicant = getSupplicant();
+sp<ISupplicantStaIface> getSupplicantStaIface(
+    const sp<ISupplicant>& supplicant) {
     if (!supplicant.get()) {
         return nullptr;
     }
@@ -267,8 +270,9 @@ sp<ISupplicantStaIface> getSupplicantStaIface() {
     return sta_iface;
 }
 
-sp<ISupplicantStaNetwork> createSupplicantStaNetwork() {
-    sp<ISupplicantStaIface> sta_iface = getSupplicantStaIface();
+sp<ISupplicantStaNetwork> createSupplicantStaNetwork(
+    const sp<ISupplicant>& supplicant) {
+    sp<ISupplicantStaIface> sta_iface = getSupplicantStaIface(supplicant);
     if (!sta_iface.get()) {
         return nullptr;
     }
@@ -288,8 +292,8 @@ sp<ISupplicantStaNetwork> createSupplicantStaNetwork() {
     return sta_network;
 }
 
-sp<ISupplicantP2pIface> getSupplicantP2pIface() {
-    sp<ISupplicant> supplicant = getSupplicant();
+sp<ISupplicantP2pIface> getSupplicantP2pIface(
+    const sp<ISupplicant>& supplicant) {
     if (!supplicant.get()) {
         return nullptr;
     }
@@ -313,8 +317,7 @@ sp<ISupplicantP2pIface> getSupplicantP2pIface() {
     return p2p_iface;
 }
 
-bool turnOnExcessiveLogging() {
-    sp<ISupplicant> supplicant = getSupplicant();
+bool turnOnExcessiveLogging(const sp<ISupplicant>& supplicant) {
     if (!supplicant.get()) {
         return false;
     }
