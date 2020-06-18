d34ad352bf704287b823c4f229a1af6ead8ce98a
liujinye <liujinye@xiaomi.com>
2020-06-19 02:17:16 +0800

    Merge "Added DISABLED flag to users flag for HAL" into rvc-dev am: 110f2bef7f am: 2334a510a5
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/11870725
    
    Change-Id: Idcfe4eb7840acc4d03bb9e6d6f980f384e070de4

--- automotive/can/1.0/default/libnetdevice/NetlinkSocket.h
+++ automotive/can/1.0/default/libnetdevice/NetlinkSocket.h
@@ -43,7 +43,7 @@ struct NetlinkSocket {
     template <class T, unsigned int BUFSIZE>
     bool send(NetlinkRequest<T, BUFSIZE>& req) {
         if (!req.isGood()) return false;
-        return send(req.header(), req.totalLength);
+        return send(req.header());
     }
 
     /**
@@ -54,13 +54,11 @@ struct NetlinkSocket {
     bool receiveAck();
 
   private:
-    const int mProtocol;
-
     uint32_t mSeq = 0;
     base::unique_fd mFd;
     bool mFailed = false;
 
-    bool send(struct nlmsghdr* msg, size_t totalLen);
+    bool send(struct nlmsghdr* msg);
 
     DISALLOW_COPY_AND_ASSIGN(NetlinkSocket);
 };
