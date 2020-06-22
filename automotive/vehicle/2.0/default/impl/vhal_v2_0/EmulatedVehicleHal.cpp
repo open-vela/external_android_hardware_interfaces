8b701ecd5a9b37b28336a10275431afee95101bc
openvela-robot <openvela-robot@xiaomi.com>
2020-06-23 01:02:46 +0800

    Snap for 6617762 from 5f86b5140842cc8d60b79ef8962d75bb143751cd to rvc-release
    Change-Id: I112e61495f9f9e947c5f58945f8e5091f5204ef7

--- automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.cpp
+++ automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.cpp
@@ -153,7 +153,7 @@ VehicleHal::VehiclePropValuePtr EmulatedVehicleHal::get(
         default:
             if (mEmulatedUserHal != nullptr && mEmulatedUserHal->isSupported(propId)) {
                 ALOGI("get(): getting value for prop %d from User HAL", propId);
-                const auto& ret = mEmulatedUserHal->onGetProperty(requestedPropValue);
+                const auto& ret = mEmulatedUserHal->onGetProperty(propId);
                 if (!ret.ok()) {
                     ALOGE("get(): User HAL returned error: %s", ret.error().message().c_str());
                     *outStatus = StatusCode(ret.error().code());
