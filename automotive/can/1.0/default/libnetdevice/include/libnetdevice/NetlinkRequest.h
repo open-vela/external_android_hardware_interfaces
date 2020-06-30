2c69d473afe6c46541e4f3b405e76558461e0d08
liujinye <liujinye@xiaomi.com>
2020-07-01 02:14:51 +0800

    Snap for 6643616 from d73ef0aeabc61cf83314e6959f0095d3ff0c85b2 to sc-release
    Change-Id: I7f95d05b23409eeeb15cae77cff46ba5c9f34904

--- automotive/can/1.0/default/libnetdevice/include/libnetdevice/NetlinkRequest.h
+++ automotive/can/1.0/default/libnetdevice/include/libnetdevice/NetlinkRequest.h
@@ -16,9 +16,9 @@
 
 #pragma once
 
-#include <android-base/macros.h>
-#include <libnetdevice/types.h>
+#include "types.h"
 
+#include <android-base/macros.h>
 #include <linux/rtnetlink.h>
 
 #include <string>
