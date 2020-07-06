c8f164ed7a93ea9230e84e9160bc148a39efc41d
liujinye <liujinye@xiaomi.com>
2020-07-07 02:31:44 +0800

    Merge "Keymaster: Fix potential bug in extractUint32()/extractUint64()"

--- radio/1.0/vts/functional/radio_hidl_hal_misc.cpp
+++ radio/1.0/vts/functional/radio_hidl_hal_misc.cpp
@@ -644,7 +644,7 @@ TEST_P(RadioHidlTest, nvResetConfig) {
     LOG(DEBUG) << "nvResetConfig";
     serial = GetRandomSerialNumber();
 
-    radio->nvResetConfig(serial, ResetNvType::FACTORY_RESET);
+    radio->nvResetConfig(serial, ResetNvType::ERASE);
     EXPECT_EQ(std::cv_status::no_timeout, wait());
     EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
     EXPECT_EQ(serial, radioRsp->rspInfo.serial);
