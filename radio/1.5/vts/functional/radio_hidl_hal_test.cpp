f761e34de36e4a72bbf7869ed7f2f8e6edd0f832
liujinye <liujinye@xiaomi.com>
2020-07-22 00:20:56 +0800

    Audio VTS: Suppress empty parameter list failures am: b9743d4728
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1368656
    
    Change-Id: I85b1f7119fd8034be07ea703b274b4eb1ba5fef7

--- radio/1.5/vts/functional/radio_hidl_hal_test.cpp
+++ radio/1.5/vts/functional/radio_hidl_hal_test.cpp
@@ -78,9 +78,3 @@ void RadioHidlTest_v1_5::updateSimCardStatus() {
     radio_v1_5->getIccCardStatus(serial);
     EXPECT_EQ(std::cv_status::no_timeout, wait());
 }
-
-void RadioHidlTest_v1_5::stopNetworkScan() {
-    serial = GetRandomSerialNumber();
-    radio_v1_5->stopNetworkScan(serial);
-    EXPECT_EQ(std::cv_status::no_timeout, wait());
-}
