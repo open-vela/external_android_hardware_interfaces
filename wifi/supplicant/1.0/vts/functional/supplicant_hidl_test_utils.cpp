25b3833de5945e632ddf2bfcafee203ce4f357d0
liujinye <liujinye@xiaomi.com>
2020-07-22 11:14:44 +0800

    Disable the test TooManyOperations in keymaster VTS
    The test fails on devices because an unknown
    client starts a keymaster BEGIN operation during
    bootup but does not finish it. This affects the
    keymaster hardware implementation's capability
    to support the maximum possible operations while
    running this test.
    
    Bug: 154801042
    
    Change-Id: Ib6adc6c28ebe76ddfdc2c66cd17cf78c04e5b468
    (cherry picked from commit 48f67e8438570eaaba8a4a5e99241ab9fd52293f)

--- wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
+++ wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
@@ -52,26 +52,24 @@ namespace {
 // Helper function to initialize the driver and firmware to STA mode
 // using the vendor HAL HIDL interface.
 void initilializeDriverAndFirmware(const std::string& wifi_instance_name) {
-    // Skip if wifi instance is not set.
-    if (wifi_instance_name == "") {
-        return;
+    if (getWifi(wifi_instance_name) != nullptr) {
+        sp<IWifiChip> wifi_chip = getWifiChip(wifi_instance_name);
+        ChipModeId mode_id;
+        EXPECT_TRUE(configureChipToSupportIfaceType(
+            wifi_chip, ::android::hardware::wifi::V1_0::IfaceType::STA, &mode_id));
+    } else {
+        LOG(WARNING) << __func__ << ": Vendor HAL not supported";
     }
-
-    sp<IWifiChip> wifi_chip = getWifiChip(wifi_instance_name);
-    ChipModeId mode_id;
-    EXPECT_TRUE(configureChipToSupportIfaceType(
-        wifi_chip, ::android::hardware::wifi::V1_0::IfaceType::STA, &mode_id));
 }
 
 // Helper function to deinitialize the driver and firmware
 // using the vendor HAL HIDL interface.
 void deInitilializeDriverAndFirmware(const std::string& wifi_instance_name) {
-    // Skip if wifi instance is not set.
-    if (wifi_instance_name == "") {
-        return;
+    if (getWifi(wifi_instance_name) != nullptr) {
+        stopWifi(wifi_instance_name);
+    } else {
+        LOG(WARNING) << __func__ << ": Vendor HAL not supported";
     }
-
-    stopWifi(wifi_instance_name);
 }
 
 // Helper function to find any iface of the desired type exposed.
