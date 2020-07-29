fef570c7788de7966eb658de2257c92d8880e0c3
liujinye <liujinye@xiaomi.com>
2020-07-29 20:56:18 +0800

    Merge "wifi(vts): Use blocking getService to wait for service to come up" into rvc-dev am: 5afe29b247 am: 1bf88e14c3
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/12204704
    
    Change-Id: I480f527cef71fbfe349129d95f94ac8507e8df2e

--- camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
+++ camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
@@ -7584,7 +7584,6 @@ void CameraHidlTest::verifyRequestTemplate(const camera_metadata_t* metadata,
     }
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(CameraHidlTest);
 INSTANTIATE_TEST_SUITE_P(
         PerInstance, CameraHidlTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(ICameraProvider::descriptor)),
