6ac5d2a96def9e036c29267c7beb9044d5470b0c
openvela-robot <openvela-robot@xiaomi.com>
2020-08-04 02:26:11 +0800

    Merge "Move HIDL interfaces not included in mainline_system.mk to system_ext"

--- keymaster/4.1/vts/functional/Keymaster4_1HidlTest.h
+++ keymaster/4.1/vts/functional/Keymaster4_1HidlTest.h
@@ -115,9 +115,7 @@ class Keymaster4_1HidlTest : public V4_0::test::KeymasterHidlTest {
     ErrorCode UseHmacKey(const HidlBuf& hmacKeyBlob) {
         auto [result, mac, out_params] =
                 ProcessMessage(hmacKeyBlob, KeyPurpose::SIGN, "1234567890123456",
-                               AuthorizationSetBuilder()
-                                       .Authorization(TAG_MAC_LENGTH, 128)
-                                       .Digest(Digest::SHA_2_256));
+                               AuthorizationSetBuilder().Authorization(TAG_MAC_LENGTH, 128));
         return result;
     }
 
