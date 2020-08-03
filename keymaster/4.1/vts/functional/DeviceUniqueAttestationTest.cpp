d4546e623c66eca427140482ed499bb1a918c749
openvela-robot <openvela-robot@xiaomi.com>
2020-08-03 15:06:23 +0800

    Netlink socket refactoring
    - merge two send() methods into one
    - use internal receive buffer instead of asking user to supply one
    - move setting sequence number to MessageFactory sending code
    - don't limit send function to Kernel as a recipient
    - move adding NLM_F_ACK to the caller side
    - getSocketPid -> getPid
    - unsigned int -> unsigned
    
    One part missing is refactoring receiveAck (b/161389935).
    
    Bug: 162032964
    Test: canhalctrl up test virtual vcan3
    Change-Id: Ie3d460dbc2ea1251469bf08504cfe2c6e80bbe75

--- keymaster/4.1/vts/functional/DeviceUniqueAttestationTest.cpp
+++ keymaster/4.1/vts/functional/DeviceUniqueAttestationTest.cpp
@@ -216,6 +216,7 @@ TEST_P(DeviceUniqueAttestationTest, NonStrongBoxOnly) {
                               .Authorization(TAG_ATTESTATION_CHALLENGE, HidlBuf("challenge"))
                               .Authorization(TAG_ATTESTATION_APPLICATION_ID, HidlBuf("foo")),
                       &cert_chain)));
+    CheckedDeleteKey();
 }
 
 TEST_P(DeviceUniqueAttestationTest, Rsa) {
