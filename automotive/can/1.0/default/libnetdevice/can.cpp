06db09313dd5846986f35d1a96d148e54a5c4819
openvela-robot <openvela-robot@xiaomi.com>
2020-08-05 23:01:34 +0800

    [automerger skipped] Suppress gtest error for tests without any instance am: 4a91699105 am: 18833972f8 am: 5555914fd7 am: fdc35e208d am: 042b0f020a -s ours
    am skip reason: Change-Id I58bc808a82b2128bb8774f762f9b9fcecd2614fb with SHA-1 ff985a85a8 is in history
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1378657
    
    Change-Id: I1854c612a560a2bbf290db2f1e2d45bd1605b7bc

--- automotive/can/1.0/default/libnetdevice/can.cpp
+++ automotive/can/1.0/default/libnetdevice/can.cpp
@@ -14,19 +14,20 @@
  * limitations under the License.
  */
 
-#include <libnetdevice/libnetdevice.h>
+#include <libnetdevice/can.h>
 
-#include "NetlinkRequest.h"
-#include "NetlinkSocket.h"
 #include "common.h"
 
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
+#include <libnl++/MessageFactory.h>
+#include <libnl++/Socket.h>
 
 #include <linux/can.h>
 #include <linux/can/error.h>
 #include <linux/can/netlink.h>
 #include <linux/can/raw.h>
+#include <linux/rtnetlink.h>
 
 namespace android::netdevice::can {
 
@@ -69,7 +70,7 @@ bool setBitrate(std::string ifname, uint32_t bitrate) {
     struct can_bittiming bt = {};
     bt.bitrate = bitrate;
 
-    NetlinkRequest<struct ifinfomsg> req(RTM_NEWLINK, NLM_F_REQUEST);
+    nl::MessageFactory<struct ifinfomsg> req(RTM_NEWLINK, NLM_F_REQUEST | NLM_F_ACK);
 
     const auto ifidx = nametoindex(ifname);
     if (ifidx == 0) {
@@ -89,8 +90,8 @@ bool setBitrate(std::string ifname, uint32_t bitrate) {
         }
     }
 
-    NetlinkSocket sock(NETLINK_ROUTE);
-    return sock.send(req) && sock.receiveAck();
+    nl::Socket sock(NETLINK_ROUTE);
+    return sock.send(req) && sock.receiveAck(req);
 }
 
 }  // namespace android::netdevice::can
