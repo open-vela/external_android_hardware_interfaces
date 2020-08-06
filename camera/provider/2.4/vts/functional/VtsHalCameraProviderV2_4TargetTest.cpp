79d107948c480eeb55391f346f8e33b478123880
openvela-robot <openvela-robot@xiaomi.com>
2020-08-06 23:22:35 +0800

    Implement NNAPI canonical interfaces
    This CL implements the canonical IDevice, IPreparedModel, and IBuffer
    interfaces for the 1.0, 1.1, 1.2, and 1.3 NN HIDL HAL interfaces.
    Further, it introduces "Resilient" adapter interfaces to automatically
    retrieve a handle to a recovered interface object after it has died and
    rebooted.
    
    This CL also updates the conversion code from returning nn::Result to
    nn::GeneralResult, which includes a ErrorStatus code in the case of an
    error.
    
    Finally, this CL introduces a new static library
    neuralnetworks_utils_hal_service which consists of a single function
    ::android::nn::hal::getDevices which can be used by the NNAPI runtime to
    retrieve the HIDL services without knowing the underlying HIDL types.
    
    Bug: 160668438
    Test: mma
    Test: NeuralNetworksTest_static
    Change-Id: Iec6ae739df196b4034ffb35ea76781fd541ffec3
    Merged-In: Iec6ae739df196b4034ffb35ea76781fd541ffec3
    (cherry picked from commit 3670c385c4b12aef975ab67e5d2b0f5fe79134c2)

--- camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
+++ camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
@@ -7592,7 +7592,6 @@ void CameraHidlTest::verifyRequestTemplate(const camera_metadata_t* metadata,
     }
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(CameraHidlTest);
 INSTANTIATE_TEST_SUITE_P(
         PerInstance, CameraHidlTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(ICameraProvider::descriptor)),
