70debc01e02b183d66f530181cb97c525f0a04c7
openvela-robot <openvela-robot@xiaomi.com>
2020-08-06 23:15:55 +0800

    Snap for 6743184 from b461849e98112f3919b9c2ac5a8af8801ffc097d to rvc-qpr1-release
    Change-Id: Ib49b3dee08508387e53800f87a6074544ae2bac2

--- automotive/can/1.0/default/libnetdevice/can.cpp
+++ automotive/can/1.0/default/libnetdevice/can.cpp
@@ -14,20 +14,19 @@
  * limitations under the License.
  */
 
-#include <libnetdevice/can.h>
+#include <libnetdevice/libnetdevice.h>
 
+#include "NetlinkRequest.h"
+#include "NetlinkSocket.h"
 #include "common.h"
 
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
-#include <libnl++/MessageFactory.h>
-#include <libnl++/Socket.h>
 
 #include <linux/can.h>
 #include <linux/can/error.h>
 #include <linux/can/netlink.h>
 #include <linux/can/raw.h>
-#include <linux/rtnetlink.h>
 
 namespace android::netdevice::can {
 
@@ -70,7 +69,7 @@ bool setBitrate(std::string ifname, uint32_t bitrate) {
     struct can_bittiming bt = {};
     bt.bitrate = bitrate;
 
-    nl::MessageFactory<struct ifinfomsg> req(RTM_NEWLINK, NLM_F_REQUEST | NLM_F_ACK);
+    NetlinkRequest<struct ifinfomsg> req(RTM_NEWLINK, NLM_F_REQUEST);
 
     const auto ifidx = nametoindex(ifname);
     if (ifidx == 0) {
@@ -90,8 +89,8 @@ bool setBitrate(std::string ifname, uint32_t bitrate) {
         }
     }
 
-    nl::Socket sock(NETLINK_ROUTE);
-    return sock.send(req) && sock.receiveAck(req);
+    NetlinkSocket sock(NETLINK_ROUTE);
+    return sock.send(req) && sock.receiveAck();
 }
 
 }  // namespace android::netdevice::can
