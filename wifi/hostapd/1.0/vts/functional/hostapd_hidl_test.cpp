e3fcf4ad3a585011c1fbeffd8f8143ce3078400d
liujinye <liujinye@xiaomi.com>
2020-08-11 23:23:16 +0800

    audio: Add example HAL implementation
    This is partial implementation of the example V7.0 audio HAL
    which passes VTS tests. Note that the 'core' part
    of the HAL (IDevice/IStream) isn't implemented yet.
    It passes VTS because it doesn't provide any devices
    (modules) and the audio HAL isn't the 'default' instance.
    
    Bug: 142480271
    Test: atest VtsHalAudioV7_0TargetTest
          atest VtsHalAudioEffectV7_0TargetTest
    Change-Id: Ie3dd62c5db1cdb5534df4dd7f326c4c8776bf3c4

--- wifi/hostapd/1.0/vts/functional/hostapd_hidl_test.cpp
+++ wifi/hostapd/1.0/vts/functional/hostapd_hidl_test.cpp
@@ -145,8 +145,8 @@ TEST_P(HostapdHidlTest, Create) {
     stopHostapd(wifi_instance_name_);
     startHostapdAndWaitForHidlService(wifi_instance_name_,
                                       hostapd_instance_name_);
-    hostapd_ = IHostapd::getService(hostapd_instance_name_);
-    EXPECT_NE(nullptr, hostapd_.get());
+    sp<IHostapd> hostapd = IHostapd::getService(hostapd_instance_name_);
+    EXPECT_NE(nullptr, hostapd.get());
 }
 
 /**
