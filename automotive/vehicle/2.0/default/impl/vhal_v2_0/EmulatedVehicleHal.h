bf20e14b73bb8510e0807dd1a68e487304a839a4
liujinye <liujinye@xiaomi.com>
2020-08-12 14:40:42 +0800

    Merge "Fix double release of ScopedWakelock" into rvc-qpr-dev am: 74896bed49
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/12351750
    
    Change-Id: I2478bae593f72eec1f9722675226c39712aa306a

--- automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.h
+++ automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.h
@@ -63,6 +63,7 @@ public:
     //  Methods from EmulatedVehicleHalIface
     bool setPropertyFromVehicle(const VehiclePropValue& propValue) override;
     std::vector<VehiclePropValue> getAllProperties() const override;
+    void getAllPropertiesOverride();
 
 private:
     constexpr std::chrono::nanoseconds hertzToNanoseconds(float hz) const {
@@ -87,6 +88,9 @@ private:
     std::unordered_set<int32_t> mHvacPowerProps;
     RecurrentTimer mRecurrentTimer;
     VehicleHalClient* mVehicleClient;
+    bool mInEmulator;
+    bool mInitVhalValueOverride;
+    std::vector<VehiclePropValue> mVehiclePropertiesOverride;
     EmulatedUserHal* mEmulatedUserHal;
 };
 
