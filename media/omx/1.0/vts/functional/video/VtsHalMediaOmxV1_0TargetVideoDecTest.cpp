1e8d0e84e3f73b35754cddaca6ff8f5de450bae1
openvela-robot <openvela-robot@xiaomi.com>
2020-08-26 14:45:16 +0800

    vts: Fix incorrect instance name in Teardown am: a6cc5825e9 am: 9ce5301add
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/12422070
    
    Change-Id: I26ba62bca03c462eb95df80f65104e5ed8409771

--- media/omx/1.0/vts/functional/video/VtsHalMediaOmxV1_0TargetVideoDecTest.cpp
+++ media/omx/1.0/vts/functional/video/VtsHalMediaOmxV1_0TargetVideoDecTest.cpp
@@ -1419,7 +1419,6 @@ TEST_P(VideoDecHidlTest, FlushTest) {
                                                     kPortIndexOutput));
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(VideoDecHidlTest);
 INSTANTIATE_TEST_SUITE_P(PerInstance, VideoDecHidlTest, testing::ValuesIn(kTestParameters),
                          android::hardware::PrintInstanceTupleNameToString<>);
 
