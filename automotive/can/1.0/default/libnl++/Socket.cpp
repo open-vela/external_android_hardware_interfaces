20e7d3688eaee4f684971325ddfeb65bf912c5f4
openvela-robot <openvela-robot@xiaomi.com>
2020-09-08 23:33:38 +0800

    Merge "Suppress gtest error for tests without any instance"

--- automotive/can/1.0/default/libnl++/Socket.cpp
+++ automotive/can/1.0/default/libnl++/Socket.cpp
@@ -153,10 +153,6 @@ std::optional<unsigned> Socket::getPid() {
     return sa.nl_pid;
 }
 
-pollfd Socket::preparePoll(short events) {
-    return {mFd.get(), events, 0};
-}
-
 Socket::receive_iterator::receive_iterator(Socket& socket, bool end)
     : mSocket(socket), mIsEnd(end) {
     if (!end) receive();
