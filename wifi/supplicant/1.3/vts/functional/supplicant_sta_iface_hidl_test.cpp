a5dd2a7c881a1946056533fadc9905727be3310a
liujinye <liujinye@xiaomi.com>
2020-09-11 02:02:41 +0800

    [automerger skipped] Merge Android R am: 1136876068 -s ours am: e8d98e4d14 -s ours
    am skip reason: Change-Id I33c8b79d82aaaecd05cc1c74f6c6a8613c42fa68 with SHA-1 f9d897c0ce is in history
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1422435
    
    Change-Id: I229165230cd1f6597b8126cb4f941dc8b47437ed

--- wifi/supplicant/1.3/vts/functional/supplicant_sta_iface_hidl_test.cpp
+++ wifi/supplicant/1.3/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -353,11 +353,7 @@ TEST_P(SupplicantStaIfaceHidlTest, GetConnectionCapabilities) {
     sta_iface_->getConnectionCapabilities(
         [&](const SupplicantStatus& status,
             ConnectionCapabilities /* capabilities */) {
-            // Since getConnectionCapabilities() is overridden by an
-            // upgraded API in newer HAL versions, allow for FAILURE_UNKNOWN
-            if (status.code != SupplicantStatusCode::FAILURE_UNKNOWN) {
-                EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
-            }
+            EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
         });
 }
 
@@ -581,7 +577,6 @@ TEST_P(SupplicantStaIfaceHidlTest, FilsHlpFlushRequest) {
         EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
     });
 }
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantStaIfaceHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantStaIfaceHidlTest,
     testing::Combine(
