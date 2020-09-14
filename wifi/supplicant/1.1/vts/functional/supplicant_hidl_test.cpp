9a77f3542c0c1f05179a8d06d4861038532b5163
openvela-robot <openvela-robot@xiaomi.com>
2020-09-15 07:24:42 +0800

    Update EmulatedUserHal to use UserHalHelper native library. am: 8eb03223e9
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1425312
    
    Change-Id: Ifbdbedbccafe23bf9f4c4b257310c6038d341b2a

--- wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
+++ wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
@@ -35,9 +35,9 @@ using ::android::hardware::wifi::supplicant::V1_0::IfaceType;
 using ::android::hardware::wifi::supplicant::V1_1::ISupplicant;
 using ::android::sp;
 
-class SupplicantHidlTest : public SupplicantHidlTestBaseV1_1 {
+class SupplicantHidlTest : public SupplicantHidlTestBase {
    public:
-    virtual void SetUp() override { SupplicantHidlTestBaseV1_1::SetUp(); }
+    virtual void SetUp() override { SupplicantHidlTestBase::SetUp(); }
 
    protected:
     std::string getWlan0IfaceName() {
