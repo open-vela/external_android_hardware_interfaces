cd469b98f0690ed3b1b6de8ca043721e73ec39f9
liujinye <liujinye@xiaomi.com>
2020-09-14 12:03:24 +0800

    Add Tuner HAL VTS tests into the exception list of the uninstantiated parameterized tests
    Test: atest VtsHalTvTunerV1_0TargetTest
    Bug: 168345664
    Change-Id: I1fabb01ea308c286d2b3f29d520afc7518bb2d8a

--- wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
+++ wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
@@ -56,14 +56,11 @@ void initilializeDriverAndFirmware(const std::string& wifi_instance_name) {
     if (wifi_instance_name == "") {
         return;
     }
-    if (getWifi(wifi_instance_name) != nullptr) {
-        sp<IWifiChip> wifi_chip = getWifiChip(wifi_instance_name);
-        ChipModeId mode_id;
-        EXPECT_TRUE(configureChipToSupportIfaceType(
-            wifi_chip, ::android::hardware::wifi::V1_0::IfaceType::STA, &mode_id));
-    } else {
-        LOG(WARNING) << __func__ << ": Vendor HAL not supported";
-    }
+
+    sp<IWifiChip> wifi_chip = getWifiChip(wifi_instance_name);
+    ChipModeId mode_id;
+    EXPECT_TRUE(configureChipToSupportIfaceType(
+        wifi_chip, ::android::hardware::wifi::V1_0::IfaceType::STA, &mode_id));
 }
 
 // Helper function to deinitialize the driver and firmware
@@ -73,11 +70,8 @@ void deInitilializeDriverAndFirmware(const std::string& wifi_instance_name) {
     if (wifi_instance_name == "") {
         return;
     }
-    if (getWifi(wifi_instance_name) != nullptr) {
-        stopWifi(wifi_instance_name);
-    } else {
-        LOG(WARNING) << __func__ << ": Vendor HAL not supported";
-    }
+
+    stopWifi(wifi_instance_name);
 }
 
 // Helper function to find any iface of the desired type exposed.
@@ -282,18 +276,4 @@ bool turnOnExcessiveLogging(const sp<ISupplicant>& supplicant) {
             }
         });
     return !operation_failed;
-}
-
-bool waitForFrameworkReady() {
-    int waitCount = 10;
-    do {
-        // Check whether package service is ready or not.
-        if (!testing::checkSubstringInCommandOutput(
-                "/system/bin/service check package", ": not found")) {
-            return true;
-        }
-        LOG(INFO) << "Framework is not ready";
-        sleep(1);
-    } while (waitCount-- > 0);
-    return false;
 }
--- wifi/supplicant/1.2/vts/functional/supplicant_p2p_iface_hidl_test.cpp
+++ wifi/supplicant/1.2/vts/functional/supplicant_p2p_iface_hidl_test.cpp
@@ -38,11 +38,12 @@ constexpr char kTestPassphrase[] = "P2pWorld1234";
 constexpr uint8_t kTestZeroMacAddr[] = {[0 ... 5] = 0x0};
 }  // namespace
 
-class SupplicantP2pIfaceHidlTest : public SupplicantHidlTestBaseV1_2 {
+class SupplicantP2pIfaceHidlTest : public SupplicantHidlTestBase {
    public:
     virtual void SetUp() override {
-        SupplicantHidlTestBaseV1_2::SetUp();
-        if (!isP2pOn_) {
+        SupplicantHidlTestBase::SetUp();
+        EXPECT_TRUE(turnOnExcessiveLogging(supplicant_));
+        if (!::testing::deviceSupportsFeature("android.hardware.wifi.direct")) {
             GTEST_SKIP() << "Wi-Fi Direct is not supported, skip this test.";
         }
         p2p_iface_ = getSupplicantP2pIface_1_2(supplicant_);
@@ -161,7 +162,6 @@ TEST_P(SupplicantP2pIfaceHidlTest, EnableMacRandomization) {
     });
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantP2pIfaceHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantP2pIfaceHidlTest,
     testing::Combine(
