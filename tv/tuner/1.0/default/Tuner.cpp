447d91be9037413f0c185bdb3458dec4f7d6f0c2
openvela-robot <openvela-robot@xiaomi.com>
2020-09-15 16:55:14 +0800

    Reorder/cleanup FCM exempt list #1
    Test: m check-vintf-all
    Bug: 110261831
    Change-Id: I710d5e72279cafa6c680d13a10ffffb520418d69

--- tv/tuner/1.0/default/Tuner.cpp
+++ tv/tuner/1.0/default/Tuner.cpp
@@ -37,6 +37,7 @@ Tuner::Tuner() {
     // Static Frontends array to maintain local frontends information
     // Array index matches their FrontendId in the default impl
     mFrontendSize = 8;
+    mFrontends.resize(mFrontendSize);
     mFrontends[0] = new Frontend(FrontendType::DVBT, 0, this);
     mFrontends[1] = new Frontend(FrontendType::ATSC, 1, this);
     mFrontends[2] = new Frontend(FrontendType::DVBC, 2, this);
@@ -45,51 +46,6 @@ Tuner::Tuner() {
     mFrontends[5] = new Frontend(FrontendType::ISDBT, 5, this);
     mFrontends[6] = new Frontend(FrontendType::ANALOG, 6, this);
     mFrontends[7] = new Frontend(FrontendType::ATSC, 7, this);
-
-    FrontendInfo::FrontendCapabilities caps;
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.dvbtCaps(FrontendDvbtCapabilities());
-    mFrontendCaps[0] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.atscCaps(FrontendAtscCapabilities());
-    mFrontendCaps[1] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.dvbcCaps(FrontendDvbcCapabilities());
-    mFrontendCaps[2] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.dvbsCaps(FrontendDvbsCapabilities());
-    mFrontendCaps[3] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.dvbtCaps(FrontendDvbtCapabilities());
-    mFrontendCaps[4] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    FrontendIsdbtCapabilities isdbtCaps{
-            .modeCap = FrontendIsdbtMode::MODE_1 | FrontendIsdbtMode::MODE_2,
-            .bandwidthCap = (unsigned int)FrontendIsdbtBandwidth::BANDWIDTH_6MHZ,
-            .modulationCap = (unsigned int)FrontendIsdbtModulation::MOD_16QAM,
-            // ISDBT shares coderate and guard interval with DVBT
-            .coderateCap = FrontendDvbtCoderate::CODERATE_4_5 | FrontendDvbtCoderate::CODERATE_6_7,
-            .guardIntervalCap = (unsigned int)FrontendDvbtGuardInterval::INTERVAL_1_128,
-    };
-    caps.isdbtCaps(isdbtCaps);
-    mFrontendCaps[5] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.analogCaps(FrontendAnalogCapabilities());
-    mFrontendCaps[6] = caps;
-
-    caps = FrontendInfo::FrontendCapabilities();
-    caps.atscCaps(FrontendAtscCapabilities());
-    mFrontendCaps[7] = caps;
-
-    mLnbs.resize(2);
-    mLnbs[0] = new Lnb(0);
-    mLnbs[1] = new Lnb(1);
 }
 
 Tuner::~Tuner() {}
@@ -137,8 +93,6 @@ Return<void> Tuner::getDemuxCaps(getDemuxCaps_cb _hidl_cb) {
 
     DemuxCapabilities caps;
 
-    // IP filter can be an MMTP filter's data source.
-    caps.linkCaps = {0x00, 0x00, 0x02, 0x00, 0x00};
     _hidl_cb(Result::SUCCESS, caps);
     return Void();
 }
@@ -152,36 +106,10 @@ Return<void> Tuner::openDescrambler(openDescrambler_cb _hidl_cb) {
     return Void();
 }
 
-Return<void> Tuner::getFrontendInfo(FrontendId frontendId, getFrontendInfo_cb _hidl_cb) {
+Return<void> Tuner::getFrontendInfo(FrontendId /* frontendId */, getFrontendInfo_cb _hidl_cb) {
     ALOGV("%s", __FUNCTION__);
 
     FrontendInfo info;
-    if (frontendId >= mFrontendSize) {
-        _hidl_cb(Result::INVALID_ARGUMENT, info);
-        return Void();
-    }
-
-    vector<FrontendStatusType> statusCaps = {
-            FrontendStatusType::DEMOD_LOCK,
-            FrontendStatusType::SNR,
-            FrontendStatusType::FEC,
-            FrontendStatusType::MODULATION,
-            FrontendStatusType::PLP_ID,
-            FrontendStatusType::LAYER_ERROR,
-            FrontendStatusType::ATSC3_PLP_INFO,
-    };
-    // assign randomly selected values for testing.
-    info = {
-            .type = mFrontends[frontendId]->getFrontendType(),
-            .minFrequency = 139,
-            .maxFrequency = 1139,
-            .minSymbolRate = 45,
-            .maxSymbolRate = 1145,
-            .acquireRange = 30,
-            .exclusiveGroupId = 57,
-            .statusCaps = statusCaps,
-            .frontendCaps = mFrontendCaps[frontendId],
-    };
 
     _hidl_cb(Result::SUCCESS, info);
     return Void();
@@ -191,24 +119,17 @@ Return<void> Tuner::getLnbIds(getLnbIds_cb _hidl_cb) {
     ALOGV("%s", __FUNCTION__);
 
     vector<LnbId> lnbIds;
-    lnbIds.resize(mLnbs.size());
-    for (int i = 0; i < lnbIds.size(); i++) {
-        lnbIds[i] = mLnbs[i]->getId();
-    }
 
     _hidl_cb(Result::SUCCESS, lnbIds);
     return Void();
 }
 
-Return<void> Tuner::openLnbById(LnbId lnbId, openLnbById_cb _hidl_cb) {
+Return<void> Tuner::openLnbById(LnbId /* lnbId */, openLnbById_cb _hidl_cb) {
     ALOGV("%s", __FUNCTION__);
 
-    if (lnbId >= mLnbs.size()) {
-        _hidl_cb(Result::INVALID_ARGUMENT, nullptr);
-        return Void();
-    }
+    sp<ILnb> lnb = new Lnb();
 
-    _hidl_cb(Result::SUCCESS, mLnbs[lnbId]);
+    _hidl_cb(Result::SUCCESS, lnb);
     return Void();
 }
 
@@ -229,24 +150,6 @@ Return<void> Tuner::openLnbByName(const hidl_string& /*lnbName*/, openLnbByName_
 
 void Tuner::setFrontendAsDemuxSource(uint32_t frontendId, uint32_t demuxId) {
     mFrontendToDemux[frontendId] = demuxId;
-    if (mFrontends[frontendId] != nullptr && mFrontends[frontendId]->isLocked()) {
-        mDemuxes[demuxId]->startFrontendInputLoop();
-    }
-}
-
-void Tuner::removeDemux(uint32_t demuxId) {
-    map<uint32_t, uint32_t>::iterator it;
-    for (it = mFrontendToDemux.begin(); it != mFrontendToDemux.end(); it++) {
-        if (it->second == demuxId) {
-            it = mFrontendToDemux.erase(it);
-            break;
-        }
-    }
-    mDemuxes.erase(demuxId);
-}
-
-void Tuner::removeFrontend(uint32_t frontendId) {
-    mFrontendToDemux.erase(frontendId);
 }
 
 void Tuner::frontendStopTune(uint32_t frontendId) {
@@ -255,15 +158,6 @@ void Tuner::frontendStopTune(uint32_t frontendId) {
     if (it != mFrontendToDemux.end()) {
         demuxId = it->second;
         mDemuxes[demuxId]->stopFrontendInput();
-    }
-}
-
-void Tuner::frontendStartTune(uint32_t frontendId) {
-    map<uint32_t, uint32_t>::iterator it = mFrontendToDemux.find(frontendId);
-    uint32_t demuxId;
-    if (it != mFrontendToDemux.end()) {
-        demuxId = it->second;
-        mDemuxes[demuxId]->startFrontendInputLoop();
     }
 }
 
