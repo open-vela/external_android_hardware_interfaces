86462c728c7018a3f59a3f5512096032184b5191
liujinye <liujinye@xiaomi.com>
2020-09-16 19:28:50 +0800

    Merge "Add Tuner HAL and TvInput HAL VTS tests into the exception list of the uninstantiated parameterized tests." am: c9e5075d36
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1428008
    
    Change-Id: I60c76b20de5f80a79d8ad3ec671a97ce4bde27d0

--- wifi/1.4/default/wifi_chip.cpp
+++ wifi/1.4/default/wifi_chip.cpp
@@ -624,15 +624,6 @@ Return<void> WifiChip::getCapabilities_1_3(getCapabilities_cb hidl_status_cb) {
 Return<void> WifiChip::debug(const hidl_handle& handle,
                              const hidl_vec<hidl_string>&) {
     if (handle != nullptr && handle->numFds >= 1) {
-        {
-            std::unique_lock<std::mutex> lk(lock_t);
-            for (const auto& item : ringbuffer_map_) {
-                forceDumpToDebugRingBufferInternal(item.first);
-            }
-            // unique_lock unlocked here
-        }
-        usleep(100 * 1000);  // sleep for 100 milliseconds to wait for
-                             // ringbuffer updates.
         int fd = handle->data[0];
         if (!writeRingbufferFilesInternal()) {
             LOG(ERROR) << "Error writing files to flash";
@@ -1129,9 +1120,6 @@ WifiStatus WifiChip::stopLoggingToDebugRingBufferInternal() {
     legacy_hal::wifi_error legacy_status =
         legacy_hal_.lock()->deregisterRingBufferCallbackHandler(
             getFirstActiveWlanIfaceName());
-    if (legacy_status == legacy_hal::WIFI_SUCCESS) {
-        debug_ring_buffer_cb_registered_ = false;
-    }
     return createWifiStatusFromLegacyError(legacy_status);
 }
 
@@ -1347,7 +1335,7 @@ WifiStatus WifiChip::registerDebugRingBufferCallback() {
                     LOG(ERROR) << "Ringname " << name << " not found";
                     return;
                 }
-                // unique_lock unlocked here
+                // unlock
             }
         };
     legacy_hal::wifi_error legacy_status =
@@ -1649,7 +1637,7 @@ bool WifiChip::writeRingbufferFilesInternal() {
                 }
             }
         }
-        // unique_lock unlocked here
+        // unlock
     }
     return true;
 }
