abb38b7ac591a486a1d9f65c796d24a8dd8e5f12
openvela-robot <openvela-robot@xiaomi.com>
2020-09-18 03:27:19 +0800

    Merge "wifi: wait for the framework to be ready before checking features" into android11-tests-dev

--- wifi/supplicant/1.3/vts/functional/supplicant_sta_iface_hidl_test.cpp
+++ wifi/supplicant/1.3/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -330,11 +330,7 @@ TEST_P(SupplicantStaIfaceHidlTest, GetConnectionCapabilities) {
     sta_iface_->getConnectionCapabilities(
         [&](const SupplicantStatus& status,
             ConnectionCapabilities /* capabilities */) {
-            // Since getConnectionCapabilities() is overridden by an
-            // upgraded API in newer HAL versions, allow for FAILURE_UNKNOWN
-            if (status.code != SupplicantStatusCode::FAILURE_UNKNOWN) {
-                EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
-            }
+            EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
         });
 }
 
@@ -558,7 +554,6 @@ TEST_P(SupplicantStaIfaceHidlTest, FilsHlpFlushRequest) {
         EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
     });
 }
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantStaIfaceHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantStaIfaceHidlTest,
     testing::Combine(
