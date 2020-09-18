2416f9aa6261f5d11e2ba7d5e9421cd7069528d1
liujinye <liujinye@xiaomi.com>
2020-09-19 00:01:13 +0800

    [automerger skipped] Return immediately when prop is not found am: 3648ac967a am: d0d568fa21 -s ours
    am skip reason: Change-Id I41f5ddf26b9ba70221d59bae10d7ca9a2133a9eb with SHA-1 26abceefbe is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/12640905
    
    Change-Id: Ia57f6563f19993dfed61449eaa2e4556aa58f17c

--- compatibility_matrices/exclude/fcm_exclude.cpp
+++ compatibility_matrices/exclude/fcm_exclude.cpp
@@ -55,6 +55,7 @@ bool ShouldCheckMissingHalsInFcm(const std::string& package) {
             "android.hardware.media.bufferpool@2.0",
             "android.hardware.radio.config@1.2",
             // AIDL
+            "android.hardware.biometrics.common",
             "android.hardware.common",
             "android.hardware.graphics.common",
             "android.hardware.keymaster",
