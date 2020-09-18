16d3789952c7bf348796b5cddbf2477fc77077b0
openvela-robot <openvela-robot@xiaomi.com>
2020-09-18 16:42:44 +0800

    Minor API changes and additional documentation for IFingerprint
    1) Removes vendorCode from onEnrollmentProgress. This is already
       being sent in ISessionCallback#onAcquired
    2) Adds missing SessionState::INVALIDATING_AUTHENTICATOR_ID state
    
    Test: make -j56 android.hardware.biometrics.fingerprint-update-api
    Test: make -j56 VtsHalBiometricsFingerprintTargetTest
    Test: make -j56
    
    Change-Id: I246153339b336c029c9f156868127456aecf1a04

--- camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
+++ camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
@@ -1209,12 +1209,7 @@ bool CameraHidlTest::DeviceCb::processCaptureResultLocked(const CaptureResult& r
             return notify;
         }
 
-        // Physical device results are only expected in the last/final
-        // partial result notification.
-        bool expectPhysicalResults = !(request->usePartialResult &&
-                (results.partialResult < request->numPartialResults));
-        if (expectPhysicalResults &&
-                (physicalCameraMetadata.size() != request->expectedPhysicalResults.size())) {
+        if (physicalCameraMetadata.size() != request->expectedPhysicalResults.size()) {
             ALOGE("%s: Frame %d: Returned physical metadata count %zu "
                     "must be equal to expected count %zu", __func__, frameNumber,
                     physicalCameraMetadata.size(), request->expectedPhysicalResults.size());
