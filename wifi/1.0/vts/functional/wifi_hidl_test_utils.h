66877926eedc1e4d2ccb2a71d5f9f7b46cb4e92f
openvela-robot <openvela-robot@xiaomi.com>
2020-09-21 19:44:34 +0800

    Merge changes from topic "revert-1431616-thirtytwo-QKCUUIXLZL"
    * changes:
      Revert "Change HIDL fmq tests to use int32_t for payload"
      Revert "Add template parameters to MQDescriptor"

--- wifi/1.0/vts/functional/wifi_hidl_test_utils.h
+++ wifi/1.0/vts/functional/wifi_hidl_test_utils.h
@@ -31,8 +31,6 @@
 // Note: We only have a single instance of each of these objects currently.
 // These helper functions should be modified to return vectors if we support
 // multiple instances.
-android::sp<android::hardware::wifi::V1_0::IWifi> getWifi(
-    const std::string& instance_name);
 android::sp<android::hardware::wifi::V1_0::IWifiChip> getWifiChip(
     const std::string& instance_name);
 android::sp<android::hardware::wifi::V1_0::IWifiApIface> getWifiApIface(
@@ -51,5 +49,3 @@ bool configureChipToSupportIfaceType(
     android::hardware::wifi::V1_0::ChipModeId* configured_mode_id);
 // Used to trigger IWifi.stop() at the end of every test.
 void stopWifi(const std::string& instance_name);
-uint32_t getChipCapabilitiesLatest(
-    const android::sp<android::hardware::wifi::V1_0::IWifiChip>& wifi_chip);
