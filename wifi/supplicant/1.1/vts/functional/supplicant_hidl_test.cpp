c028e365a72a2e92c9c12c533c7b03886dfb6116
liujinye <liujinye@xiaomi.com>
2020-09-23 14:11:39 +0800

    Tweaks to fingerprint AIDL
    1) Explicit numbering for Error enum, which contains intentionally
       skipped entries for backwards compatibility
    2) Documentation for vendorCode. The vendorCodes must have a well
       defined behavior with the array API for things like GSI to work,
       e.g. generic system image with non-generic HAL implementation -
       with this explicit requirement, both components, as long as the
       contract is followed, would allow proper string feedback to be
       loaded/shown.
    
    Bug: 168843220
    Test: Builds
    Change-Id: Ie78e42b926351a03162a4a6e40f3325641c34609

--- wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
+++ wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
@@ -33,7 +33,6 @@ using ::android::hardware::wifi::supplicant::V1_0::SupplicantStatus;
 using ::android::hardware::wifi::supplicant::V1_0::SupplicantStatusCode;
 using ::android::hardware::wifi::supplicant::V1_0::IfaceType;
 using ::android::hardware::wifi::supplicant::V1_1::ISupplicant;
-using ::android::hardware::wifi::V1_0::IWifi;
 using ::android::sp;
 
 class SupplicantHidlTest : public SupplicantHidlTestBaseV1_1 {
@@ -140,11 +139,21 @@ TEST_P(SupplicantHidlTest, RemoveP2pInterface) {
  */
 TEST_P(SupplicantHidlTest, Terminate) { supplicant_->terminate(); }
 
+static std::vector<std::string> get_wifi_instances() {
+    std::vector<std::string> instances =
+        android::hardware::getAllHalInstanceNames(
+            android::hardware::wifi::V1_0::IWifi::descriptor);
+    // Also test when wifi instance is not set.
+    instances.push_back("");
+
+    return instances;
+}
+
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantHidlTest,
     testing::Combine(
-        testing::ValuesIn(
-            android::hardware::getAllHalInstanceNames(IWifi::descriptor)),
+        testing::ValuesIn(get_wifi_instances()),
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(
             android::hardware::wifi::supplicant::V1_1::ISupplicant::
                 descriptor))),
