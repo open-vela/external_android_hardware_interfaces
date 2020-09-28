f0e733a21422385568d7ad7fd7d4a51df234f8ad
openvela-robot <openvela-robot@xiaomi.com>
2020-09-28 19:04:08 +0800

    supplicant(vts): Stop adding empty instance for vendor HAL am: b9ddf021d3 am: 8a4e68b788 am: 329fe1ca18 am: a8cec5d5ce
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1438028
    
    Change-Id: I332a0a820f480104557d9fd69657dcc401b2da0b

--- wifi/supplicant/1.2/vts/functional/supplicant_p2p_iface_hidl_test.cpp
+++ wifi/supplicant/1.2/vts/functional/supplicant_p2p_iface_hidl_test.cpp
@@ -38,10 +38,11 @@ constexpr char kTestPassphrase[] = "P2pWorld1234";
 constexpr uint8_t kTestZeroMacAddr[] = {[0 ... 5] = 0x0};
 }  // namespace
 
-class SupplicantP2pIfaceHidlTest : public SupplicantHidlTestBaseV1_2 {
+class SupplicantP2pIfaceHidlTest : public SupplicantHidlTestBase {
    public:
     virtual void SetUp() override {
-        SupplicantHidlTestBaseV1_2::SetUp();
+        SupplicantHidlTestBase::SetUp();
+        EXPECT_TRUE(turnOnExcessiveLogging(supplicant_));
         if (!isP2pOn_) {
             GTEST_SKIP() << "Wi-Fi Direct is not supported, skip this test.";
         }
