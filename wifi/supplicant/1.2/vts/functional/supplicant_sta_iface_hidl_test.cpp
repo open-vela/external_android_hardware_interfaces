b0056ebd56de049732e6b51f991404ab1ddbb0f5
openvela-robot <openvela-robot@xiaomi.com>
2020-10-21 16:28:40 +0800

    Update CardState HAL and make setSimPower synchronous
    Bug: 171433370
    Test: make; make vts
    Change-Id: I0d298209a00f5f194547b0d6e368baa44b0c18ec
    Merged-In: I0d298209a00f5f194547b0d6e368baa44b0c18ec

--- wifi/supplicant/1.2/vts/functional/supplicant_sta_iface_hidl_test.cpp
+++ wifi/supplicant/1.2/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -53,10 +53,11 @@ using ::android::hardware::wifi::supplicant::V1_2::ISupplicantStaNetwork;
 #define TIMEOUT_PERIOD 60
 class IfaceDppCallback;
 
-class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBaseV1_2 {
+class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBase {
    public:
     virtual void SetUp() override {
-        SupplicantHidlTestBaseV1_2::SetUp();
+        SupplicantHidlTestBase::SetUp();
+        EXPECT_TRUE(turnOnExcessiveLogging(supplicant_));
         sta_iface_ = getSupplicantStaIface_1_2(supplicant_);
         ASSERT_NE(sta_iface_.get(), nullptr);
         count_ = 0;
