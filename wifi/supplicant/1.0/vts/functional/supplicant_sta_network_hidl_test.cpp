dbdf18d376bd9269c5d0570f7b975a08ca8bd7a6
liujinye <liujinye@xiaomi.com>
2020-10-22 21:53:09 +0800

    Merge "Reland "Remove legacy/O/O-MR1 matrices."" am: a53f69e158 am: c035573845
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1467927
    
    Change-Id: I62a03be182ec178c6221d4b34c718f1c002d4fe2

--- wifi/supplicant/1.0/vts/functional/supplicant_sta_network_hidl_test.cpp
+++ wifi/supplicant/1.0/vts/functional/supplicant_sta_network_hidl_test.cpp
@@ -79,10 +79,21 @@ constexpr uint32_t kTestPairwiseCipher =
      ISupplicantStaNetwork::PairwiseCipherMask::TKIP);
 }  // namespace
 
-class SupplicantStaNetworkHidlTest : public SupplicantHidlTestBaseV1_0 {
+class SupplicantStaNetworkHidlTest
+    : public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
    public:
     virtual void SetUp() override {
-        SupplicantHidlTestBaseV1_0::SetUp();
+        wifi_instance_name_ = std::get<0>(GetParam());
+        supplicant_instance_name_ = std::get<1>(GetParam());
+        isP2pOn_ =
+            testing::deviceSupportsFeature("android.hardware.wifi.direct");
+        // Stop Framework
+        std::system("/system/bin/stop");
+        stopSupplicant(wifi_instance_name_);
+        startSupplicantAndWaitForHidlService(wifi_instance_name_,
+                                             supplicant_instance_name_);
+        supplicant_ = getSupplicant(supplicant_instance_name_, isP2pOn_);
+        EXPECT_TRUE(turnOnExcessiveLogging(supplicant_));
         sta_network_ = createSupplicantStaNetwork(supplicant_);
         ASSERT_NE(sta_network_.get(), nullptr);
         /* variable used to check if the underlying HAL version is 1.3 or
@@ -92,6 +103,12 @@ class SupplicantStaNetworkHidlTest : public SupplicantHidlTestBaseV1_0 {
             ISupplicantStaNetwork::castFrom(sta_network_);
 
         ssid_.assign(kTestSsidStr, kTestSsidStr + strlen(kTestSsidStr));
+    }
+
+    virtual void TearDown() override {
+        stopSupplicant(wifi_instance_name_);
+        // Start Framework
+        std::system("/system/bin/start");
     }
 
    protected:
@@ -111,10 +128,14 @@ class SupplicantStaNetworkHidlTest : public SupplicantHidlTestBaseV1_0 {
 
     sp<::android::hardware::wifi::supplicant::V1_3::ISupplicantStaNetwork>
         v1_3 = nullptr;
+    bool isP2pOn_ = false;
+    sp<ISupplicant> supplicant_;
     // ISupplicantStaNetwork object used for all tests in this fixture.
     sp<ISupplicantStaNetwork> sta_network_;
     // SSID to use for various tests.
     std::vector<uint8_t> ssid_;
+    std::string wifi_instance_name_;
+    std::string supplicant_instance_name_;
 };
 
 class NetworkCallback : public ISupplicantStaNetworkCallback {
@@ -137,8 +158,8 @@ class NetworkCallback : public ISupplicantStaNetworkCallback {
  * successfully created.
  */
 TEST_P(SupplicantStaNetworkHidlTest, Create) {
-    stopSupplicant(wifi_v1_0_instance_name_);
-    startSupplicantAndWaitForHidlService(wifi_v1_0_instance_name_,
+    stopSupplicant(wifi_instance_name_);
+    startSupplicantAndWaitForHidlService(wifi_instance_name_,
                                          supplicant_instance_name_);
     sp<ISupplicant> supplicant =
         getSupplicant(supplicant_instance_name_, isP2pOn_);
@@ -818,6 +839,7 @@ TEST_P(SupplicantStaNetworkHidlTest, GetWpsNfcConfigurationToken) {
     EXPECT_FALSE(0 == status_and_token.second.size());
 }
 
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantStaNetworkHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantStaNetworkHidlTest,
     testing::Combine(
