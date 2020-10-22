005a10a6d3aa0f29a2a0a77759ab731426c11e63
openvela-robot <openvela-robot@xiaomi.com>
2020-10-22 21:05:10 +0800

    Merge "Reland "Remove legacy/O/O-MR1 matrices."" am: a53f69e158
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1467927
    
    Change-Id: I255eb47ffce9881741a56b2c34a734c257d4769f

--- wifi/supplicant/1.1/vts/functional/supplicant_sta_iface_hidl_test.cpp
+++ wifi/supplicant/1.1/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -39,10 +39,11 @@ using ::android::hardware::wifi::supplicant::V1_1::ISupplicant;
 using ::android::hardware::wifi::supplicant::V1_1::ISupplicantStaIface;
 using ::android::hardware::wifi::supplicant::V1_1::ISupplicantStaIfaceCallback;
 
-class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBaseV1_1 {
+class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBase {
    public:
     virtual void SetUp() override {
-        SupplicantHidlTestBaseV1_1::SetUp();
+        SupplicantHidlTestBase::SetUp();
+        EXPECT_TRUE(turnOnExcessiveLogging(supplicant_));
         sta_iface_ = getSupplicantStaIface_1_1(supplicant_);
         ASSERT_NE(sta_iface_.get(), nullptr);
     }
