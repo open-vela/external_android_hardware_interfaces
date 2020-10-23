4d80b0ef2ab911152eb1205bc35cac683ed11814
openvela-robot <openvela-robot@xiaomi.com>
2020-10-23 17:12:20 +0800

    Added global retry support
    The retry timer suggested by the network will persist
    accross network requests. Also changed the retry timer
    to a 64-bit value to be consistent with types used in
    Android time APIs.
    
    (clean cherry-pick)
    
    Test: FrameworksTelephonytests
    Fix: 159672248
    Merged-In: I763afdf34378512c60c0838830cb84875b1c66f0
    Change-Id: I763afdf34378512c60c0838830cb84875b1c66f0

--- wifi/supplicant/1.2/vts/functional/supplicant_sta_iface_hidl_test.cpp
+++ wifi/supplicant/1.2/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -53,16 +53,14 @@ using ::android::hardware::wifi::supplicant::V1_2::ISupplicantStaNetwork;
 #define TIMEOUT_PERIOD 60
 class IfaceDppCallback;
 
-class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBaseV1_2 {
+class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBase {
    public:
     virtual void SetUp() override {
-        SupplicantHidlTestBaseV1_2::SetUp();
+        SupplicantHidlTestBase::SetUp();
+        EXPECT_TRUE(turnOnExcessiveLogging(supplicant_));
         sta_iface_ = getSupplicantStaIface_1_2(supplicant_);
         ASSERT_NE(sta_iface_.get(), nullptr);
         count_ = 0;
-
-        v1_3 = ::android::hardware::wifi::supplicant::V1_3::
-            ISupplicantStaIface::castFrom(sta_iface_);
     }
 
     enum DppCallbackType {
@@ -105,7 +103,6 @@ class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBaseV1_2 {
    protected:
     // ISupplicantStaIface object used for all tests in this fixture.
     sp<ISupplicantStaIface> sta_iface_;
-    sp<::android::hardware::wifi::supplicant::V1_3::ISupplicantStaIface> v1_3;
 
     bool isDppSupported() {
         uint32_t keyMgmtMask = 0;
@@ -266,13 +263,10 @@ class IfaceDppCallback : public IfaceCallback {
  * RegisterCallback_1_2
  */
 TEST_P(SupplicantStaIfaceHidlTest, RegisterCallback_1_2) {
-    SupplicantStatusCode expectedCode =
-        (nullptr != v1_3) ? SupplicantStatusCode::FAILURE_UNKNOWN
-                          : SupplicantStatusCode::SUCCESS;
-    sta_iface_->registerCallback_1_2(new IfaceCallback(),
-                                     [&](const SupplicantStatus& status) {
-                                         EXPECT_EQ(expectedCode, status.code);
-                                     });
+    sta_iface_->registerCallback_1_2(
+        new IfaceCallback(), [](const SupplicantStatus& status) {
+            EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
+        });
 }
 
 /*
@@ -346,6 +340,9 @@ TEST_P(SupplicantStaIfaceHidlTest, StartDppEnrolleeInitiator) {
      * it is waiting for will never be called. Note that this test is also
      * implemented in the 1.3 VTS test.
      */
+    sp<::android::hardware::wifi::supplicant::V1_3::ISupplicantStaIface> v1_3 =
+        ::android::hardware::wifi::supplicant::V1_3::ISupplicantStaIface::
+            castFrom(sta_iface_);
     if (v1_3 != nullptr) {
         GTEST_SKIP() << "Test not supported with this HAL version";
     }
@@ -408,6 +405,10 @@ TEST_P(SupplicantStaIfaceHidlTest, StartDppConfiguratorInitiator) {
      * it is waiting for will never be called. Note that this test is also
      * implemented in the 1.3 VTS test.
      */
+    sp<::android::hardware::wifi::supplicant::V1_3::ISupplicantStaIface> v1_3 =
+        ::android::hardware::wifi::supplicant::V1_3::ISupplicantStaIface::
+            castFrom(sta_iface_);
+
     if (v1_3 != nullptr) {
         GTEST_SKIP() << "Test not supported with this HAL version";
         return;
