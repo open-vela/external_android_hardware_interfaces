a5300cb936937f7ec1a731c3e722cb4a3cdc662b
liujinye <liujinye@xiaomi.com>
2020-10-29 12:37:00 +0800

    Move UUID conversions into their own class
    Mechanical extraction of HidlUtils::uuidFrom/ToHal into
    a dedicated class UuidUtils.
    
    Bug: 142480271
    Test: m
    Change-Id: Ic5333ba32dc293f32c5562d0ef05bde8e5f9b302
    Merged-In: Ic5333ba32dc293f32c5562d0ef05bde8e5f9b302

--- audio/effect/all-versions/vts/functional/VtsHalAudioEffectTargetTest.cpp
+++ audio/effect/all-versions/vts/functional/VtsHalAudioEffectTargetTest.cpp
@@ -263,7 +263,7 @@ void AudioEffectHidlTest::getChannelCount(uint32_t* channelCount) {
         static_cast<audio_channel_mask_t>(currentConfig.outputCfg.channels));
 #else
     *channelCount = android::audio::policy::configuration::V7_0::getChannelCount(
-            currentConfig.outputCfg.base.channelMask);
+            currentConfig.outputCfg.channels);
     ASSERT_NE(*channelCount, 0);
 #endif
 }
@@ -353,14 +353,8 @@ inline bool operator==(const AudioBuffer& lhs, const AudioBuffer& rhs) {
 }
 
 inline bool operator==(const EffectBufferConfig& lhs, const EffectBufferConfig& rhs) {
-    return lhs.buffer == rhs.buffer &&
-#if MAJOR_VERSION <= 6
-           lhs.samplingRateHz == rhs.samplingRateHz && lhs.channels == rhs.channels &&
-           lhs.format == rhs.format &&
-#else
-           lhs.base.sampleRateHz == rhs.base.sampleRateHz &&
-           lhs.base.channelMask == rhs.base.channelMask && lhs.base.format == rhs.base.format &&
-#endif
+    return lhs.buffer == rhs.buffer && lhs.samplingRateHz == rhs.samplingRateHz &&
+           lhs.channels == rhs.channels && lhs.format == rhs.format &&
            lhs.accessMode == rhs.accessMode && lhs.mask == rhs.mask;
 }
 
