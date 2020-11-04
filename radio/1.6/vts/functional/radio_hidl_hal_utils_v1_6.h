a404069f2859a9a0c07c8cb41ef60e52af55deaa
liujinye <liujinye@xiaomi.com>
2020-11-05 03:36:07 +0800

    Skip togglingUiccApplicationsSimPresent if it's eSIM empty profile. am: f72c5c861e
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1486347
    
    Change-Id: I39bc532da430333a2fd9f6aa3a17a831e7d7edce

--- radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
+++ radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
@@ -58,7 +58,7 @@ using ::android::hardware::Void;
 class RadioHidlTest_v1_6;
 extern ::android::hardware::radio::V1_5::CardStatus cardStatus;
 
-/* Callback class for radio response v1_6 */
+/* Callback class for radio response v1_5 */
 class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioResponse {
   protected:
     RadioHidlTest_v1_6& parent_v1_6;
@@ -773,7 +773,7 @@ class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioRespon
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
             const SendSmsResult& sms);
 
-    Return<void> setNrDualConnectivityStateResponse(
+    Return<void> enableNrDualConnectivityResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
     Return<void> isNrDualConnectivityEnabledResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info, bool isEnabled);
@@ -788,9 +788,6 @@ class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioRespon
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 
     Return<void> cancelHandoverResponse(
-            const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
-
-    Return<void> setAllowedNetworkTypeBitmapResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 };
 
