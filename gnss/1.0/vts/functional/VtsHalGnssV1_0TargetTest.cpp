a31beef4c156f23de62a50190a8f35439e196fcb
openvela-robot <openvela-robot@xiaomi.com>
2020-11-09 18:14:28 +0800

    VTS - avoid same MAC for AP and STA
    Having the AP and STA set the same MAC address in tests could cause
    unintended failures when WifiApIfaceHidlTest and WifiStaIfaceHidlTest
    are executed back to back.
    
    Bug: 172528120
    Test: atest VtsHalWifiApV1_4TargetTest VtsHalWifiV1_2TargetTest
    
    Change-Id: I336d21cd896c46b064f16ecfa184a26dab67deaa

--- gnss/1.0/vts/functional/VtsHalGnssV1_0TargetTest.cpp
+++ gnss/1.0/vts/functional/VtsHalGnssV1_0TargetTest.cpp
@@ -511,7 +511,6 @@ TEST_P(GnssHalTest, SchedulingCapabilities) {
     }
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(GnssHalTest);
 INSTANTIATE_TEST_SUITE_P(
         PerInstance, GnssHalTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(IGnss::descriptor)),
