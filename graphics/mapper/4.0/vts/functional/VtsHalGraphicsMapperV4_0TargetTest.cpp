efb4726cd659d9ec1be8b1826e3e4813213e4b43
liujinye <liujinye@xiaomi.com>
2020-11-11 12:34:27 +0800

    identity: Fix attestation and documentation problems.
    - The docs said that IdentityCredential.createEphemeralKey() returned
      data encoded PKCS#8 which is wrong. It's supposed to be in DER format
      which is also what the VTS tests and credstore expects.
    
    - Clarify that createEphemeralKeyPair(), setReaderEphemeralPublicKey(),
      and createAuthChallenge() are all optional.
    
    - Avoid passing an invalid profile ID in the IdentityCredentialTests.
      verifyOneProfileAndEntryPass test.
    
    - Update requirements for which tags must be present in the attestation
      for CredentialKey as well as the requirements on expiration date and
      the issuer name.  Update default implementation to satisfy these
      requirements. Update VTS tests to carefully verify these requrements
      are met.
    
    - Clarify requirements for X.509 cert for AuthenticationKey. Add VTS
      test to verify.
    
    - Mandate that TAG_IDENTITY_CREDENTIAL_KEY must not be set for test
      credentials. Add VTS test to verify this.
    
    - Make default implementation pretend to be implemented in a trusted
      environment and streamline VTS tests to not special-case for the
      default implementation.
    
    - Switch to using the attestation extension parser from the KM 4.1
      support library instead of the one from system/keymaster. The latter
      one did not support the latest attestation extension and thus would
      fail for pretty much anything that wasn't the default HAL impl.
    
    - Fix a couple of bugs in keymaster::V4_1::parse_attestation_record():
      - Report root_of_trust.security_level
      - Add support for Tag::IDENTITY_CREDENTIAL_KEY
    
    - Fix how EMacKey is calculated.
    
    - Add test vectors to verify how EMacKey and DeviceMac is calculated.
    
    Test: atest VtsHalIdentityTargetTest
    Test: atest android.security.identity.cts
    Bug: 171745570
    Change-Id: Ic906fa24baa3a475585e543dc03aaf1d0f8d19a0
    Merged-In: I2f8bd772de078556733f769cec2021918d1d7de6

--- graphics/mapper/4.0/vts/functional/VtsHalGraphicsMapperV4_0TargetTest.cpp
+++ graphics/mapper/4.0/vts/functional/VtsHalGraphicsMapperV4_0TargetTest.cpp
@@ -2592,7 +2592,6 @@ TEST_P(GraphicsMapperHidlTest, GetReservedRegionBadBuffer) {
     ASSERT_EQ(0, reservedSize);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(GraphicsMapperHidlTest);
 INSTANTIATE_TEST_CASE_P(
         PerInstance, GraphicsMapperHidlTest,
         testing::Combine(
