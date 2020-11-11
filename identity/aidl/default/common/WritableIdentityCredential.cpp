9adc833ffd21cf5dd3e5432120d00d247a0ee90f
openvela-robot <openvela-robot@xiaomi.com>
2020-11-11 18:28:50 +0800

    Add utils for AIDL types conversions
    Add conversions between canonical types and NNAPI AIDL interface types
    that are needed for AIDL sample driver implementation.
    
    Bug: 172922059
    Test: VtsNeuralnetworksTargetTest
    Change-Id: I02803302e02457e52c752114b47b94239eff20e9

--- identity/aidl/default/common/WritableIdentityCredential.cpp
+++ identity/aidl/default/common/WritableIdentityCredential.cpp
@@ -40,20 +40,7 @@ using namespace ::android::hardware::identity;
 
 bool WritableIdentityCredential::initialize() {
     if (!hwProxy_->initialize(testCredential_)) {
-        LOG(ERROR) << "hwProxy->initialize() failed";
-        return false;
-    }
-    startPersonalizationCalled_ = false;
-    firstEntry_ = true;
-
-    return true;
-}
-
-// Used when updating a credential. Returns false on failure.
-bool WritableIdentityCredential::initializeForUpdate(
-        const vector<uint8_t>& encryptedCredentialKeys) {
-    if (!hwProxy_->initializeForUpdate(testCredential_, docType_, encryptedCredentialKeys)) {
-        LOG(ERROR) << "hwProxy->initializeForUpdate() failed";
+        LOG(ERROR) << "hwProxy->initialize failed";
         return false;
     }
     startPersonalizationCalled_ = false;
