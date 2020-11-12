775d4580f1fa0255fc66999f62a44ac88a870b24
liujinye <liujinye@xiaomi.com>
2020-11-12 22:10:41 +0800

    Rename enableNrDualConnectivity am: e9525fdd5f
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1497160
    
    Change-Id: Id42e90d10fc836bdccb316b91753b8a7cacb701d

--- radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
+++ radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
@@ -792,9 +792,6 @@ class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioRespon
 
     Return<void> setAllowedNetworkTypeBitmapResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
-
-    Return<void> setDataThrottlingResponse(
-            const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 };
 
 /* Callback class for radio indication */
@@ -867,10 +864,6 @@ class RadioIndication_v1_6 : public ::android::hardware::radio::V1_6::IRadioIndi
     Return<void> currentLinkCapacityEstimate(
             RadioIndicationType type,
             const ::android::hardware::radio::V1_2::LinkCapacityEstimate& lce);
-
-    Return<void> currentLinkCapacityEstimate_1_6(
-            RadioIndicationType type,
-            const ::android::hardware::radio::V1_6::LinkCapacityEstimate& lce);
 
     Return<void> currentPhysicalChannelConfigs(
             RadioIndicationType type,
