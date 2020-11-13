9b47c4ef3fdeb1b2c3d8ce7363f9a4df675e002c
liujinye <liujinye@xiaomi.com>
2020-11-13 22:46:50 +0800

    [automerger skipped] drm@1.2 vts: force provision multiple security levels am: 8a006c411a -s ours
    am skip reason: skipped by user dmulabagal
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1208526
    
    Change-Id: I848f4306bccf934ef1d4a73ec864d7b00adce17e

--- wifi/supplicant/1.0/vts/functional/supplicant_sta_iface_hidl_test.cpp
+++ wifi/supplicant/1.0/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -22,7 +22,6 @@
 #include <VtsCoreUtil.h>
 #include <android/hardware/wifi/1.0/IWifi.h>
 #include <android/hardware/wifi/supplicant/1.0/ISupplicantStaIface.h>
-#include <android/hardware/wifi/supplicant/1.1/ISupplicantStaIface.h>
 
 #include "supplicant_hidl_call_util.h"
 #include "supplicant_hidl_test_utils.h"
@@ -74,16 +73,11 @@ class SupplicantStaIfaceHidlTest : public SupplicantHidlTestBaseV1_0 {
         sta_iface_ = getSupplicantStaIface(supplicant_);
         ASSERT_NE(sta_iface_.get(), nullptr);
 
-        v1_1 = ::android::hardware::wifi::supplicant::V1_1::
-            ISupplicantStaIface::castFrom(sta_iface_);
-
         memcpy(mac_addr_.data(), kTestMacAddr, mac_addr_.size());
     }
 
    protected:
     bool isP2pOn_ = false;
-    sp<::android::hardware::wifi::supplicant::V1_1::ISupplicantStaIface> v1_1 =
-        nullptr;
     // ISupplicantStaIface object used for all tests in this fixture.
     sp<ISupplicantStaIface> sta_iface_;
     // MAC address to use for various tests.
@@ -181,13 +175,10 @@ TEST_P(SupplicantStaIfaceHidlTest, Create) {
  * RegisterCallback
  */
 TEST_P(SupplicantStaIfaceHidlTest, RegisterCallback) {
-    SupplicantStatusCode expectedCode =
-        (nullptr != v1_1) ? SupplicantStatusCode::FAILURE_UNKNOWN
-                          : SupplicantStatusCode::SUCCESS;
-    sta_iface_->registerCallback(new IfaceCallback(),
-                                 [&](const SupplicantStatus& status) {
-                                     EXPECT_EQ(expectedCode, status.code);
-                                 });
+    sta_iface_->registerCallback(
+        new IfaceCallback(), [](const SupplicantStatus& status) {
+            EXPECT_EQ(SupplicantStatusCode::SUCCESS, status.code);
+        });
 }
 
 /*
@@ -547,7 +538,6 @@ TEST_P(SupplicantStaIfaceHidlTest, RemoveExtRadioWork) {
         HIDL_INVOKE(sta_iface_, removeExtRadioWork, kTestRadioWorkId).code);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantStaIfaceHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantStaIfaceHidlTest,
     testing::Combine(
