262096915c51d25e5164058b2e983c06b82f86d6
liujinye <liujinye@xiaomi.com>
2020-11-17 17:51:31 +0800

    Merge "Implement keymint V1 aidl interfaces, service module, and vts tests." am: 0ab081bfa6 am: ce18601982 am: ed6b1b32cb am: e8fd22b6fb
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1298415
    
    Change-Id: Ie71bd3250c441b39c82a9c62ab0c1f83f748ddc1

--- radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
+++ radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
@@ -784,13 +784,16 @@ class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioRespon
     Return<void> releasePduSessionIdResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 
-    Return<void> beginHandoverResponse(
+    Return<void> startHandoverResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 
     Return<void> cancelHandoverResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 
     Return<void> setAllowedNetworkTypeBitmapResponse(
+            const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
+
+    Return<void> setDataThrottlingResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 };
 
