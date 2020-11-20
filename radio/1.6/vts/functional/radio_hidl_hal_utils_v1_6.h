200aff54b21a5f51aed59f06309e2c72a7d1a411
liujinye <liujinye@xiaomi.com>
2020-11-21 00:40:45 +0800

    [automerger skipped] Merge "Added pdu session id to setupDataCall" am: 78293592cc am: 264e65b092 am: 70cea03ae8 -s ours
    am skip reason: Change-Id I0e11c8997674cdaad4bb82491d818ca85ee197d1 with SHA-1 7674b03f38 is in history
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1507689
    
    Change-Id: I4e234fb9fc38ab2a490a7816e50e34fe506ca852

--- radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
+++ radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
@@ -792,6 +792,12 @@ class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioRespon
 
     Return<void> setAllowedNetworkTypeBitmapResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
+
+    Return<void> setDataThrottlingResponse(
+            const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
+
+    Return<void> getSystemSelectionChannelsResponse(
+            const ::android::hardware::radio::V1_6::RadioResponseInfo& info);
 };
 
 /* Callback class for radio indication */
