7926a6a15a89bbb2bc38ee076177172cff9479d8
liujinye <liujinye@xiaomi.com>
2020-11-24 19:14:08 +0800

    Merge "wifi: fix old vts tests on newer HAL"

--- identity/support/src/IdentityCredentialSupport.cpp
+++ identity/support/src/IdentityCredentialSupport.cpp
@@ -1023,12 +1023,11 @@ optional<vector<vector<uint8_t>>> createAttestation(
     // relying party is ever going to trust our batch key and those keys above
     // it.
     //
-    ::keymaster::PureSoftKeymasterContext context(::keymaster::KmVersion::KEYMASTER_4_1,
-                                                  KM_SECURITY_LEVEL_TRUSTED_ENVIRONMENT);
+    ::keymaster::PureSoftKeymasterContext context(KM_SECURITY_LEVEL_TRUSTED_ENVIRONMENT);
 
     error = generate_attestation_from_EVP(key, swEnforced, hwEnforced, auth_set, context,
-                                          *attestation_chain, *attestation_signing_key,
-                                          &cert_chain_out);
+                                          ::keymaster::kCurrentKeymasterVersion, *attestation_chain,
+                                          *attestation_signing_key, &cert_chain_out);
 
     if (KM_ERROR_OK != error || !cert_chain_out) {
         LOG(ERROR) << "Error generate attestation from EVP key" << error;
@@ -2402,6 +2401,7 @@ vector<vector<uint8_t>> chunkVector(const vector<uint8_t>& content, size_t maxCh
 
     return ret;
 }
+
 
 vector<uint8_t> testHardwareBoundKey = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
