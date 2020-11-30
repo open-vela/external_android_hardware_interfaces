164362cc3830132433eeef405c4b8839fd08873e
liujinye <liujinye@xiaomi.com>
2020-11-30 20:49:52 +0800

    [automerger skipped] identity: Fix attestation and documentation problems. am: e4f58f898f -s ours
    am skip reason: Change-Id I2f8bd772de078556733f769cec2021918d1d7de6 with SHA-1 34abaaefcb is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/13036974
    
    Change-Id: Ie81f83da014f88616ce2dfc1e621508da5b37a7c

--- radio/1.6/vts/functional/radio_response.cpp
+++ radio/1.6/vts/functional/radio_response.cpp
@@ -1163,10 +1163,3 @@ Return<void> RadioResponse_v1_6::setDataThrottlingResponse(
     parent_v1_6.notify(info.serial);
     return Void();
 }
-
-Return<void> RadioResponse_v1_6::getSystemSelectionChannelsResponse(
-        const ::android::hardware::radio::V1_6::RadioResponseInfo& info) {
-    rspInfo = info;
-    parent_v1_6.notify(info.serial);
-    return Void();
-}
