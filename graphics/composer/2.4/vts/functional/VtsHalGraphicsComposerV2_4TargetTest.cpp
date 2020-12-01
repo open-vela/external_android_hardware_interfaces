1be3cbfc8f4e7385ab8c4fbd07247749f3c59ce0
liujinye <liujinye@xiaomi.com>
2020-12-02 00:32:27 +0800

    Merge rvc-qpr-dev-plus-aosp-without-vendor@6881855
    Bug: 172690556
    Merged-In: If5afd00334e8b09250df1ae97dec9285e39ad930
    Change-Id: Ic66bb1389f1bbecfdaa641e1fcdbf79044f55d23

--- graphics/composer/2.4/vts/functional/VtsHalGraphicsComposerV2_4TargetTest.cpp
+++ graphics/composer/2.4/vts/functional/VtsHalGraphicsComposerV2_4TargetTest.cpp
@@ -21,7 +21,6 @@
 #include <thread>
 
 #include <android-base/logging.h>
-#include <android-base/properties.h>
 #include <android/hardware/graphics/mapper/2.0/IMapper.h>
 #include <composer-command-buffer/2.4/ComposerCommandBuffer.h>
 #include <composer-vts/2.4/ComposerVts.h>
@@ -684,15 +683,3 @@ TEST_P(GraphicsComposerHidlCommandTest, getLayerGenericMetadataKeys) {
 }  // namespace graphics
 }  // namespace hardware
 }  // namespace android
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-
-    using namespace std::chrono_literals;
-    if (!android::base::WaitForProperty("init.svc.surfaceflinger", "stopped", 10s)) {
-        ALOGE("Failed to stop init.svc.surfaceflinger");
-        return -1;
-    }
-
-    return RUN_ALL_TESTS();
-}
