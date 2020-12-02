0e443ec623117c62b096aa147fac0d81e7bf18bd
openvela-robot <openvela-robot@xiaomi.com>
2020-12-02 14:38:19 +0800

    Camera: fix HAL1 removeCamera crash
    Test: partner testing
    Bug: 173511749
    Merged-In: Ifd5fc7c63e3835945194291d161b491bd5acb342
    Change-Id: Ifd5fc7c63e3835945194291d161b491bd5acb342

--- wifi/hostapd/1.1/vts/functional/hostapd_hidl_test.cpp
+++ wifi/hostapd/1.1/vts/functional/hostapd_hidl_test.cpp
@@ -58,10 +58,7 @@ class HostapdHidlTest
         ASSERT_NE(hostapd_.get(), nullptr);
     }
 
-    virtual void TearDown() override {
-        HIDL_INVOKE_VOID_WITHOUT_ARGUMENTS(hostapd_, terminate);
-        stopHostapd(wifi_instance_name_);
-    }
+    virtual void TearDown() override { stopHostapd(wifi_instance_name_); }
 
    protected:
     std::string getPrimaryWlanIfaceName() {
@@ -312,7 +309,6 @@ TEST_P(HostapdHidlTest, AddInvalidPskAccessPointWithoutAcs) {
     EXPECT_NE(HostapdStatusCode::SUCCESS, status.code);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(HostapdHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, HostapdHidlTest,
     testing::Combine(
--- wifi/supplicant/1.0/vts/functional/VtsHalWifiSupplicantP2pV1_0TargetTest.cpp
+++ wifi/supplicant/1.0/vts/functional/VtsHalWifiSupplicantP2pV1_0TargetTest.cpp
@@ -18,6 +18,9 @@
 #include "supplicant_hidl_test_utils.h"
 
 int main(int argc, char** argv) {
+    if (!::testing::deviceSupportsFeature("android.hardware.wifi.direct"))
+        return 0;
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
