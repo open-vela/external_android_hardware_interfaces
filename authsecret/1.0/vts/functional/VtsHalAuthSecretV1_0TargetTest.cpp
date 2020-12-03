9dc04627a8448c4d3e0acc5b71e63fd4bdfbb76e
liujinye <liujinye@xiaomi.com>
2020-12-03 22:13:27 +0800

    Merge "Camera: fix HAL1 removeCamera crash" into rvc-qpr-dev

--- authsecret/1.0/vts/functional/VtsHalAuthSecretV1_0TargetTest.cpp
+++ authsecret/1.0/vts/functional/VtsHalAuthSecretV1_0TargetTest.cpp
@@ -81,7 +81,6 @@ TEST_P(AuthSecretHidlTest, provisionPrimaryUserCredentialAndWrongSecret) {
     authsecret->primaryUserCredential(WRONG_SECRET);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(AuthSecretHidlTest);
 INSTANTIATE_TEST_SUITE_P(
         PerInstance, AuthSecretHidlTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(IAuthSecret::descriptor)),
