6a157e366a80a34d13e4361e85221aee40834d08
liujinye <liujinye@xiaomi.com>
2020-12-03 16:39:53 +0800

    Implement power policy change listener in AudioControl HAL
    Bug: 173719953
    Test: manual test
    Change-Id: Id773bd2bb1c8becc8f3e9e69e23ed8034dd36937

--- audio/core/all-versions/vts/functional/AudioPrimaryHidlHalTest.h
+++ audio/core/all-versions/vts/functional/AudioPrimaryHidlHalTest.h
@@ -522,9 +522,7 @@ using DeviceConfigParameter = std::tuple<DeviceParameter, AudioConfig, std::vect
 
 #if MAJOR_VERSION >= 6
 const std::vector<DeviceConfigParameter>& getInputDeviceConfigParameters();
-const std::vector<DeviceConfigParameter>& getInputDeviceSingleConfigParameters();
 const std::vector<DeviceConfigParameter>& getOutputDeviceConfigParameters();
-const std::vector<DeviceConfigParameter>& getOutputDeviceSingleConfigParameters();
 #endif
 
 #if MAJOR_VERSION >= 4
@@ -885,7 +883,7 @@ class OpenStreamTest : public AudioHidlTestWithDeviceConfigParameter {
         AudioHidlTestWithDeviceConfigParameter::TearDown();
     }
 
-  protected:
+   protected:
     AudioConfig audioConfig;
     DeviceAddress address = {};
     sp<Stream> stream;
@@ -975,7 +973,7 @@ class InputStreamTest : public OpenStreamTest<IStreamIn> {
         ASSERT_NO_FATAL_FAILURE(OpenStreamTest::SetUp());  // setup base
 #if MAJOR_VERSION <= 6
         address.device = AudioDevice::IN_DEFAULT;
-#elif MAJOR_VERSION >= 7
+#elif MAJOR_VERSION <= 7
         address.deviceType = toString(xsd::AudioDevice::AUDIO_DEVICE_IN_DEFAULT);
 #endif
         const AudioConfig& config = getConfig();
@@ -990,7 +988,7 @@ class InputStreamTest : public OpenStreamTest<IStreamIn> {
 
    protected:
 #if MAJOR_VERSION == 2
-     const AudioSource initMetadata = AudioSource::DEFAULT;
+    const AudioSource initMetadata = AudioSource::DEFAULT;
 #elif MAJOR_VERSION >= 4 && MAJOR_VERSION <= 6
      const SinkMetadata initMetadata = {{ {.source = AudioSource::DEFAULT, .gain = 1 } }};
 #elif MAJOR_VERSION >= 7
