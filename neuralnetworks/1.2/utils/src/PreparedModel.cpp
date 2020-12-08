3be426a0555ac0f81f86e9cb4ffb333df47a0cc3
openvela-robot <openvela-robot@xiaomi.com>
2020-12-08 18:38:36 +0800

    Validate during NN conversions by default -- hal am: 6547b2ac9c am: ee3a8f4970
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1520618
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I00283ad0353ea76bfd0927d2fdd98640b5f94208

--- neuralnetworks/1.2/utils/src/PreparedModel.cpp
+++ neuralnetworks/1.2/utils/src/PreparedModel.cpp
@@ -83,7 +83,7 @@ PreparedModel::executeSynchronously(const V1_0::Request& request, MeasureTiming 
     };
 
     const auto ret = kPreparedModel->executeSynchronously(request, measure, cb);
-    HANDLE_TRANSPORT_FAILURE(ret);
+    NN_TRY(hal::utils::makeExecutionFailure(hal::utils::handleTransportError(ret)));
 
     return result;
 }
@@ -94,7 +94,8 @@ PreparedModel::executeAsynchronously(const V1_0::Request& request, MeasureTiming
     const auto scoped = kDeathHandler.protectCallback(cb.get());
 
     const auto ret = kPreparedModel->execute_1_2(request, measure, cb);
-    const auto status = HANDLE_TRANSPORT_FAILURE(ret);
+    const auto status =
+            NN_TRY(hal::utils::makeExecutionFailure(hal::utils::handleTransportError(ret)));
     if (status != V1_0::ErrorStatus::NONE) {
         const auto canonical = nn::convert(status).value_or(nn::ErrorStatus::GENERAL_FAILURE);
         return NN_ERROR(canonical) << "execute failed with " << toString(status);
