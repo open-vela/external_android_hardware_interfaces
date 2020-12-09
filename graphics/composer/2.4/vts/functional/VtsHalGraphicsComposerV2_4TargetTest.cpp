8a34df71968417bf259f502f40d49e53930e1703
liujinye <liujinye@xiaomi.com>
2020-12-10 03:59:02 +0800

    [automerger skipped] DO NOT MERGE - Merge Android R QPR1 am: abf2b89f2d -s ours am: 145c97fbaf -s ours am: 6bcf14f5fa -s ours
    am skip reason: subject contains skip directive
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1521166
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I10753ba00fbe39f0c45a3ab1e9fc2d5333c39631

--- graphics/composer/2.4/vts/functional/VtsHalGraphicsComposerV2_4TargetTest.cpp
+++ graphics/composer/2.4/vts/functional/VtsHalGraphicsComposerV2_4TargetTest.cpp
@@ -21,6 +21,7 @@
 #include <thread>
 
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <android/hardware/graphics/mapper/2.0/IMapper.h>
 #include <composer-command-buffer/2.4/ComposerCommandBuffer.h>
 #include <composer-vts/2.4/ComposerVts.h>
@@ -656,6 +657,7 @@ TEST_P(GraphicsComposerHidlTest, setGameContentType) {
     Test_setContentType(ContentType::GAME, "GAME");
 }
 
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(GraphicsComposerHidlTest);
 INSTANTIATE_TEST_SUITE_P(
         PerInstance, GraphicsComposerHidlTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(IComposer::descriptor)),
@@ -691,5 +693,14 @@ TEST_P(GraphicsComposerHidlTest, getLayerGenericMetadataKeys) {
 }  // namespace hardware
 }  // namespace android
 
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
 
+    using namespace std::chrono_literals;
+    if (!android::base::WaitForProperty("init.svc.surfaceflinger", "stopped", 10s)) {
+        ALOGE("Failed to stop init.svc.surfaceflinger");
+        return -1;
+    }
 
+    return RUN_ALL_TESTS();
+}
