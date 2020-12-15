91cf7c7c243fb7eaad64c8fe9b1ca90c70aa2b32
liujinye <liujinye@xiaomi.com>
2020-12-15 17:35:42 +0800

    Merge "Use safe_union correctly" am: baf61434fd am: 8e4e5639c9
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1510995
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I971a63dc8a73982a5bb394c9c148037cb3fe12b8

--- radio/1.6/vts/functional/radio_response.cpp
+++ radio/1.6/vts/functional/radio_response.cpp
@@ -1157,6 +1157,14 @@ Return<void> RadioResponse_v1_6::setAllowedNetworkTypeBitmapResponse(
     return Void();
 }
 
+Return<void> RadioResponse_v1_6::getAllowedNetworkTypeBitmapResponse(
+        const ::android::hardware::radio::V1_6::RadioResponseInfo& /*info*/,
+        const ::android::hardware::hidl_bitfield<
+                ::android::hardware::radio::V1_4::RadioAccessFamily>
+        /*networkTypeBitmap*/) {
+    return Void();
+}
+
 Return<void> RadioResponse_v1_6::setDataThrottlingResponse(
         const ::android::hardware::radio::V1_6::RadioResponseInfo& info) {
     rspInfo = info;
@@ -1195,6 +1203,14 @@ Return<void> RadioResponse_v1_6::getVoiceRegistrationStateResponse_1_6(
 Return<void> RadioResponse_v1_6::getDataRegistrationStateResponse_1_6(
         const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
         const ::android::hardware::radio::V1_6::RegStateResult& /*regResponse*/) {
+    rspInfo = info;
+    parent_v1_6.notify(info.serial);
+    return Void();
+}
+
+Return<void> RadioResponse_v1_6::getCurrentCallsResponse_1_6(
+        const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
+        const ::android::hardware::hidl_vec<::android::hardware::radio::V1_6::Call>& /*calls*/) {
     rspInfo = info;
     parent_v1_6.notify(info.serial);
     return Void();
