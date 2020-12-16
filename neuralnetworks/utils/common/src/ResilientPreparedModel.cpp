c057ae8088dd27a852315c71b8ac15cd53ad9794
liujinye <liujinye@xiaomi.com>
2020-12-16 20:05:07 +0800

    Merge changes from topic "aosp-default-wrapper"
    * changes:
      HidlUtils: remove temporary conversion functions
      audio: Update default effect HAL wrapper to support V7
      audio: Update default wrapper to support V7
      audio: Extend HidlUtils for the default wrapper needs

--- neuralnetworks/utils/common/src/ResilientPreparedModel.cpp
+++ neuralnetworks/utils/common/src/ResilientPreparedModel.cpp
@@ -64,17 +64,16 @@ nn::SharedPreparedModel ResilientPreparedModel::recover(
 nn::ExecutionResult<std::pair<std::vector<nn::OutputShape>, nn::Timing>>
 ResilientPreparedModel::execute(const nn::Request& request, nn::MeasureTiming measure,
                                 const nn::OptionalTimePoint& deadline,
-                                const nn::OptionalDuration& loopTimeoutDuration) const {
+                                const nn::OptionalTimeoutDuration& loopTimeoutDuration) const {
     return getPreparedModel()->execute(request, measure, deadline, loopTimeoutDuration);
 }
 
 nn::GeneralResult<std::pair<nn::SyncFence, nn::ExecuteFencedInfoCallback>>
-ResilientPreparedModel::executeFenced(const nn::Request& request,
-                                      const std::vector<nn::SyncFence>& waitFor,
-                                      nn::MeasureTiming measure,
-                                      const nn::OptionalTimePoint& deadline,
-                                      const nn::OptionalDuration& loopTimeoutDuration,
-                                      const nn::OptionalDuration& timeoutDurationAfterFence) const {
+ResilientPreparedModel::executeFenced(
+        const nn::Request& request, const std::vector<nn::SyncFence>& waitFor,
+        nn::MeasureTiming measure, const nn::OptionalTimePoint& deadline,
+        const nn::OptionalTimeoutDuration& loopTimeoutDuration,
+        const nn::OptionalTimeoutDuration& timeoutDurationAfterFence) const {
     return getPreparedModel()->executeFenced(request, waitFor, measure, deadline,
                                              loopTimeoutDuration, timeoutDurationAfterFence);
 }
