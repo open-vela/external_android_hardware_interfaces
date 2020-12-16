f966dbb6fd574c7efc6d543a729421793fe9ac1d
liujinye <liujinye@xiaomi.com>
2020-12-16 23:57:08 +0800

    Merge "Update OWNERS for 2020" am: a9cfd015bf am: f1715f4b0b am: 1969d5ab7e
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1532199
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I9e2e1eb1f98e8068ec89ac9c037688361046da1c

--- audio/core/all-versions/vts/functional/7.0/PolicyConfig.h
+++ audio/core/all-versions/vts/functional/7.0/PolicyConfig.h
@@ -33,14 +33,10 @@ class PolicyConfig {
         if (mConfig) {
             mStatus = OK;
             mPrimaryModule = getModuleFromName(DeviceManager::kPrimaryDevice);
-            if (mConfig->getFirstModules()) {
-                for (const auto& module : mConfig->getFirstModules()->get_module()) {
-                    if (module.getFirstAttachedDevices()) {
-                        auto attachedDevices = module.getFirstAttachedDevices()->getItem();
-                        if (!attachedDevices.empty()) {
-                            mModulesWithDevicesNames.insert(module.getName());
-                        }
-                    }
+            for (const auto& module : mConfig->getFirstModules()->get_module()) {
+                auto attachedDevices = module.getFirstAttachedDevices()->getItem();
+                if (!attachedDevices.empty()) {
+                    mModulesWithDevicesNames.insert(module.getName());
                 }
             }
         }
@@ -56,7 +52,7 @@ class PolicyConfig {
     }
     const std::string& getFilePath() const { return mFilePath; }
     const xsd::Module* getModuleFromName(const std::string& name) const {
-        if (mConfig && mConfig->getFirstModules()) {
+        if (mConfig) {
             for (const auto& module : mConfig->getFirstModules()->get_module()) {
                 if (module.getName() == name) return &module;
             }
@@ -69,10 +65,8 @@ class PolicyConfig {
     }
     bool haveInputProfilesInModule(const std::string& name) const {
         auto module = getModuleFromName(name);
-        if (module && module->getFirstMixPorts()) {
-            for (const auto& mixPort : module->getFirstMixPorts()->getMixPort()) {
-                if (mixPort.getRole() == xsd::Role::sink) return true;
-            }
+        for (const auto& mixPort : module->getFirstMixPorts()->getMixPort()) {
+            if (mixPort.getRole() == xsd::Role::sink) return true;
         }
         return false;
     }
