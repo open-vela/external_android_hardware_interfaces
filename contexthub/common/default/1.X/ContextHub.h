37a4f023dadb713e749494eebcc2a498d91b8da2
liujinye <liujinye@xiaomi.com>
2020-12-17 22:01:20 +0800

    Merge "Added IAudioControl#onDevicesToMuteChange"

--- contexthub/common/default/1.X/ContextHub.h
+++ contexthub/common/default/1.X/ContextHub.h
@@ -60,6 +60,14 @@ struct ContextHub : public IContextHubInterface {
         return Void();
     }
 
+    Return<Result> registerCallback(uint32_t hubId, const sp<IContexthubCallback>& cb) override {
+        if (hubId == kMockHubId) {
+            mCallback = cb;
+            return Result::OK;
+        }
+        return Result::BAD_PARAMS;
+    }
+
     // We don't expose any nanoapps, therefore all nanoapp-related API calls return with BAD_PARAMS
     Return<Result> sendMessageToHub(uint32_t /*hubId*/, const ContextHubMsg& /*msg*/) override {
         return Result::BAD_PARAMS;
@@ -85,8 +93,19 @@ struct ContextHub : public IContextHubInterface {
         return Result::BAD_PARAMS;
     }
 
-  protected:
+    Return<Result> queryApps(uint32_t hubId) override {
+        if (hubId == kMockHubId && mCallback != nullptr) {
+            std::vector<HubAppInfo> nanoapps;
+            mCallback->handleAppsInfo(nanoapps);
+            return Result::OK;
+        }
+        return Result::BAD_PARAMS;
+    }
+
+  private:
     static constexpr uint32_t kMockHubId = 0;
+
+    sp<IContexthubCallback> mCallback;
 };
 
 }  // namespace implementation
