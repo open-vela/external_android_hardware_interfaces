cdc6478076272a9fe81a078702f76a2ec75f90be
openvela-robot <openvela-robot@xiaomi.com>
2020-12-22 02:10:59 +0800

    Snap for 7045433 from 1fd4b9d0705ca95d58ba7a5bb359fef9228dbcd2 to sc-release
    Change-Id: I59045be5e9c1be8d7142b5e4b339f15731932a7f

--- automotive/can/1.0/default/libnl++/include/libnl++/Attributes.h
+++ automotive/can/1.0/default/libnl++/include/libnl++/Attributes.h
@@ -93,29 +93,14 @@ class Attributes : private Buffer<nlattr> {
      */
     template <typename T>
     T get(nlattrtype_t attrtype) const {
-        const auto buffer = getBuffer(attrtype);
-        if (!buffer.has_value()) {
+        const auto& ind = index();
+        const auto it = ind.find(attrtype);
+        if (it == ind.end()) {
             LOG(WARNING) << "Netlink attribute is missing: " << attrtype;
             return T{};
         }
 
-        return parse<T>(*buffer);
-    }
-
-    /**
-     * Fetches underlying buffer of a given attribute.
-     *
-     * This is a low-level access method unlikely to be useful in most cases. Please consider
-     * using #get instead.
-     *
-     * \param attrtype Attribute to fetch
-     * \return Attribute buffer.
-     */
-    std::optional<Buffer<nlattr>> getBuffer(nlattrtype_t attrtype) const {
-        const auto& ind = index();
-        const auto it = ind.find(attrtype);
-        if (it == ind.end()) return std::nullopt;
-        return it->second;
+        return parse<T>(it->second);
     }
 
     /**
