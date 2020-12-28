a506a454177498cb0cc6ad9175ffa6cf6e9b0c3e
liujinye <liujinye@xiaomi.com>
2020-12-29 02:11:04 +0800

    Snap for 7054508 from 5ef7bb5a1c04ffb5150df637a87eab19c6677dfa to sc-release
    Change-Id: I56ccdbc826e102f2c276cc0dd4395142bfa3a4d4

--- gnss/common/utils/default/Utils.cpp
+++ gnss/common/utils/default/Utils.cpp
@@ -228,8 +228,7 @@ V1_0::GnssLocation Utils::getMockLocationV1_0() {
             .verticalAccuracyMeters = kMockVerticalAccuracyMeters,
             .speedAccuracyMetersPerSecond = kMockSpeedAccuracyMetersPerSecond,
             .bearingAccuracyDegrees = kMockBearingAccuracyDegrees,
-            .timestamp =
-                    static_cast<int64_t>(kMockTimestamp + ::android::elapsedRealtimeNano() / 1e6)};
+            .timestamp = kMockTimestamp};
     return location;
 }
 
