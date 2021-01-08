ef195ddb6005e351e9559f32ca9cdaf4afa73a14
openvela-robot <openvela-robot@xiaomi.com>
2021-01-08 19:25:09 +0800

    Update KM4.1 owners am: e21ce63ba6 am: 75a296e89d am: b1303cb21b
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1543369
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I073275e35d7a61789d8ae3d87167323ce23e7415

--- radio/1.4/vts/functional/radio_hidl_hal_api.cpp
+++ radio/1.4/vts/functional/radio_hidl_hal_api.cpp
@@ -34,9 +34,6 @@ TEST_P(RadioHidlTest_v1_4, emergencyDial) {
     if (!deviceSupportsFeature(FEATURE_VOICE_CALL)) {
         ALOGI("Skipping emergencyDial because voice call is not supported in device");
         return;
-    } else if (!deviceSupportsFeature(FEATURE_TELEPHONY)) {
-        ALOGI("Skipping emergencyDial because telephony radio is not supported in device");
-        return;
     } else {
         ALOGI("Running emergencyDial because voice call is supported in device");
     }
@@ -89,9 +86,6 @@ TEST_P(RadioHidlTest_v1_4, emergencyDial_withServices) {
     if (!deviceSupportsFeature(FEATURE_VOICE_CALL)) {
         ALOGI("Skipping emergencyDial because voice call is not supported in device");
         return;
-    } else if (!deviceSupportsFeature(FEATURE_TELEPHONY)) {
-        ALOGI("Skipping emergencyDial because telephony radio is not supported in device");
-        return;
     } else {
         ALOGI("Running emergencyDial because voice call is supported in device");
     }
@@ -143,9 +137,6 @@ TEST_P(RadioHidlTest_v1_4, emergencyDial_withServices) {
 TEST_P(RadioHidlTest_v1_4, emergencyDial_withEmergencyRouting) {
     if (!deviceSupportsFeature(FEATURE_VOICE_CALL)) {
         ALOGI("Skipping emergencyDial because voice call is not supported in device");
-        return;
-    } else if (!deviceSupportsFeature(FEATURE_TELEPHONY)) {
-        ALOGI("Skipping emergencyDial because telephony radio is not supported in device");
         return;
     } else {
         ALOGI("Running emergencyDial because voice call is supported in device");
