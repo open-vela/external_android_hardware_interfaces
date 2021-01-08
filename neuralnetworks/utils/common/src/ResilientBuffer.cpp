065f617746b9c447a1b127120b053f433c2254d5
liujinye <liujinye@xiaomi.com>
2021-01-08 09:53:47 +0800

    Audio: Add VTS tests for invalid enum-strings, Part 1b am: 9139399bad
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1543608
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I06e23b9ae40a078f88ffdbe6ac7bd47a76787576

--- neuralnetworks/utils/common/src/ResilientBuffer.cpp
+++ neuralnetworks/utils/common/src/ResilientBuffer.cpp
@@ -20,7 +20,6 @@
 #include <android-base/thread_annotations.h>
 #include <nnapi/IBuffer.h>
 #include <nnapi/Result.h>
-#include <nnapi/TypeUtils.h>
 #include <nnapi/Types.h>
 
 #include <functional>
@@ -30,34 +29,6 @@
 #include <vector>
 
 namespace android::hardware::neuralnetworks::utils {
-namespace {
-
-template <typename FnType>
-auto protect(const ResilientBuffer& resilientBuffer, const FnType& fn)
-        -> decltype(fn(*resilientBuffer.getBuffer())) {
-    auto buffer = resilientBuffer.getBuffer();
-    auto result = fn(*buffer);
-
-    // Immediately return if device is not dead.
-    if (result.has_value() || result.error().code != nn::ErrorStatus::DEAD_OBJECT) {
-        return result;
-    }
-
-    // Attempt recovery and return if it fails.
-    auto maybeBuffer = resilientBuffer.recover(buffer.get());
-    if (!maybeBuffer.has_value()) {
-        const auto& [resultErrorMessage, resultErrorCode] = result.error();
-        const auto& [recoveryErrorMessage, recoveryErrorCode] = maybeBuffer.error();
-        return nn::error(resultErrorCode)
-               << resultErrorMessage << ", and failed to recover dead buffer with error "
-               << recoveryErrorCode << ": " << recoveryErrorMessage;
-    }
-    buffer = std::move(maybeBuffer).value();
-
-    return fn(*buffer);
-}
-
-}  // namespace
 
 nn::GeneralResult<std::shared_ptr<const ResilientBuffer>> ResilientBuffer::create(
         Factory makeBuffer) {
@@ -82,16 +53,9 @@ nn::SharedBuffer ResilientBuffer::getBuffer() const {
     std::lock_guard guard(mMutex);
     return mBuffer;
 }
-nn::GeneralResult<nn::SharedBuffer> ResilientBuffer::recover(
-        const nn::IBuffer* failingBuffer) const {
+nn::SharedBuffer ResilientBuffer::recover(const nn::IBuffer* /*failingBuffer*/,
+                                          bool /*blocking*/) const {
     std::lock_guard guard(mMutex);
-
-    // Another caller updated the failing prepared model.
-    if (mBuffer.get() != failingBuffer) {
-        return mBuffer;
-    }
-
-    mBuffer = NN_TRY(kMakeBuffer());
     return mBuffer;
 }
 
@@ -100,16 +64,12 @@ nn::Request::MemoryDomainToken ResilientBuffer::getToken() const {
 }
 
 nn::GeneralResult<void> ResilientBuffer::copyTo(const nn::Memory& dst) const {
-    const auto fn = [&dst](const nn::IBuffer& buffer) { return buffer.copyTo(dst); };
-    return protect(*this, fn);
+    return getBuffer()->copyTo(dst);
 }
 
 nn::GeneralResult<void> ResilientBuffer::copyFrom(const nn::Memory& src,
                                                   const nn::Dimensions& dimensions) const {
-    const auto fn = [&src, &dimensions](const nn::IBuffer& buffer) {
-        return buffer.copyFrom(src, dimensions);
-    };
-    return protect(*this, fn);
+    return getBuffer()->copyFrom(src, dimensions);
 }
 
 }  // namespace android::hardware::neuralnetworks::utils
