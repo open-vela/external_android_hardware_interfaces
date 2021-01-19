df38084853d71405ca7f6e8c8f88142b86beb8b9
openvela-robot <openvela-robot@xiaomi.com>
2021-01-19 23:17:39 +0800

    Add isUpdatable to NNAPI canonical IDevice -- hal am: 9adab0cf49
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1554335
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: I36ad611e555707a6b4cefa0ab3efb7d99fa4dc3d

--- neuralnetworks/utils/common/src/InvalidPreparedModel.cpp
+++ neuralnetworks/utils/common/src/InvalidPreparedModel.cpp
@@ -42,10 +42,6 @@ InvalidPreparedModel::executeFenced(
     return NN_ERROR() << "InvalidPreparedModel";
 }
 
-nn::GeneralResult<nn::SharedBurst> InvalidPreparedModel::configureExecutionBurst() const {
-    return NN_ERROR() << "InvalidPreparedModel";
-}
-
 std::any InvalidPreparedModel::getUnderlyingResource() const {
     return {};
 }
