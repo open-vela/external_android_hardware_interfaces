8196f1f1cae15df10e9487de2960f515f30a7aed
openvela-robot <openvela-robot@xiaomi.com>
2021-01-20 21:14:13 +0800

    wifi: fix a use of an uninitialized value
    This `if` is executed once, so using `legacy_status` from a prior
    execution is nonsensical.
    
    Caught by the static analyzer:
    > hardware/interfaces/wifi/1.5/default/wifi_chip.cpp:1053:39: warning:
    1st function call argument is an uninitialized value
    [clang-analyzer-core.CallAndMessage]
    
    Bug: 173999527
    Test: atest -c VtsHalWifiApV1_5TargetTest
    Merged-In: Ib81f1496be53e453d9c99ede129a77bdaf860de7
    Change-Id: Ib81f1496be53e453d9c99ede129a77bdaf860de7

--- identity/aidl/default/common/IdentityCredential.h
+++ identity/aidl/default/common/IdentityCredential.h
@@ -45,11 +45,9 @@ using ::std::vector;
 
 class IdentityCredential : public BnIdentityCredential {
   public:
-    IdentityCredential(sp<SecureHardwareProxyFactory> hwProxyFactory,
-                       sp<SecureHardwarePresentationProxy> hwProxy,
+    IdentityCredential(sp<SecureHardwarePresentationProxy> hwProxy,
                        const vector<uint8_t>& credentialData)
-        : hwProxyFactory_(hwProxyFactory),
-          hwProxy_(hwProxy),
+        : hwProxy_(hwProxy),
           credentialData_(credentialData),
           numStartRetrievalCalls_(0),
           expectedDeviceNameSpacesSize_(0) {}
@@ -60,11 +58,6 @@ class IdentityCredential : public BnIdentityCredential {
 
     // Methods from IIdentityCredential follow.
     ndk::ScopedAStatus deleteCredential(vector<uint8_t>* outProofOfDeletionSignature) override;
-    ndk::ScopedAStatus deleteCredentialWithChallenge(
-            const vector<uint8_t>& challenge,
-            vector<uint8_t>* outProofOfDeletionSignature) override;
-    ndk::ScopedAStatus proveOwnership(const vector<uint8_t>& challenge,
-                                      vector<uint8_t>* outProofOfOwnershipSignature) override;
     ndk::ScopedAStatus createEphemeralKeyPair(vector<uint8_t>* outKeyPair) override;
     ndk::ScopedAStatus setReaderEphemeralPublicKey(const vector<uint8_t>& publicKey) override;
     ndk::ScopedAStatus createAuthChallenge(int64_t* outChallenge) override;
@@ -86,16 +79,8 @@ class IdentityCredential : public BnIdentityCredential {
     ndk::ScopedAStatus generateSigningKeyPair(vector<uint8_t>* outSigningKeyBlob,
                                               Certificate* outSigningKeyCertificate) override;
 
-    ndk::ScopedAStatus updateCredential(
-            shared_ptr<IWritableIdentityCredential>* outWritableCredential) override;
-
   private:
-    ndk::ScopedAStatus deleteCredentialCommon(const vector<uint8_t>& challenge,
-                                              bool includeChallenge,
-                                              vector<uint8_t>* outProofOfDeletionSignature);
-
     // Set by constructor
-    sp<SecureHardwareProxyFactory> hwProxyFactory_;
     sp<SecureHardwarePresentationProxy> hwProxy_;
     vector<uint8_t> credentialData_;
     int numStartRetrievalCalls_;
@@ -103,7 +88,6 @@ class IdentityCredential : public BnIdentityCredential {
     // Set by initialize()
     string docType_;
     bool testCredential_;
-    vector<uint8_t> encryptedCredentialKeys_;
 
     // Set by createEphemeralKeyPair()
     vector<uint8_t> ephemeralPublicKey_;
--- identity/aidl/vts/VtsIdentityTestUtils.cpp
+++ identity/aidl/vts/VtsIdentityTestUtils.cpp
@@ -14,18 +14,15 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "Util"
+#define LOG_TAG "VtsIdentityTestUtils"
 
-#include "Util.h"
-
-#include <android-base/logging.h>
+#include "VtsIdentityTestUtils.h"
 
 #include <aidl/Gtest.h>
-#include <android-base/stringprintf.h>
+#include <android-base/logging.h>
 #include <keymaster/km_openssl/openssl_utils.h>
 #include <keymasterV4_1/attestation_record.h>
 #include <charconv>
-
 #include <map>
 
 namespace android::hardware::identity::test_utils {
@@ -38,7 +35,6 @@ using std::vector;
 
 using ::android::sp;
 using ::android::String16;
-using ::android::base::StringPrintf;
 using ::android::binder::Status;
 using ::keymaster::X509_Ptr;
 
@@ -90,7 +86,7 @@ optional<vector<uint8_t>> generateReaderCertificate(string serialDecimal,
 
     return support::ecPublicKeyGenerateCertificate(readerPublicKey.value(), readerKey.value(),
                                                    serialDecimal, issuer, subject,
-                                                   validityNotBefore, validityNotAfter, {});
+                                                   validityNotBefore, validityNotAfter);
 }
 
 optional<vector<SecureAccessControlProfile>> addAccessControlProfiles(
