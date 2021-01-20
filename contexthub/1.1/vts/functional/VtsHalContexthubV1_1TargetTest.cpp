b8851c0c07ebba9b01717e4a05aeb1ac93160338
openvela-robot <openvela-robot@xiaomi.com>
2021-01-20 13:57:28 +0800

    OMX VTS: free graphics buffers at the end of test
    Bug: 173454295
    Test: atest VtsHalMediaOmxV1_0TargetVideoDecTest
    Test: atest VtsHalMediaOmxV1_0TargetVideoEncTest
    Change-Id: Ib9660ef8967b34114b36d71f9ed424f819b57e7a
    (cherry picked from commit 179a52cbbffd27ff939fd59f0d4921eaeaf0f664)
    Merged-In: Ib9660ef8967b34114b36d71f9ed424f819b57e7a

--- contexthub/1.1/vts/functional/VtsHalContexthubV1_1TargetTest.cpp
+++ contexthub/1.1/vts/functional/VtsHalContexthubV1_1TargetTest.cpp
@@ -54,7 +54,6 @@ TEST_P(ContexthubHidlTest, TestOnSettingChanged) {
     ASSERT_OK(registerCallback(nullptr));
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(ContexthubHidlTest);
 INSTANTIATE_TEST_SUITE_P(HubIdSpecificTests, ContexthubHidlTest, testing::ValuesIn(kTestParameters),
                          android::hardware::PrintInstanceTupleNameToString<>);
 
