7f158db04a1a206b0b3c8916c62f3cb736f3d9b3
lesl <lesl@google.com>
2021-01-22 19:37:47 +0800

    wifi: Add new HAL API: getBridgedInstances
    Also fix the instances doesn't update after removing the instance
    
    Cherry-pick from ag/13382534 which finished the review in master branch
    
    Bug: 173999527
    Test: atest -c VtsHalWifiApV1_5TargetTest
    
    Change-Id: I439922d38c35b5367823f95f5f7d0e4c6ed05f57

--- wifi/1.5/default/wifi_chip.cpp
+++ wifi/1.5/default/wifi_chip.cpp
@@ -1066,6 +1066,7 @@ WifiStatus WifiChip::removeIfaceInstanceFromBridgedApIfaceInternal(
         }
     }
     br_ifaces_ap_instances_.erase(ifInstanceName);
+    iface->removeInstance(ifInstanceName);
     return createWifiStatus(WifiStatusCode::SUCCESS);
 }
 
