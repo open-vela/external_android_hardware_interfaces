b8eb836e60e12ef082c273010c34fae612730953
liujinye <liujinye@xiaomi.com>
2021-01-27 18:48:54 +0800

    Merge "Add S-NSSAI"

--- radio/1.6/vts/functional/radio_response.cpp
+++ radio/1.6/vts/functional/radio_response.cpp
@@ -1150,14 +1150,14 @@ Return<void> RadioResponse_v1_6::cancelHandoverResponse(
     return Void();
 }
 
-Return<void> RadioResponse_v1_6::setAllowedNetworkTypesBitmapResponse(
+Return<void> RadioResponse_v1_6::setAllowedNetworkTypeBitmapResponse(
         const ::android::hardware::radio::V1_6::RadioResponseInfo& info) {
     rspInfo = info;
     parent_v1_6.notify(info.serial);
     return Void();
 }
 
-Return<void> RadioResponse_v1_6::getAllowedNetworkTypesBitmapResponse(
+Return<void> RadioResponse_v1_6::getAllowedNetworkTypeBitmapResponse(
         const ::android::hardware::radio::V1_6::RadioResponseInfo& /*info*/,
         const ::android::hardware::hidl_bitfield<
                 ::android::hardware::radio::V1_4::RadioAccessFamily>
