f1a5098b56966d5da5c3c25f3560069cfaad1a66
openvela-robot <openvela-robot@xiaomi.com>
2021-02-02 16:32:36 +0800

    Merge "Address leftover comments on aosp/1212191"

--- security/keymint/aidl/vts/functional/KeyMintAidlTestBase.h
+++ security/keymint/aidl/vts/functional/KeyMintAidlTestBase.h
@@ -175,12 +175,9 @@ class KeyMintAidlTestBase : public ::testing::TestWithParam<string> {
     inline const vector<KeyParameter>& SecLevelAuthorizations() {
         return SecLevelAuthorizations(key_characteristics_);
     }
-    const vector<KeyParameter>& SecLevelAuthorizations(
-            const vector<KeyCharacteristics>& key_characteristics, SecurityLevel securityLevel);
-
-    AuthorizationSet HwEnforcedAuthorizations(
+    const vector<KeyParameter>& HwEnforcedAuthorizations(
             const vector<KeyCharacteristics>& key_characteristics);
-    AuthorizationSet SwEnforcedAuthorizations(
+    const vector<KeyParameter>& SwEnforcedAuthorizations(
             const vector<KeyCharacteristics>& key_characteristics);
 
   private:
