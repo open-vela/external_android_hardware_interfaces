99f72adaca3c143b137bcdb1f6ca33edfb6cf80a
liujinye <liujinye@xiaomi.com>
2021-02-03 08:29:07 +0800

    Merge "Update HAL 1.6 for 5G Slicing" into sc-dev am: 028fd2c1e6
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/13434544
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ia81d8a8c8cc972fc13e18488b01b8cb5c57d33d1

--- drm/1.0/default/CryptoPlugin.cpp
+++ drm/1.0/default/CryptoPlugin.cpp
@@ -79,7 +79,7 @@ namespace implementation {
             }
         }
 
-        android::CryptoPlugin::Mode legacyMode = android::CryptoPlugin::kMode_Unencrypted;
+        android::CryptoPlugin::Mode legacyMode;
         switch(mode) {
         case Mode::UNENCRYPTED:
             legacyMode = android::CryptoPlugin::kMode_Unencrypted;
@@ -142,10 +142,7 @@ namespace implementation {
                 return Void();
             }
 
-            size_t totalDstSize = 0;
-            if (__builtin_add_overflow(destBuffer.offset, destBuffer.size, &totalDstSize) ||
-                totalDstSize > destBase->getSize()) {
-                android_errorWriteLog(0x534e4554, "176496353");
+            if (destBuffer.offset + destBuffer.size > destBase->getSize()) {
                 _hidl_cb(Status::ERROR_DRM_CANNOT_HANDLE, 0, "invalid buffer size");
                 return Void();
             }
