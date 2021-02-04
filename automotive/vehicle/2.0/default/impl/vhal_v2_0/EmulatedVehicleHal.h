a71e264dd8bd7879ee6b7cfb195d0024f6d83e44
openvela-robot <openvela-robot@xiaomi.com>
2021-02-05 00:05:48 +0800

    Snap for 7124858 from 92d4d99b989363a8c566fb334a41fb9f97975fa1 to rvc-qpr3-release
    Change-Id: I3ef20bb3176aa63b7698aa64a12f2fa38d91057d

--- automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.h
+++ automotive/vehicle/2.0/default/impl/vhal_v2_0/EmulatedVehicleHal.h
@@ -63,7 +63,6 @@ public:
     //  Methods from EmulatedVehicleHalIface
     bool setPropertyFromVehicle(const VehiclePropValue& propValue) override;
     std::vector<VehiclePropValue> getAllProperties() const override;
-    void getAllPropertiesOverride();
 
 private:
     constexpr std::chrono::nanoseconds hertzToNanoseconds(float hz) const {
@@ -82,17 +81,12 @@ private:
                                    VehiclePropValue* outValue);
     StatusCode fillObd2DtcInfo(VehiclePropValue* outValue);
     StatusCode clearObd2FreezeFrames(const VehiclePropValue& propValue);
-    VehicleHal::VehiclePropValuePtr doInternalHealthCheck();
-    VehicleHal::VehiclePropValuePtr createVhalHeartBeatProp();
 
     /* Private members */
     VehiclePropertyStore* mPropStore;
     std::unordered_set<int32_t> mHvacPowerProps;
     RecurrentTimer mRecurrentTimer;
     VehicleHalClient* mVehicleClient;
-    bool mInEmulator;
-    bool mInitVhalValueOverride;
-    std::vector<VehiclePropValue> mVehiclePropertiesOverride;
     EmulatedUserHal* mEmulatedUserHal;
 };
 
