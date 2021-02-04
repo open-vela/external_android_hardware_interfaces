26ab3f35926eebcd48ecc21253c0ac820732b8e5
openvela-robot <openvela-robot@xiaomi.com>
2021-02-04 15:57:30 +0800

    audio: Allow using vendor extensions in surround formats (V6)
    The 'surroundFormats' section must allow specifying
    vendor-provided formats (vendor extensions). This has been
    fixed in Audio HAL V7. Backporting to earlier versions.
    
    Bug: 173647783
    Test: atest VtsHalAudioV6_0TargetTest
    Change-Id: I8ae1687087bab3f1c9020f351bdf7b81b9e74e50
    (cherry picked from commit 14fa6ee5b1ef7214847b8d443f3e01f116e42099)

--- wifi/hostapd/1.2/vts/functional/hostapd_hidl_test.cpp
+++ wifi/hostapd/1.2/vts/functional/hostapd_hidl_test.cpp
@@ -74,6 +74,8 @@ class HostapdHidlTest
 
     virtual void TearDown() override {
         HIDL_INVOKE_VOID_WITHOUT_ARGUMENTS(hostapd_, terminate);
+        // Wait 3 seconds to allow terminate processing before kill hostapd.
+        sleep(3);
         stopHostapd(wifi_instance_name_);
     }
 
@@ -417,7 +419,6 @@ TEST_P(HostapdHidlTest, SetDebugParams) {
     EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(HostapdHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, HostapdHidlTest,
     testing::Combine(
