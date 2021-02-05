f2fc0a8c14e208140d98313154a8cad168769043
liujinye <liujinye@xiaomi.com>
2021-02-06 00:05:02 +0800

    Snap for 7128020 from b2bb9879513e9e535a73cef1037818d5b27c153a to rvc-qpr3-release
    Change-Id: Icc90e0dd6516284d16f61d2ce97270f73938bdf2

--- camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
+++ camera/provider/2.4/vts/functional/VtsHalCameraProviderV2_4TargetTest.cpp
@@ -1211,12 +1211,7 @@ bool CameraHidlTest::DeviceCb::processCaptureResultLocked(const CaptureResult& r
             return notify;
         }
 
-        // Physical device results are only expected in the last/final
-        // partial result notification.
-        bool expectPhysicalResults = !(request->usePartialResult &&
-                (results.partialResult < request->numPartialResults));
-        if (expectPhysicalResults &&
-                (physicalCameraMetadata.size() != request->expectedPhysicalResults.size())) {
+        if (physicalCameraMetadata.size() != request->expectedPhysicalResults.size()) {
             ALOGE("%s: Frame %d: Returned physical metadata count %zu "
                     "must be equal to expected count %zu", __func__, frameNumber,
                     physicalCameraMetadata.size(), request->expectedPhysicalResults.size());
@@ -1267,27 +1262,9 @@ bool CameraHidlTest::DeviceCb::processCaptureResultLocked(const CaptureResult& r
             ADD_FAILURE();
             return notify;
         }
-
-        // Verify no duplicate tags between partial results
-        const camera_metadata_t* partialMetadata =
-                reinterpret_cast<const camera_metadata_t*>(resultMetadata.data());
-        const camera_metadata_t* collectedMetadata = request->collectedResult.getAndLock();
-        camera_metadata_ro_entry_t searchEntry, foundEntry;
-        for (size_t i = 0; i < get_camera_metadata_size(partialMetadata); i++) {
-            if (0 != get_camera_metadata_ro_entry(partialMetadata, i, &searchEntry)) {
-                ADD_FAILURE();
-                request->collectedResult.unlock(collectedMetadata);
-                return notify;
-            }
-            if (-ENOENT !=
-                find_camera_metadata_ro_entry(collectedMetadata, searchEntry.tag, &foundEntry)) {
-                ADD_FAILURE();
-                request->collectedResult.unlock(collectedMetadata);
-                return notify;
-            }
-        }
-        request->collectedResult.unlock(collectedMetadata);
-        request->collectedResult.append(partialMetadata);
+        request->collectedResult.append(
+                reinterpret_cast<const camera_metadata_t*>(
+                    resultMetadata.data()));
 
         isPartialResult =
             (results.partialResult < request->numPartialResults);
@@ -1695,7 +1672,7 @@ bool CameraHidlTest::isSecureOnly(sp<ICameraProvider> provider, const hidl_strin
     Return<void> ret;
     ::android::sp<ICameraDevice> device3_x;
     bool retVal = false;
-    if (getCameraDeviceVersion(name, mProviderType) == CAMERA_DEVICE_API_VERSION_1_0) {
+    if (getCameraDeviceVersion(mProviderType, name) == CAMERA_DEVICE_API_VERSION_1_0) {
         return false;
     }
     ret = provider->getCameraDeviceInterface_V3_x(name, [&](auto status, const auto& device) {
@@ -3360,8 +3337,8 @@ TEST_P(CameraHidlTest, configureConcurrentStreamsAvailableOutputs) {
             size_t j = 0;
             for (const auto& it : outputStreams) {
                 V3_2::Stream stream3_2;
-                V3_2::DataspaceFlags dataspaceFlag = getDataspace(
-                        static_cast<PixelFormat>(it.format));
+                V3_2::DataspaceFlags dataspaceFlag =
+                        getDataspace(static_cast<PixelFormat>(it.format));
                 stream3_2 = {streamId++,
                              StreamType::OUTPUT,
                              static_cast<uint32_t>(it.width),
@@ -5912,7 +5889,7 @@ V3_2::DataspaceFlags CameraHidlTest::getDataspace(PixelFormat format) {
         case PixelFormat::RAW_OPAQUE:
         case PixelFormat::RAW10:
         case PixelFormat::RAW12:
-            return  static_cast<V3_2::DataspaceFlags>(Dataspace::ARBITRARY);
+            return static_cast<V3_2::DataspaceFlags>(Dataspace::ARBITRARY);
         default:
             return static_cast<V3_2::DataspaceFlags>(Dataspace::UNKNOWN);
     }
@@ -6292,8 +6269,8 @@ void CameraHidlTest::configureOfflineStillStream(const std::string &name,
     ASSERT_EQ(Status::OK, rc);
     ASSERT_FALSE(outputStreams.empty());
 
-    V3_2::DataspaceFlags dataspaceFlag = getDataspace(
-            static_cast<PixelFormat>(outputStreams[idx].format));
+    V3_2::DataspaceFlags dataspaceFlag =
+            getDataspace(static_cast<PixelFormat>(outputStreams[idx].format));
 
     ::android::hardware::hidl_vec<V3_4::Stream> streams3_4(/*size*/1);
     V3_4::Stream stream3_4 = {{ 0 /*streamId*/, StreamType::OUTPUT,
@@ -7600,7 +7577,6 @@ void CameraHidlTest::verifyRequestTemplate(const camera_metadata_t* metadata,
     }
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(CameraHidlTest);
 INSTANTIATE_TEST_SUITE_P(
         PerInstance, CameraHidlTest,
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(ICameraProvider::descriptor)),
--- wifi/hostapd/1.1/vts/functional/hostapd_hidl_test.cpp
+++ wifi/hostapd/1.1/vts/functional/hostapd_hidl_test.cpp
@@ -23,7 +23,6 @@
 
 #include <android/hardware/wifi/1.0/IWifi.h>
 #include <android/hardware/wifi/hostapd/1.1/IHostapd.h>
-#include <android/hardware/wifi/hostapd/1.3/IHostapd.h>
 
 #include "hostapd_hidl_call_util.h"
 #include "hostapd_hidl_test_utils.h"
@@ -44,7 +43,6 @@ constexpr unsigned char kNwSsid[] = {'t', 'e', 's', 't', '1',
 constexpr char kNwPassphrase[] = "test12345";
 constexpr int kIfaceChannel = 6;
 constexpr int kIfaceInvalidChannel = 567;
-
 }  // namespace
 
 class HostapdHidlTest
@@ -60,10 +58,7 @@ class HostapdHidlTest
         ASSERT_NE(hostapd_.get(), nullptr);
     }
 
-    virtual void TearDown() override {
-        HIDL_INVOKE_VOID_WITHOUT_ARGUMENTS(hostapd_, terminate);
-        stopHostapd(wifi_instance_name_);
-    }
+    virtual void TearDown() override { stopHostapd(wifi_instance_name_); }
 
    protected:
     std::string getPrimaryWlanIfaceName() {
@@ -175,17 +170,10 @@ class IfaceCallback : public IHostapdCallback {
     }
 };
 
-bool is_1_3(const sp<IHostapd>& hostapd) {
-    sp<::android::hardware::wifi::hostapd::V1_3::IHostapd> hostapd_1_3 =
-        ::android::hardware::wifi::hostapd::V1_3::IHostapd::castFrom(hostapd);
-    return hostapd_1_3.get() != nullptr;
-}
-
 /*
  * RegisterCallback
  */
 TEST_P(HostapdHidlTest, registerCallback) {
-    if (is_1_3(hostapd_)) GTEST_SKIP() << "Ignore since current HIDL over 1.3";
     hostapd_->registerCallback(
         new IfaceCallback(), [](const HostapdStatus& status) {
             EXPECT_EQ(HostapdStatusCode::SUCCESS, status.code);
@@ -321,7 +309,6 @@ TEST_P(HostapdHidlTest, AddInvalidPskAccessPointWithoutAcs) {
     EXPECT_NE(HostapdStatusCode::SUCCESS, status.code);
 }
 
-GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(HostapdHidlTest);
 INSTANTIATE_TEST_CASE_P(
     PerInstance, HostapdHidlTest,
     testing::Combine(
