6cb527e9b9cfda7956880b5b4721f23363b3f2bd
liujinye <liujinye@xiaomi.com>
2021-02-17 19:42:23 +0800

    Add KeyPurpose::ATTEST_KEY. am: 7c13039711
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1534063
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ibd239ffc5407842834bb07bb70a50f53a61a771e

--- neuralnetworks/utils/common/include/nnapi/hal/HandleError.h
+++ neuralnetworks/utils/common/include/nnapi/hal/HandleError.h
@@ -14,9 +14,6 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_HARDWARE_INTERFACES_NEURALNETWORKS_UTILS_COMMON_HANDLE_ERROR_H
-#define ANDROID_HARDWARE_INTERFACES_NEURALNETWORKS_UTILS_COMMON_HANDLE_ERROR_H
-
 #include <android/hidl/base/1.0/IBase.h>
 #include <hidl/HidlSupport.h>
 #include <nnapi/Result.h>
@@ -53,8 +50,7 @@ nn::GeneralResult<Type> handleTransportError(const hardware::Return<Type>& ret) 
     })
 
 template <typename Type>
-nn::GeneralResult<Type> makeGeneralFailure(
-        nn::Result<Type> result, nn::ErrorStatus status = nn::ErrorStatus::GENERAL_FAILURE) {
+nn::GeneralResult<Type> makeGeneralFailure(nn::Result<Type> result, nn::ErrorStatus status) {
     if (!result.has_value()) {
         return nn::error(status) << std::move(result).error();
     }
@@ -79,8 +75,7 @@ nn::ExecutionResult<Type> makeExecutionFailure(nn::GeneralResult<Type> result) {
 }
 
 template <typename Type>
-nn::ExecutionResult<Type> makeExecutionFailure(
-        nn::Result<Type> result, nn::ErrorStatus status = nn::ErrorStatus::GENERAL_FAILURE) {
+nn::ExecutionResult<Type> makeExecutionFailure(nn::Result<Type> result, nn::ErrorStatus status) {
     return makeExecutionFailure(makeGeneralFailure(result, status));
 }
 
@@ -92,5 +87,3 @@ nn::ExecutionResult<Type> makeExecutionFailure(
         return NN_ERROR(canonical)
 
 }  // namespace android::hardware::neuralnetworks::utils
-
-#endif  // ANDROID_HARDWARE_INTERFACES_NEURALNETWORKS_UTILS_COMMON_HANDLE_ERROR_H
