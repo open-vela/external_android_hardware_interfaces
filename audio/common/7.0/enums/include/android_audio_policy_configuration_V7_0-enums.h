f2b358df3b7ef407756c58946a2961b49b855009
openvela-robot <openvela-robot@xiaomi.com>
2021-02-23 04:06:19 +0800

    Snap for 7163758 from f707dd6a1096ad45875c518a2f2b792708504ed9 to tm-release
    Change-Id: I356ed5a6adf69a900e05124dde0213b7740af961

--- audio/common/7.0/enums/include/android_audio_policy_configuration_V7_0-enums.h
+++ audio/common/7.0/enums/include/android_audio_policy_configuration_V7_0-enums.h
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-#ifndef ANDROID_AUDIO_POLICY_CONFIGURATION_V7_0__ENUMS_H
-#define ANDROID_AUDIO_POLICY_CONFIGURATION_V7_0__ENUMS_H
+#ifndef ANDROID_AUDIO_POLICY_CONFIGURATION_V7_0_ENUMS_H
+#define ANDROID_AUDIO_POLICY_CONFIGURATION_V7_0_ENUMS_H
 
 #include <sys/types.h>
-#include <regex>
-#include <string>
+#include <algorithm>
+#include <cctype>
 
-#include <android_audio_policy_configuration_V7_0_enums.h>
+#include <android_audio_policy_configuration_V7_0.h>
 
 namespace android::audio::policy::configuration::V7_0 {
 
@@ -219,9 +219,11 @@ static inline bool maybeVendorExtension(const std::string& s) {
 }
 
 static inline bool isVendorExtension(const std::string& s) {
-    // Must be the same as the "vendorExtension" rule from the XSD file.
-    static const std::regex vendorExtension("VX_[A-Z0-9]{3,}_[_A-Z0-9]+");
-    return std::regex_match(s.begin(), s.end(), vendorExtension);
+    // Must match the "vendorExtension" rule from the XSD file.
+    static const std::string vendorPrefix = "VX_";
+    return maybeVendorExtension(s) &&
+           std::all_of(s.begin() + vendorPrefix.size(), s.end(),
+                       [](unsigned char c) { return c == '_' || std::isalnum(c); });
 }
 
 static inline bool isUnknownAudioChannelMask(const std::string& mask) {
@@ -262,4 +264,4 @@ static inline bool isUnknownAudioUsage(const std::string& usage) {
 
 }  // namespace android::audio::policy::configuration::V7_0
 
-#endif  // ANDROID_AUDIO_POLICY_CONFIGURATION_V7_0__ENUMS_H
+#endif  // ANDROID_AUDIO_POLICY_CONFIGURATION_V7_0_ENUMS_H
