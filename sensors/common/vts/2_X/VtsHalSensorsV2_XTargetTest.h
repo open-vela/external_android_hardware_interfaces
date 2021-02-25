8cf41af790b7cfb19b4868a811ce22416f4676fa
openvela-robot <openvela-robot@xiaomi.com>
2021-02-25 21:57:31 +0800

    Camera: Fix double-free in removeCamera
    When a camera is removed, |hidlMetadata| takes over ownership of
    |metadata|. Therefore, we should not free |metadata| again.
    
    Bug: 180014486
    Test: Remove an external camera, and no crashes were observed.
    Marged-In: I85246067f8753911cbcb58af24a46f12962226f3
    Change-Id: I85246067f8753911cbcb58af24a46f12962226f3
    (cherry picked from commit f36a439e3a97315a9f0a8661ea77aff61fb79c82)
    (cherry picked from commit 682abf403969efcbc8072a26f687560ad9def69a)

--- sensors/common/vts/2_X/VtsHalSensorsV2_XTargetTest.h
+++ sensors/common/vts/2_X/VtsHalSensorsV2_XTargetTest.h
@@ -226,7 +226,6 @@ class SensorsHidlTest : public SensorsHidlTestBaseV2_X {
     void activateAllSensors(bool enable);
     std::vector<SensorInfoType> getNonOneShotSensors();
     std::vector<SensorInfoType> getNonOneShotAndNonSpecialSensors();
-    std::vector<SensorInfoType> getNonOneShotAndNonOnChangeAndNonSpecialSensors();
     std::vector<SensorInfoType> getOneShotSensors();
     std::vector<SensorInfoType> getInjectEventSensors();
     int32_t getInvalidSensorHandle();
@@ -327,19 +326,6 @@ std::vector<SensorInfoType> SensorsHidlTest::getNonOneShotAndNonSpecialSensors()
     return sensors;
 }
 
-std::vector<SensorInfoType> SensorsHidlTest::getNonOneShotAndNonOnChangeAndNonSpecialSensors() {
-    std::vector<SensorInfoType> sensors;
-    for (const SensorInfoType& info : getSensorsList()) {
-        SensorFlagBits reportMode = extractReportMode(info.flags);
-        if (reportMode != SensorFlagBits::ONE_SHOT_MODE &&
-            reportMode != SensorFlagBits::ON_CHANGE_MODE &&
-            reportMode != SensorFlagBits::SPECIAL_REPORTING_MODE) {
-            sensors.push_back(info);
-        }
-    }
-    return sensors;
-}
-
 std::vector<SensorInfoType> SensorsHidlTest::getOneShotSensors() {
     std::vector<SensorInfoType> sensors;
     for (const SensorInfoType& info : getSensorsList()) {
@@ -366,7 +352,7 @@ int32_t SensorsHidlTest::getInvalidSensorHandle() {
     for (const SensorInfoType& sensor : getSensorsList()) {
         maxHandle = std::max(maxHandle, sensor.sensorHandle);
     }
-    return maxHandle + 42;
+    return maxHandle + 1;
 }
 
 // Test if sensor list returned is valid
@@ -740,8 +726,8 @@ TEST_P(SensorsHidlTest, NoStaleEvents) {
     EventCallback callback;
     getEnvironment()->registerCallback(&callback);
 
-    // This test is not valid for one-shot, on-change or special-report-mode sensors
-    const std::vector<SensorInfoType> sensors = getNonOneShotAndNonOnChangeAndNonSpecialSensors();
+    // This test is not valid for one-shot or special-report-mode sensors
+    const std::vector<SensorInfoType> sensors = getNonOneShotAndNonSpecialSensors();
     milliseconds maxMinDelay(0);
     for (const SensorInfoType& sensor : sensors) {
         milliseconds minDelay = duration_cast<milliseconds>(microseconds(sensor.minDelay));
@@ -764,7 +750,10 @@ TEST_P(SensorsHidlTest, NoStaleEvents) {
                      << " handle=0x" << std::hex << std::setw(8) << std::setfill('0')
                      << sensor.sensorHandle << std::dec << " type=" << static_cast<int>(sensor.type)
                      << " name=" << sensor.name);
-
+        // Some on-change sensors may not report an event without stimulus
+        if (extractReportMode(sensor.flags) != SensorFlagBits::ON_CHANGE_MODE) {
+            ASSERT_GE(callback.getEvents(sensor.sensorHandle).size(), 1);
+        }
         if (callback.getEvents(sensor.sensorHandle).size() >= 1) {
             lastEventTimestampMap[sensor.sensorHandle] =
                     callback.getEvents(sensor.sensorHandle).back().timestamp;
@@ -790,7 +779,10 @@ TEST_P(SensorsHidlTest, NoStaleEvents) {
         if (lastEventTimestampMap.find(sensor.sensorHandle) == lastEventTimestampMap.end()) {
             continue;
         }
-
+        // Skip on-change sensors that do not consistently report an initial event
+        if (callback.getEvents(sensor.sensorHandle).size() < 1) {
+            continue;
+        }
         // Ensure that the first event received is not stale by ensuring that its timestamp is
         // sufficiently different from the previous event
         const EventType newEvent = callback.getEvents(sensor.sensorHandle).front();
@@ -845,11 +837,7 @@ void SensorsHidlTest::verifyRegisterDirectChannel(
         std::shared_ptr<SensorsTestSharedMemory<SensorTypeVersion, EventType>> mem,
         int32_t* directChannelHandle, bool supportsSharedMemType, bool supportsAnyDirectChannel) {
     char* buffer = mem->getBuffer();
-    size_t size = mem->getSize();
-
-    if (supportsSharedMemType) {
-        memset(buffer, 0xff, size);
-    }
+    memset(buffer, 0xff, mem->getSize());
 
     registerDirectChannel(mem->getSharedMemInfo(), [&](Result result, int32_t channelHandle) {
         if (supportsSharedMemType) {
