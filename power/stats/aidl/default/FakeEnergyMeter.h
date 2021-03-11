28535b425da429795f5ca55f1dcfa19083fbf01f
openvela-robot <openvela-robot@xiaomi.com>
2021-03-12 02:06:32 +0800

    Snap for 7202128 from 03893b97f4eeca1b50e4f12d62ce3f8464057e5e to sc-release
    Change-Id: If6a4d9962955c91bcff31383072f20ba0a518226

--- power/stats/aidl/default/FakeEnergyMeter.h
+++ power/stats/aidl/default/FakeEnergyMeter.h
@@ -60,12 +60,9 @@ class FakeEnergyMeter : public PowerStats::IEnergyMeter {
             *_aidl_return = mEnergyMeasurements;
         } else {
             for (int32_t id : in_channelIds) {
-                // check for invalid ids
-                if (id < 0 || id >= mEnergyMeasurements.size()) {
-                    return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
+                if (id >= 0 && id < mEnergyMeasurements.size()) {
+                    _aidl_return->push_back(mEnergyMeasurements[id]);
                 }
-
-                _aidl_return->push_back(mEnergyMeasurements[id]);
             }
         }
 
