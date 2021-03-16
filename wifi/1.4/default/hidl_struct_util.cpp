e516eaf6e8c37ed42f12013f622fa1c6b0a3eab8
openvela-robot <openvela-robot@xiaomi.com>
2021-03-16 19:42:56 +0800

    Merge "Fix emergencyDialResponse in VTS 1.6"

--- wifi/1.4/default/hidl_struct_util.cpp
+++ wifi/1.4/default/hidl_struct_util.cpp
@@ -2292,9 +2292,10 @@ WifiChannelWidthInMhz convertLegacyWifiChannelWidthToHidl(
             return WifiChannelWidthInMhz::WIDTH_5;
         case legacy_hal::WIFI_CHAN_WIDTH_10:
             return WifiChannelWidthInMhz::WIDTH_10;
-        default:
+        case legacy_hal::WIFI_CHAN_WIDTH_INVALID:
             return WifiChannelWidthInMhz::WIDTH_INVALID;
     };
+    CHECK(false) << "Unknown legacy type: " << type;
 }
 
 legacy_hal::wifi_rtt_preamble convertHidlRttPreambleToLegacy(RttPreamble type) {
