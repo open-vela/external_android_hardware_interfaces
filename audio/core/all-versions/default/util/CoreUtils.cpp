6b85757e195ecc0209c9bc1872b3c98254dbb1eb
liujinye <liujinye@xiaomi.com>
2021-03-24 23:23:28 +0800

    [SP32.1] Put OffloadControl 1.1 HAL into apex
    Test: atest TetheringTests
    Bug: 149467454
    Merged-In: I0e4dc6758d87c320fc622da06144b4ceceb91964
    Change-Id: I0e4dc6758d87c320fc622da06144b4ceceb91964
      (cherry-picked from ag/13981664)

--- audio/core/all-versions/default/util/CoreUtils.cpp
+++ audio/core/all-versions/default/util/CoreUtils.cpp
@@ -66,13 +66,13 @@ status_t CoreUtils::microphoneInfoFromHal(
     CONVERT_CHECKED(
             deviceAddressFromHal(halMicInfo.device, halMicInfo.address, &micInfo->deviceAddress),
             result);
-    int chCount;
-    for (chCount = AUDIO_CHANNEL_COUNT_MAX - 1; chCount >= 0; --chCount) {
-        if (halMicInfo.channel_mapping[chCount] != AUDIO_MICROPHONE_CHANNEL_MAPPING_UNUSED) {
+    size_t chCount;
+    for (chCount = 0; chCount < AUDIO_CHANNEL_COUNT_MAX; ++chCount) {
+        if (halMicInfo.channel_mapping[chCount] == AUDIO_MICROPHONE_CHANNEL_MAPPING_UNUSED) {
             break;
         }
     }
-    micInfo->channelMapping.resize(chCount + 1);
+    micInfo->channelMapping.resize(chCount);
     for (size_t ch = 0; ch < micInfo->channelMapping.size(); ch++) {
         micInfo->channelMapping[ch] = AudioMicrophoneChannelMapping(halMicInfo.channel_mapping[ch]);
     }
