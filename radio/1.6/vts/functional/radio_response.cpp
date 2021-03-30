9ea85e7b5dcce678e653aa361348b025bafcb8b9
liujinye <liujinye@xiaomi.com>
2021-03-30 13:44:43 +0800

    Refactor Tuner 1.0 vts to use dynamic configuration
    This CL starts the refatoring of Tuner 1.0 vts configuration file
    to replace the previous manual config with the current dynamic config
    using xml/xsd.
    
    Note that this CL only connects the frontend setting section with
    the dynamic config. The CLs to connect other hardware configs will come as
    child CLs.
    
    This CL also has some default implementation/vts test impl changes to
    sync the sc-dev vts 1.0 with the aosp.
    
    Child CL will also replace the 1.1 manual config with the dynamic
    config.
    
    Test: atest VtsHalTvTunerV1_0TargetTest
    Bug: 182519645
    CTS-Coverage-Bug: 184077478
    Change-Id: I13e6ea3d91e474c10e4f822a6ce59d99cc7c7d1d

--- radio/1.6/vts/functional/radio_response.cpp
+++ radio/1.6/vts/functional/radio_response.cpp
@@ -87,9 +87,7 @@ Return<void> RadioResponse_v1_6::getIMSIForAppResponse(
 }
 
 Return<void> RadioResponse_v1_6::hangupConnectionResponse(
-        const ::android::hardware::radio::V1_0::RadioResponseInfo& info) {
-    rspInfo_v1_0 = info;
-    parent_v1_6.notify(info.serial);
+        const ::android::hardware::radio::V1_0::RadioResponseInfo& /*info*/) {
     return Void();
 }
 
@@ -751,9 +749,7 @@ Return<void> RadioResponse_v1_6::acknowledgeRequest(int32_t /*serial*/) {
 
 /* 1.1 Apis */
 Return<void> RadioResponse_v1_6::setCarrierInfoForImsiEncryptionResponse(
-        const ::android::hardware::radio::V1_0::RadioResponseInfo& info) {
-    rspInfo_v1_0 = info;
-    parent_v1_6.notify(info.serial);
+        const ::android::hardware::radio::V1_0::RadioResponseInfo& /*info*/) {
     return Void();
 }
 
@@ -1233,31 +1229,6 @@ Return<void> RadioResponse_v1_6::getSlicingConfigResponse(
         const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
         const ::android::hardware::radio::V1_6::SlicingConfig& /*slicingConfig*/) {
     rspInfo = info;
-    parent_v1_6.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_6::getSimPhonebookRecordsResponse(
-        const ::android::hardware::radio::V1_6::RadioResponseInfo& info) {
-    rspInfo = info;
-    parent_v1_6.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_6::getSimPhonebookCapacityResponse(
-        const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
-        const ::android::hardware::radio::V1_6::PhonebookCapacity& capacity) {
-    rspInfo = info;
-    this->capacity = capacity;
-    parent_v1_6.notify(info.serial);
-    return Void();
-}
-
-Return<void> RadioResponse_v1_6::updateSimPhonebookRecordsResponse(
-        const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
-        int32_t updatedRecordIndex) {
-    rspInfo = info;
-    this->updatedRecordIndex = updatedRecordIndex;
     parent_v1_6.notify(info.serial);
     return Void();
 }
