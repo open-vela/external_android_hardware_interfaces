a7cad964f48f90f796bb002ecfe4d782f22e8cc7
liujinye <liujinye@xiaomi.com>
2021-03-31 01:07:02 +0800

    Snap for 7246408 from 457837f6b25301e09be194e5897c94891c5baff4 to sc-release
    Change-Id: Idd7268629ee8fbdfa75a650b3d71f40cf7b1abf4

--- radio/1.6/vts/functional/radio_hidl_hal_api.cpp
+++ radio/1.6/vts/functional/radio_hidl_hal_api.cpp
@@ -172,7 +172,13 @@ TEST_P(RadioHidlTest_v1_6, getSlicingConfig) {
     EXPECT_EQ(std::cv_status::no_timeout, wait());
     EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp_v1_6->rspInfo.type);
     EXPECT_EQ(serial, radioRsp_v1_6->rspInfo.serial);
-    EXPECT_EQ(::android::hardware::radio::V1_6::RadioError::NONE, radioRsp_v1_6->rspInfo.error);
+    if (getRadioHalCapabilities().modemReducedFeatureSet1) {
+        ASSERT_TRUE(CheckAnyOfErrors(
+                radioRsp_v1_6->rspInfo.error,
+                {::android::hardware::radio::V1_6::RadioError::REQUEST_NOT_SUPPORTED}));
+    } else {
+        EXPECT_EQ(::android::hardware::radio::V1_6::RadioError::NONE, radioRsp_v1_6->rspInfo.error);
+    }
 }
 
 /*
@@ -812,7 +818,7 @@ TEST_P(RadioHidlTest_v1_6, getSimPhonebookCapacity) {
 /*
  * Test IRadio.updateSimPhonebookRecords() for the response returned.
  */
-TEST_F(RadioHidlTest_v1_6, updateSimPhonebookRecords) {
+TEST_P(RadioHidlTest_v1_6, updateSimPhonebookRecords) {
     serial = GetRandomSerialNumber();
     radio_v1_6->getSimPhonebookCapacity(serial);
     EXPECT_EQ(std::cv_status::no_timeout, wait());
