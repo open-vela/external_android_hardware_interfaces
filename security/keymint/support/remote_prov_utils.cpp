836340e10f34805805016b29461584f8e71900f5
openvela-robot <openvela-robot@xiaomi.com>
2021-03-31 15:28:25 +0800

    Merge "identity: add TEST_MAPPING file to run CTS, VTS, and unit tests in presubmit." am: ae33614d00 am: 16454f9854 am: 0f54cb03c0
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1650507
    
    Change-Id: I112442fb4489adde670701f477ccc430def18494

--- security/keymint/support/remote_prov_utils.cpp
+++ security/keymint/support/remote_prov_utils.cpp
@@ -54,8 +54,6 @@ ErrMsgOr<EekChain> generateEekChain(size_t length, const bytevec& eekId) {
                                             {} /* AAD */);
         if (!coseSign1) return coseSign1.moveMessage();
         eekChain.add(coseSign1.moveValue());
-
-        prev_priv_key = priv_key;
     }
 
     bytevec pub_key(X25519_PUBLIC_VALUE_LEN);
