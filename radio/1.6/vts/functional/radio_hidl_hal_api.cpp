642dece9ae07277dac5335d45de3feb7c9b717bb
openvela-robot <openvela-robot@xiaomi.com>
2021-04-12 13:53:05 +0800

    [ANAPIC Review] Remove config 1.3 hal
    Bug: 183738012
    Test: build
    Change-Id: Ie0eec5576d08ddcd9a8f5ee1a1d9e2b32772653e
    Merged-In: Ie0eec5576d08ddcd9a8f5ee1a1d9e2b32772653e

--- radio/1.6/vts/functional/radio_hidl_hal_api.cpp
+++ radio/1.6/vts/functional/radio_hidl_hal_api.cpp
@@ -158,9 +158,6 @@ TEST_P(RadioHidlTest_v1_6, setupDataCall_1_6_osAppId) {
                 {::android::hardware::radio::V1_6::RadioError::NONE,
                  ::android::hardware::radio::V1_6::RadioError::RADIO_NOT_AVAILABLE,
                  ::android::hardware::radio::V1_6::RadioError::OP_NOT_ALLOWED_BEFORE_REG_TO_NW}));
-        if (radioRsp_v1_6->setupDataCallResult.trafficDescriptors.size() <= 0) {
-            return;
-        }
         EXPECT_EQ(optionalTrafficDescriptor.value().osAppId.value().osAppId,
                 radioRsp_v1_6->setupDataCallResult.trafficDescriptors[0].osAppId.value().osAppId);
     }
@@ -175,18 +172,7 @@ TEST_P(RadioHidlTest_v1_6, getSlicingConfig) {
     EXPECT_EQ(std::cv_status::no_timeout, wait());
     EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp_v1_6->rspInfo.type);
     EXPECT_EQ(serial, radioRsp_v1_6->rspInfo.serial);
-    if (getRadioHalCapabilities()) {
-        ASSERT_TRUE(CheckAnyOfErrors(
-                radioRsp_v1_6->rspInfo.error,
-                {::android::hardware::radio::V1_6::RadioError::REQUEST_NOT_SUPPORTED}));
-    } else {
-        ASSERT_TRUE(
-                CheckAnyOfErrors(radioRsp_v1_6->rspInfo.error,
-                                 {::android::hardware::radio::V1_6::RadioError::NONE,
-                                  ::android::hardware::radio::V1_6::RadioError::RADIO_NOT_AVAILABLE,
-                                  ::android::hardware::radio::V1_6::RadioError::INTERNAL_ERR,
-                                  ::android::hardware::radio::V1_6::RadioError::MODEM_ERR}));
-    }
+    EXPECT_EQ(::android::hardware::radio::V1_6::RadioError::NONE, radioRsp_v1_6->rspInfo.error);
 }
 
 /*
@@ -218,10 +204,10 @@ TEST_P(RadioHidlTest_v1_6, sendSms_1_6) {
 }
 
 /*
- * Test IRadio_1_6.sendSmsExpectMore() for the response returned.
+ * Test IRadio_1_6.sendSMSExpectMore() for the response returned.
  */
-TEST_P(RadioHidlTest_v1_6, sendSmsExpectMore_1_6) {
-    LOG(DEBUG) << "sendSmsExpectMore";
+TEST_P(RadioHidlTest_v1_6, sendSMSExpectMore_1_6) {
+    LOG(DEBUG) << "sendSMSExpectMore";
     serial = GetRandomSerialNumber();
     GsmSmsMessage msg;
     msg.smscPdu = "";
@@ -241,7 +227,7 @@ TEST_P(RadioHidlTest_v1_6, sendSmsExpectMore_1_6) {
              ::android::hardware::radio::V1_6::RadioError::SIM_ABSENT},
             CHECK_GENERAL_ERROR));
     }
-    LOG(DEBUG) << "sendSmsExpectMore finished";
+    LOG(DEBUG) << "sendSMSExpectMore finished";
 }
 
 /*
@@ -827,7 +813,7 @@ TEST_P(RadioHidlTest_v1_6, getSimPhonebookCapacity) {
 /*
  * Test IRadio.updateSimPhonebookRecords() for the response returned.
  */
-TEST_P(RadioHidlTest_v1_6, updateSimPhonebookRecords) {
+TEST_F(RadioHidlTest_v1_6, updateSimPhonebookRecords) {
     serial = GetRandomSerialNumber();
     radio_v1_6->getSimPhonebookCapacity(serial);
     EXPECT_EQ(std::cv_status::no_timeout, wait());
