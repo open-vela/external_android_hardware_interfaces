3ceedc76062220b5f7d43089a81cba3ac849f83f
openvela-robot <openvela-robot@xiaomi.com>
2021-04-14 01:25:31 +0800

    Merge "audio HAL - fix UAFs" into sc-dev am: 1cabad5e1e
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/14174123
    
    Change-Id: I84219e9ad5bb18abcfd62eede2251f0f421f6a20

--- radio/config/1.3/vts/functional/radio_config_hidl_hal_utils.h
+++ radio/config/1.3/vts/functional/radio_config_hidl_hal_utils.h
@@ -29,6 +29,7 @@
 #include <android/hardware/radio/config/1.2/types.h>
 #include <android/hardware/radio/config/1.3/IRadioConfig.h>
 #include <android/hardware/radio/config/1.3/IRadioConfigResponse.h>
+#include <android/hardware/radio/config/1.3/types.h>
 #include <gtest/gtest.h>
 #include <hidl/GtestPrinter.h>
 #include <hidl/ServiceManagement.h>
@@ -47,6 +48,7 @@ using ::android::hardware::radio::config::V1_1::ModemsConfig;
 using ::android::hardware::radio::config::V1_1::PhoneCapability;
 using ::android::hardware::radio::config::V1_2::IRadioConfigIndication;
 using ::android::hardware::radio::config::V1_2::SimSlotStatus;
+using ::android::hardware::radio::config::V1_3::HalDeviceCapabilities;
 using ::android::hardware::radio::config::V1_3::IRadioConfig;
 using ::android::hardware::radio::config::V1_3::IRadioConfigResponse;
 using ::android::hardware::radio::V1_0::RadioResponseInfo;
@@ -63,7 +65,7 @@ class RadioConfigResponse : public IRadioConfigResponse {
   public:
     RadioResponseInfo rspInfo;
     PhoneCapability phoneCap;
-    bool modemReducedFeatureSet1;
+    HalDeviceCapabilities halDeviceCapabilities;
 
     RadioConfigResponse(RadioResponseWaiter& parent);
     virtual ~RadioConfigResponse() = default;
@@ -91,7 +93,7 @@ class RadioConfigResponse : public IRadioConfigResponse {
 
     Return<void> getHalDeviceCapabilitiesResponse(
             const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
-            bool modemReducedFeatureSet1);
+            const HalDeviceCapabilities& halDeviceCapabilities);
 };
 
 /* Callback class for radio config indication */
