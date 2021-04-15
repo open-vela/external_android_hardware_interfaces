a09d7705c39571d8dbc92f1d20483456cf78cb97
liujinye <liujinye@xiaomi.com>
2021-04-16 00:43:08 +0800

    Merge "Refactor Tuner 1.0 vts to use dynamic configuration"

--- radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
+++ radio/1.6/vts/functional/radio_hidl_hal_utils_v1_6.h
@@ -38,12 +38,14 @@ using namespace ::android::hardware::radio::V1_3;
 using namespace ::android::hardware::radio::V1_2;
 using namespace ::android::hardware::radio::V1_1;
 using namespace ::android::hardware::radio::V1_0;
+using namespace ::android::hardware::radio::config::V1_3;
 
 using ::android::sp;
 using ::android::hardware::hidl_string;
 using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
+using ::android::hardware::radio::config::V1_3::HalDeviceCapabilities;
 
 #define MODEM_EMERGENCY_CALL_ESTABLISH_TIME 3
 #define MODEM_EMERGENCY_CALL_DISCONNECT_TIME 3
@@ -60,7 +62,6 @@ class RadioResponse_v1_6 : public ::android::hardware::radio::V1_6::IRadioRespon
 
   public:
     hidl_vec<RadioBandMode> radioBandModes;
-    hidl_vec<OperatorInfo> networkInfos;
 
     ::android::hardware::radio::V1_0::RadioResponseInfo rspInfo_v1_0;
     ::android::hardware::radio::V1_6::RadioResponseInfo rspInfo;
@@ -1114,7 +1115,7 @@ class RadioHidlTest_v1_6 : public ::testing::TestWithParam<std::string>,
   public:
     virtual void SetUp() override;
 
-    bool getRadioHalCapabilities();
+    HalDeviceCapabilities getRadioHalCapabilities();
 
     /* radio service handle */
     sp<::android::hardware::radio::V1_6::IRadio> radio_v1_6;
