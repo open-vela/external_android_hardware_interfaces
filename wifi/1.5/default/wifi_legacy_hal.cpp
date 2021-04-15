1af1f8d0d6b23d16f19eac4ef3b8ff94ef94f5b3
openvela-robot <openvela-robot@xiaomi.com>
2021-04-16 03:06:44 +0800

    Snap for 7287426 from 9993e73e619bead2c997c03e72a925a293ff0cc5 to tm-release
    Change-Id: Ia4d72f1a67ce0074e4527e9c173ff12274401163

--- wifi/1.5/default/wifi_legacy_hal.cpp
+++ wifi/1.5/default/wifi_legacy_hal.cpp
@@ -1677,7 +1677,7 @@ WifiLegacyHal::getUsableChannels(uint32_t band_mask, uint32_t iface_mode_mask,
 }
 
 wifi_error WifiLegacyHal::triggerSubsystemRestart() {
-    return global_func_table_.wifi_trigger_subsystem_restart(global_handle_);
+    return global_func_table_.wifi_trigger_subsystem_restart();
 }
 
 void WifiLegacyHal::invalidate() {
