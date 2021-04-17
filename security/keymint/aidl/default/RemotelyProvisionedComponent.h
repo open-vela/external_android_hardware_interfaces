d18e08564a8e7dddfad57b99e51c490b70134878
liujinye <liujinye@xiaomi.com>
2021-04-17 23:07:10 +0800

    Snap for 7292112 from 67047d5a0320a953512445f3c86ebc0037d6fed8 to sc-release
    Change-Id: Ib3111033372865e070280a676f56b7bc34a354e0

--- security/keymint/aidl/default/RemotelyProvisionedComponent.h
+++ security/keymint/aidl/default/RemotelyProvisionedComponent.h
@@ -32,8 +32,6 @@ class RemotelyProvisionedComponent : public BnRemotelyProvisionedComponent {
     explicit RemotelyProvisionedComponent(std::shared_ptr<keymint::AndroidKeyMintDevice> keymint);
     virtual ~RemotelyProvisionedComponent();
 
-    ScopedAStatus getHardwareInfo(RpcHardwareInfo* info) override;
-
     ScopedAStatus generateEcdsaP256KeyPair(bool testMode, MacedPublicKey* macedPublicKey,
                                            std::vector<uint8_t>* privateKeyHandle) override;
 
