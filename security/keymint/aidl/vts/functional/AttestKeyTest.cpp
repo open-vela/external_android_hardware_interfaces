b384d7cb86a3d13a235b1a6839f45f30b950d334
liujinye <liujinye@xiaomi.com>
2021-05-03 22:44:45 +0800

    Merge "Expand Gatekeeper test" am: a3645d38e9 am: 3a17b7adf6 am: 2b180742ae am: 2049dbaf56
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1609723
    
    Change-Id: I12ea87bf177a467c23ad373a1d328e3fbf6fdcc2

--- security/keymint/aidl/vts/functional/AttestKeyTest.cpp
+++ security/keymint/aidl/vts/functional/AttestKeyTest.cpp
@@ -56,7 +56,6 @@ TEST_P(AttestKeyTest, AllRsaSizes) {
                                              {} /* attestation signing key */, &attest_key.keyBlob,
                                              &attest_key_characteristics, &attest_key_cert_chain));
 
-        ASSERT_GT(attest_key_cert_chain.size(), 0);
         EXPECT_EQ(attest_key_cert_chain.size(), 1);
         EXPECT_TRUE(IsSelfSigned(attest_key_cert_chain)) << "Failed on size " << size;
 
@@ -495,7 +494,6 @@ TEST_P(AttestKeyTest, AllEcCurves) {
                                              {} /* attestation siging key */, &attest_key.keyBlob,
                                              &attest_key_characteristics, &attest_key_cert_chain));
 
-        ASSERT_GT(attest_key_cert_chain.size(), 0);
         EXPECT_EQ(attest_key_cert_chain.size(), 1);
         EXPECT_TRUE(IsSelfSigned(attest_key_cert_chain)) << "Failed on curve " << curve;
 
@@ -579,7 +577,6 @@ TEST_P(AttestKeyTest, AttestWithNonAttestKey) {
                     {} /* attestation siging key */, &non_attest_key.keyBlob,
                     &non_attest_key_characteristics, &non_attest_key_cert_chain));
 
-    ASSERT_GT(non_attest_key_cert_chain.size(), 0);
     EXPECT_EQ(non_attest_key_cert_chain.size(), 1);
     EXPECT_TRUE(IsSelfSigned(non_attest_key_cert_chain));
 
