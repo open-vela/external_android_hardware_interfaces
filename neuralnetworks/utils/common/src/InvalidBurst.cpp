53beba050144e1fbc80a3f0cd4a74b0a8dec33f4
liujinye <liujinye@xiaomi.com>
2021-05-11 07:39:52 +0800

    Introduce reusable execution to canonical interface -- HAL. am: 5f6bedb43a am: 0a924716b0
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1701931
    
    Change-Id: Ib58e2d075cdbcbfc7b96a05d21968d2b2f878b7f

--- neuralnetworks/utils/common/src/InvalidBurst.cpp
+++ neuralnetworks/utils/common/src/InvalidBurst.cpp
@@ -38,10 +38,4 @@ nn::ExecutionResult<std::pair<std::vector<nn::OutputShape>, nn::Timing>> Invalid
     return NN_ERROR() << "InvalidBurst";
 }
 
-nn::GeneralResult<nn::SharedExecution> InvalidBurst::createReusableExecution(
-        const nn::Request& /*request*/, nn::MeasureTiming /*measure*/,
-        const nn::OptionalDuration& /*loopTimeoutDuration*/) const {
-    return NN_ERROR() << "InvalidBurst";
-}
-
 }  // namespace android::hardware::neuralnetworks::utils
