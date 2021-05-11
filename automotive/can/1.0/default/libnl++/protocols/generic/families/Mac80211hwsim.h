5b61270d7aa026494ffbc3403e90745e6b33fcb7
liujinye <liujinye@xiaomi.com>
2021-05-12 00:59:14 +0800

    [automerger skipped] Introduce reusable execution to canonical interface -- HAL. am: 5f6bedb43a am: 0a924716b0 am: 5df141b422 -s ours am: 8ac62976d5 -s ours
    am skip reason: Merged-In I91790bb5ccf5ae648687fe603f88ffda2c9fd2b2 with SHA-1 727a7b2104 is already in history
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1701931
    
    Change-Id: I4b7ed788f542cb209fa7cf0f9f6aaaeab89ece94

--- automotive/can/1.0/default/libnl++/protocols/generic/families/Mac80211hwsim.h
+++ automotive/can/1.0/default/libnl++/protocols/generic/families/Mac80211hwsim.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,25 +14,15 @@
  * limitations under the License.
  */
 
-package android.hardware.power.stats;
+#pragma once
 
-@VintfStability
-parcelable EnergyMeasurement {
-    /**
-     * ID of the Channel associated with this measurement
-     */
-    int id;
-    /**
-     * Approximate time of data capture in millseconds since boot
-     */
-    long timestampMs;
-    /**
-     * Duration in milliseconds that energy has been accumulated
-     */
-    long durationMs;
-    /**
-     * Accumulated energy in microwatt-seconds (uWs)
-     */
-    long energyUWs;
-}
+#include "../GenericMessageBase.h"
 
+namespace android::nl::protocols::generic::families {
+
+class Mac80211hwsim : public GenericMessageBase {
+  public:
+    Mac80211hwsim(nlmsgtype_t familyId);
+};
+
+}  // namespace android::nl::protocols::generic::families
