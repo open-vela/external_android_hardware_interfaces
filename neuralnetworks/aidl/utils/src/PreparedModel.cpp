b32b8bcd21db1c4a1df37a07488ebf52087a51e1
liujinye <liujinye@xiaomi.com>
2021-05-12 16:39:58 +0800

    identity: fix CBOR 8-byte integer encoding am: 29ba064996
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1705256
    
    Change-Id: Iad2bc1309a66e2f73b3c7a219eb03d77b784707e

--- neuralnetworks/aidl/utils/src/PreparedModel.cpp
+++ neuralnetworks/aidl/utils/src/PreparedModel.cpp
@@ -80,8 +80,7 @@ nn::ExecutionResult<std::pair<std::vector<nn::OutputShape>, nn::Timing>> Prepare
     hal::utils::RequestRelocation relocation;
     const nn::Request& requestInShared =
             NN_TRY(hal::utils::makeExecutionFailure(hal::utils::convertRequestFromPointerToShared(
-                    &request, nn::kDefaultRequestMemoryAlignment, nn::kDefaultRequestMemoryPadding,
-                    &maybeRequestInShared, &relocation)));
+                    &request, &maybeRequestInShared, &relocation)));
 
     const auto aidlRequest = NN_TRY(hal::utils::makeExecutionFailure(convert(requestInShared)));
     const auto aidlMeasure = NN_TRY(hal::utils::makeExecutionFailure(convert(measure)));
@@ -128,8 +127,7 @@ PreparedModel::executeFenced(const nn::Request& request, const std::vector<nn::S
     std::optional<nn::Request> maybeRequestInShared;
     hal::utils::RequestRelocation relocation;
     const nn::Request& requestInShared = NN_TRY(hal::utils::convertRequestFromPointerToShared(
-            &request, nn::kDefaultRequestMemoryAlignment, nn::kDefaultRequestMemoryPadding,
-            &maybeRequestInShared, &relocation));
+            &request, &maybeRequestInShared, &relocation));
 
     const auto aidlRequest = NN_TRY(convert(requestInShared));
     const auto aidlWaitFor = NN_TRY(convert(waitFor));
@@ -199,8 +197,7 @@ nn::GeneralResult<nn::SharedExecution> PreparedModel::createReusableExecution(
     std::optional<nn::Request> maybeRequestInShared;
     hal::utils::RequestRelocation relocation;
     const nn::Request& requestInShared = NN_TRY(hal::utils::convertRequestFromPointerToShared(
-            &request, nn::kDefaultRequestMemoryAlignment, nn::kDefaultRequestMemoryPadding,
-            &maybeRequestInShared, &relocation));
+            &request, &maybeRequestInShared, &relocation));
 
     auto aidlRequest = NN_TRY(convert(requestInShared));
     auto aidlMeasure = NN_TRY(convert(measure));
