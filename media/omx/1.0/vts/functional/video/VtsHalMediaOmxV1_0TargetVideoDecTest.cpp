f3648f83e302a34f189e296f349d984a70c657ab
openvela-robot <openvela-robot@xiaomi.com>
2021-05-19 16:48:29 +0800

    [automerger skipped] [SP37] Change requirement of older event callback. am: 88b237cfb5 am: 68d5aacea7 am: 631cfe246e -s ours am: 3b99da73cc -s ours am: 4be3b223f1 -s ours
    am skip reason: Merged-In I75c1a7788e5d48fb3e2836d54b4fd854c0247214 with SHA-1 6cb491a386 is already in history
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1689586
    
    Change-Id: I59aa2d36a821361ddd8b005f956907a2bc77529e

--- media/omx/1.0/vts/functional/video/VtsHalMediaOmxV1_0TargetVideoDecTest.cpp
+++ media/omx/1.0/vts/functional/video/VtsHalMediaOmxV1_0TargetVideoDecTest.cpp
@@ -1420,6 +1420,7 @@ TEST_P(VideoDecHidlTest, FlushTest) {
                                                     true));
 }
 
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(VideoDecHidlTest);
 INSTANTIATE_TEST_SUITE_P(PerInstance, VideoDecHidlTest, testing::ValuesIn(kTestParameters),
                          android::hardware::PrintInstanceTupleNameToString<>);
 
