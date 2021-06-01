8ed59082caedf9d7d18fd22a62eaf4eaab93c24c
openvela-robot <openvela-robot@xiaomi.com>
2021-06-01 22:24:36 +0800

    identity: Don't pass invalid profileIds in VTS test. am: 1b8be7166f
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1721603
    
    Change-Id: I05b8f9dc2a8e63727ded9424fc0d8e2ba3b37c7d

--- audio/core/all-versions/default/include/core/default/StreamOut.h
+++ audio/core/all-versions/default/include/core/default/StreamOut.h
@@ -29,7 +29,6 @@
 #include <fmq/MessageQueue.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
-#include <mediautils/Synchronization.h>
 #include <utils/Thread.h>
 
 namespace android {
@@ -159,9 +158,9 @@ struct StreamOut : public IStreamOut {
     audio_stream_out_t* mStream;
     const sp<Stream> mStreamCommon;
     const sp<StreamMmap<audio_stream_out_t>> mStreamMmap;
-    mediautils::atomic_sp<IStreamOutCallback> mCallback;  // for non-blocking write and drain
+    sp<IStreamOutCallback> mCallback;  // Callback for non-blocking write and drain
 #if MAJOR_VERSION >= 6
-    mediautils::atomic_sp<IStreamOutEventCallback> mEventCallback;
+    sp<IStreamOutEventCallback> mEventCallback;
 #endif
     std::unique_ptr<CommandMQ> mCommandMQ;
     std::unique_ptr<DataMQ> mDataMQ;
