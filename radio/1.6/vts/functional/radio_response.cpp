c6ba89b04dfab3a183c166f762eb2a0ab02389d5
liujinye <liujinye@xiaomi.com>
2021-06-14 22:41:47 +0800

    Merge "Increase test coverage"

--- radio/1.6/vts/functional/radio_response.cpp
+++ radio/1.6/vts/functional/radio_response.cpp
@@ -1172,13 +1172,10 @@ Return<void> RadioResponse_v1_6::setAllowedNetworkTypesBitmapResponse(
 }
 
 Return<void> RadioResponse_v1_6::getAllowedNetworkTypesBitmapResponse(
-        const ::android::hardware::radio::V1_6::RadioResponseInfo& info,
+        const ::android::hardware::radio::V1_6::RadioResponseInfo& /*info*/,
         const ::android::hardware::hidl_bitfield<
                 ::android::hardware::radio::V1_4::RadioAccessFamily>
-                networkTypeBitmap) {
-    rspInfo = info;
-    networkTypeBitmapResponse = networkTypeBitmap;
-    parent_v1_6.notify(info.serial);
+        /*networkTypeBitmap*/) {
     return Void();
 }
 
