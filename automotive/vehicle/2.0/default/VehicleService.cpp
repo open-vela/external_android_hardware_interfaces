3f7e3a983958e6612d3bae498fc698169494cfc9
openvela-robot <openvela-robot@xiaomi.com>
2021-07-27 23:50:43 +0800

    Allow uninstantiated remote provisioning tests am: 8aee4a857c am: 1dfa556e61
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/15404613
    
    Change-Id: I511946d1794d438d588cea448e22ce8cd465dd44

--- automotive/vehicle/2.0/default/VehicleService.cpp
+++ automotive/vehicle/2.0/default/VehicleService.cpp
@@ -20,8 +20,9 @@
 
 #include <iostream>
 
-#include <EmulatedVehicleConnector.h>
-#include <EmulatedVehicleHal.h>
+#include <vhal_v2_0/EmulatedUserHal.h>
+#include <vhal_v2_0/EmulatedVehicleConnector.h>
+#include <vhal_v2_0/EmulatedVehicleHal.h>
 #include <vhal_v2_0/VehicleHalManager.h>
 
 using namespace android;
@@ -31,8 +32,9 @@ using namespace android::hardware::automotive::vehicle::V2_0;
 int main(int /* argc */, char* /* argv */ []) {
     auto store = std::make_unique<VehiclePropertyStore>();
     auto connector = std::make_unique<impl::EmulatedVehicleConnector>();
-    auto hal = std::make_unique<impl::EmulatedVehicleHal>(store.get(), connector.get());
-    auto emulator = connector->getEmulator();
+    auto userHal = connector->getEmulatedUserHal();
+    auto hal = std::make_unique<impl::EmulatedVehicleHal>(store.get(), connector.get(), userHal);
+    auto emulator = std::make_unique<impl::VehicleEmulator>(hal.get());
     auto service = std::make_unique<VehicleHalManager>(hal.get());
     connector->setValuePool(hal->getValuePool());
 
