c84115b963961e544ef0ee2fb69e37580740edfc
openvela-robot <openvela-robot@xiaomi.com>
2021-09-01 03:07:00 +0800

    Snap for 7692502 from c0478bb1f83efc967e92e798071b4dd10241dd74 to tm-release
    Change-Id: I3f0cbdff59e8486fd1d50edbca8d84b61c3eca27

--- soundtrigger/aidl/cli/java/android/hardware/soundtrigger3/cli/SthalCli.java
+++ soundtrigger/aidl/cli/java/android/hardware/soundtrigger3/cli/SthalCli.java
@@ -21,7 +21,6 @@ import android.hardware.soundtrigger3.ISoundTriggerHwCallback;
 import android.hardware.soundtrigger3.ISoundTriggerHwGlobalCallback;
 import android.media.audio.common.AudioChannelLayout;
 import android.media.audio.common.AudioConfig;
-import android.media.audio.common.AudioConfigBase;
 import android.media.audio.common.AudioFormatDescription;
 import android.media.audio.common.AudioFormatType;
 import android.media.audio.common.PcmType;
@@ -349,12 +348,11 @@ public class SthalCli {
 
         private static AudioConfig createConfig() {
             AudioConfig config = new AudioConfig();
-            config.base = new AudioConfigBase();
-            config.base.channelMask = AudioChannelLayout.layoutMask(AudioChannelLayout.LAYOUT_MONO);
-            config.base.format = new AudioFormatDescription();
-            config.base.format.type = AudioFormatType.PCM;
-            config.base.format.pcm = PcmType.INT_16_BIT;
-            config.base.sampleRate = 16000;
+            config.channelMask = AudioChannelLayout.layoutMask(AudioChannelLayout.LAYOUT_MONO);
+            config.format = new AudioFormatDescription();
+            config.format.type = AudioFormatType.PCM;
+            config.format.pcm = PcmType.INT_16_BIT;
+            config.sampleRateHz = 16000;
             return config;
         }
     }
