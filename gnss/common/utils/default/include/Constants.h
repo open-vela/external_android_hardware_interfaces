750ce6c1923fd39a1c3ae19580ada21e4de2acb2
liujinye <liujinye@xiaomi.com>
2021-09-01 08:24:01 +0800

    Merge "Revert "AesInvalidKeySize skip 192 on SB devices""

--- gnss/common/utils/default/include/Constants.h
+++ gnss/common/utils/default/include/Constants.h
@@ -24,27 +24,16 @@ namespace hardware {
 namespace gnss {
 namespace common {
 
+const float kMockLatitudeDegrees = 37.4219999;
+const float kMockLongitudeDegrees = -122.0840575;
+const float kMockAltitudeMeters = 1.60062531;
+const float kMockSpeedMetersPerSec = 0;
+const float kMockBearingDegrees = 0;
 const float kMockHorizontalAccuracyMeters = 5;
 const float kMockVerticalAccuracyMeters = 5;
 const float kMockSpeedAccuracyMetersPerSecond = 1;
 const float kMockBearingAccuracyDegrees = 90;
 const int64_t kMockTimestamp = 1519930775453L;
-const float kGpsL1FreqHz = 1575.42 * 1e6;
-const float kGpsL5FreqHz = 1176.45 * 1e6;
-const float kGloG1FreqHz = 1602.0 * 1e6;
-const float kIrnssL5FreqHz = 1176.45 * 1e6;
-
-// Location replay constants
-constexpr char GNSS_PATH[] = "/dev/gnss0";
-constexpr int INPUT_BUFFER_SIZE = 256;
-constexpr char CMD_GET_LOCATION[] = "CMD_GET_LOCATION";
-constexpr char CMD_GET_RAWMEASUREMENT[] = "CMD_GET_RAWMEASUREMENT";
-constexpr char LINE_SEPARATOR = '\n';
-constexpr char COMMA_SEPARATOR = ',';
-constexpr char GPGA_RECORD_TAG[] = "$GPGGA";
-constexpr char GPRMC_RECORD_TAG[] = "$GPRMC";
-constexpr double TIMESTAMP_EPSILON = 0.001;
-constexpr int MIN_COL_NUM = 13;
 
 }  // namespace common
 }  // namespace gnss
