4ec9df535f46d9cb4f7308d49d1ff49f5ed1f866
openvela-robot <openvela-robot@xiaomi.com>
2021-09-02 16:49:34 +0800

    Merge "Fix closure of record thread"

--- graphics/composer/2.2/utils/vts/RenderEngineVts.cpp
+++ graphics/composer/2.2/utils/vts/RenderEngineVts.cpp
@@ -60,7 +60,6 @@ void TestRenderEngine::initGraphicBuffer(uint32_t width, uint32_t height, uint32
 
 void TestRenderEngine::drawLayers() {
     base::unique_fd bufferFence;
-    base::unique_fd readyFence;
 
     std::vector<const renderengine::LayerSettings*> compositionLayerPointers;
     compositionLayerPointers.reserve(mCompositionLayers.size());
@@ -69,9 +68,12 @@ void TestRenderEngine::drawLayers() {
                    [](renderengine::LayerSettings& settings) -> renderengine::LayerSettings* {
                        return &settings;
                    });
-    mRenderEngine->drawLayers(mDisplaySettings, compositionLayerPointers,
-                              mGraphicBuffer->getNativeBuffer(), true, std::move(bufferFence),
-                              &readyFence);
+    auto texture = std::make_shared<renderengine::ExternalTexture>(
+            mGraphicBuffer, *mRenderEngine, renderengine::ExternalTexture::Usage::WRITEABLE);
+    auto [status, readyFence] = mRenderEngine
+                                        ->drawLayers(mDisplaySettings, compositionLayerPointers,
+                                                     texture, true, std::move(bufferFence))
+                                        .get();
     int fd = readyFence.release();
     if (fd != -1) {
         ASSERT_EQ(0, sync_wait(fd, -1));
