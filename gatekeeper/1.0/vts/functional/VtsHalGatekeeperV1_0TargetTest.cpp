ef22e1d958b52ce2e2e2baa53c5549b0558c10fd
openvela-robot <openvela-robot@xiaomi.com>
2021-09-17 23:45:46 +0800

    vts(wifi): Stop wifi fully instead of stopping framework am: 874239047a am: 356437d474 am: 4df2e04aa9
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/15847168
    
    Change-Id: I75d2e814c5bd9b1ee78f6321f8d849e1d7bf5045

--- gatekeeper/1.0/vts/functional/VtsHalGatekeeperV1_0TargetTest.cpp
+++ gatekeeper/1.0/vts/functional/VtsHalGatekeeperV1_0TargetTest.cpp
@@ -236,10 +236,6 @@ TEST_P(GatekeeperHidlTest, VerifySuccess) {
   generatePassword(password, 0);
   enrollNewPassword(password, enrollRsp, true);
   verifyPassword(password, enrollRsp.data, 1, verifyRsp, true);
-
-  ALOGI("Testing unenrolled password doesn't verify");
-  generatePassword(password, 1);
-  verifyPassword(password, enrollRsp.data, 1, verifyRsp, false);
   ALOGI("Testing Enroll+Verify done");
 }
 
@@ -310,6 +306,8 @@ TEST_P(GatekeeperHidlTest, UntrustedReenroll) {
   if (first != nullptr && second != nullptr) {
     EXPECT_NE(first->user_id, second->user_id);
   }
+  // the old enrollment should be invalid now
+  verifyPassword(password, enrollRsp.data, 0, verifyRsp, false);
   ALOGI("Testing Untrusted Reenroll done");
 }
 
