20f50c9616bff7c4ec9da583328429fa685385aa
openvela-robot <openvela-robot@xiaomi.com>
2021-09-21 16:32:07 +0800

    Add command buffer support for AidlComposer
    Bug: 198190384
    Test: build
    
    Change-Id: Ia28cf992b0ab06099a9ea3f871925839e92e8dc9

--- neuralnetworks/1.3/utils/src/Conversions.cpp
+++ neuralnetworks/1.3/utils/src/Conversions.cpp
@@ -131,8 +131,9 @@ GeneralResult<Capabilities> unvalidatedConvert(const hal::V1_3::Capabilities& ca
     }
 
     auto operandPerformance = NN_TRY(unvalidatedConvert(capabilities.operandPerformance));
-    auto table =
-            NN_TRY(Capabilities::OperandPerformanceTable::create(std::move(operandPerformance)));
+    auto table = NN_TRY(hal::utils::makeGeneralFailure(
+            Capabilities::OperandPerformanceTable::create(std::move(operandPerformance)),
+            nn::ErrorStatus::GENERAL_FAILURE));
 
     return Capabilities{
             .relaxedFloat32toFloat16PerformanceScalar = NN_TRY(
