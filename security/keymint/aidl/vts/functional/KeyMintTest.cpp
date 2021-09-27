cf60f9224f95bb009d6e8ccc88b04938b6fb4f07
liujinye <liujinye@xiaomi.com>
2021-09-27 17:16:01 +0800

    Merge "Move IRadioConfig into IRadio"

--- security/keymint/aidl/vts/functional/KeyMintTest.cpp
+++ security/keymint/aidl/vts/functional/KeyMintTest.cpp
@@ -69,6 +69,8 @@ namespace aidl::android::hardware::security::keymint::test {
 
 namespace {
 
+bool check_patchLevels = false;
+
 // The maximum number of times we'll attempt to verify that corruption
 // of an ecrypted blob results in an error. Retries are necessary as there
 // is a small (roughly 1/256) chance that corrupting ciphertext still results
@@ -527,12 +529,14 @@ class NewKeyGenerationTest : public KeyMintAidlTestBase {
         EXPECT_TRUE(os_pl);
         EXPECT_EQ(*os_pl, os_patch_level());
 
-        // Should include vendor and boot patchlevels.
-        auto vendor_pl = auths.GetTagValue(TAG_VENDOR_PATCHLEVEL);
-        EXPECT_TRUE(vendor_pl);
-        EXPECT_EQ(*vendor_pl, vendor_patch_level());
-        auto boot_pl = auths.GetTagValue(TAG_BOOT_PATCHLEVEL);
-        EXPECT_TRUE(boot_pl);
+        if (check_patchLevels) {
+            // Should include vendor and boot patchlevels.
+            auto vendor_pl = auths.GetTagValue(TAG_VENDOR_PATCHLEVEL);
+            EXPECT_TRUE(vendor_pl);
+            EXPECT_EQ(*vendor_pl, vendor_patch_level());
+            auto boot_pl = auths.GetTagValue(TAG_BOOT_PATCHLEVEL);
+            EXPECT_TRUE(boot_pl);
+        }
 
         return auths;
     }
@@ -6672,6 +6676,10 @@ int main(int argc, char** argv) {
                         dump_Attestations = true;
             } else {
                 std::cout << "NOT dumping attestations" << std::endl;
+            }
+            // TODO(drysdale): Remove this flag when available KeyMint devices comply with spec
+            if (std::string(argv[i]) == "--check_patchLevels") {
+                aidl::android::hardware::security::keymint::test::check_patchLevels = true;
             }
         }
     }
