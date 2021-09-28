58b54834cd2db564d2a95d05b49f4d0c8439232f
liujinye <liujinye@xiaomi.com>
2021-09-28 17:53:51 +0800

    Config the number of live modems based on current multisim number. am: 400ce4ff76
    Original change: https://googleplex-android-review.googlesource.com/c/platform/hardware/interfaces/+/15924337
    
    Change-Id: Ia9d077e3db491e5546d7899b4c757a6addd3d509

--- security/keymint/aidl/vts/functional/KeyMintAidlTestBase.cpp
+++ security/keymint/aidl/vts/functional/KeyMintAidlTestBase.cpp
@@ -77,18 +77,12 @@ bool KeyCharacteristicsBasicallyValid(SecurityLevel secLevel,
 
     std::unordered_set<SecurityLevel> levels_seen;
     for (auto& entry : key_characteristics) {
-        if (entry.authorizations.empty()) {
-            GTEST_LOG_(ERROR) << "empty authorizations for " << entry.securityLevel;
-            return false;
-        }
+        if (entry.authorizations.empty()) return false;
 
         // Just ignore the SecurityLevel::KEYSTORE as the KM won't do any enforcement on this.
         if (entry.securityLevel == SecurityLevel::KEYSTORE) continue;
 
-        if (levels_seen.find(entry.securityLevel) != levels_seen.end()) {
-            GTEST_LOG_(ERROR) << "duplicate authorizations for " << entry.securityLevel;
-            return false;
-        }
+        if (levels_seen.find(entry.securityLevel) != levels_seen.end()) return false;
         levels_seen.insert(entry.securityLevel);
 
         // Generally, we should only have one entry, at the same security level as the KM
@@ -98,10 +92,7 @@ bool KeyCharacteristicsBasicallyValid(SecurityLevel secLevel,
                                        (secLevel == SecurityLevel::STRONGBOX &&
                                         entry.securityLevel == SecurityLevel::TRUSTED_ENVIRONMENT);
 
-        if (!isExpectedSecurityLevel) {
-            GTEST_LOG_(ERROR) << "Unexpected security level " << entry.securityLevel;
-            return false;
-        }
+        if (!isExpectedSecurityLevel) return false;
     }
     return true;
 }
