74d81c987c438168677a7498a6bb326f7fde435c
liujinye <liujinye@xiaomi.com>
2021-10-18 17:41:51 +0800

    Merge "Backfill OWNERS for VtsHalGraphicsMapper tests."

--- tv/cec/1.0/default/HdmiCecDefault.h
+++ tv/cec/1.0/default/HdmiCecDefault.h
@@ -16,8 +16,6 @@
 
 #include <android/hardware/tv/cec/1.0/IHdmiCec.h>
 #include <hardware/hdmi_cec.h>
-#include <linux/cec.h>
-#include <thread>
 
 namespace android {
 namespace hardware {
@@ -26,10 +24,7 @@ namespace cec {
 namespace V1_0 {
 namespace implementation {
 
-using std::thread;
-
-class HdmiCecDefault : public IHdmiCec, public hidl_death_recipient {
-  public:
+struct HdmiCecDefault : public IHdmiCec, public hidl_death_recipient {
     HdmiCecDefault();
     ~HdmiCecDefault();
     // Methods from ::android::hardware::tv::cec::V1_0::IHdmiCec follow.
@@ -52,36 +47,11 @@ class HdmiCecDefault : public IHdmiCec, public hidl_death_recipient {
 
     Return<Result> init();
     Return<void> release();
-
-  private:
-    void event_thread();
-    static int getOpcode(cec_msg message);
-    static int getFirstParam(cec_msg message);
-    static bool isWakeupMessage(cec_msg message);
-    static bool isTransferableInSleep(cec_msg message);
-    static bool isPowerUICommand(cec_msg message);
-
-    thread mEventThread;
-
-    // When set to false, all the CEC commands are discarded. True by default after initialization.
-    bool mCecEnabled;
-    /*
-     * When set to false, HAL does not wake up the system upon receiving <Image View On> or
-     * <Text View On>. True by default after initialization.
-     */
-    bool mWakeupEnabled;
-    /*
-     * Updated when system goes into or comes out of standby mode.
-     * When set to true, Android system is handling CEC commands.
-     * When set to false, microprocessor is handling CEC commands.
-     * True by default after initialization.
-     */
-    bool mCecControlEnabled;
-    sp<IHdmiCecCallback> mCallback;
-
-    int mCecFd;
-    int mExitFd;
+    static void* event_thread(void*);
+    static int getOpcode(struct cec_msg message);
+    static bool isWakeupMessage(struct cec_msg message);
 };
+
 }  // namespace implementation
 }  // namespace V1_0
 }  // namespace cec
