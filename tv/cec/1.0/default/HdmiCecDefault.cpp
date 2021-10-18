c419240e9272adb27177fe7daa12968302591c07
openvela-robot <openvela-robot@xiaomi.com>
2021-10-18 17:41:51 +0800

    Merge "Backfill OWNERS for VtsHalGraphicsMapper tests."

--- tv/cec/1.0/default/HdmiCecDefault.cpp
+++ tv/cec/1.0/default/HdmiCecDefault.cpp
@@ -16,13 +16,14 @@
 
 #define LOG_TAG "android.hardware.tv.cec@1.0-impl"
 #include <android-base/logging.h>
-#include <android-base/properties.h>
 
 #include <cutils/properties.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/cec.h>
 #include <linux/ioctl.h>
 #include <poll.h>
+#include <pthread.h>
 #include <sys/eventfd.h>
 #include <algorithm>
 
@@ -35,14 +36,24 @@ namespace cec {
 namespace V1_0 {
 namespace implementation {
 
-using android::base::GetUintProperty;
+// When set to false, all the CEC commands are discarded. True by default after initialization.
+bool mCecEnabled;
+/*
+ * When set to false, HAL does not wake up the system upon receiving <Image View On> or
+ * <Text View On>. True by default after initialization.
+ */
+bool mWakeupEnabled;
+
+int mCecFd;
+int mExitFd;
+pthread_t mEventThread;
+sp<IHdmiCecCallback> mCallback;
 
 HdmiCecDefault::HdmiCecDefault() {
     mCecFd = -1;
     mExitFd = -1;
     mCecEnabled = false;
     mWakeupEnabled = false;
-    mCecControlEnabled = false;
     mCallback = nullptr;
 }
 
@@ -57,7 +68,7 @@ Return<Result> HdmiCecDefault::addLogicalAddress(CecLogicalAddress addr) {
         return Result::FAILURE_INVALID_ARGS;
     }
 
-    cec_log_addrs cecLogAddrs;
+    struct cec_log_addrs cecLogAddrs;
     int ret = ioctl(mCecFd, CEC_ADAP_G_LOG_ADDRS, &cecLogAddrs);
     if (ret) {
         LOG(ERROR) << "Add logical address failed, Error = " << strerror(errno);
@@ -133,7 +144,7 @@ Return<Result> HdmiCecDefault::addLogicalAddress(CecLogicalAddress addr) {
 }
 
 Return<void> HdmiCecDefault::clearLogicalAddress() {
-    cec_log_addrs cecLogAddrs;
+    struct cec_log_addrs cecLogAddrs;
     memset(&cecLogAddrs, 0, sizeof(cecLogAddrs));
     int ret = ioctl(mCecFd, CEC_ADAP_S_LOG_ADDRS, &cecLogAddrs);
     if (ret) {
@@ -159,7 +170,7 @@ Return<SendMessageResult> HdmiCecDefault::sendMessage(const CecMessage& message)
         return SendMessageResult::FAIL;
     }
 
-    cec_msg cecMsg;
+    struct cec_msg cecMsg;
     memset(&cecMsg, 0, sizeof(cec_msg));
 
     int initiator = static_cast<cec_logical_address_t>(message.initiator);
@@ -222,7 +233,7 @@ Return<void> HdmiCecDefault::getPortInfo(getPortInfo_cb callback) {
         LOG(ERROR) << "Get port info failed, Error = " << strerror(errno);
     }
 
-    uint32_t type = GetUintProperty<uint32_t>("ro.hdmi.device_type", CEC_DEVICE_PLAYBACK);
+    unsigned int type = property_get_int32("ro.hdmi.device_type", CEC_DEVICE_PLAYBACK);
     hidl_vec<HdmiPortInfo> portInfos(1);
     portInfos[0] = {.type = (type == CEC_DEVICE_TV ? HdmiPortType::INPUT : HdmiPortType::OUTPUT),
                     .portId = 1,
@@ -243,9 +254,7 @@ Return<void> HdmiCecDefault::setOption(OptionKey key, bool value) {
             LOG(DEBUG) << "setOption: WAKEUP: " << value;
             mWakeupEnabled = value;
             break;
-        case OptionKey::SYSTEM_CEC_CONTROL:
-            LOG(DEBUG) << "setOption: SYSTEM_CEC_CONTROL: " << value;
-            mCecControlEnabled = value;
+        default:
             break;
     }
     return Void();
@@ -288,7 +297,7 @@ Return<Result> HdmiCecDefault::init() {
     }
 
     // Ensure the CEC device supports required capabilities
-    cec_caps caps = {};
+    struct cec_caps caps = {};
     int ret = ioctl(mCecFd, CEC_ADAP_G_CAPS, &caps);
     if (ret) {
         LOG(ERROR) << "Unable to query cec adapter capabilities, Error = " << strerror(errno);
@@ -310,11 +319,15 @@ Return<Result> HdmiCecDefault::init() {
         return Result::FAILURE_NOT_SUPPORTED;
     }
 
-    mEventThread = thread(&HdmiCecDefault::event_thread, this);
+    /* thread loop for receiving cec messages and hotplug events*/
+    if (pthread_create(&mEventThread, NULL, event_thread, NULL)) {
+        LOG(ERROR) << "Can't create event thread: " << strerror(errno);
+        release();
+        return Result::FAILURE_NOT_SUPPORTED;
+    }
 
     mCecEnabled = true;
     mWakeupEnabled = true;
-    mCecControlEnabled = true;
     return Result::SUCCESS;
 }
 
@@ -322,9 +335,7 @@ Return<void> HdmiCecDefault::release() {
     if (mExitFd > 0) {
         uint64_t tmp = 1;
         write(mExitFd, &tmp, sizeof(tmp));
-        if (mEventThread.joinable()) {
-            mEventThread.join();
-        }
+        pthread_join(mEventThread, NULL);
     }
     if (mExitFd > 0) {
         close(mExitFd);
@@ -334,13 +345,12 @@ Return<void> HdmiCecDefault::release() {
     }
     mCecEnabled = false;
     mWakeupEnabled = false;
-    mCecControlEnabled = false;
     setCallback(nullptr);
     return Void();
 }
 
-void HdmiCecDefault::event_thread() {
-    pollfd ufds[3] = {
+void* HdmiCecDefault::event_thread(void*) {
+    struct pollfd ufds[3] = {
             {mCecFd, POLLIN, 0},
             {mCecFd, POLLERR, 0},
             {mExitFd, POLLIN, 0},
@@ -362,15 +372,15 @@ void HdmiCecDefault::event_thread() {
         }
 
         if (ufds[1].revents == POLLERR) { /* CEC Event */
-            cec_event ev;
+            struct cec_event ev;
             ret = ioctl(mCecFd, CEC_DQEVENT, &ev);
 
-            if (ret) {
-                LOG(ERROR) << "CEC_DQEVENT failed, Error = " << strerror(errno);
+            if (!mCecEnabled) {
                 continue;
             }
 
-            if (!mCecEnabled) {
+            if (ret) {
+                LOG(ERROR) << "CEC_DQEVENT failed, Error = " << strerror(errno);
                 continue;
             }
 
@@ -387,8 +397,12 @@ void HdmiCecDefault::event_thread() {
         }
 
         if (ufds[0].revents == POLLIN) { /* CEC Driver */
-            cec_msg msg = {};
+            struct cec_msg msg = {};
             ret = ioctl(mCecFd, CEC_RECEIVE, &msg);
+
+            if (!mCecEnabled) {
+                continue;
+            }
 
             if (ret) {
                 LOG(ERROR) << "CEC_RECEIVE failed, Error = " << strerror(errno);
@@ -400,17 +414,8 @@ void HdmiCecDefault::event_thread() {
                 continue;
             }
 
-            if (!mCecEnabled) {
-                continue;
-            }
-
             if (!mWakeupEnabled && isWakeupMessage(msg)) {
                 LOG(DEBUG) << "Filter wakeup message";
-                continue;
-            }
-
-            if (!mCecControlEnabled && !isTransferableInSleep(msg)) {
-                LOG(DEBUG) << "Filter message in standby mode";
                 continue;
             }
 
@@ -430,13 +435,14 @@ void HdmiCecDefault::event_thread() {
             }
         }
     }
+    return NULL;
 }
 
-int HdmiCecDefault::getOpcode(cec_msg message) {
-    return static_cast<uint8_t>(message.msg[1]);
+int HdmiCecDefault::getOpcode(struct cec_msg message) {
+    return (static_cast<uint8_t>(message.msg[1]) & 0xff);
 }
 
-bool HdmiCecDefault::isWakeupMessage(cec_msg message) {
+bool HdmiCecDefault::isWakeupMessage(struct cec_msg message) {
     int opcode = getOpcode(message);
     switch (opcode) {
         case CEC_MESSAGE_TEXT_VIEW_ON:
@@ -447,48 +453,6 @@ bool HdmiCecDefault::isWakeupMessage(cec_msg message) {
     }
 }
 
-bool HdmiCecDefault::isTransferableInSleep(cec_msg message) {
-    int opcode = getOpcode(message);
-    switch (opcode) {
-        case CEC_MESSAGE_ABORT:
-        case CEC_MESSAGE_DEVICE_VENDOR_ID:
-        case CEC_MESSAGE_GET_CEC_VERSION:
-        case CEC_MESSAGE_GET_MENU_LANGUAGE:
-        case CEC_MESSAGE_GIVE_DEVICE_POWER_STATUS:
-        case CEC_MESSAGE_GIVE_DEVICE_VENDOR_ID:
-        case CEC_MESSAGE_GIVE_OSD_NAME:
-        case CEC_MESSAGE_GIVE_PHYSICAL_ADDRESS:
-        case CEC_MESSAGE_REPORT_PHYSICAL_ADDRESS:
-        case CEC_MESSAGE_REPORT_POWER_STATUS:
-        case CEC_MESSAGE_SET_OSD_NAME:
-        case CEC_MESSAGE_DECK_CONTROL:
-        case CEC_MESSAGE_PLAY:
-        case CEC_MESSAGE_IMAGE_VIEW_ON:
-        case CEC_MESSAGE_TEXT_VIEW_ON:
-        case CEC_MESSAGE_SYSTEM_AUDIO_MODE_REQUEST:
-            return true;
-        case CEC_MESSAGE_USER_CONTROL_PRESSED:
-            return isPowerUICommand(message);
-        default:
-            return false;
-    }
-}
-
-int HdmiCecDefault::getFirstParam(cec_msg message) {
-    return static_cast<uint8_t>(message.msg[2]);
-}
-
-bool HdmiCecDefault::isPowerUICommand(cec_msg message) {
-    int uiCommand = getFirstParam(message);
-    switch (uiCommand) {
-        case CEC_OP_UI_CMD_POWER:
-        case CEC_OP_UI_CMD_DEVICE_ROOT_MENU:
-        case CEC_OP_UI_CMD_POWER_ON_FUNCTION:
-            return true;
-        default:
-            return false;
-    }
-}
 }  // namespace implementation
 }  // namespace V1_0
 }  // namespace cec
--- tv/tuner/1.1/vts/functional/DvrTests.cpp
+++ tv/tuner/1.1/vts/functional/DvrTests.cpp
@@ -123,9 +123,11 @@ void DvrCallback::testRecordOutput() {
     while (mDataOutputBuffer.empty()) {
         if (-ETIMEDOUT == mMsgCondition.waitRelative(mMsgLock, WAIT_TIMEOUT)) {
             EXPECT_TRUE(false) << "record output matching pid does not output within timeout";
+            stopRecordThread();
             return;
         }
     }
+    stopRecordThread();
     ALOGW("[vts] record pass and stop");
 }
 
