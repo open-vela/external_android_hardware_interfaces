34b05d7be0ee58fe435dbe88254a5a623ac3474b
liujinye <liujinye@xiaomi.com>
2021-10-20 00:23:46 +0800

    Merge "VTS fix for getAllowedNetworkTypesBitmap and setAllowedNetworkTypesBitmap" into android12-tests-dev

--- tv/tuner/1.0/vts/functional/FrontendTests.cpp
+++ tv/tuner/1.0/vts/functional/FrontendTests.cpp
@@ -370,12 +370,11 @@ AssertionResult FrontendTests::tuneFrontend(FrontendConfig config, bool testWith
     mIsSoftwareFe = config.isSoftwareFe;
     bool result = true;
     if (mIsSoftwareFe && testWithDemux) {
-        result &=
-                getDvrTests()->openDvrInDemux(mDvrConfig.type, mDvrConfig.bufferSize) == success();
-        result &= getDvrTests()->configDvrPlayback(mDvrConfig.settings) == success();
-        result &= getDvrTests()->getDvrPlaybackMQDescriptor() == success();
-        getDvrTests()->startPlaybackInputThread(mDvrConfig.playbackInputFile,
-                                                mDvrConfig.settings.playback());
+        result &= mDvrTests.openDvrInDemux(mDvrConfig.type, mDvrConfig.bufferSize) == success();
+        result &= mDvrTests.configDvrPlayback(mDvrConfig.settings) == success();
+        result &= mDvrTests.getDvrPlaybackMQDescriptor() == success();
+        mDvrTests.startPlaybackInputThread(mDvrConfig.playbackInputFile,
+                                           mDvrConfig.settings.playback());
         if (!result) {
             ALOGW("[vts] Software frontend dvr configure failed.");
             return failure();
@@ -398,8 +397,8 @@ AssertionResult FrontendTests::stopTuneFrontend(bool testWithDemux) {
     Result status;
     status = mFrontend->stopTune();
     if (mIsSoftwareFe && testWithDemux) {
-        getDvrTests()->stopPlaybackThread();
-        getDvrTests()->closeDvrPlayback();
+        mDvrTests.stopPlaybackThread();
+        mDvrTests.closeDvrPlayback();
     }
     return AssertionResult(status == Result::SUCCESS);
 }
