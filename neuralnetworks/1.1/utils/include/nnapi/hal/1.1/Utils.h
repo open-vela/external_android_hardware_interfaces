e9e9ff34410bb5481c0e4852e09997890a6eb611
openvela-robot <openvela-robot@xiaomi.com>
2021-11-01 20:41:37 +0800

    [automerger skipped] Ensure non-empty SvInfo is received am: 41b8fd1f9e -s ours
    am skip reason: skipped by user dmulabagal
    
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1769028
    
    Change-Id: Id05de156bea9d9aff2bb2f267bd8ac70fa3750be

--- neuralnetworks/1.1/utils/include/nnapi/hal/1.1/Utils.h
+++ neuralnetworks/1.1/utils/include/nnapi/hal/1.1/Utils.h
@@ -26,6 +26,7 @@
 #include <nnapi/Types.h>
 #include <nnapi/Validation.h>
 #include <nnapi/hal/1.0/Conversions.h>
+#include <nnapi/hal/HandleError.h>
 
 namespace android::hardware::neuralnetworks::V1_1::utils {
 
@@ -51,8 +52,8 @@ bool valid(const Type& halObject) {
 }
 
 template <typename Type>
-nn::Result<void> compliantVersion(const Type& canonical) {
-    const auto version = NN_TRY(nn::validate(canonical));
+nn::GeneralResult<void> compliantVersion(const Type& canonical) {
+    const auto version = NN_TRY(hal::utils::makeGeneralFailure(nn::validate(canonical)));
     if (version > kVersion) {
         return NN_ERROR() << "Insufficient version: " << version << " vs required " << kVersion;
     }
