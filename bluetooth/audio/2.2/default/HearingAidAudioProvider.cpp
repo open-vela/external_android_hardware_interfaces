8ebab742b30f869bd57e4e6095cc69b21dc8d981
liujinye <liujinye@xiaomi.com>
2021-11-19 10:11:28 +0800

    Add white point plumbing into command buffer
    Bug: 200310158
    Test: builds, boots
    Change-Id: I67cadb441b24663346452850a86e3d9151868749

--- bluetooth/audio/2.2/default/HearingAidAudioProvider.cpp
+++ bluetooth/audio/2.2/default/HearingAidAudioProvider.cpp
@@ -66,7 +66,7 @@ bool HearingAidAudioProvider::isValid(const V2_1::SessionType& sessionType) {
 }
 
 Return<void> HearingAidAudioProvider::startSession(
-    const sp<V2_0::IBluetoothAudioPort>& hostIf,
+    const sp<IBluetoothAudioPort>& hostIf,
     const AudioConfiguration& audioConfig, startSession_cb _hidl_cb) {
   /**
    * Initialize the audio platform if audioConfiguration is supported.
