9567871e5d5c1d0027e8b52f8d7d66a69d432fc9
liujinye <liujinye@xiaomi.com>
2021-12-24 01:40:30 +0800

    Merge "Revert "VTS for IRadio AIDL"" am: 3a16f8fe50
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1932964
    
    Change-Id: I1e1ace18716aef1978ec51db4137f264cfc76aba

--- radio/aidl/compat/libradiocompat/include/libradiocompat/RadioIndication.h
+++ radio/aidl/compat/libradiocompat/include/libradiocompat/RadioIndication.h
@@ -15,8 +15,6 @@
  */
 #pragma once
 
-#include "DriverContext.h"
-
 #include <aidl/android/hardware/radio/data/IRadioDataIndication.h>
 #include <aidl/android/hardware/radio/messaging/IRadioMessagingIndication.h>
 #include <aidl/android/hardware/radio/modem/IRadioModemIndication.h>
@@ -28,8 +26,6 @@
 namespace android::hardware::radio::compat {
 
 class RadioIndication : public V1_6::IRadioIndication {
-    std::shared_ptr<DriverContext> mContext;
-
     std::shared_ptr<::aidl::android::hardware::radio::data::IRadioDataIndication> mDataCb;
     std::shared_ptr<::aidl::android::hardware::radio::messaging::IRadioMessagingIndication>
             mMessagingCb;
@@ -185,8 +181,6 @@ class RadioIndication : public V1_6::IRadioIndication {
             const hidl_vec<V1_6::PhonebookRecordInfo>& records) override;
 
   public:
-    RadioIndication(std::shared_ptr<DriverContext> context);
-
     void setResponseFunction(
             std::shared_ptr<::aidl::android::hardware::radio::data::IRadioDataIndication> dataCb);
     void setResponseFunction(
