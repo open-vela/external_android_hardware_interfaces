92c653502bd7803ea026e90837c7e8ae75cb3d1e
liujinye <liujinye@xiaomi.com>
2022-01-05 22:44:10 +0800

    Merge "HAL doc: iccOpenLogicalChannel for MEP-A" am: 93cb928676 am: d0fbd7606f
    Original change: https://android-review.googlesource.com/c/platform/hardware/interfaces/+/1936878
    
    Change-Id: I06386b2e9a89a96d109370310f1a005ea2d23ff3

--- radio/aidl/compat/libradiocompat/include/libradiocompat/RadioNetwork.h
+++ radio/aidl/compat/libradiocompat/include/libradiocompat/RadioNetwork.h
@@ -23,8 +23,6 @@ namespace android::hardware::radio::compat {
 
 class RadioNetwork : public RadioCompatBase,
                      public aidl::android::hardware::radio::network::BnRadioNetwork {
-    std::shared_ptr<::aidl::android::hardware::radio::network::IRadioNetworkResponse> respond();
-
     ::ndk::ScopedAStatus getAllowedNetworkTypesBitmap(int32_t serial) override;
     ::ndk::ScopedAStatus getAvailableBandModes(int32_t serial) override;
     ::ndk::ScopedAStatus getAvailableNetworks(int32_t serial) override;
