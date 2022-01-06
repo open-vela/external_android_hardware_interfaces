6c8acb02d7ce53fe20cac2af7a097551e58464be
openvela-robot <openvela-robot@xiaomi.com>
2022-01-06 15:46:23 +0800

    KeyMint VTS: police Ed25519 msg size limit
    Ed25519 signing operations require the secure world to accumulate the
    entirety of the message; consequently, impose a limit on message size
    for this operation.
    
    Bug: 194358913
    Test: VtsAidlKeyMintTargetTest
    Change-Id: Ibfb6a54c1d546b5b4e51f42795d2bb4660add772

--- neuralnetworks/utils/common/test/MockDevice.h
+++ neuralnetworks/utils/common/test/MockDevice.h
@@ -39,9 +39,7 @@ class MockDevice final : public IDevice {
     MOCK_METHOD(GeneralResult<SharedPreparedModel>, prepareModel,
                 (const Model& model, ExecutionPreference preference, Priority priority,
                  OptionalTimePoint deadline, const std::vector<SharedHandle>& modelCache,
-                 const std::vector<SharedHandle>& dataCache, const CacheToken& token,
-                 const std::vector<TokenValuePair>& hints,
-                 const std::vector<nn::ExtensionNameAndPrefix>& extensionNameToPrefix),
+                 const std::vector<SharedHandle>& dataCache, const CacheToken& token),
                 (const, override));
     MOCK_METHOD(GeneralResult<SharedPreparedModel>, prepareModelFromCache,
                 (OptionalTimePoint deadline, const std::vector<SharedHandle>& modelCache,
