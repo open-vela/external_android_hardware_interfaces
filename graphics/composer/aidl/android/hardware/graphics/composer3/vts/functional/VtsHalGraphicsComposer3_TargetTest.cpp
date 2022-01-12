d9298d91af423a475e9c13e082aaa95c558d9a85
openvela-robot <openvela-robot@xiaomi.com>
2022-01-12 18:59:19 +0800

    Merge "[IHostapd] Support vendor-specific AP config elem"

--- graphics/composer/aidl/android/hardware/graphics/composer3/vts/functional/VtsHalGraphicsComposer3_TargetTest.cpp
+++ graphics/composer/aidl/android/hardware/graphics/composer3/vts/functional/VtsHalGraphicsComposer3_TargetTest.cpp
@@ -1498,7 +1498,7 @@ TEST_P(GraphicsComposerAidlCommandTest, SetDisplayBrightness) {
     execute();
     {
         const auto errors = mReader.takeErrors();
-        ASSERT_EQ(1, errors.size());
+        EXPECT_EQ(1, errors.size());
         EXPECT_EQ(IComposerClient::EX_BAD_PARAMETER, errors[0].errorCode);
     }
 
@@ -1506,7 +1506,7 @@ TEST_P(GraphicsComposerAidlCommandTest, SetDisplayBrightness) {
     execute();
     {
         const auto errors = mReader.takeErrors();
-        ASSERT_EQ(1, errors.size());
+        EXPECT_EQ(1, errors.size());
         EXPECT_EQ(IComposerClient::EX_BAD_PARAMETER, errors[0].errorCode);
     }
 }
