63945db673495795e6970cfc235172346e9eee04
liujinye <liujinye@xiaomi.com>
2022-01-25 14:24:43 +0800

    Allow null DataProfileInfo for IA APN to clear
    Test: build
    Bug: 216357727
    Change-Id: If003316192f579a3970ac48eaf50cb714cf757e6
    Merged-In: If003316192f579a3970ac48eaf50cb714cf757e6

--- identity/aidl/default/libeic/EicProvisioning.c
+++ identity/aidl/default/libeic/EicProvisioning.c
@@ -17,21 +17,8 @@
 #include "EicProvisioning.h"
 #include "EicCommon.h"
 
-#include <inttypes.h>
-
-// Global used for assigning ids for provisioning objects.
-//
-static uint32_t gProvisioningLastIdAssigned = 0;
-
 bool eicProvisioningInit(EicProvisioning* ctx, bool testCredential) {
     eicMemSet(ctx, '\0', sizeof(EicProvisioning));
-
-    if (!eicNextId(&gProvisioningLastIdAssigned)) {
-        eicDebug("Error getting id for object");
-        return false;
-    }
-    ctx->id = gProvisioningLastIdAssigned;
-
     ctx->testCredential = testCredential;
     if (!eicOpsRandom(ctx->storageKey, EIC_AES_128_KEY_SIZE)) {
         return false;
@@ -60,13 +47,6 @@ bool eicProvisioningInitForUpdate(EicProvisioning* ctx, bool testCredential, con
     }
 
     eicMemSet(ctx, '\0', sizeof(EicProvisioning));
-
-    if (!eicNextId(&gProvisioningLastIdAssigned)) {
-        eicDebug("Error getting id for object");
-        return false;
-    }
-    ctx->id = gProvisioningLastIdAssigned;
-
     ctx->testCredential = testCredential;
 
     if (!eicOpsDecryptAes128Gcm(eicOpsGetHardwareBoundKey(testCredential), encryptedCredentialKeys,
@@ -116,27 +96,9 @@ bool eicProvisioningInitForUpdate(EicProvisioning* ctx, bool testCredential, con
     return true;
 }
 
-bool eicProvisioningShutdown(EicProvisioning* ctx) {
-    if (ctx->id == 0) {
-        eicDebug("Trying to shut down provsioning with id 0");
-        return false;
-    }
-    eicDebug("Shut down provsioning with id %" PRIu32, ctx->id);
-    eicMemSet(ctx, '\0', sizeof(EicProvisioning));
-    return true;
-}
-
-bool eicProvisioningGetId(EicProvisioning* ctx, uint32_t* outId) {
-    *outId = ctx->id;
-    return true;
-}
-
 bool eicProvisioningCreateCredentialKey(EicProvisioning* ctx, const uint8_t* challenge,
                                         size_t challengeSize, const uint8_t* applicationId,
-                                        size_t applicationIdSize, const uint8_t* attestationKeyBlob,
-                                        size_t attestationKeyBlobSize,
-                                        const uint8_t* attestationKeyCert,
-                                        size_t attestationKeyCertSize, uint8_t* publicKeyCert,
+                                        size_t applicationIdSize, uint8_t* publicKeyCert,
                                         size_t* publicKeyCertSize) {
     if (ctx->isUpdate) {
         eicDebug("Cannot create CredentialKey on update");
@@ -145,9 +107,7 @@ bool eicProvisioningCreateCredentialKey(EicProvisioning* ctx, const uint8_t* cha
 
     if (!eicOpsCreateCredentialKey(ctx->credentialPrivateKey, challenge, challengeSize,
                                    applicationId, applicationIdSize, ctx->testCredential,
-                                   attestationKeyBlob, attestationKeyBlobSize, attestationKeyCert,
-                                   attestationKeyCertSize, publicKeyCert, publicKeyCertSize)) {
-        eicDebug("Error creating credential key");
+                                   publicKeyCert, publicKeyCertSize)) {
         return false;
     }
     return true;
