1ce12ed9c6eae82e6e72b0c8788d8cfca0e6f08d
liujinye <liujinye@xiaomi.com>
2022-01-27 09:58:16 +0800

    audio: add Ultrasound materials to 7.1
    Bug: 200256985
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: I50ba1272c0b29e482532010f1aca0b6ec83e925c
    (cherry picked from commit bb2fe3f4d557cf1034287f3c1c2360673dc2f401)
    Merged-In: I50ba1272c0b29e482532010f1aca0b6ec83e925c

--- gnss/common/utils/default/Utils.cpp
+++ gnss/common/utils/default/Utils.cpp
@@ -202,8 +202,7 @@ GnssData Utils::getMockMeasurement(const bool enableCorrVecOutputs) {
                        .biasUncertaintyNs = 274.989972114563,
                        .driftNsps = -124.3742360,
                        .driftUncertaintyNsps = 239.6234285828,
-                       .hwClockDiscontinuityCount = 999,
-                       .referenceSignalTypeForIsb = signalType};
+                       .hwClockDiscontinuityCount = 999};
 
     ElapsedRealtime timestamp = {
             .flags = ElapsedRealtime::HAS_TIMESTAMP_NS | ElapsedRealtime::HAS_TIME_UNCERTAINTY_NS,
