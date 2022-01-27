40e7736339123e382e3eadac38d8a3b583f3453a
liujinye <liujinye@xiaomi.com>
2022-01-27 17:42:43 +0800

    Merge "Add vts tests to verify DRM AIDL interface"

--- wifi/1.6/default/wifi_chip.h
+++ wifi/1.6/default/wifi_chip.h
@@ -160,8 +160,6 @@ class WifiChip : public V1_6::IWifiChip {
                                        hidl_bitfield<V1_5::WifiIfaceMode> ifaceModeMask,
                                        hidl_bitfield<UsableChannelFilter> filterMask,
                                        getUsableChannels_1_6_cb _hidl_cb) override;
-    Return<void> getSupportedRadioCombinationsMatrix(
-            getSupportedRadioCombinationsMatrix_cb hidl_status_cb) override;
 
   private:
     void invalidateAndRemoveAllIfaces();
@@ -269,7 +267,6 @@ class WifiChip : public V1_6::IWifiChip {
             const sp<IWifiIface>& bound_iface);
     std::pair<WifiStatus, std::vector<V1_6::WifiUsableChannel>> getUsableChannelsInternal_1_6(
             WifiBand band, uint32_t ifaceModeMask, uint32_t filterMask);
-    std::pair<WifiStatus, WifiRadioCombinationMatrix> getSupportedRadioCombinationsMatrixInternal();
 
     ChipId chip_id_;
     std::weak_ptr<legacy_hal::WifiLegacyHal> legacy_hal_;
