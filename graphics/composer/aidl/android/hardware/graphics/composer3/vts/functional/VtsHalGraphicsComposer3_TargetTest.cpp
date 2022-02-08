79c66fe2283f0f0213e7ff2bf0595e79e2ef37c9
liujinye <liujinye@xiaomi.com>
2022-02-09 03:34:07 +0800

    Merge "Wifi: add maximum channel bandwidth limit to softAp"

--- graphics/composer/aidl/android/hardware/graphics/composer3/vts/functional/VtsHalGraphicsComposer3_TargetTest.cpp
+++ graphics/composer/aidl/android/hardware/graphics/composer3/vts/functional/VtsHalGraphicsComposer3_TargetTest.cpp
@@ -984,6 +984,7 @@ class GraphicsComposerAidlCommandTest : public GraphicsComposerAidlTest {
         const auto errors = mReader.takeErrors();
         ASSERT_TRUE(mReader.takeErrors().empty());
         ASSERT_TRUE(mReader.takeChangedCompositionTypes(getPrimaryDisplayId()).empty());
+        ASSERT_TRUE(mReader.takeBufferAheadResultLayers(getPrimaryDisplayId()).empty());
 
         ASSERT_NO_FATAL_FAILURE(GraphicsComposerAidlTest::TearDown());
     }
