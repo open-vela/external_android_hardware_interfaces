2ac32ff859c36283945388acf0542899380a82d9
openvela-robot <openvela-robot@xiaomi.com>
2022-03-02 02:07:29 +0800

    Snap for 8236285 from f5d795200a2dfec66ec4156115c9533c148da4b2 to tm-release
    Change-Id: I1b8863443222e01b131216a038d18d8cf7a27605

--- security/keymint/aidl/vts/functional/SecureElementProvisioningTest.cpp
+++ security/keymint/aidl/vts/functional/SecureElementProvisioningTest.cpp
@@ -38,7 +38,7 @@ using std::vector;
 
 class SecureElementProvisioningTest : public testing::Test {
   protected:
-    static void SetUpTestSuite() {
+    static void SetupTestSuite() {
         auto params = ::android::getAidlHalInstanceNames(IKeyMintDevice::descriptor);
         for (auto& param : params) {
             ASSERT_TRUE(AServiceManager_isDeclared(param.c_str()))
