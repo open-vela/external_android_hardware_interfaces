a4afcc185209026014b28d223f34badabd61a31c
liujinye <liujinye@xiaomi.com>
2022-03-15 16:02:35 +0800

    Merge "Fuzzer for default vibrator service."

--- automotive/evs/1.1/vts/functional/VtsHalEvsV1_1TargetTest.cpp
+++ automotive/evs/1.1/vts/functional/VtsHalEvsV1_1TargetTest.cpp
@@ -648,7 +648,7 @@ TEST_P(EvsHidlTest, CameraToDisplayRoundTrip) {
         ASSERT_GT(height, 0);
 
         android::ui::DisplayState* pState = (android::ui::DisplayState*)state.data();
-        ASSERT_NE(pState->layerStack, android::ui::INVALID_LAYER_STACK);
+        ASSERT_NE(pState->layerStack, -1);
     });
 
     // Test each reported camera
