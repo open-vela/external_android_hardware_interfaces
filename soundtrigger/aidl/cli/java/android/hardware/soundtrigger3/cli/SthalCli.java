b261b37488e0314a0f870373cf2d5a589d4cfacf
liujinye <liujinye@xiaomi.com>
2022-03-31 03:46:33 +0800

    Merge "Replace instances of strncpy and sprintf in the wifi vendor HAL with strlcpy and snprintf." into tm-dev

--- soundtrigger/aidl/cli/java/android/hardware/soundtrigger3/cli/SthalCli.java
+++ soundtrigger/aidl/cli/java/android/hardware/soundtrigger3/cli/SthalCli.java
@@ -357,15 +357,5 @@ public class SthalCli {
             config.base.sampleRate = 16000;
             return config;
         }
-
-        @Override
-        public int getInterfaceVersion() {
-            return ISoundTriggerHw.VERSION;
-        }
-
-        @Override
-        public String getInterfaceHash() {
-            return ISoundTriggerHw.HASH;
-        }
     }
 }
