0279d506b4e1ae3f6d18b9d3859c7b834dbad565
liujinye <liujinye@xiaomi.com>
2022-03-31 01:27:27 +0800

    Merge "Add default config for new VHAL properties in Android T." into tm-dev

--- graphics/composer/aidl/include/android/hardware/graphics/composer3/ComposerClientWriter.h
+++ graphics/composer/aidl/include/android/hardware/graphics/composer3/ComposerClientWriter.h
@@ -80,9 +80,8 @@ class ComposerClientWriter {
         getDisplayCommand(display).colorTransformMatrix.emplace(std::move(matVec));
     }
 
-    void setDisplayBrightness(int64_t display, float brightness, float brightnessNits) {
-        getDisplayCommand(display).brightness.emplace(
-                DisplayBrightness{.brightness = brightness, .brightnessNits = brightnessNits});
+    void setDisplayBrightness(int64_t display, float brightness) {
+        getDisplayCommand(display).brightness.emplace(DisplayBrightness{.brightness = brightness});
     }
 
     void setClientTarget(int64_t display, uint32_t slot, const native_handle_t* target,
--- graphics/composer/aidl/vts/VtsHalGraphicsComposer3_ReadbackTest.cpp
+++ graphics/composer/aidl/vts/VtsHalGraphicsComposer3_ReadbackTest.cpp
@@ -974,7 +974,7 @@ TEST_P(GraphicsCompositionTest, SetLayerBrightnessDims) {
     // Preconditions to successfully run are knowing the max brightness and successfully applying
     // the max brightness
     ASSERT_GT(maxBrightnessNits, 0.f);
-    mWriter.setDisplayBrightness(getPrimaryDisplayId(), /*brightness*/ 1.f, maxBrightnessNits);
+    mWriter.setDisplayBrightness(getPrimaryDisplayId(), /*brightness*/ 1.f);
     execute();
     ASSERT_TRUE(mReader.takeErrors().empty());
 
