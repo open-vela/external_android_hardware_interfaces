8011dcce3aee45a0beacec4fde0e16e92df55515
openvela-robot <openvela-robot@xiaomi.com>
2022-04-12 21:24:03 +0800

    check all active ports
    Bug: 226018537
    Test: run vts -m VtsHalRadioTargetTest
    
    Change-Id: Ie3d8f7387ebb2846ea772d4f879039a127282c9a
    Merged-In: Ie3d8f7387ebb2846ea772d4f879039a127282c9a

--- audio/core/all-versions/vts/functional/AudioPrimaryHidlHalTest.h
+++ audio/core/all-versions/vts/functional/AudioPrimaryHidlHalTest.h
@@ -34,7 +34,6 @@
 
 #include <hwbinder/IPCThreadState.h>
 
-#include <android-base/expected.h>
 #include <android-base/logging.h>
 #include <system/audio_config.h>
 
@@ -131,33 +130,6 @@ class HidlTest : public ::testing::Test {
     using IDevice = ::android::hardware::audio::CPP_VERSION::IDevice;
     using IDevicesFactory = ::android::hardware::audio::CPP_VERSION::IDevicesFactory;
 
-    static android::base::expected<std::vector<std::string>, std::string> getAllFactoryInstances() {
-        using ::android::hardware::audio::CPP_VERSION::IDevicesFactory;
-        const std::string factoryDescriptor = IDevicesFactory::descriptor;
-        // Make sure that the instance is the exact minor version.
-        // Using a 7.1 factory for 7.0 test is not always possible because
-        // 7.1 can be configured via the XML config to use features that are
-        // absent in 7.0.
-        auto instances = ::android::hardware::getAllHalInstanceNames(factoryDescriptor);
-        if (instances.empty()) return instances;
-        // Use the default instance for checking the implementation version.
-        auto defaultInstance = IDevicesFactory::getService("default");
-        if (defaultInstance == nullptr) {
-            return ::android::base::unexpected("Failed to obtain IDevicesFactory/default");
-        }
-        std::string actualDescriptor;
-        auto intDescRet = defaultInstance->interfaceDescriptor(
-                [&](const auto& descriptor) { actualDescriptor = descriptor; });
-        if (!intDescRet.isOk()) {
-            return ::android::base::unexpected("Failed to obtain interface descriptor: " +
-                                               intDescRet.description());
-        }
-        if (factoryDescriptor == actualDescriptor)
-            return instances;
-        else
-            return {};
-    }
-
     virtual ~HidlTest() = default;
     // public access to avoid annoyances when using this method in template classes
     // derived from test classes
@@ -202,11 +174,9 @@ const PolicyConfig& getCachedPolicyConfig() {
 }
 
 TEST(CheckConfig, audioPolicyConfigurationValidation) {
-    const auto factories = HidlTest::getAllFactoryInstances();
-    if (!factories.ok()) {
-        FAIL() << factories.error();
-    }
-    if (factories.value().size() == 0) {
+    const auto factories = ::android::hardware::getAllHalInstanceNames(
+            ::android::hardware::audio::CPP_VERSION::IDevicesFactory::descriptor);
+    if (factories.size() == 0) {
         GTEST_SKIP() << "Skipping audioPolicyConfigurationValidation because no factory instances "
                         "are found.";
     }
@@ -235,11 +205,11 @@ static inline std::string DeviceParameterToString(
 const std::vector<DeviceParameter>& getDeviceParameters() {
     static std::vector<DeviceParameter> parameters = [] {
         std::vector<DeviceParameter> result;
-        const auto factories = HidlTest::getAllFactoryInstances();
-        if (!factories.ok()) return result;
+        const auto factories = ::android::hardware::getAllHalInstanceNames(
+                ::android::hardware::audio::CPP_VERSION::IDevicesFactory::descriptor);
         const auto devices = getCachedPolicyConfig().getModulesWithDevicesNames();
         result.reserve(devices.size());
-        for (const auto& factoryName : factories.value()) {
+        for (const auto& factoryName : factories) {
             for (const auto& deviceName : devices) {
                 if (DeviceManager::getInstance().get(factoryName, deviceName) != nullptr) {
                     result.emplace_back(factoryName, deviceName);
@@ -254,9 +224,9 @@ const std::vector<DeviceParameter>& getDeviceParameters() {
 const std::vector<DeviceParameter>& getDeviceParametersForFactoryTests() {
     static std::vector<DeviceParameter> parameters = [] {
         std::vector<DeviceParameter> result;
-        const auto factories = HidlTest::getAllFactoryInstances();
-        if (!factories.ok()) return result;
-        for (const auto& factoryName : factories.value()) {
+        const auto factories = ::android::hardware::getAllHalInstanceNames(
+                ::android::hardware::audio::CPP_VERSION::IDevicesFactory::descriptor);
+        for (const auto& factoryName : factories) {
             result.emplace_back(factoryName,
                                 DeviceManager::getInstance().getPrimary(factoryName) != nullptr
                                         ? DeviceManager::kPrimaryDevice
@@ -1041,7 +1011,6 @@ class StreamWriter : public StreamWorker<StreamWriter> {
 
 class OutputStreamTest
     : public OpenStreamTest<::android::hardware::audio::CPP_VERSION::IStreamOut> {
-  protected:
     void SetUp() override {
         ASSERT_NO_FATAL_FAILURE(OpenStreamTest::SetUp());  // setup base
 #if MAJOR_VERSION <= 6
@@ -1067,6 +1036,7 @@ class OutputStreamTest
     }
 #if MAJOR_VERSION >= 4 && MAJOR_VERSION <= 6
 
+  protected:
     const SourceMetadata initMetadata = {
         { { AudioUsage::MEDIA,
             AudioContentType::MUSIC,
