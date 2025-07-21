335015939hi/dumbos-platform_hardware_interfaces

Added cc_defaults for android.hardware.bluetooth@1.0-impl
Created a new library - android.hardware.bluetooth@1.0-impl-test,
that shall be used for testing by bluetoothV1.0_fuzzer

Test: Build android.hardware.bluetooth@1.0-impl
Test: Build android.hardware.bluetooth@1.0-impl-test
Bug: 187131546

Change-Id: If67bf4ff59fc446dfa0548ecb8f67359717f6cc9

bluetooth/1.0/default/vendor_interface.cc
@@ -27,7 +27,11 @@
 #include "h4_protocol.h"
 #include "mct_protocol.h"
 
+#ifdef BT_FUZZER
+static const char* VENDOR_LIBRARY_NAME = "libbt-vendor-default.so";
+#else
 static const char* VENDOR_LIBRARY_NAME = "libbt-vendor.so";
+#endif
 static const char* VENDOR_LIBRARY_SYMBOL_NAME =
     "BLUETOOTH_VENDOR_LIB_INTERFACE";
 

open-vela/external_android_hardware_interfaces

bluetooth/1.0/default/vendor_interface.cc
@@ -27,7 +27,11 @@
 #include "h4_protocol.h"
 #include "mct_protocol.h"
 
+#ifdef BT_FUZZER
+static const char* VENDOR_LIBRARY_NAME = "libbt-vendor-fuzz.so";
+#else
 static const char* VENDOR_LIBRARY_NAME = "libbt-vendor.so";
+#endif
 static const char* VENDOR_LIBRARY_SYMBOL_NAME =
     "BLUETOOTH_VENDOR_LIB_INTERFACE";
 