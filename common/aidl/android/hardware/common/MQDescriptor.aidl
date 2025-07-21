335015939hi/dumbos-platform_hardware_interfaces

Add template parameters to MQDescriptor
First parameter is used to determine the type of the payload, and the
second parameter is used to determine the type of queue.
Added two types to be use in that second parameter.
SynchronizedReadWrite and UnsynchronizedWrite.

Test: atest fmq_unit_tests fmq_test
Bug: 142326204

Change-Id: If736389ebecd02b87879850714606523e5145579

common/aidl/android/hardware/common/MQDescriptor.aidl
@@ -22,9 +22,12 @@ import android.hardware.common.GrantorDescriptor;
  * For use with libfmq. This is created from an instance of AidlMessageQueue,
  * and is used to pass information required to create another instance of that
  * queue for fast communication.
+ * T - is used to specify the type of the payload
+ * U - is used to specify the type of the queue using
+ * android.hardware.common.SynchronizedReadWrite or UnsynchronizedWrite
  */
 @VintfStability
-parcelable MQDescriptor {
+parcelable MQDescriptor<T, U> {
     /*
      * Describes each of the grantors for the message queue. They are used to
      * get the readptr, writeptr, dataptr, and the optional EventFlag word

open-vela/external_android_hardware_interfaces

common/aidl/android/hardware/common/MQDescriptor.aidl
@@ -22,9 +22,12 @@ import android.hardware.common.GrantorDescriptor;
  * For use with libfmq. This is created from an instance of AidlMessageQueue,
  * and is used to pass information required to create another instance of that
  * queue for fast communication.
+ * T - is used to specify the type of the payload
+ * Flavor - is used to specify the type of the queue using
+ * android.hardware.common.SynchronizedReadWrite or UnsynchronizedWrite
  */
 @VintfStability
-parcelable MQDescriptor {
+parcelable MQDescriptor<T, Flavor> {
     /*
      * Describes each of the grantors for the message queue. They are used to
      * get the readptr, writeptr, dataptr, and the optional EventFlag word